e350c3848397544946307f7a93be4a108f0db7ce
kalenhaha <chenkl198812@gmail.com>
2014-03-24 23:23:11 +0800

    change the regression demo data set

--- demo/regression/runexp.sh
+++ demo/regression/runexp.sh
@@ -2,13 +2,10 @@
 # map feature using indicator encoding, also produce featmap.txt
 python mapfeat.py
 # split train and test
-python mknfold.py agaricus.txt 1
+python mknfold.py machine.txt 1
 # training and output the models
-../../xgboost mushroom.conf
+../../xgboost machine.conf
 # output predictions of test data
-../../xgboost mushroom.conf task=pred model_in=0003.model
+../../xgboost machine.conf task=pred model_in=0003.model
 # print the boosters of 00003.model in dump.raw.txt
-../../xgboost mushroom.conf task=dump model_in=0003.model name_dump=dump.raw.txt 
-# use the feature map in printing for better visualization
-../../xgboost mushroom.conf task=dump model_in=0003.model fmap=featmap.txt name_dump=dump.nice.txt
-cat dump.nice.txt
+../../xgboost machine.conf task=dump model_in=0003.model name_dump=dump.raw.txt 
