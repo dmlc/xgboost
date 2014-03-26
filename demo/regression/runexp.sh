2aa1031d24586a5ad798637fec380ecc01a9b013
tqchen <tianqi.tchen@gmail.com>
2014-03-26 16:47:01 -0700

    add dump nice to regression demo

--- demo/regression/runexp.sh
+++ demo/regression/runexp.sh
@@ -7,5 +7,10 @@ python mknfold.py machine.txt 1
 ../../xgboost machine.conf
 # output predictions of test data
 ../../xgboost machine.conf task=pred model_in=0002.model
-# print the boosters of 00002.model in dump.raw.txt
-../../xgboost machine.conf task=dump model_in=0002.model name_dump=dump.raw.txt 
+# print the boosters of 0002.model in dump.raw.txt
+../../xgboost machine.conf task=dump model_in=0002.model name_dump=dump.raw.txt
+# print the boosters of 0002.model in dump.nice.txt with feature map
+../../xgboost machine.conf task=dump model_in=0002.model fmap=featmap.txt name_dump=dump.nice.txt 
+
+# cat the result
+cat dump.nice.txt
