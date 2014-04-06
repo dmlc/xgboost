6bc71df49434258f192958ade54f8b7f68743768
kalenhaha <chenkl198812@gmail.com>
2014-04-06 22:51:52 +0800

    compiled

--- utils/xgboost_utils.h
+++ utils/xgboost_utils.h
@@ -61,7 +61,17 @@ namespace xgboost{
                 exit( -1 );
             }
             return fp;
-        }       
+        }
+        
+        /*! \brief replace fopen, */
+        inline FILE *FopenTry( const char *fname , const char *flag ){
+            FILE *fp = fopen64( fname , flag );
+            if( fp == NULL ){
+                fprintf( stderr, "can not open file \"%s\"\n",fname );
+                exit( -1 );
+            }
+            return fp;
+        }
     };
 };
 
