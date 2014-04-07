60d79eb2e799bec1833fb6ba13dbf8aa35951559
Tianqi Chen <tqchen@users.noreply.github.com>
2014-04-07 16:25:21 -0700

    Update xgboost_utils.h

--- utils/xgboost_utils.h
+++ utils/xgboost_utils.h
@@ -62,16 +62,6 @@ namespace xgboost{
             }
             return fp;
         }
-
-        /*! \brief replace fopen, */
-        inline FILE *FopenTry(const char *fname, const char *flag){
-            FILE *fp = fopen64(fname, flag);
-            if (fp == NULL){
-                fprintf(stderr, "can not open file \"%s\"\n", fname);
-                exit(-1);
-            }
-            return fp;
-        }
     };
 };
 
