c8b2f46b893327a87ad535a0c6ee197e43f99b17
kalenhaha <chenkl198812@gmail.com>
2014-04-10 22:09:19 +0800

    lambda rank added

--- regression/xgboost_reg.h
+++ regression/xgboost_reg.h
@@ -213,7 +213,7 @@ namespace xgboost{
             inline void InteractPredict(std::vector<float> &preds, const DMatrix &data, unsigned buffer_offset){
                 preds.resize(data.Size());
                 const unsigned ndata = static_cast<unsigned>(data.Size());
-#pragma omp parallel for schedule( static )
+		#pragma omp parallel for schedule( static )
                 for (unsigned j = 0; j < ndata; ++j){
                     preds[j] = mparam.PredTransform
                         (mparam.base_score + base_gbm.InteractPredict(data.data, j, buffer_offset + j));
@@ -222,7 +222,7 @@ namespace xgboost{
             /*! \brief repredict trial */
             inline void InteractRePredict(const DMatrix &data, unsigned buffer_offset){
                 const unsigned ndata = static_cast<unsigned>(data.Size());
-#pragma omp parallel for schedule( static )
+		#pragma omp parallel for schedule( static )
                 for (unsigned j = 0; j < ndata; ++j){
                     base_gbm.InteractRePredict(data.data, j, buffer_offset + j);
                 }
