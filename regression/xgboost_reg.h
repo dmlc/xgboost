0f8a3d21a549afd2d9ff98dfece155617de18340
tqchen <tianqi.tchen@gmail.com>
2014-04-29 09:59:10 -0700

    chg fmap format

--- regression/xgboost_reg.h
+++ regression/xgboost_reg.h
@@ -166,6 +166,7 @@ namespace xgboost{
                     buffer_offset += static_cast<int>(evals_[i]->Size());
                 }
                 fprintf(fo, "\n");
+                fflush(fo);
             }
             /*! \brief get prediction, without buffering */
             inline void Predict(std::vector<float> &preds, const DMatrix &data){
--- utils/xgboost_fmap.h
+++ utils/xgboost_fmap.h
@@ -32,7 +32,7 @@ namespace xgboost{
             inline void LoadText(FILE *fi){
                 int fid;
                 char fname[256], ftype[256];
-                while (fscanf(fi, "%d%s%s", &fid, fname, ftype) == 3){
+                while (fscanf(fi, "%d\t%[^\t]\t%s\n", &fid, fname, ftype) == 3){
                     utils::Assert(fid == (int)names_.size(), "invalid fmap format");
                     names_.push_back(std::string(fname));
                     types_.push_back(GetType(ftype));
