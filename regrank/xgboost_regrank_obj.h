bf64608cc941a5bc30478acd1516d46d80335612
tqchen <tianqi.tchen@gmail.com>
2014-04-30 22:11:26 -0700

    add softmax

--- regrank/xgboost_regrank_obj.h
+++ regrank/xgboost_regrank_obj.h
@@ -107,6 +107,7 @@ namespace xgboost{
         IObjFunction* CreateObjFunction( const char *name ){
             if( !strcmp("reg", name ) ) return new RegressionObj();
             if( !strcmp("rank", name ) ) return new PairwiseRankObj();
+            if( !strcmp("softmax", name ) ) return new SoftmaxObj();
             utils::Error("unknown objective function type");
             return NULL;
         }
