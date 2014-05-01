ef7df40bc8db3b127d7efb06299e4efa3cf38815
tqchen <tianqi.tchen@gmail.com>
2014-05-01 11:00:50 -0700

    cleanup of evaluation metric, move c++11 codes into sample.h for backup, add lambda in a clean way latter

--- utils/xgboost_random.h
+++ utils/xgboost_random.h
@@ -129,17 +129,15 @@ namespace xgboost{
     };
     
     namespace random{
+        /*! \brief random number generator with independent random number seed*/
         struct Random{
             /*! \brief set random number seed */
             inline void Seed( unsigned sd ){
                 this->rseed = sd;
             }
             /*! \brief return a real number uniform in [0,1) */
-            inline double RandDouble( void ){
-               
-            //    return static_cast<double>( rand_( &rseed ) ) / (static_cast<double>( RAND_MAX )+1.0);
-                return static_cast<double>(rand()) / (static_cast<double>(RAND_MAX)+1.0);
-
+            inline double RandDouble( void ){               
+                return static_cast<double>( rand_r( &rseed ) ) / (static_cast<double>( RAND_MAX )+1.0);
             }
             // random number seed
             unsigned rseed;
