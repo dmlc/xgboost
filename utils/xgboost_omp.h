f93ccda0750402b9cb31d372e6b9fdd2fc3c85a4
Tianqi Chen <tqchen@users.noreply.github.com>
2014-05-01 10:16:05 -0700

    Update xgboost_omp.h

--- utils/xgboost_omp.h
+++ utils/xgboost_omp.h
@@ -10,7 +10,7 @@
 #if defined(_OPENMP)
 #include <omp.h>
 #else
-//#warning "OpenMP is not available, compile to single thread code"
+#warning "OpenMP is not available, compile to single thread code"
 inline int omp_get_thread_num() { return 0; }
 inline int omp_get_num_threads() { return 1; }
 inline void omp_set_num_threads(int nthread) {}
