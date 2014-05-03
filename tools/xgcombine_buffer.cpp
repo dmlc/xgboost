8f75b0ef7512fdeaa78d3f98667dbaae45356dc0
tqchen <tianqi.tchen@gmail.com>
2014-05-02 18:04:45 -0700

    pass test

--- tools/xgcombine_buffer.cpp
+++ tools/xgcombine_buffer.cpp
@@ -136,25 +136,24 @@ public:
             }
             ngleft -= 1; ngacc += 1;
 
+            int pass = 1;
             if( fwlist != NULL ){
-                int pass;
                 utils::Assert( fscanf( fwlist, "%u", &pass ) ==1 );
-                if( pass == 0 ){
-                    vskip( fheader ); ngacc -= 1;
-                }
             }
-
-            const int nfeat = readnum( fheader );
-            std::vector<unsigned> findex;
-            std::vector<float> fvalue;
-            // pairs 
-            this->Load( findex, fvalue, fheader );
-            utils::Assert( findex.size() == (unsigned)nfeat );
-            if( rescale != 0 ) this->DoRescale( fvalue );
-            // push back data :)
-            this->info.labels.push_back( label );
-            this->data.AddRow( findex, fvalue );
-
+            if( pass == 0 ){
+                vskip( fheader ); ngacc -= 1;
+            }else{            
+                const int nfeat = readnum( fheader );
+                std::vector<unsigned> findex;
+                std::vector<float> fvalue;
+                // pairs 
+                this->Load( findex, fvalue, fheader );
+                utils::Assert( findex.size() == (unsigned)nfeat );
+                if( rescale != 0 ) this->DoRescale( fvalue );
+                // push back data :)
+                this->info.labels.push_back( label );
+                this->data.AddRow( findex, fvalue );
+            }             
             if( ngleft == 0 && fgroup != NULL && ngacc != 0 ){
                 info.group_ptr.push_back( info.group_ptr.back() + ngacc );
                 utils::Assert( info.group_ptr.back() == data.NumRow(), "group size must match num rows" );
