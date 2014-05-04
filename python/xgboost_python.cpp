6fd77cbb24da2fc597cb70fd033582b70abf70b3
tqchen <tianqi.tchen@gmail.com>
2014-05-03 23:04:02 -0700

    add python interface for xgboost

--- python/xgboost_python.cpp
+++ python/xgboost_python.cpp
@@ -32,6 +32,7 @@ namespace xgboost{
                 mat.row_data_.resize( mat.row_ptr_.back() + len );
                 memcpy( &mat.row_data_[mat.row_ptr_.back()], data, sizeof(XGEntry)*len );
                 mat.row_ptr_.push_back( mat.row_ptr_.back() + len );
+                init_col_ = false;
             }
             inline const XGEntry* GetRow(unsigned ridx, size_t* len) const{
                 const xgboost::booster::FMatrixS &mat = this->data;
@@ -72,7 +73,7 @@ namespace xgboost{
                 return &(this->info.labels[0]);
             }
             inline void CheckInit(void){
-                if(!this->data.HaveColAccess()){
+                if(!init_col_){
                     this->data.InitData();
                 }
                 utils::Assert( this->data.NumRow() == this->info.labels.size(), "DMatrix: number of labels must match number of rows in matrix");
@@ -163,7 +164,9 @@ extern "C"{
     void *XGBoosterCreate( void *dmats[], size_t len ){
         std::vector<const xgboost::regrank::DMatrix*> mats;
         for( size_t i = 0; i < len; ++i ){
-            mats.push_back( static_cast<DMatrix*>(dmats[i]) );
+            DMatrix *dtr = static_cast<DMatrix*>(dmats[i]);
+            dtr->CheckInit();
+            mats.push_back( dtr );
         }
         return new Booster( mats );
     }
--- regrank/xgboost_regrank_data.h
+++ regrank/xgboost_regrank_data.h
@@ -112,7 +112,9 @@ namespace xgboost{
                     unsigned ngptr;
                     if( fs.Read(&ngptr, sizeof(unsigned) ) != 0 ){
                         info.group_ptr.resize( ngptr );
-                        utils::Assert( fs.Read(&info.group_ptr[0], sizeof(unsigned) * ngptr) != 0, "Load group file");
+                        if( ngptr != 0 ){
+                            utils::Assert( fs.Read(&info.group_ptr[0], sizeof(unsigned) * ngptr) != 0, "Load group file");
+                        }
                     }
                 }
                 fs.Close();
@@ -143,7 +145,9 @@ namespace xgboost{
                 {// write out group ptr
                     unsigned ngptr = static_cast<unsigned>( info.group_ptr.size() );                    
                     fs.Write(&ngptr, sizeof(unsigned) );
-                    fs.Write(&info.group_ptr[0], sizeof(unsigned) * ngptr);                    
+                    if( ngptr != 0 ){
+                        fs.Write(&info.group_ptr[0], sizeof(unsigned) * ngptr);                    
+                    }
                 }
                 fs.Close();
                 if (!silent){
