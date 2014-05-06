833cf298677395f7a6c3a6a8bcdfb13f83ed88c4
tqchen <tianqi.tchen@gmail.com>
2014-05-06 16:53:37 -0700

    fix

--- regrank/xgboost_regrank_data.h
+++ regrank/xgboost_regrank_data.h
@@ -118,7 +118,6 @@ namespace xgboost{
                     unsigned ngptr;
                     if( fs.Read(&ngptr, sizeof(unsigned) ) != 0 ){
                         info.group_ptr.resize( ngptr );
-                        utils::Assert( fs.Read(&info.group_ptr[0], sizeof(unsigned) * ngptr) != 0, "Load group file");
                         if( ngptr != 0 ){
                             utils::Assert( fs.Read(&info.group_ptr[0], sizeof(unsigned) * ngptr) != 0, "Load group file");
                             utils::Assert( info.group_ptr.back() == data.NumRow(), "number of group must match number of record" );
