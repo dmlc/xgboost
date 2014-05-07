0bf6261961a68711c66b28aabbee61c686379c1a
tqchen <tianqi.tchen@gmail.com>
2014-05-07 11:52:12 -0700

    fix omp for bug in obj

--- regrank/xgboost_regrank_eval.h
+++ regrank/xgboost_regrank_eval.h
@@ -83,7 +83,7 @@ namespace xgboost{
                 float sum = 0.0f, wsum = 0.0f;
                 #pragma omp parallel for reduction(+:sum,wsum) schedule( static )
                 for (unsigned i = 0; i < ndata; ++i){
-                    const float wt = info.GetWeight(i);
+                    const float wt = info.GetWeight(i);                    
                     if (preds[i] > 0.5f){
                         if (info.labels[i] < 0.5f) sum += wt;
                     }
@@ -96,6 +96,28 @@ namespace xgboost{
             }
             virtual const char *Name(void) const{
                 return "error";
+            }
+        };
+
+
+        /*! \brief Error */
+        struct EvalMatchError : public IEvaluator{
+            virtual float Eval(const std::vector<float> &preds,
+                               const DMatrix::Info &info) const {
+                const unsigned ndata = static_cast<unsigned>(preds.size());
+                float sum = 0.0f, wsum = 0.0f;
+                #pragma omp parallel for reduction(+:sum,wsum) schedule( static )
+                for (unsigned i = 0; i < ndata; ++i){
+                    const float wt = info.GetWeight(i);                    
+                    if (static_cast<int>(preds[i]) != static_cast<int>(info.labels[i]) ){
+                        sum += wt;
+                    }
+                    wsum += wt;
+                }
+                return sum / wsum;
+            }
+            virtual const char *Name(void) const{
+                return "merror";
             }
         };
 
@@ -270,6 +292,7 @@ namespace xgboost{
                 }
                 if (!strcmp(name, "rmse"))    evals_.push_back(new EvalRMSE());
                 if (!strcmp(name, "error"))   evals_.push_back(new EvalError());
+                if (!strcmp(name, "merror"))   evals_.push_back(new EvalMatchError());
                 if (!strcmp(name, "logloss")) evals_.push_back(new EvalLogLoss());
                 if (!strcmp(name, "auc"))    evals_.push_back(new EvalAuc());
                 if (!strncmp(name, "pre@", 4)) evals_.push_back(new EvalPrecision(name));
