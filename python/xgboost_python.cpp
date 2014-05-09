5e0d52cb8cf452b8aaba9345d0e6abc68cba220d
tqchen <tianqi.tchen@gmail.com>
2014-05-08 20:15:23 -0700

    add python o3

--- python/xgboost_python.cpp
+++ python/xgboost_python.cpp
@@ -75,6 +75,7 @@ namespace xgboost{
             inline void CheckInit(void){
                 if(!init_col_){
                     this->data.InitData();
+                    init_col_ = true;
                 }
                 utils::Assert( this->data.NumRow() == this->info.labels.size(), "DMatrix: number of labels must match number of rows in matrix");
             }
