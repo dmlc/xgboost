e8d81c1da559c98238c304b683bf39921436c7f8
kalenhaha <chenkl198812@gmail.com>
2014-05-12 22:21:07 +0800

    Add LETOR MQ2008 for rank demo

---
+++ demo/rank/runexp.sh
@@ -0,0 +1,11 @@
+python trans_data.py train.txt mq2008.train mq2008.train.group
+
+python trans_data.py test.txt mq2008.test mq2008.test.group
+
+python trans_data.py vali.txt mq2008.vali mq2008.vali.group
+
+../../xgboost mq2008.conf
+
+../../xgboost mq2008.conf task=pred model_in=0002.model
+
+../../xgboost mq2008.conf task=dump model_in=0002.model name_dump=dump.raw.txt
