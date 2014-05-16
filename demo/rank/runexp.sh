e3a0c0efe565fb5c1d2e4d7603269a9cfe532e9e
yepyao <yepyao@gmail.com>
2014-05-16 21:18:32 +0800

    Download data set from web site

--- demo/rank/runexp.sh
+++ demo/rank/runexp.sh
@@ -1,8 +1,14 @@
-python trans_data.py train.txt mq2008.train mq2008.train.group
+#Download the dataset from web site
+wget http://research.microsoft.com/en-us/um/beijing/projects/letor/LETOR4.0/Data/MQ2008.rar
 
-python trans_data.py test.txt mq2008.test mq2008.test.group
+#please first install the unrar package
+unrar x MQ2008
 
-python trans_data.py vali.txt mq2008.vali mq2008.vali.group
+python MQ2008/Fold1/trans_data.py train.txt mq2008.train mq2008.train.group
+
+python MQ2008/Fold1/trans_data.py test.txt mq2008.test mq2008.test.group
+
+python MQ2008/Fold1/trans_data.py vali.txt mq2008.vali mq2008.vali.group
 
 ../../xgboost mq2008.conf
 
