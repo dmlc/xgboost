8a0f8a93c76a55a6f6a42894e5646c91793136c5
tqchen <tianqi.tchen@gmail.com>
2014-05-16 18:46:43 -0700

    chg scripts

--- demo/rank/runexp.sh
+++ demo/rank/runexp.sh
@@ -1,7 +1,5 @@
 python trans_data.py train.txt mq2008.train mq2008.train.group
-
 python trans_data.py test.txt mq2008.test mq2008.test.group
-
 python trans_data.py vali.txt mq2008.vali mq2008.vali.group
 
 ../../xgboost mq2008.conf
