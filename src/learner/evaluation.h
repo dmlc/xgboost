de50aae3852037cbb4ae17e69b45f61b82fd0e72
tqchen <tianqi.tchen@gmail.com>
2014-08-19 08:02:29 -0700

    add pratio

--- src/learner/evaluation.h
+++ src/learner/evaluation.h
@@ -41,6 +41,7 @@ inline IEvaluator* CreateEvaluator(const char *name) {
   if (!strcmp(name, "auc")) return new EvalAuc();
   if (!strncmp(name, "ams@", 4)) return new EvalAMS(name);
   if (!strncmp(name, "pre@", 4)) return new EvalPrecision(name);
+  if (!strncmp(name, "pratio@", 4)) return new EvalPrecisionRatio(name);
   if (!strncmp(name, "map", 3)) return new EvalMAP(name);
   if (!strncmp(name, "ndcg", 3)) return new EvalNDCG(name);
   utils::Error("unknown evaluation metric type: %s", name);
