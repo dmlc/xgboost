db7a2d91bda51d4cd1a4f0e8d525f0aaeb3aeaae
tqchen@graphlab.com <tqchen@graphlab.com>
2014-08-19 12:20:31 -0700

    ok

--- src/data.h
+++ src/data.h
@@ -206,8 +206,8 @@ class FMatrixS : public FMatrixInterface<FMatrixS>{
   /*! \brief get col sorted iterator */
   inline ColIter GetSortedCol(size_t cidx) const {
     utils::Assert(cidx < this->NumCol(), "col id exceed bound");
-    return ColIter(&col_data_[col_ptr_[cidx]] - 1,
-                   &col_data_[col_ptr_[cidx + 1]] - 1);
+    return ColIter(&col_data_[0] + col_ptr_[cidx] - 1,
+                   &col_data_[0] + col_ptr_[cidx + 1] - 1);
   }
   /*!
    * \brief get reversed col iterator,
@@ -215,8 +215,8 @@ class FMatrixS : public FMatrixInterface<FMatrixS>{
    */
   inline ColBackIter GetReverseSortedCol(size_t cidx) const {
     utils::Assert(cidx < this->NumCol(), "col id exceed bound");
-    return ColBackIter(&col_data_[col_ptr_[cidx + 1]],
-                       &col_data_[col_ptr_[cidx]]);
+    return ColBackIter(&col_data_[0] + col_ptr_[cidx + 1],
+                       &col_data_[0] + col_ptr_[cidx]);
   }
   /*! \brief get col size */
   inline size_t GetColSize(size_t cidx) const {
