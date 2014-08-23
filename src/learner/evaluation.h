9d210f9bd3a8d0a9d5c0d9419d5462414a6b6c44
tqchen <tianqi.tchen@gmail.com>
2014-08-22 20:14:43 -0700

    ok

--- src/learner/evaluation.h
+++ src/learner/evaluation.h
@@ -43,6 +43,7 @@ inline IEvaluator* CreateEvaluator(const char *name) {
   if (!strncmp(name, "ams@", 4)) return new EvalAMS(name);
   if (!strncmp(name, "pre@", 4)) return new EvalPrecision(name);
   if (!strncmp(name, "pratio@", 7)) return new EvalPrecisionRatio(name);
+  if (!strncmp(name, "apratio@", 8)) return new EvalPrecisionRatio(name);
   if (!strncmp(name, "map", 3)) return new EvalMAP(name);
   if (!strncmp(name, "ndcg", 3)) return new EvalNDCG(name);
   utils::Error("unknown evaluation metric type: %s", name);
