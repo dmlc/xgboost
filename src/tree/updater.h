ba9fbd380c9ad3024e73ae8b25cd0ac20297883a
tqchen <tianqi.tchen@gmail.com>
2014-08-24 15:22:11 -0700

    templatize refresher

--- src/tree/updater.h
+++ src/tree/updater.h
@@ -60,7 +60,7 @@ namespace tree {
 template<typename FMatrix>
 inline IUpdater<FMatrix>* CreateUpdater(const char *name) {
   if (!strcmp(name, "prune")) return new TreePruner<FMatrix>();
-  if (!strcmp(name, "refresh")) return new TreeRefresher<FMatrix>();
+  if (!strcmp(name, "refresh")) return new TreeRefresher<FMatrix, GradStats>();
   if (!strcmp(name, "grow_colmaker")) return new ColMaker<FMatrix, GradStats>();
   utils::Error("unknown updater:%s", name);
   return NULL;
