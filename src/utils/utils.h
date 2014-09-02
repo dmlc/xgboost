6f9a8b5fdb208310eed98a1b5011f47e92e8a924
tqchen <tianqi.tchen@gmail.com>
2014-09-01 17:23:44 -0700

    fine fix

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -7,9 +7,13 @@
  */
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
-#include <cstdarg>
 #include <string>
 #include <cstdlib>
+
+#ifndef XGBOOST_STRICT_CXX98_
+#include <cstdarg>
+#endif
+
 #if !defined(__GNUC__)
 #define fopen64 fopen
 #endif
@@ -72,7 +76,7 @@ inline void HandlePrint(const char *msg) {
   printf("%s", msg);
 }
 #else
-#ifndef XGBOOST_STRICT_CXX98__
+#ifndef XGBOOST_STRICT_CXX98_
 // include declarations, some one must implement this
 void HandleAssertError(const char *msg);
 void HandleCheckError(const char *msg);
