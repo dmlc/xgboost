5668d26ca54e0088cfa7627e63e4142786cdd8e2
tqchen <tianqi.tchen@gmail.com>
2014-09-01 17:02:42 -0700

    make R package strict c99

--- R-package/src/xgboost_R.cpp
+++ R-package/src/xgboost_R.cpp
@@ -8,21 +8,22 @@
 #include "src/utils/utils.h"
 #include "src/utils/omp.h"
 #include "src/utils/matrix_csr.h"
-
 using namespace xgboost;
+
+extern "C" {
+  void XGBoostAssert_R(int exp, const char *fmt, ...);
+  void XGBoostCheck_R(int exp, const char *fmt, ...);
+}
+
 // implements error handling
 namespace xgboost {
 namespace utils {
-void HandleAssertError(const char *msg) {
-  error("%s", msg);
-}
-void HandleCheckError(const char *msg) {
-  error("%s", msg);
-}
-void HandlePrint(const char *msg) {
-  Rprintf("%s", msg);
-}
+void (*Printf)(const char *fmt, ...) = Rprintf;
+void (*Assert)(int exp, const char *fmt, ...) = XGBoostAssert_R;
+void (*Check)(int exp, const char *fmt, ...) = XGBoostCheck_R;
+void (*Error)(const char *fmt, ...) = error;
 }  // namespace utils
+
 namespace random {
 void Seed(unsigned seed) {
   warning("parameter seed is ignored, please set random seed using set.seed");
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -70,14 +70,23 @@ inline void HandlePrint(const char *msg) {
   printf("%s", msg);
 }
 #else
+#ifndef XGBOOST_STRICT_CXX98__
 // include declarations, some one must implement this
 void HandleAssertError(const char *msg);
 void HandleCheckError(const char *msg);
 void HandlePrint(const char *msg);
 #endif
+#endif
 
+#ifdef XGBOOST_STRICT_CXX98_
+// these function pointers are to be assigned 
+extern void (*Printf)(const char *fmt, ...);
+extern void (*Assert)(int exp, const char *fmt, ...);
+extern void (*Check)(int exp, const char *fmt, ...);
+extern void (*Error)(const char *fmt, ...);
+#else
 /*! \brief printf, print message to the console */
-inline void Printf(const char *fmt, ...) {
+inline void Printf(const char *fmt, ...) {  
   std::string msg(kPrintBuffer, '\0');
   va_list args;
   va_start(args, fmt);
@@ -121,6 +130,7 @@ inline void Error(const char *fmt, ...) {
     HandleCheckError(msg.c_str());
   }
 }
+#endif
 
 /*! \brief replace fopen, report error when the file open fails */
 inline FILE *FopenCheck(const char *fname, const char *flag) {
