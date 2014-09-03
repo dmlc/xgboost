ed8727b695f2782656464ccb70367d297635bca6
tqchen <tianqi.tchen@gmail.com>
2014-09-03 13:13:54 -0700

    move python example

---
+++ demo/data/README.md
@@ -0,0 +1,2 @@
+This folder contains processed example dataset used by the demos.
+Copyright of the dataset belongs to the original copyright holder
---
+++ demo/guide-python/runall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python basic_walkthrough.py
+python custom_objective.py
+python boost_from_prediction.py
+python boost_from_prediction.py
+rm *~ *.model *.buffer 
