80636cd8044cb0d68ba78310da4c2a8512fe9f8e
hetong <hetong007@gmail.com>
2014-09-06 23:06:47 -0700

    improve runall.R

--- R-package/demo/runall.R
+++ R-package/demo/runall.R
@@ -1,8 +1,8 @@
 # running all scripts in demo folder
-source('basic_walkthrough.R')
-source('custom_objective.R')
-source('boost_from_prediction.R')
-source('predict_first_ntree.R')
-source('generalized_linear_model.R')
-source('cross_validation.R')
+demo(basic_walkthrough)
+demo(custom_objective)
+demo(boost_from_prediction)
+demo(predict_first_ntree)
+demo(generalized_linear_model)
+demo(cross_validation)
 
