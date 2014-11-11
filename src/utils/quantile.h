e7ea87b5fd75b96b36111d0f6c2ad20fb549d3c1
tqchen <tianqi.tchen@gmail.com>
2014-11-10 22:03:42 -0800

    ok for now

--- src/utils/quantile.h
+++ src/utils/quantile.h
@@ -174,9 +174,9 @@ struct WQSummary {
     for (size_t k = 1; k < n; ++k) {
       RType dx2 =  2 * ((k * range) / n + begin);
       // find first i such that  d < (rmax[i+1] + rmin[i+1]) / 2 
-      while (i < src.size - 1 &&
-             dx2 >= src.data[i + 1].rmax + src.data[i + 1].rmin) ++i;
-      if (i == src.size - 1) break;
+      while (i < src.size - 1 
+             && dx2 >= src.data[i + 1].rmax + src.data[i + 1].rmin) ++i;
+      utils::Assert(i != src.size - 1, "this cannot happen");
       if (dx2 < src.data[i].rmin_next() + src.data[i + 1].rmax_prev()) {
         if (i != lastidx) {
           data[size++] = src.data[i]; lastidx = i;
