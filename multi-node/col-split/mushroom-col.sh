9af464303aec12fc820163d23d7e2af7a55b4c89
tqchen <tianqi.tchen@gmail.com>
2014-11-19 20:09:26 -0800

    checkin row continue training

--- multi-node/col-split/mushroom-col.sh
+++ multi-node/col-split/mushroom-col.sh
@@ -16,4 +16,9 @@ mpirun -n $k ../../xgboost-mpi mushroom-col.conf dsplit=col
 
 # the model can be directly loaded by single machine xgboost solver, as usuall
 ../../xgboost mushroom-col.conf task=dump model_in=0002.model fmap=../../demo/data/featmap.txt name_dump=dump.nice.$k.txt
+
+# run for one round, and continue training
+mpirun -n $k ../../xgboost-mpi mushroom-col.conf dsplit=col num_round=1
+mpirun -n $k ../../xgboost-mpi mushroom-col.conf dsplit=col model_in=0001.model
+
 cat dump.nice.$k.txt
