90b9f1a98a2056a235ec6d792b97320f2c07514c
tqchen <tianqi.tchen@gmail.com>
2014-12-03 15:04:30 -0800

    add keepalive script

--- test/keepalive.sh
+++ test/keepalive.sh
@@ -5,8 +5,10 @@ then
     echo "Repeatively run program until success"
     exit -1
 fi
+nrep=0
 echo ./$@ job_id=$OMPI_COMM_WORLD_RANK
-until ./$@ job_id=$OMPI_COMM_WORLD_RANK; do
-    echo "Server "$1" crashed with exit code $?.  Respawning.." >&2
+until ./$@ job_id=$OMPI_COMM_WORLD_RANK repeat=$nrep; do
     sleep 1
+    nrep=$((nrep+1))
+    echo ./$@ job_id=$OMPI_COMM_WORLD_RANK repeat=$nrep
 done
