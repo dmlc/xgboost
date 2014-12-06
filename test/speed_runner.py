e0053c62e1e18bfef85c9e4db55be3d6e4b909df
nachocano <nachoacano@gmail.com>
2014-12-06 12:05:08 -0800

    adding executable

--- test/speed_runner.py
+++ test/speed_runner.py
@@ -5,6 +5,7 @@ def main():
   parser = argparse.ArgumentParser(description='TODO')
   parser.add_argument('-ho', '--host_dir', required=True)
   parser.add_argument('-s', '--submit_script', required=True)
+  parser.add_argument('-ex', '--executable', required=True)
   args = parser.parse_args()
 
   ndata = [10^4, 10^5, 10^6, 10^7, 10^8]
@@ -16,7 +17,7 @@ def main():
     for repeat in nrepeat:
       for machine in machines:
         host_file = os.path.join(args.host_dir, 'host%d' % machine)
-        cmd = 'python %s %d %s %d %d' % (args.submit_script, machine, host_file, data, repeat)
+        cmd = 'python %s %d %s %s %d %d' % (args.submit_script, machine, host_file, args.executable, data, repeat)
         print 'data=%d, repeat=%d, machine=%d' % (data, repeat, machine)
         os.system(cmd)
 
