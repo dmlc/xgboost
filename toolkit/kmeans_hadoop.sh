fd533d9a767d58b4ee190cc7a779448d673bfd8c
tqchen <tianqi.tchen@gmail.com>
2014-12-22 00:32:08 -0800

    add kmeans

--- toolkit/kmeans_hadoop.sh
+++ toolkit/kmeans_hadoop.sh
@@ -4,6 +4,6 @@ then
     echo "Usage: <nslaves> <input_data> <ncluster> <max_iteration> <output>"
     exit -1
 fi
-python ../rabit_hadoop.py -s $1 -i $2  -m kmeans --args "stdin "$3" "$4" stdout" -o $5
-
- 
+#set path to hadoop streaming jar here
+STREAMING_JAR=
+python ../rabit_hadoop.py -hs $STREAMING_JAR -s $1 -i $2  -m kmeans --args "stdin "$3" "$4" stdout" -o $5
