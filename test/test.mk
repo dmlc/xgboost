12399a1d42d2a0ad62400b728b1cbfb0d93b4365
tqchen <tianqi.tchen@gmail.com>
2014-12-21 17:59:12 -0800

    add more mocktest

--- test/test.mk
+++ test/test.mk
@@ -19,3 +19,8 @@ local_recover_10_10k:
 model_recover_10_10k:
 	../tracker/rabit_mpi.py 10 local keepalive.sh test_model_recover 10000 mock=0,0,1,0 mock=1,1,1,0
 
+model_recover_10_10k_die_same:
+	../tracker/rabit_mpi.py 10 local keepalive.sh test_model_recover 10000 mock=0,0,1,0 mock=1,1,1,0 mock=0,1,1,0 mock=4,1,1,0 mock=9,1,1,0
+
+model_recover_10_10k_die_hard:
+	../tracker/rabit_mpi.py 10 local keepalive.sh test_model_recover 10000 mock=0,0,1,0 mock=1,1,1,0 mock=1,1,1,1 mock=0,1,1,0 mock=4,1,1,0 mock=9,1,1,0 mock=8,1,2,0 mock=4,1,3,0
