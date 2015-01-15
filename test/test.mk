6d5ac6446c2cad5c7dd29ea2dcec526b917e6cad
tqchen <tianqi.tchen@gmail.com>
2015-01-15 10:24:58 -0800

    chg test folder

--- test/test.mk
+++ test/test.mk
@@ -16,6 +16,9 @@ model_recover_10_10k_die_hard:
 local_recover_10_10k:
 	../tracker/rabit_demo.py -n 10 local_recover 10000 mock=0,0,1,0 mock=1,1,1,0 mock=0,1,1,0 mock=4,1,1,0 mock=9,1,1,0 mock=1,1,1,1
 
+pylocal_recover_10_10k:
+	../tracker/rabit_demo.py -n 10 ./local_recover.py 10000 mock=0,0,1,0 mock=1,1,1,0 mock=0,1,1,0 mock=4,1,1,0 mock=9,1,1,0 mock=1,1,1,1
+
 lazy_recover_10_10k_die_hard:
 	../tracker/rabit_demo.py -n 10 lazy_recover 10000 mock=0,0,1,0 mock=1,1,1,0 mock=1,1,1,1 mock=0,1,1,0 mock=4,1,1,0 mock=9,1,1,0 mock=8,1,2,0 mock=4,1,3,0
 
