395d5c29d537ec0512fce6a2b6d1d17e61d0368d
tqchen <tianqi.tchen@gmail.com>
2015-03-06 22:30:23 -0800

    add make system

--- rabit-learn/make/common.mk
+++ rabit-learn/make/common.mk
@@ -1,19 +1,28 @@
 # this is the common build script for rabit programs
-# you do not have to use it 
-export CC  = gcc
-export CXX = g++
-export MPICXX = mpicxx
+# you do not have to use it
 export LDFLAGS= -pthread -lm -L../../lib -lrt
-export CFLAGS = -Wall  -msse2  -Wno-unknown-pragmas -fPIC -I../../include 
+export CFLAGS = -Wall  -msse2  -Wno-unknown-pragmas -fPIC -I../../include  
+
+# setup opencv
+ifeq ($(USE_HDFS),1)
+	CFLAGS+= -DRABIT_USE_HDFS=1 -I$(HDFS_HOME)/include
+	LDFLAGS+= -L$(HDFS_HOME)/lib/native -lhdfs
+else
+	CFLAGS+= -DRABIT_USE_HDFS=0
+endif
+
 
 .PHONY: clean all lib mpi
+
 all: $(BIN) $(MOCKBIN)
+
 mpi: $(MPIBIN)
 
 lib:
 	cd ../..;make lib/librabit.a lib/librabit_mock.a; cd -
 libmpi:
 	cd ../..;make lib/librabit_mpi.a;cd -
+
 
 $(BIN) : 
 	$(CXX) $(CFLAGS) -o $@ $(filter %.cpp %.o %.c %.cc,  $^) -lrabit $(LDFLAGS) 
