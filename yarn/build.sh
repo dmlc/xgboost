d6b68286ee35afac534ec55bde637c04ac3e47b5
Tianqi Chen <tqchen@users.noreply.github.com>
2015-03-12 09:03:02 -0700

    Update build.sh

--- yarn/build.sh
+++ yarn/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CPATH=`${HADOOP_PREFIX}/bin/hadoop classpath`
+CPATH=`${HADOOP_HOME}/bin/hadoop classpath`
 javac -cp $CPATH -d bin src/org/apache/hadoop/yarn/rabit/*
 jar cf rabit-yarn.jar -C bin . 
