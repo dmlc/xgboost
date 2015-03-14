5dc843cff3db3062715441be888d2c5fa8263ade
tqchen <tianqi.tchen@gmail.com>
2015-03-14 16:46:54 -0700

    refactor fileio

--- rabit-learn/io/hdfs-inl.h
+++ rabit-learn/io/hdfs-inl.h
@@ -15,7 +15,7 @@
 /*! \brief io interface */
 namespace rabit {
 namespace io {
-class HDFSStream : public utils::ISeekStream {
+class HDFSStream : public ISeekStream {
  public:
   HDFSStream(hdfsFS fs,
              const char *fname,
@@ -93,7 +93,7 @@ class HDFSStream : public utils::ISeekStream {
     }
   }  
   
- private:
+ private:  
   hdfsFS fs_;
   hdfsFile fp_;
   bool at_end_;
@@ -101,15 +101,14 @@ class HDFSStream : public utils::ISeekStream {
 };
 
 /*! \brief line split from normal file system */
-class HDFSSplit : public LineSplitBase {
+class HDFSProvider : public LineSplitter::IFileProvider {
  public:
-  explicit HDFSSplit(const char *uri, unsigned rank, unsigned nsplit) {
+  explicit HDFSProvider(const char *uri) {
     fs_ = hdfsConnect("default", 0);
     utils::Check(fs_ != NULL, "error when connecting to default HDFS");
     std::vector<std::string> paths;
-    LineSplitBase::SplitNames(&paths, uri, "#");
+    LineSplitter::SplitNames(&paths, uri, "#");
     // get the files
-    std::vector<size_t> fsize;
     for (size_t  i = 0; i < paths.size(); ++i) {
       hdfsFileInfo *info = hdfsGetPathInfo(fs_, paths[i].c_str());
       utils::Check(info != NULL, "path %s do not exist", paths[i].c_str());
@@ -118,34 +117,37 @@ class HDFSSplit : public LineSplitBase {
         hdfsFileInfo *files = hdfsListDirectory(fs_, info->mName, &nentry);
         utils::Check(files != NULL, "error when ListDirectory %s", info->mName);
         for (int i = 0; i < nentry; ++i) {
-          if (files[i].mKind == 'F') {
-            fsize.push_back(files[i].mSize);
+          if (files[i].mKind == 'F' && files[i].mSize != 0) {
+            fsize_.push_back(files[i].mSize);            
             fnames_.push_back(std::string(files[i].mName));
           }
         }
         hdfsFreeFileInfo(files, nentry);
       } else {
-        fsize.push_back(info->mSize);
-        fnames_.push_back(std::string(info->mName));
+        if (info->mSize != 0) {
+          fsize_.push_back(info->mSize);
+          fnames_.push_back(std::string(info->mName));
+        }
       }
       hdfsFreeFileInfo(info, 1);
     }
-    LineSplitBase::Init(fsize, rank, nsplit);
   }
-  virtual ~HDFSSplit(void) {
-    LineSplitBase::Destroy();
+  virtual ~HDFSProvider(void) {
     utils::Check(hdfsDisconnect(fs_) == 0, "hdfsDisconnect error");
+  }  
+  virtual const std::vector<size_t> &FileSize(void) const {
+    return fsize_;
   }
-  
- protected:
-  virtual utils::ISeekStream *GetFile(size_t file_index) {
+  virtual ISeekStream *Open(size_t file_index) {
     utils::Assert(file_index < fnames_.size(), "file index exceed bound"); 
     return new HDFSStream(fs_, fnames_[file_index].c_str(), "r", false);
   }
-
+  
  private:
   // hdfs handle
   hdfsFS fs_;
+  // file sizes
+  std::vector<size_t> fsize_;
   // file names
   std::vector<std::string> fnames_;
 };
--- rabit-learn/io/io-inl.h
+++ rabit-learn/io/io-inl.h
@@ -30,16 +30,16 @@ inline InputSplit *CreateInputSplit(const char *uri,
     return new SingleFileSplit(uri);
   }
   if (!strncmp(uri, "file://", 7)) {
-    return new FileSplit(uri, part, nsplit);
+    return new LineSplitter(new FileProvider(uri), part, nsplit);
   }
   if (!strncmp(uri, "hdfs://", 7)) {
 #if RABIT_USE_HDFS
-    return new HDFSSplit(uri, part, nsplit);
+    return new LineSplitter(new HDFSProvider(uri), part, nsplit);
 #else
     utils::Error("Please compile with RABIT_USE_HDFS=1");
 #endif
   }
-  return new FileSplit(uri, part, nsplit);  
+  return new LineSplitter(new FileProvider(uri), part, nsplit);
 }
 /*!
  * \brief create an stream, the stream must be able to close
