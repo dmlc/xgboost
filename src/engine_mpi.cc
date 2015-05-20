df14bb1671d19d68487ca0f45128d7c9e6442f57
Chuntao Hong <chuntao.hong@gmail.com>
2015-05-20 11:36:17 +0800

    fix type

--- src/engine_mpi.cc
+++ src/engine_mpi.cc
@@ -110,8 +110,8 @@ inline MPI::Datatype GetType(mpi::DataType dtype) {
     case kULong: return MPI::UNSIGNED_LONG;
     case kFloat: return MPI::FLOAT;
     case kDouble: return MPI::DOUBLE;
-    case kLong: return MPI::LONG_LONG;
-    case kULong: return MPI::UNSIGNED_LONG_LONG;
+    case kLongLong: return MPI::LONG_LONG;
+    case kULongLong: return MPI::UNSIGNED_LONG_LONG;
   }
   utils::Error("unknown mpi::DataType");
   return MPI::CHAR;
