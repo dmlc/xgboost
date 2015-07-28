32e19558e6cd910d0c4cc622df74f06bb435feb2
tqchen <tianqi.tchen@gmail.com>
2015-07-27 18:38:22 -0700

    ok

--- doc/conf.py
+++ doc/conf.py
@@ -155,10 +155,10 @@ source_parsers = {
 def setup_path(app):
     read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
     if read_the_docs_build:
-        subprocess.call("cd ..; git clone https://github.com/tqchen/recommonmark recommonmark-customized", shell=True)
-    sys.path.insert(0, os.path.abspath('../recommonmark-customized'))
-    reload(recommonmark)
-    from recommonmark import parser
+        subprocess.call('cd ..; git clone https://github.com/tqchen/recommonmark recommonmark-customized;' +
+                        'mv recomonmark-customized/recommonmark recom', shell=True)
+    sys.path.insert(0, os.path.abspath('..'))
+    from recom import parser
     global source_parsers
     source_parsers['.md'] = parser.CommonMarkParser
 
