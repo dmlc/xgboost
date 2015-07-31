60217a2c02dc80376f1bc6084b10d003b2aeef1f
tqchen <tianqi.tchen@gmail.com>
2015-07-30 22:08:48 -0700

    checkin all python

---
+++ python-package/xgboost/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+"""XGBoost: eXtreme Gradient Boosting library.
+
+Contributors: https://github.com/dmlc/xgboost/blob/master/CONTRIBUTORS.md
+"""
+
+from __future__ import absolute_import
+from .core import DMatrix, Booster
+from .training import train, cv
+from .sklearn import XGBModel, XGBClassifier, XGBRegressor
+
+__version__ = '0.4'
