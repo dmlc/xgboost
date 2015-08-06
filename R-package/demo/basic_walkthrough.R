d3a19ff355ddc0a7d5a8af7778de512b757c2e31
Ajinkya Kale <kaleajinkya@gmail.com>
2015-08-05 17:26:52 -0700

    change prints to cats

--- R-package/demo/basic_walkthrough.R
+++ R-package/demo/basic_walkthrough.R
@@ -14,28 +14,28 @@ class(train$data)
 # this is the basic usage of xgboost you can put matrix in data field
 # note: we are puting in sparse matrix here, xgboost naturally handles sparse input
 # use sparse matrix when your feature is sparse(e.g. when you using one-hot encoding vector)
-print("training xgboost with sparseMatrix")
+cat("Training xgboost with sparseMatrix")
 bst <- xgboost(data = train$data, label = train$label, max.depth = 2, eta = 1, nround = 2,
                nthread = 2, objective = "binary:logistic")
 # alternatively, you can put in dense matrix, i.e. basic R-matrix
-print("training xgboost with Matrix")
+cat("Training xgboost with Matrix")
 bst <- xgboost(data = as.matrix(train$data), label = train$label, max.depth = 2, eta = 1, nround = 2,
                nthread = 2, objective = "binary:logistic")
 
 # you can also put in xgb.DMatrix object, stores label, data and other meta datas needed for advanced features
-print("training xgboost with xgb.DMatrix")
+cat("Training xgboost with xgb.DMatrix")
 dtrain <- xgb.DMatrix(data = train$data, label = train$label)
 bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nround = 2, nthread = 2, 
                objective = "binary:logistic")
 
 # Verbose = 0,1,2
-print ('train xgboost with verbose 0, no message')
+cat('Train xgboost with verbose 0, no message')
 bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nround = 2,
                nthread = 2, objective = "binary:logistic", verbose = 0)
-print ('train xgboost with verbose 1, print evaluation metric')
+cat('Train xgboost with verbose 1, print evaluation metric')
 bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nround = 2,
                nthread = 2, objective = "binary:logistic", verbose = 1)
-print ('train xgboost with verbose 2, also print information about tree')
+cat('Train xgboost with verbose 2, also print information about tree')
 bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nround = 2,
                nthread = 2, objective = "binary:logistic", verbose = 2)
 
@@ -48,7 +48,7 @@ bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nround = 2,
 # you can put in Matrix, sparseMatrix, or xgb.DMatrix 
 pred <- predict(bst, test$data)
 err <- mean(as.numeric(pred > 0.5) != test$label)
-print(paste("test-error=", err))
+cat(paste("test-error=", err))
 
 #-------------------save and load models-------------------------
 # save model to binary local file
@@ -57,7 +57,7 @@ xgb.save(bst, "xgboost.model")
 bst2 <- xgb.load("xgboost.model")
 pred2 <- predict(bst2, test$data)
 # pred2 should be identical to pred
-print(paste("sum(abs(pred2-pred))=", sum(abs(pred2-pred))))
+cat(paste("sum(abs(pred2-pred))=", sum(abs(pred2-pred))))
 
 # save model to R's raw vector
 raw = xgb.save.raw(bst)
@@ -65,7 +65,7 @@ raw = xgb.save.raw(bst)
 bst3 <- xgb.load(raw)
 pred3 <- predict(bst3, test$data)
 # pred2 should be identical to pred
-print(paste("sum(abs(pred3-pred))=", sum(abs(pred2-pred))))
+cat(paste("sum(abs(pred3-pred))=", sum(abs(pred2-pred))))
 
 #----------------Advanced features --------------
 # to use advanced features, we need to put data in xgb.DMatrix
@@ -76,11 +76,11 @@ dtest <- xgb.DMatrix(data = test$data, label=test$label)
 watchlist <- list(train=dtrain, test=dtest)
 # to train with watchlist, use xgb.train, which contains more advanced features
 # watchlist allows us to monitor the evaluation result on all data in the list 
-print ('train xgboost using xgb.train with watchlist')
+cat('Train xgboost using xgb.train with watchlist')
 bst <- xgb.train(data=dtrain, max.depth=2, eta=1, nround=2, watchlist=watchlist,
                  nthread = 2, objective = "binary:logistic")
 # we can change evaluation metrics, or use multiple evaluation metrics
-print ('train xgboost using xgb.train with watchlist, watch logloss and error')
+cat('Train xgboost using xgb.train with watchlist, watch logloss and error')
 bst <- xgb.train(data=dtrain, max.depth=2, eta=1, nround=2, watchlist=watchlist,
                  eval.metric = "error", eval.metric = "logloss",
                  nthread = 2, objective = "binary:logistic")
@@ -95,13 +95,14 @@ bst <- xgb.train(data=dtrain2, max.depth=2, eta=1, nround=2, watchlist=watchlist
 label = getinfo(dtest, "label")
 pred <- predict(bst, dtest)
 err <- as.numeric(sum(as.integer(pred > 0.5) != label))/length(label)
-print(paste("test-error=", err))
+cat(paste("test-error=", err))
 
 # You can dump the tree you learned using xgb.dump into a text file
 xgb.dump(bst, "dump.raw.txt", with.stats = T)
 
 # Finally, you can check which features are the most important.
-print("Most important features (look at column Gain):")
+cat("Most important features (look at column Gain):")
+
 imp_matrix <- xgb.importance(feature_names = train$data@Dimnames[[2]], filename_dump = "dump.raw.txt")
 print(imp_matrix)
 
