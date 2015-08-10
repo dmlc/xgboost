b662ce68896f55fedb46eeaf9754b9d85fc558d0
terrytangyuan <terrytangyuan@gmail.com>
2015-08-10 10:11:38 -0400

    finished unit tests for glm

--- R-package/tests/testthat/test_glm.R
+++ R-package/tests/testthat/test_glm.R
@@ -7,4 +7,11 @@ test_that("glm works", {
 	data(agaricus.test, package='xgboost')
 	dtrain <- xgb.DMatrix(agaricus.train$data, label = agaricus.train$label)
 	dtest <- xgb.DMatrix(agaricus.test$data, label = agaricus.test$label)
+	param <- list(objective = "binary:logistic", booster = "gblinear",
+              nthread = 2, alpha = 0.0001, lambda = 1)
+	watchlist <- list(eval = dtest, train = dtrain)
+	num_round <- 2
+	bst <- xgb.train(param, dtrain, num_round, watchlist)
+	ypred <- predict(bst, dtest)
+	labels <- getinfo(dtest, 'label')
 })
