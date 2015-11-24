4b3313a3fe6b8e485f6478ebf0f52a724d1cc44b
tqchen <tianqi.tchen@gmail.com>
2015-11-24 14:25:56 -0800

    [REFACTOR] cleanup structure

---
+++ include/xgboost/data.h
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2014 by Contributors
+ * \file data.h
+ * \brief the input data structure for gradient boosting
+ * \author Tianqi Chen
+ */
+#ifndef XGBOOST_DATA_H_
+#define XGBOOST_DATA_H_
