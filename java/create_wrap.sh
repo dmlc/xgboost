d867579a696e4d0d351d9c95250370ac9786f5ae
Vadim Khotilovich <khotilovich@gmail.com>
2015-12-18 14:18:28 -0600

    make it possible to run create_wrap.sh not only from its directory

--- java/create_wrap.sh
+++ java/create_wrap.sh
@@ -1,4 +1,10 @@
+#!/bin/env sh
+
 echo "build java wrapper"
+
+# cd to script's directory
+pushd `dirname $0` > /dev/null
+
 cd ..
 make java
 cd java
@@ -12,4 +18,5 @@ fi
 rm -f xgboost4j/src/main/resources/lib/libxgboostjavawrapper.so
 mv libxgboostjavawrapper.so xgboost4j/src/main/resources/lib/
 
+popd > /dev/null
 echo "complete"
