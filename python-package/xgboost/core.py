d9e50fd7f3f3bd01111efcde1bacc19e9d883eff
Kai Luo <gluokai@gmail.com>
2016-01-20 19:57:20 +0800

    __copy__ calls __deepcopy__ with an argument

--- python-package/xgboost/core.py
+++ python-package/xgboost/core.py
@@ -641,7 +641,7 @@ class Booster(object):
         self.set_param({'seed': 0})
 
     def __copy__(self):
-        return self.__deepcopy__()
+        return self.__deepcopy__(None)
 
     def __deepcopy__(self, memo):
         return Booster(model_file=self.save_raw())
