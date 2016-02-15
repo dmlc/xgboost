2443cb9ca8d54ea7b37c600aadbf0857da4c2125
Florian <florian.simond@outlook.com>
2016-02-15 12:00:35 +0100

    Fix CV which was monitoring train-metric
    https://github.com/dmlc/xgboost/issues/807

--- R-package/R/xgb.cv.R
+++ R-package/R/xgb.cv.R
@@ -191,7 +191,7 @@ xgb.cv <- function(params=list(), data, nrounds, nfold, label = NULL, missing = 
 
         # early_Stopping
         if (!is.null(early.stop.round)){
-            score <- strsplit(ret,'\\s+')[[1]][2 + length(metrics)]
+            score <- strsplit(ret,'\\s+')[[1]][1 + length(metrics) + 2]
             score <- strsplit(score,'\\+|:')[[1]][[2]]
             score <- as.numeric(score)
             if ( (maximize && score > bestScore) || (!maximize && score < bestScore)) {
