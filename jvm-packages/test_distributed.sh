dda226ab85fe64a4cea61154c802e7ee94b2ed24
tqchen <tianqi.tchen@gmail.com>
2016-03-06 21:09:13 -0800

    Add doc badge

--- jvm-packages/test_distributed.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Simple script to test distributed version, to be deleted later.
-cd xgboost4j-flink
-flink run -c ml.dmlc.xgboost4j.flink.Test -p 2  target/xgboost4j-flink-0.1-jar-with-dependencies.jar
-cd ..
