3b8f1b4b7856471a1b13d2df286cf118cca7e0d6
Mircea Neagovici-Negoescu <mircean@microsoft.com>
2016-04-23 20:47:14 -0700

    In mknfold init random seed only if the seed parameter is sent by the caller

--- python-package/xgboost/training.py
+++ python-package/xgboost/training.py
@@ -265,7 +265,8 @@ def mknfold(dall, nfold, param, seed, evals=(), fpreproc=None, stratified=False,
     Make an n-fold list of CVPack from random indices.
     """
     evals = list(evals)
-    np.random.seed(seed)
+    if seed != None:
+        np.random.seed(seed)
 
     if stratified is False and folds is None:
         randidx = np.random.permutation(dall.num_row())
@@ -358,7 +359,7 @@ def aggcv(rlist, show_stdv=True, verbose_eval=None, as_pandas=True, trial=0):
 
 def cv(params, dtrain, num_boost_round=10, nfold=3, stratified=False, folds=None,
        metrics=(), obj=None, feval=None, maximize=False, early_stopping_rounds=None,
-       fpreproc=None, as_pandas=True, verbose_eval=None, show_stdv=True, seed=0):
+       fpreproc=None, as_pandas=True, verbose_eval=None, show_stdv=True, seed=None):
     # pylint: disable = invalid-name
     """Cross-validation with given paramaters.
 
