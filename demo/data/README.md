a25e2b89d463e8cbb55dab3b1a2d421176fe1c42
Shengwen Yang <yangshengwen@gmail.com>
2016-07-06 17:25:09 +0800

    Add a script to generate the autoclaims dataset

--- demo/data/README.md
+++ demo/data/README.md
@@ -1,5 +1,2 @@
 This folder contains processed example dataset used by the demos.
 Copyright of the dataset belongs to the original copyright holder
-
-Note: The autoclaims datasets for the gamma regression demo are adapted
-from https://cran.r-project.org/web/packages/insuranceData/.
--- demo/guide-python/gamma_regression.py
+++ demo/guide-python/gamma_regression.py
@@ -2,10 +2,13 @@
 import xgboost as xgb
 import numpy as np
 
-#  this script demonstrate how to fit gamma regression model (with log link function)
-#  in xgboost
-dtrain = xgb.DMatrix('../data/autoclaims.train')
-dtest = xgb.DMatrix('../data/autoclaims.test')
+#  this script demonstrates how to fit gamma regression model (with log link function)
+#  in xgboost, before running the demo you need to generate the autoclaims dataset
+#  by running gen_autoclaims.R located in xgboost/demo/data.
+
+data = np.genfromtxt('../data/autoclaims.csv', delimiter=',')
+dtrain = xgb.DMatrix(data[0:4741, 0:34], data[0:4741, 34])
+dtest = xgb.DMatrix(data[4741:6773, 0:34], data[4741:6773, 34])
 
 # for gamma regression, we need to set the objective to 'reg:gamma', it also suggests
 # to set the base_score to a value between 1 to 5 if the number of iteration is small
@@ -13,7 +16,7 @@ param = {'silent':1, 'objective':'reg:gamma', 'booster':'gbtree', 'base_score':3
 
 # the rest of settings are the same
 watchlist  = [(dtest,'eval'), (dtrain,'train')]
-num_round = 25
+num_round = 30
 
 # training and evaluation
 bst = xgb.train(param, dtrain, num_round, watchlist)
