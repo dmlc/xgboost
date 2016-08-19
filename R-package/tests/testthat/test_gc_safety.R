88985c9a3b635de0bac5993b5f7dab2178aedc43
Tong He <hetong007@gmail.com>
2016-08-19 16:40:51 -0700

    Update test_gc_safety.R

--- R-package/tests/testthat/test_gc_safety.R
+++ R-package/tests/testthat/test_gc_safety.R
@@ -11,4 +11,5 @@ test_that("train and prediction when gctorture is on", {
   bst <- xgboost(data = train$data, label = train$label, max.depth = 2,
   eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
   pred <- predict(bst, test$data)
+  gctorture(FALSE)
 })
