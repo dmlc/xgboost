b4cd3fe2189d0686b47f894ac80ef870a75db7bb
hetong007 <hetong007@gmail.com>
2016-08-19 15:53:24 -0700

    improve the test

--- R-package/tests/testthat/test_gc_safety.R
+++ R-package/tests/testthat/test_gc_safety.R
@@ -2,11 +2,13 @@ require(xgboost)
 
 context("Garbage Collection Safety Check")
 
-data(agaricus.train, package='xgboost')
-data(agaricus.test, package='xgboost')
-train <- agaricus.train
-test <- agaricus.test
-gctorture(TRUE)
-bst <- xgboost(data = train$data, label = train$label, max.depth = 2,
-eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
-pred <- predict(bst, test$data)
+test_that("train and prediction when gctorture is on", {
+  data(agaricus.train, package='xgboost')
+  data(agaricus.test, package='xgboost')
+  train <- agaricus.train
+  test <- agaricus.test
+  gctorture(TRUE)
+  bst <- xgboost(data = train$data, label = train$label, max.depth = 2,
+  eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
+  pred <- predict(bst, test$data)
+})
