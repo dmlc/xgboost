a7c6033e43ebeb84e37e38dd49af08a01306d40a
hetong007 <hetong007@gmail.com>
2016-08-19 15:22:39 -0700

    Add test for garbage collection safety

---
+++ R-package/tests/testthat/test_gc_safety.R
@@ -0,0 +1,12 @@
+require(xgboost)
+
+context("Garbage Collection Safety Check")
+
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+train <- agaricus.train
+test <- agaricus.test
+gctorture(TRUE)
+bst <- xgboost(data = train$data, label = train$label, max.depth = 2,
+eta = 1, nthread = 2, nround = 2, objective = "binary:logistic")
+pred <- predict(bst, test$data)
