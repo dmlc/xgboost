1007a2664108e4cc307f02c2cc2a671bfc605ee6
tomlaube <tom.laube@gmail.com>
2016-08-27 00:04:41 +0200

    Fixing the imports to work with MPI (#30)

--- src/engine_mpi.cc
+++ src/engine_mpi.cc
@@ -11,8 +11,8 @@
 #define NOMINMAX
 #include <mpi.h>
 #include <cstdio>
-#include "../include/rabit/engine.h"
-#include "../include/rabit/utils.h"
+#include "../include/rabit/internal/engine.h"
+#include "../include/rabit/internal/utils.h"
 
 namespace rabit {
 namespace engine {
