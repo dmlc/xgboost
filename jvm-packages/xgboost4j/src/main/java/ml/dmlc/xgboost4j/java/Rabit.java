3e28ae5c0f123a34c0d57f081cb9e35249cf7e72
Xin Yin <killkeeper@gmail.com>
2016-11-14 17:28:51 -0500

    Add comments to indicate that allReduce is for tests only. Removed all other variants.

--- jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/Rabit.java
+++ jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/Rabit.java
@@ -132,6 +132,15 @@ public class Rabit {
     return out[0];
   }
 
+  /**
+   * perform Allreduce on distributed float vectors using operator op.
+   * This implementation of allReduce does not support customized prepare function callback in the
+   * native code, as this function is meant for testing purposes only (to test the Rabit tracker.)
+   *
+   * @param elements local elements on distributed workers.
+   * @param op operator used for Allreduce.
+   * @return All-reduced float elements according to the given operator.
+     */
   public static float[] allReduce(float[] elements, OpType op) {
     DataType dataType = DataType.FLOAT;
     ByteBuffer buffer = ByteBuffer.allocateDirect(dataType.getSize() * elements.length)
@@ -148,56 +157,4 @@ public class Rabit {
 
     return results;
   }
-
-  public static double[] allReduce(double[] elements, OpType op) {
-    DataType dataType = DataType.DOUBLE;
-    ByteBuffer buffer = ByteBuffer.allocateDirect(dataType.getSize() * elements.length)
-            .order(ByteOrder.nativeOrder());
-
-    for (double el : elements) {
-      buffer.putDouble(el);
-    }
-    buffer.flip();
-
-    XGBoostJNI.RabitAllreduce(buffer, elements.length, dataType.getEnumOp(), op.getOperand());
-    double[] results = new double[elements.length];
-    buffer.asDoubleBuffer().get(results);
-
-    return results;
-  }
-
-  public static int[] allReduce(int[] elements, OpType op) {
-    DataType dataType = DataType.INT;
-    ByteBuffer buffer = ByteBuffer.allocateDirect(dataType.getSize() * elements.length)
-            .order(ByteOrder.nativeOrder());
-
-    for (int el : elements) {
-      buffer.putInt(el);
-    }
-    buffer.flip();
-
-    XGBoostJNI.RabitAllreduce(buffer, elements.length, dataType.getEnumOp(), op.getOperand());
-    int[] results = new int[elements.length];
-    buffer.asIntBuffer().get(results);
-
-    return results;
-  }
-
-  public static long[] allReduce(long[] elements, OpType op) {
-    DataType dataType = DataType.LONG;
-    ByteBuffer buffer = ByteBuffer.allocateDirect(dataType.getSize() * elements.length)
-            .order(ByteOrder.nativeOrder());
-
-    for (long el : elements) {
-      buffer.putLong(el);
-    }
-    buffer.flip();
-
-    XGBoostJNI.RabitAllreduce(buffer, elements.length, dataType.getEnumOp(), op.getOperand());
-    long[] results = new long[elements.length];
-    buffer.asLongBuffer().get(results);
-
-    return results;
-  }
-
 }
