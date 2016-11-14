b69e347cad4f722866be3be4f11d0dc62ec4b8c4
Vadim Khotilovich <khotilovich@gmail.com>
2016-11-13 22:39:30 -0600

    [R-package] added check for nrounds when is_update

--- R-package/R/xgb.train.R
+++ R-package/R/xgb.train.R
@@ -296,17 +296,20 @@ xgb.train <- function(params = list(), data, nrounds, watchlist = list(),
   num_parallel_tree <- max(as.numeric(NVL(params[['num_parallel_tree']], 1)), 1)
 
   # When the 'xgb_model' was set, find out how many boosting iterations it has
-  niter_skip <- 0
-  if (!is.null(xgb_model) && !is_update) {
-    niter_skip <- as.numeric(xgb.attr(bst, 'niter')) + 1
-    if (length(niter_skip) == 0) {
-      niter_skip <- xgb.ntree(bst) %/% (num_parallel_tree * num_class)
+  niter_init <- 0
+  if (!is.null(xgb_model)) {
+    niter_init <- as.numeric(xgb.attr(bst, 'niter')) + 1
+    if (length(niter_init) == 0) {
+      niter_init <- xgb.ntree(bst) %/% (num_parallel_tree * num_class)
     }
   }
+  if(is_update && nrounds > niter_init)
+    stop("nrounds cannot be larger than ", niter_init, " (nrounds of xgb_model)")
 
   # TODO: distributed code
   rank <- 0
   
+  niter_skip <- ifelse(is_update, 0, niter_init)
   begin_iteration <- niter_skip + 1
   end_iteration <- niter_skip + nrounds
   
