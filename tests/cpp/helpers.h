aae1dcc6c3552ffff77204486c26de481e3fad2e
AbdealiJK <abdealikothari@gmail.com>
2016-12-03 14:06:51 +0530

    tests/cpp: Add tests for regression_obj.cc
    Test the objective functions in regression_obj.cc
    
    tests/cpp: Add tests for objective.cc and RegLossObj

--- tests/cpp/helpers.h
+++ tests/cpp/helpers.h
@@ -5,10 +5,14 @@
 #include <fstream>
 #include <cstdio>
 #include <string>
+#include <vector>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #include <gtest/gtest.h>
+
+#include <xgboost/base.h>
+#include <xgboost/objective.h>
 
 std::string TempFileName();
 
@@ -17,5 +21,12 @@ bool FileExists(const std::string name);
 long GetFileSize(const std::string filename);
 
 std::string CreateSimpleTestData();
+
+void CheckObjFunction(xgboost::ObjFunction * obj,
+                      std::vector<xgboost::bst_float> preds,
+                      std::vector<xgboost::bst_float> labels,
+                      std::vector<xgboost::bst_float> weights,
+                      std::vector<xgboost::bst_float> out_grad,
+                      std::vector<xgboost::bst_float> out_hess);
 
 #endif
