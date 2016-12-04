900ff6e3a88fc9f2c0128e2205c11f2f469fe997
AbdealiJK <abdealikothari@gmail.com>
2016-12-04 12:42:18 +0530

    tests/cpp: Add tests for metric.cc

---
+++ tests/cpp/metric/test_metric.cc
@@ -0,0 +1,11 @@
+// Copyright by Contributors
+#include <xgboost/metric.h>
+
+#include "../helpers.h"
+
+TEST(Metric, UnknownMetric) {
+  EXPECT_ANY_THROW(xgboost::Metric::Create("unknown_name"));
+  EXPECT_NO_THROW(xgboost::Metric::Create("rmse"));
+  EXPECT_ANY_THROW(xgboost::Metric::Create("unknown_name@1"));
+  EXPECT_NO_THROW(xgboost::Metric::Create("error@0.5"));
+}
