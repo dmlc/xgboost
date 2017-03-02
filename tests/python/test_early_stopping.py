a5665541fc02e4d977cb8973f9122e9a93395ab7
Icyblade Dai <ycdai@jointwisdom.cn>
2017-03-02 10:04:41 +0800

    fix syntax

--- tests/python/test_early_stopping.py
+++ tests/python/test_early_stopping.py
@@ -12,7 +12,7 @@ class TestEarlyStopping(unittest.TestCase):
         tm._skip_if_no_sklearn()
         from sklearn.datasets import load_digits
         try:
-            from from sklearn.model_selection import train_test_split
+            from sklearn.model_selection import train_test_split
         except:
             from sklearn.cross_validation import train_test_split
 
