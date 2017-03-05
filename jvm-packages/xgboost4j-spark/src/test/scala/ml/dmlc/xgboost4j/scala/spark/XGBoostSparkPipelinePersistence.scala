582fada873a0943237b0d0108418d0ebd57cae88
Georg Heiler <georg.kf.heiler@gmail.com>
2017-03-05 13:38:34 +0100

    Remove whitespace

--- jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostSparkPipelinePersistence.scala
+++ jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostSparkPipelinePersistence.scala
@@ -25,7 +25,7 @@ import scala.concurrent.duration._
 
 class XGBoostSparkPipelinePersistence extends SharedSparkContext with Utils {
   case class Foobar(TARGET: Int, bar: Double, baz: Double)
-  
+
   test("test sparks pipeline persistence of dataframe-based model") {
     //  maybe move to shared context, but requires session to import implicits.
     // what about introducing https://github.com/holdenk/spark-testing-base ?
