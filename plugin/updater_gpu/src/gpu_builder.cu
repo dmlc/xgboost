b8298b7775950c77e0e191c5144b043ebffe6cdf
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2017-04-15 16:47:48 +1200

    [GPU-Plugin] * Add fast histogram algorithm * Fix Linux build * Add 'gpu_id' parameter

--- plugin/updater_gpu/src/gpu_builder.cu
+++ plugin/updater_gpu/src/gpu_builder.cu
@@ -1,6 +1,7 @@
 /*!
  * Copyright 2016 Rory mitchell
-*/
+ */
+#include "gpu_builder.cuh"
 #include <cub/cub.cuh>
 #include <cuda_profiler_api.h>
 #include <cuda_runtime.h>
@@ -11,31 +12,35 @@
 #include <thrust/host_vector.h>
 #include <thrust/sequence.h>
 #include <algorithm>
-#include <random>
 #include <numeric>
+#include <random>
 #include <vector>
 #include "../../../src/common/random.h"
+#include "common.cuh"
 #include "device_helpers.cuh"
 #include "find_split.cuh"
-#include "gpu_builder.cuh"
-#include "types_functions.cuh"
 #include "gpu_data.cuh"
+#include "types.cuh"
 
 namespace xgboost {
 namespace tree {
-
 GPUBuilder::GPUBuilder() { gpu_data = new GPUData(); }
 
-void GPUBuilder::Init(const TrainParam &param_in) {
+void GPUBuilder::Init(const TrainParam& param_in) {
   param = param_in;
   CHECK(param.max_depth < 16) << "Tree depth too large.";
+
+  dh::safe_cuda(cudaSetDevice(param.gpu_id));
+  if (!param.silent) {
+    LOG(CONSOLE) << "Device: [" << param.gpu_id << "] " << dh::device_name();
+  }
 }
 
 GPUBuilder::~GPUBuilder() { delete gpu_data; }
 
 void GPUBuilder::UpdateNodeId(int level) {
-  auto *d_node_id_instance = gpu_data->node_id_instance.data();
-  Node *d_nodes = gpu_data->nodes.data();
+  auto* d_node_id_instance = gpu_data->node_id_instance.data();
+  Node* d_nodes = gpu_data->nodes.data();
 
   dh::launch_n(gpu_data->node_id_instance.size(), [=] __device__(int i) {
     NodeIdT item_node_id = d_node_id_instance[i];
@@ -57,10 +62,10 @@ void GPUBuilder::UpdateNodeId(int level) {
 
   dh::safe_cuda(cudaDeviceSynchronize());
 
-  auto *d_fvalues = gpu_data->fvalues.data();
-  auto *d_instance_id = gpu_data->instance_id.data();
-  auto *d_node_id = gpu_data->node_id.data();
-  auto *d_feature_id = gpu_data->feature_id.data();
+  auto* d_fvalues = gpu_data->fvalues.data();
+  auto* d_instance_id = gpu_data->instance_id.data();
+  auto* d_node_id = gpu_data->node_id.data();
+  auto* d_feature_id = gpu_data->feature_id.data();
 
   // Update node based on fvalue where exists
   dh::launch_n(gpu_data->fvalues.size(), [=] __device__(int i) {
@@ -128,74 +133,51 @@ void GPUBuilder::Sort(int level) {
 }
 
 void GPUBuilder::ColsampleTree() {
-  unsigned n = static_cast<unsigned>(
-    param.colsample_bytree * gpu_data->n_features);
+  unsigned n =
+      static_cast<unsigned>(param.colsample_bytree * gpu_data->n_features);
   CHECK_GT(n, 0);
 
   feature_set_tree.resize(gpu_data->n_features);
   std::iota(feature_set_tree.begin(), feature_set_tree.end(), 0);
   std::shuffle(feature_set_tree.begin(), feature_set_tree.end(),
-    common::GlobalRandom());
+               common::GlobalRandom());
 }
 
-void GPUBuilder::Update(const std::vector<bst_gpair> &gpair, DMatrix *p_fmat,
-                        RegTree *p_tree) {
-  try {
-    dh::Timer update;
-    dh::Timer t;
-    this->InitData(gpair, *p_fmat, *p_tree);
-    t.printElapsed("init data");
-    this->InitFirstNode();
-    this->ColsampleTree();
+void GPUBuilder::Update(const std::vector<bst_gpair>& gpair, DMatrix* p_fmat,
+                        RegTree* p_tree) {
+  this->InitData(gpair, *p_fmat, *p_tree);
+  this->InitFirstNode();
+  this->ColsampleTree();
 
-    for (int level = 0; level < param.max_depth; level++) {
-      bool use_multiscan_algorithm = level < multiscan_levels;
+  for (int level = 0; level < param.max_depth; level++) {
+    bool use_multiscan_algorithm = level < multiscan_levels;
 
-      t.reset();
-      if (level > 0) {
-        dh::Timer update_node;
-        this->UpdateNodeId(level);
-        update_node.printElapsed("node");
-      }
-
-      if (level > 0 && !use_multiscan_algorithm) {
-        dh::Timer s;
-        this->Sort(level);
-        s.printElapsed("sort");
-      }
-
-      dh::Timer split;
-      find_split(gpu_data, param, level, use_multiscan_algorithm,
-        feature_set_tree, &feature_set_level);
-
-      split.printElapsed("split");
-
-      t.printElapsed("level");
+    if (level > 0) {
+      this->UpdateNodeId(level);
     }
-    this->CopyTree(*p_tree);
-    update.printElapsed("update");
-  } catch (thrust::system_error &e) {
-    std::cerr << "CUDA error: " << e.what() << std::endl;
-    exit(-1);
-  } catch (const std::exception &e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    exit(-1);
-  } catch (...) {
-    std::cerr << "Unknown exception." << std::endl;
-    exit(-1);
+
+    if (level > 0 && !use_multiscan_algorithm) {
+      this->Sort(level);
+    }
+
+    find_split(gpu_data, param, level, use_multiscan_algorithm,
+               feature_set_tree, &feature_set_level);
   }
+
+  dense2sparse_tree(p_tree, gpu_data->nodes.tbegin(), gpu_data->nodes.tend(),
+                    param);
 }
 
-void GPUBuilder::InitData(const std::vector<bst_gpair> &gpair, DMatrix &fmat,
-                          const RegTree &tree) {
-  CHECK(fmat.SingleColBlock()) << "GPUMaker: must have single column block";
+void GPUBuilder::InitData(const std::vector<bst_gpair>& gpair, DMatrix& fmat,
+                          const RegTree& tree) {
+  CHECK(fmat.SingleColBlock()) << "grow_gpu: must have single column block. "
+                                  "Try setting 'tree_method' parameter to "
+                                  "'exact'";
 
   if (gpu_data->IsAllocated()) {
     gpu_data->Reset(gpair, param.subsample);
     return;
   }
-
-  dh::Timer t;
 
   MetaInfo info = fmat.info();
 
@@ -208,31 +190,27 @@ void GPUBuilder::InitData(const std::vector<bst_gpair> &gpair, DMatrix &fmat,
   instance_id.reserve(info.num_col * info.num_row);
   feature_id.reserve(info.num_col * info.num_row);
 
-  dmlc::DataIter<ColBatch> *iter = fmat.ColIterator();
+  dmlc::DataIter<ColBatch>* iter = fmat.ColIterator();
 
   while (iter->Next()) {
-    const ColBatch &batch = iter->Value();
+    const ColBatch& batch = iter->Value();
 
     for (int i = 0; i < batch.size; i++) {
-      const ColBatch::Inst &col = batch[i];
+      const ColBatch::Inst& col = batch[i];
 
-      for (const ColBatch::Entry *it = col.data; it != col.data + col.length;
+      for (const ColBatch::Entry* it = col.data; it != col.data + col.length;
            it++) {
         bst_uint inst_id = it->index;
-          fvalues.push_back(it->fvalue);
-          instance_id.push_back(inst_id);
-          feature_id.push_back(i);
+        fvalues.push_back(it->fvalue);
+        instance_id.push_back(inst_id);
+        feature_id.push_back(i);
       }
       foffsets.push_back(fvalues.size());
     }
   }
 
-  t.printElapsed("dmatrix");
-  t.reset();
   gpu_data->Init(fvalues, foffsets, instance_id, feature_id, gpair,
                  info.num_row, info.num_col, param.max_depth, param);
-
-  t.printElapsed("gpu init");
 }
 
 void GPUBuilder::InitFirstNode() {
@@ -247,61 +225,6 @@ void GPUBuilder::InitFirstNode() {
       CalcWeight(gpu_data->param, sum_gradients.grad(), sum_gradients.hess()));
 
   thrust::copy_n(&tmp, 1, gpu_data->nodes.tbegin());
-}
-
-enum NodeType {
-  NODE = 0,
-  LEAF = 1,
-  UNUSED = 2,
-};
-
-// Recursively label node types
-void flag_nodes(const thrust::host_vector<Node> &nodes,
-                std::vector<NodeType> *node_flags, int nid, NodeType type) {
-  if (nid >= nodes.size() || type == UNUSED) {
-    return;
-  }
-
-  const Node &n = nodes[nid];
-
-  // Current node and all children are valid
-  if (n.split.loss_chg > rt_eps) {
-    (*node_flags)[nid] = NODE;
-    flag_nodes(nodes, node_flags, nid * 2 + 1, NODE);
-    flag_nodes(nodes, node_flags, nid * 2 + 2, NODE);
-  } else {
-    // Current node is leaf, therefore is valid but all children are invalid
-    (*node_flags)[nid] = LEAF;
-    flag_nodes(nodes, node_flags, nid * 2 + 1, UNUSED);
-    flag_nodes(nodes, node_flags, nid * 2 + 2, UNUSED);
-  }
-}
-
-// Copy gpu dense representation of tree to xgboost sparse representation
-void GPUBuilder::CopyTree(RegTree &tree) {
-  std::vector<Node> h_nodes = gpu_data->nodes.as_vector();
-  std::vector<NodeType> node_flags(h_nodes.size(), UNUSED);
-  flag_nodes(h_nodes, &node_flags, 0, NODE);
-
-  int nid = 0;
-  for (int gpu_nid = 0; gpu_nid < h_nodes.size(); gpu_nid++) {
-    NodeType flag = node_flags[gpu_nid];
-    const Node &n = h_nodes[gpu_nid];
-    if (flag == NODE) {
-      tree.AddChilds(nid);
-      tree[nid].set_split(n.split.findex, n.split.fvalue, n.split.missing_left);
-      tree.stat(nid).loss_chg = n.split.loss_chg;
-      tree.stat(nid).base_weight = n.weight;
-      tree.stat(nid).sum_hess = n.sum_gradients.hess();
-      tree[tree[nid].cleft()].set_leaf(0);
-      tree[tree[nid].cright()].set_leaf(0);
-      nid++;
-    } else if (flag == LEAF) {
-      tree[nid].set_leaf(n.weight * param.learning_rate);
-      tree.stat(nid).sum_hess = n.sum_gradients.hess();
-      nid++;
-    }
-  }
 }
 }  // namespace tree
 }  // namespace xgboost
---
+++ plugin/updater_gpu/test/test.py
@@ -0,0 +1,221 @@
+#pylint: skip-file
+import sys
+sys.path.append("../../tests/python")
+import xgboost as xgb
+import testing as tm
+import numpy as np
+import unittest
+
+rng = np.random.RandomState(1994)
+
+dpath = '../../demo/data/'
+ag_dtrain = xgb.DMatrix(dpath + 'agaricus.txt.train')
+ag_dtest = xgb.DMatrix(dpath + 'agaricus.txt.test')
+
+
+class TestGPU(unittest.TestCase):
+    def test_grow_gpu(self):
+        tm._skip_if_no_sklearn()
+        from sklearn.datasets import load_digits
+        try:
+            from sklearn.model_selection import train_test_split
+        except:
+            from sklearn.cross_validation import train_test_split
+
+        ag_param = {'max_depth': 2,
+                    'tree_method': 'exact',
+                    'nthread': 1,
+                    'eta': 1,
+                    'silent': 1,
+                    'objective': 'binary:logistic',
+                    'eval_metric': 'auc'}
+        ag_param2 = {'max_depth': 2,
+                     'updater': 'grow_gpu',
+                     'eta': 1,
+                     'silent': 1,
+                     'objective': 'binary:logistic',
+                     'eval_metric': 'auc'}
+        ag_res = {}
+        ag_res2 = {}
+
+        num_rounds = 10
+        xgb.train(ag_param, ag_dtrain, num_rounds, [(ag_dtrain, 'train'), (ag_dtest, 'test')],
+                  evals_result=ag_res)
+        xgb.train(ag_param2, ag_dtrain, num_rounds, [(ag_dtrain, 'train'), (ag_dtest, 'test')],
+                  evals_result=ag_res2)
+        assert ag_res['train']['auc'] == ag_res2['train']['auc']
+        assert ag_res['test']['auc'] == ag_res2['test']['auc']
+
+        digits = load_digits(2)
+        X = digits['data']
+        y = digits['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        dtrain = xgb.DMatrix(X_train, y_train)
+        dtest = xgb.DMatrix(X_test, y_test)
+
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu',
+                 'max_depth': 3,
+                 'eval_metric': 'auc'}
+        res = {}
+        xgb.train(param, dtrain, 10, [(dtrain, 'train'), (dtest, 'test')],
+                  evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert self.non_decreasing(res['test']['auc'])
+
+        # fail-safe test for dense data
+        from sklearn.datasets import load_svmlight_file
+        X2, y2 = load_svmlight_file(dpath + 'agaricus.txt.train')
+        X2 = X2.toarray()
+        dtrain2 = xgb.DMatrix(X2, label=y2)
+
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu',
+                 'max_depth': 2,
+                 'eval_metric': 'auc'}
+        res = {}
+        xgb.train(param, dtrain2, 10, [(dtrain2, 'train')], evals_result=res)
+
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        for j in range(X2.shape[1]):
+            for i in rng.choice(X2.shape[0], size=10, replace=False):
+                X2[i, j] = 2
+
+        dtrain3 = xgb.DMatrix(X2, label=y2)
+        res = {}
+
+        xgb.train(param, dtrain3, num_rounds, [(dtrain3, 'train')], evals_result=res)
+
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        for j in range(X2.shape[1]):
+            for i in np.random.choice(X2.shape[0], size=10, replace=False):
+                X2[i, j] = 3
+
+        dtrain4 = xgb.DMatrix(X2, label=y2)
+        res = {}
+        xgb.train(param, dtrain4, 10, [(dtrain4, 'train')], evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+
+    def test_grow_gpu_hist(self):
+        tm._skip_if_no_sklearn()
+        from sklearn.datasets import load_digits
+        try:
+            from sklearn.model_selection import train_test_split
+        except:
+            from sklearn.cross_validation import train_test_split
+
+        # regression test --- hist must be same as exact on all-categorial data
+        ag_param = {'max_depth': 2,
+                    'tree_method': 'exact',
+                    'nthread': 1,
+                    'eta': 1,
+                    'silent': 1,
+                    'objective': 'binary:logistic',
+                    'eval_metric': 'auc'}
+        ag_param2 = {'max_depth': 2,
+                     'updater': 'grow_gpu_hist',
+                     'eta': 1,
+                     'silent': 1,
+                     'objective': 'binary:logistic',
+                     'eval_metric': 'auc'}
+        ag_res = {}
+        ag_res2 = {}
+
+        num_rounds = 10
+        xgb.train(ag_param, ag_dtrain, num_rounds, [(ag_dtrain, 'train'), (ag_dtest, 'test')],
+                  evals_result=ag_res)
+        xgb.train(ag_param2, ag_dtrain, num_rounds, [(ag_dtrain, 'train'), (ag_dtest, 'test')],
+                  evals_result=ag_res2)
+        assert ag_res['train']['auc'] == ag_res2['train']['auc']
+        assert ag_res['test']['auc'] == ag_res2['test']['auc']
+
+        digits = load_digits(2)
+        X = digits['data']
+        y = digits['target']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        dtrain = xgb.DMatrix(X_train, y_train)
+        dtest = xgb.DMatrix(X_test, y_test)
+
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu_hist',
+                 'max_depth': 3,
+                 'eval_metric': 'auc'}
+        res = {}
+        xgb.train(param, dtrain, 10, [(dtrain, 'train'), (dtest, 'test')],
+                  evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert self.non_decreasing(res['test']['auc'])
+
+        # fail-safe test for dense data
+        from sklearn.datasets import load_svmlight_file
+        X2, y2 = load_svmlight_file(dpath + 'agaricus.txt.train')
+        X2 = X2.toarray()
+        dtrain2 = xgb.DMatrix(X2, label=y2)
+
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu_hist',
+                 'grow_policy': 'depthwise',
+                 'max_depth': 2,
+                 'eval_metric': 'auc'}
+        res = {}
+        xgb.train(param, dtrain2, 10, [(dtrain2, 'train')], evals_result=res)
+
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        for j in range(X2.shape[1]):
+            for i in rng.choice(X2.shape[0], size=10, replace=False):
+                X2[i, j] = 2
+
+        dtrain3 = xgb.DMatrix(X2, label=y2)
+        res = {}
+
+        xgb.train(param, dtrain3, num_rounds, [(dtrain3, 'train')], evals_result=res)
+
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        for j in range(X2.shape[1]):
+            for i in np.random.choice(X2.shape[0], size=10, replace=False):
+                X2[i, j] = 3
+
+        dtrain4 = xgb.DMatrix(X2, label=y2)
+        res = {}
+        xgb.train(param, dtrain4, 10, [(dtrain4, 'train')], evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        # fail-safe test for max_bin=2
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu_hist',
+                 'max_depth': 2,
+                 'eval_metric': 'auc',
+                 'max_bin': 2}
+        res = {}
+        xgb.train(param, dtrain2, 10, [(dtrain2, 'train')], evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+        # subsampling
+        param = {'objective': 'binary:logistic',
+                 'updater': 'grow_gpu_hist',
+                 'max_depth': 3,
+                 'eval_metric': 'auc',
+                 'colsample_bytree': 0.5,
+                 'colsample_bylevel': 0.5,
+                 'subsample': 0.5
+                 }
+        res = {}
+        xgb.train(param, dtrain2, 10, [(dtrain2, 'train')], evals_result=res)
+        assert self.non_decreasing(res['train']['auc'])
+        assert res['train']['auc'][0] >= 0.85
+
+
+    def non_decreasing(self, L):
+            return all((x - y) < 0.001 for x, y in zip(L, L[1:]))
