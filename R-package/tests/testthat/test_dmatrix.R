bbea69df6cced29af369dd51e90a81427fe9f1cd
khotilov <khotilovich@gmail.com>
2017-04-29 15:56:39 -0500

    [R] allow xgb.DMatrix construction from integer dense matrices

--- R-package/tests/testthat/test_dmatrix.R
+++ R-package/tests/testthat/test_dmatrix.R
@@ -7,14 +7,25 @@ data(agaricus.test, package='xgboost')
 test_data <- agaricus.test$data[1:100,]
 test_label <- agaricus.test$label[1:100]
 
-test_that("xgb.DMatrix: basic construction, saving, loading", {
+test_that("xgb.DMatrix: basic construction", {
   # from sparse matrix
   dtest1 <- xgb.DMatrix(test_data, label=test_label)
+  
   # from dense matrix 
   dtest2 <- xgb.DMatrix(as.matrix(test_data), label=test_label)
   expect_equal(getinfo(dtest1, 'label'), getinfo(dtest2, 'label'))
+  expect_equal(dim(dtest1), dim(dtest2))
   
+  #from dense integer matrix
+  int_data <- as.matrix(test_data)
+  storage.mode(int_data) <- "integer"
+  dtest3 <- xgb.DMatrix(int_data, label=test_label)
+  expect_equal(dim(dtest1), dim(dtest3))
+})
+
+test_that("xgb.DMatrix: saving, loading", {
   # save to a local file
+  dtest1 <- xgb.DMatrix(test_data, label=test_label)
   tmp_file <- tempfile('xgb.DMatrix_')
   expect_true(xgb.DMatrix.save(dtest1, tmp_file))
   # read from a local file
