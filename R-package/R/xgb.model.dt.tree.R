052c9f0c82d9aab314ea65816037c4a6314c0bf1
khotilov <khotilovich@gmail.com>
2017-04-29 19:03:07 -0500

    [R] xgb.model.dt.tree code style changes

--- R-package/R/xgb.model.dt.tree.R
+++ R-package/R/xgb.model.dt.tree.R
@@ -97,7 +97,7 @@ xgb.model.dt.tree <- function(feature_names = NULL, model = NULL, text = NULL,
   
   anynumber_regex <- "[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?"
   
-  td <- data.table(t=text)
+  td <- data.table(t = text)
   td[position, Tree := 1L]
   td[, Tree := cumsum(ifelse(is.na(Tree), 0L, Tree)) - 1L]
   
@@ -113,33 +113,35 @@ xgb.model.dt.tree <- function(feature_names = NULL, model = NULL, text = NULL,
   td[, isLeaf := !is.na(stri_match_first_regex(t, "leaf"))]
 
   # parse branch lines
-  rx <- paste0("f(\\d+)<(", anynumber_regex, ")\\] yes=(\\d+),no=(\\d+),missing=(\\d+),",
-               "gain=(", anynumber_regex, "),cover=(", anynumber_regex, ")")
-  td[isLeaf==FALSE, 
-     c("Feature", "Split", "Yes", "No", "Missing", "Quality", "Cover") := {
+  branch_rx <- paste0("f(\\d+)<(", anynumber_regex, ")\\] yes=(\\d+),no=(\\d+),missing=(\\d+),",
+                      "gain=(", anynumber_regex, "),cover=(", anynumber_regex, ")")
+  branch_cols <- c("Feature", "Split", "Yes", "No", "Missing", "Quality", "Cover")
+  td[isLeaf == FALSE, 
+     (branch_cols) := {
       # skip some indices with spurious capture groups from anynumber_regex
-      xtr <- stri_match_first_regex(t, rx)[, c(2,3,5,6,7,8,10), drop=FALSE]
+      xtr <- stri_match_first_regex(t, branch_rx)[, c(2,3,5,6,7,8,10), drop = FALSE]
       xtr[, 3:5] <- add.tree.id(xtr[, 3:5], Tree)
       lapply(1:ncol(xtr), function(i) xtr[,i])
     }]
   # assign feature_names when available
-  td[isLeaf==FALSE & !is.null(feature_names), 
+  td[isLeaf == FALSE & !is.null(feature_names), 
      Feature := feature_names[as.numeric(Feature) + 1] ]
   
   # parse leaf lines
-  td[isLeaf==TRUE,
-     c("Feature", "Quality", "Cover") := {
-      rx <- paste0("leaf=(", anynumber_regex, "),cover=(", anynumber_regex, ")")
-      xtr <- stri_match_first_regex(t, rx)[, c(2,4)]
+  leaf_rx <- paste0("leaf=(", anynumber_regex, "),cover=(", anynumber_regex, ")")
+  leaf_cols <- c("Feature", "Quality", "Cover")
+  td[isLeaf == TRUE,
+     (leaf_cols) := {
+      xtr <- stri_match_first_regex(t, leaf_rx)[, c(2,4)]
       c("Leaf", lapply(1:ncol(xtr), function(i) xtr[,i]))
     }]
   
   # convert some columns to numeric
   numeric_cols <- c("Split", "Quality", "Cover")
-  td[, (numeric_cols) := lapply(.SD, as.numeric), .SDcols=numeric_cols]
+  td[, (numeric_cols) := lapply(.SD, as.numeric), .SDcols = numeric_cols]
   if (use_int_id) {
     int_cols <- c("Yes", "No", "Missing")
-    td[, (int_cols) := lapply(.SD, as.integer), .SDcols=int_cols]
+    td[, (int_cols) := lapply(.SD, as.integer), .SDcols = int_cols]
   }
   
   td[, t := NULL]
