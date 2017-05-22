e25a8d6281d91312e4ddf19f7239c5ee28bb9eea
gaw89 <gideonwhitehead@gmail.com>
2017-05-22 07:07:19 -0400

    Set n_jobs to 1.

--- python-package/xgboost/sklearn.py
+++ python-package/xgboost/sklearn.py
@@ -121,7 +121,7 @@ class XGBModel(XGBModelBase):
 
     def __init__(self, max_depth=3, learning_rate=0.1, n_estimators=100,
                  silent=True, objective="reg:linear", booster='gbtree',
-                 n_jobs=-1, nthread=None, gamma=0, min_child_weight=1, max_delta_step=0,
+                 n_jobs=1, nthread=None, gamma=0, min_child_weight=1, max_delta_step=0,
                  subsample=1, colsample_bytree=1, colsample_bylevel=1,
                  reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
                  base_score=0.5, random_state=0, seed=None, missing=None):
@@ -385,7 +385,7 @@ class XGBClassifier(XGBModel, XGBClassifierBase):
     def __init__(self, max_depth=3, learning_rate=0.1,
                  n_estimators=100, silent=True,
                  objective="binary:logistic", booster='gbtree',
-                 n_jobs=-1, nthread=None, gamma=0, min_child_weight=1,
+                 n_jobs=1, nthread=None, gamma=0, min_child_weight=1,
                  max_delta_step=0, subsample=1, colsample_bytree=1, colsample_bylevel=1,
                  reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
                  base_score=0.5, random_state=0, seed=None, missing=None):
