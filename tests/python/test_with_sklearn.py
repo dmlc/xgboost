f874d618639fd7ccab31516c92781bab037698f3
Whitehead <WhiteheG@uphs.pennhealth.prv>
2017-05-22 06:18:14 -0400

    Attempted fix of deprecation tests.

--- tests/python/test_with_sklearn.py
+++ tests/python/test_with_sklearn.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import xgboost as xgb
 import testing as tm
+import warnings
 
 rng = np.random.RandomState(1994)
 
@@ -330,7 +331,7 @@ def test_split_value_histograms():
 
 def test_sklearn_random_state():
     tm._skip_if_no_sklearn()
-    
+
     clf = xgb.XGBClassifier(random_state=402)
     assert clf.get_params()['seed'] == 402
 
@@ -340,7 +341,7 @@ def test_sklearn_random_state():
 
 def test_seed_deprecation():
     tm._skip_if_no_sklearn()
-    import warnings
+
     with warnings.catch_warnings(record=True) as w:
         xgb.XGBClassifier(seed=1)
         assert w[0].category == DeprecationWarning
@@ -348,17 +349,17 @@ def test_seed_deprecation():
 
 def test_sklearn_n_jobs():
     tm._skip_if_no_sklearn()
-    
+
     clf = xgb.XGBClassifier(n_jobs=1)
     assert clf.get_params()['nthread'] == 1
-    
+
     clf = xgb.XGBClassifier(nthread=2)
     assert clf.get_params()['nthread'] == 2
 
 
 def test_nthread_deprecation():
     tm._skip_if_no_sklearn()
-    import warnings
+
     with warnings.catch_warnings(record=True) as w:
         xgb.XGBClassifier(nthread=1)
         assert w[0].category == DeprecationWarning
