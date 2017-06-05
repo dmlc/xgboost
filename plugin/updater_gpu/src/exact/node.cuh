21e6e58a4d74223c94bc1a278c4461bda66bfc8f
Thejaswi Rao <snanditale@nvidia.com>
2017-06-05 10:36:02 +0530

    updated copyright statement as per Rory's request

--- plugin/updater_gpu/src/exact/node.cuh
+++ plugin/updater_gpu/src/exact/node.cuh
@@ -1,9 +1,5 @@
-/*!
- * Copyright 2016 Rory Mitchell
- */
-
 /*
- * Copyright (c) 2017, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2017, NVIDIA CORPORATION, Xgboost contributors.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
