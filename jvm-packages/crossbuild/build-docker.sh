388be35c0f346d0192da6e63402469239006a083
CodingCat <zhunansjtu@gmail.com>
2017-06-17 23:50:48 -0700

    build with docker

--- jvm-packages/crossbuild/build-docker.sh
+++ jvm-packages/crossbuild/build-docker.sh
@@ -2,10 +2,4 @@
 
 set -x
 
-DOCKER_LINUX_X64_CONTAINER=`docker ps -aqf name=xgboost_linux_x64-be`; 
-
-if [ -z "$DOCKER_LINUX_X64_CONTAINER" ]; then \
-		docker container create --attach stdin --attach stdout --attach stderr --volume `pwd`:/xgboost --name xgboost_linux_x64 codingcat/xgboost:latest /xgboost/jvm-packages/crossbuild/build.sh; 
-fi
-
-docker start -a xgboost_linux_x64
+sudo docker run --rm -m 4g -e JAVA_OPTS='-Xmx6g' --attach stdin --attach stdout --attach stderr --volume `pwd`:/xgboost codingcat/xgboost:base /xgboost/jvm-packages/crossbuild/build.sh
