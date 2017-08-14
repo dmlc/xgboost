2f3eeb73421eed60fcd5205d64ea4c68ee07f7a1
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2017-08-14 16:32:05 +1200

    Resolve linker error for predictor

--- src/predictor/predictor.cc
+++ src/predictor/predictor.cc
@@ -61,7 +61,9 @@ Predictor* Predictor::Create(std::string name) {
 namespace xgboost {
 namespace predictor {
 // List of files that will be force linked in static links.
+#ifdef XGBOOST_USE_CUDA
 DMLC_REGISTRY_LINK_TAG(gpu_predictor);
+#endif
 DMLC_REGISTRY_LINK_TAG(cpu_predictor);
 }  // namespace predictor
 }  // namespace xgboost
