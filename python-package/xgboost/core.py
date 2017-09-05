c57937eff8a6c1ceda98979e195629cdd66a8663
Icyblade Dai <icyblade@users.noreply.github.com>
2017-09-05 13:57:06 +0800

    fix Index/MultiIndex

--- python-package/xgboost/core.py
+++ python-package/xgboost/core.py
@@ -185,9 +185,9 @@ Did not expect the data types in fields """
 
     if feature_names is None:
         from functools import reduce
-        try:
+        if hasattr(data.columns, 'to_frame'):  # MultiIndex
             feature_names = [reduce(lambda x, y: '{x} {y}'.format(x=x, y=y), i) for i in data.columns]
-        except TypeError:  # single Index, rather than MultiIndex
+        else:
             feature_names = data.columns.format()
 
     if feature_types is None:
