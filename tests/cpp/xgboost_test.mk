c14a3eddff924d24e653a45ede023a2154dfef4c
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2017-09-07 11:15:12 +1200

    Move GPU tests

--- tests/cpp/xgboost_test.mk
+++ tests/cpp/xgboost_test.mk
@@ -5,17 +5,6 @@ UNITTEST=$(UTEST_ROOT)/xgboost_test
 UNITTEST_SRC=$(wildcard $(UTEST_ROOT)/*.cc $(UTEST_ROOT)/*/*.cc)
 UNITTEST_OBJ=$(patsubst $(UTEST_ROOT)%.cc, $(UTEST_OBJ_ROOT)%.o, $(UNITTEST_SRC))
 
-# for if and when we add cuda source files into xgboost core
-UNITTEST_CU_SRC=$(wildcard $(UTEST_ROOT)/*.cu $(UTEST_ROOT)/*/*.cu)
-UNITTEST_OBJ += $(patsubst $(UTEST_ROOT)%.cu, $(UTEST_OBJ_ROOT)%.o, $(UNITTEST_CU_SRC))
-
-# tests from grow_gpu plugin (only if CUDA path is enabled!)
-ifeq ($(PLUGIN_UPDATER_GPU),ON)
-  GPU_PLUGIN_FOLDER = plugin/updater_gpu
-  UNITTEST_CU_PLUGIN_SRC = $(wildcard $(GPU_PLUGIN_FOLDER)/test/cpp/*.cu)
-  UNITTEST_OBJ += $(patsubst %.cu, $(UTEST_OBJ_ROOT)/%.o, $(UNITTEST_CU_PLUGIN_SRC))
-endif
-
 GTEST_LIB=$(GTEST_PATH)/lib/
 GTEST_INC=$(GTEST_PATH)/include/
 
@@ -26,14 +15,6 @@ UNITTEST_DEPS=lib/libxgboost.a $(DMLC_CORE)/libdmlc.a $(RABIT)/lib/$(LIB_RABIT)
 COVER_OBJ=$(patsubst %.o, %.gcda, $(ALL_OBJ)) $(patsubst %.o, %.gcda, $(UNITTEST_OBJ))
 
 # the order of the below targets matter!
-$(UTEST_OBJ_ROOT)/$(GPU_PLUGIN_FOLDER)/test/cpp/%.o: $(GPU_PLUGIN_FOLDER)/test/cpp/%.cu
-	@mkdir -p $(@D)
-	$(NVCC) $(NVCC_FLAGS) -I$(GTEST_INC) -o $@ -c $<
-
-$(UTEST_OBJ_ROOT)/%.o: $(UTEST_ROOT)/%.cu
-	@mkdir -p $(@D)
-	$(NVCC) $(NVCC_FLAGS) -I$(GTEST_INC) -o $@ -c $<
-
 $(UTEST_OBJ_ROOT)/$(GTEST_PATH)/%.o: $(GTEST_PATH)/%.cc
 	@mkdir -p $(@D)
 	$(CXX) $(UNITTEST_CFLAGS) -I$(GTEST_INC) -I$(GTEST_PATH) -o $@ -c $<
