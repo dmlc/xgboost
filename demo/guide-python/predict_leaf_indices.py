f02868b429c27a73a7f06d5d61564508ae6cc923
LevineHuang <levinehuang@163.com>
2017-10-19 12:02:20 +0800

    Minor changes to code style
    Some minor changes to code style in file 'predict_leaf_indices.py'.

--- demo/guide-python/predict_leaf_indices.py
+++ demo/guide-python/predict_leaf_indices.py
@@ -4,16 +4,16 @@ import xgboost as xgb
 ### load data in do training
 dtrain = xgb.DMatrix('../data/agaricus.txt.train')
 dtest = xgb.DMatrix('../data/agaricus.txt.test')
-param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
-watchlist  = [(dtest,'eval'), (dtrain,'train')]
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
+watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 num_round = 3
 bst = xgb.train(param, dtrain, num_round, watchlist)
 
-print ('start testing predict the leaf indices')
+print('start testing predict the leaf indices')
 ### predict using first 2 tree
 leafindex = bst.predict(dtest, ntree_limit=2, pred_leaf=True)
 print(leafindex.shape)
 print(leafindex)
 ### predict all trees
-leafindex = bst.predict(dtest, pred_leaf = True)
+leafindex = bst.predict(dtest, pred_leaf=True)
 print(leafindex.shape)
