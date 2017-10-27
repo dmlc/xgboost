91af8f710667884d8c841a916f1073b4a7891eaa
LevineHuang <levinehuang@163.com>
2017-10-27 11:12:54 +0800

    Minor edits to coding style (#2835)
    * Some minor changes to the code style
    
    Some minor changes to the code style in file basic_walkthrough.py
    
    * coding style changes
    
    * coding style changes arrcording PEP8
    
    * Update basic_walkthrough.py

--- demo/guide-python/basic_walkthrough.py
+++ demo/guide-python/basic_walkthrough.py
@@ -10,22 +10,22 @@ dtrain = xgb.DMatrix('../data/agaricus.txt.train')
 dtest = xgb.DMatrix('../data/agaricus.txt.test')
 
 # specify parameters via map, definition are same as c++ version
-param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
 
 # specify validations set to watch performance
-watchlist  = [(dtest,'eval'), (dtrain,'train')]
+watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 num_round = 2
 bst = xgb.train(param, dtrain, num_round, watchlist)
 
 # this is prediction
 preds = bst.predict(dtest)
 labels = dtest.get_label()
-print ('error=%f' % ( sum(1 for i in range(len(preds)) if int(preds[i]>0.5)!=labels[i]) /float(len(preds))))
+print('error=%f' % (sum(1 for i in range(len(preds)) if int(preds[i] > 0.5) != labels[i]) / float(len(preds))))
 bst.save_model('0001.model')
 # dump model
 bst.dump_model('dump.raw.txt')
 # dump model with feature map
-bst.dump_model('dump.nice.txt','../data/featmap.txt')
+bst.dump_model('dump.nice.txt', '../data/featmap.txt')
 
 # save dmatrix into binary buffer
 dtest.save_binary('dtest.buffer')
@@ -36,7 +36,7 @@ bst2 = xgb.Booster(model_file='xgb.model')
 dtest2 = xgb.DMatrix('dtest.buffer')
 preds2 = bst2.predict(dtest2)
 # assert they are the same
-assert np.sum(np.abs(preds2-preds)) == 0
+assert np.sum(np.abs(preds2 - preds)) == 0
 
 # alternatively, you can pickle the booster
 pks = pickle.dumps(bst2)
@@ -44,11 +44,11 @@ pks = pickle.dumps(bst2)
 bst3 = pickle.loads(pks)
 preds3 = bst3.predict(dtest2)
 # assert they are the same
-assert np.sum(np.abs(preds3-preds)) == 0
+assert np.sum(np.abs(preds3 - preds)) == 0
 
 ###
 # build dmatrix from scipy.sparse
-print ('start running example of build DMatrix from scipy.sparse CSR Matrix')
+print('start running example of build DMatrix from scipy.sparse CSR Matrix')
 labels = []
 row = []; col = []; dat = []
 i = 0
@@ -59,24 +59,22 @@ for l in open('../data/agaricus.txt.train'):
         k,v = it.split(':')
         row.append(i); col.append(int(k)); dat.append(float(v))
     i += 1
-csr = scipy.sparse.csr_matrix((dat, (row,col)))
-dtrain = xgb.DMatrix(csr, label = labels)
-watchlist  = [(dtest,'eval'), (dtrain,'train')]
+csr = scipy.sparse.csr_matrix((dat, (row, col)))
+dtrain = xgb.DMatrix(csr, label=labels)
+watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 bst = xgb.train(param, dtrain, num_round, watchlist)
 
-print ('start running example of build DMatrix from scipy.sparse CSC Matrix')
+print('start running example of build DMatrix from scipy.sparse CSC Matrix')
 # we can also construct from csc matrix
-csc = scipy.sparse.csc_matrix((dat, (row,col)))
+csc = scipy.sparse.csc_matrix((dat, (row, col)))
 dtrain = xgb.DMatrix(csc, label=labels)
-watchlist  = [(dtest,'eval'), (dtrain,'train')]
+watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 bst = xgb.train(param, dtrain, num_round, watchlist)
 
-print ('start running example of build DMatrix from numpy array')
+print('start running example of build DMatrix from numpy array')
 # NOTE: npymat is numpy array, we will convert it into scipy.sparse.csr_matrix in internal implementation
 # then convert to DMatrix
 npymat = csr.todense()
-dtrain = xgb.DMatrix(npymat, label = labels)
-watchlist  = [(dtest,'eval'), (dtrain,'train')]
+dtrain = xgb.DMatrix(npymat, label=labels)
+watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 bst = xgb.train(param, dtrain, num_round, watchlist)
-
-
