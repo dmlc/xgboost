b0f0c9d95e5843037587e41c8d0e31e0cb68f331
Sergei Lebedev <s.lebedev@criteo.com>
2017-10-31 19:02:25 +0100

    Assembly plugin works!

--- jvm-packages/build.sbt
+++ jvm-packages/build.sbt
@@ -21,19 +21,24 @@ lazy val spark = (project in file("xgboost4j-spark")).
       libraryDependencies ++= Dependencies.sparkDependencies,
 
       // Not supported by [[PerTest]] trait.
-      parallelExecution in Test := false
+      parallelExecution in Test := false,
+
+      assemblyMergeStrategy in assembly := (_ => MergeStrategy.first)
     )
 
 lazy val flink = (project in file("xgboost4j-flink")).
     dependsOn(core).
     settings(Common.settings: _*).
-    settings(libraryDependencies ++= Dependencies.flinkDependencies)
+    settings(libraryDependencies ++= Dependencies.flinkDependencies).
 
 lazy val example = (project in file("xgboost4j-examples")).
+    disablePlugins(sbtassembly.AssemblyPlugin).
     dependsOn(core, spark, flink).
     settings(Common.settings: _*).
     settings(libraryDependencies ++= Dependencies.exampleDependencies)
 
 lazy val root = (project in file(".")).
+    disablePlugins(sbtassembly.AssemblyPlugin).
     aggregate(core, spark, flink, example).
+    settings(name := "xgboost").
     settings(Common.settings: _*)
--- jvm-packages/project/Dependencies.scala
+++ jvm-packages/project/Dependencies.scala
@@ -6,30 +6,38 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % "3.0.0" % "test"
   )
 
-  val akkaVersion = "2.3.11"
+  val flakkaVersion = "2.3-custom"
 
   val coreDependencies: Seq[ModuleID] = commonDependencies ++ Seq(
-    "com.esotericsoftware.kryo" % "kryo" % "2.21",
-    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-    "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+    // TODO: remove Kryo dependency.
+    "com.esotericsoftware" % "kryo-shaded" % "3.0.3",
+    "com.data-artisans" %% "flakka-actor" % flakkaVersion,
+    "com.data-artisans" %% "flakka-testkit" % flakkaVersion % "test",
     "junit" % "junit" % "4.11" % "test"
   )
 
   val sparkVersion = "2.1.0"
 
   val sparkDependencies: Seq[ModuleID] = commonDependencies ++ Seq(
-    "org.apache.spark" %% "spark-core" % sparkVersion,
+    ("org.apache.spark" %% "spark-core" % sparkVersion).
+        exclude("commons-beanutils", "commons-beanutils-core").
+        exclude("com.esotericsoftware.minlog", "minlog").
+        exclude("commons-collections", "commons-collections").
+        exclude("commons-logging", "commons-logging").
+        exclude("org.glassfish.hk2.external", "*").
+        exclude("org.apache.hadoop", "hadoop-yarn-api").
+        exclude("org.slf4j", "jcl-over-slf4j"),
     "org.apache.spark" %% "spark-sql" % sparkVersion,
-    "org.apache.spark" %% "spark-mllib" % sparkVersion,
-    "org.apache.spark" %% "spark-streaming" % sparkVersion)
+    "org.apache.spark" %% "spark-mllib" % sparkVersion)
 
-  val flinkVersion = "1.3.1"
+  val flinkVersion = "1.3.2"
 
   val flinkDependencies: Seq[ModuleID] = commonDependencies ++ Seq(
     "org.apache.flink" %% "flink-scala" % flinkVersion,
-    "org.apache.flink" %% "flink-clients" % flinkVersion,
     "org.apache.flink" %% "flink-ml" % flinkVersion
-  )
+  ).map(_.
+      exclude("com.esotericsoftware.kryo", "kryo").
+      exclude("net.java.dev.jets3t", "jets3t"))
 
   val exampleDependencies: Seq[ModuleID] =
     coreDependencies ++ sparkDependencies ++ flinkDependencies
