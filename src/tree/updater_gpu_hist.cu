bc25ee6762f1839e85812755d64df4de7b043872
pseudotensor <pseudotensor@gmail.com>
2017-11-07 20:48:32 -0800

    add atomicAdd for compute capability <=6.0

--- src/tree/updater_gpu_hist.cu
+++ src/tree/updater_gpu_hist.cu
@@ -57,6 +57,26 @@ struct DeviceGMat {
   }
 };
 
+#if __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double* address, double val)
+{
+        unsigned long long int* address_as_ull =
+          (unsigned long long int*)address;
+        unsigned long long int old = *address_as_ull, assumed;
+
+        do {
+          assumed = old;
+          old = atomicCAS(address_as_ull, assumed,
+                          __double_as_longlong(val +
+                                               __longlong_as_double(assumed)));
+
+          // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+        } while (assumed != old);
+
+        return __longlong_as_double(old);
+}
+#endif
+
 struct HistHelper {
   gpair_sum_t* d_hist;
   int n_bins;
