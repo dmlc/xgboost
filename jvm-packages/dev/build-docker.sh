f86cf608646194619c71e1d8106af849acac2a66
Nan Zhu <nanzhu@uber.com>
2018-04-26 20:52:49 -0700

    fix path

--- jvm-packages/dev/build-docker.sh
+++ jvm-packages/dev/build-docker.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-sudo docker run --rm -m 4g -e JAVA_OPTS='-Xmx6g' --attach stdin --attach stdout --attach stderr --volume `pwd`/../:/xgboost codingcat/xgboost:latest /xgboost/jvm-packages/crossbuild/build.sh
+sudo docker run --rm -m 4g -e JAVA_OPTS='-Xmx6g' --attach stdin --attach stdout --attach stderr --volume `pwd`/../:/xgboost codingcat/xgboost:latest /xgboost/jvm-packages/dev/build.sh
