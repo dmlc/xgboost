d2363f03ca5b1e22b6f5901c552e0a5be40f0b65
Philip Cho <chohyu01@cs.washington.edu>
2018-05-09 12:00:32 -0700

    Add more source files in dmlc-minimum to get latest dmlc-core working

--- amalgamation/dmlc-minimum0.cc
+++ amalgamation/dmlc-minimum0.cc
@@ -7,6 +7,8 @@
 #include "../dmlc-core/src/io/recordio_split.cc"
 #include "../dmlc-core/src/io/input_split_base.cc"
 #include "../dmlc-core/src/io/local_filesys.cc"
+#include "../dmlc-core/src/io/filesys.cc"
+#include "../dmlc-core/src/io/indexed_recordio_split.cc"
 #include "../dmlc-core/src/data.cc"
 #include "../dmlc-core/src/io.cc"
 #include "../dmlc-core/src/recordio.cc"
