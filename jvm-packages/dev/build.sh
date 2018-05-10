49b9f39818d9002c3fffcd00da0bec6d573f4f17
Nan Zhu <CodingCat@users.noreply.github.com>
2018-05-10 06:39:44 -0700

    [jvm-packages] update xgboost4j cross build script to be compatible with older glibc (#3307)
    * add back train method but mark as deprecated
    
    * add back train method but mark as deprecated
    
    * fix scalastyle error
    
    * fix scalastyle error
    
    * static glibc glibc++
    
    * update to build with glib 2.12
    
    * remove unsupported flags
    
    * update version number
    
    * remove properties
    
    * remove unnecessary command
    
    * update poms

--- jvm-packages/dev/build.sh
+++ jvm-packages/dev/build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+set -x
 
-apt-get update
-apt-get -y install g++-4.8
-apt-get -y install git openjdk-8-jdk maven python cmake
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export CXX=g++
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0
 export MAVEN_OPTS="-Xmx3000m"
+export CMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
+export CC=/opt/rh/devtoolset-2/root/usr/bin/gcc
+
+export PATH=$CXX:$CC:/opt/rh/python27/root/usr/bin/python:$PATH
+
+scl enable devtoolset-2 bash
+scl enable python27 bash
+
+rm /usr/bin/python
+ln -s /opt/rh/python27/root/usr/bin/python /usr/bin/python
 
 # build xgboost
 cd /xgboost/jvm-packages;mvn package
