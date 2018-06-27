e2743548ed136207a67c70a7cd82e1cfe07a9b3d
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2018-06-27 22:12:56 +0000

    Fix wget for google tests in tests (#3414)
    CI tests were failing because wget prompts "the user" for a response
    whenever the google test archive is already on the disk.
    
    Fix: Use `-nc` option to skip download when the archive already
    exists

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -116,7 +116,7 @@ fi
 if [ ${TASK} == "cmake_test" ]; then
     set -e
     # Build gtest via cmake
-    wget https://github.com/google/googletest/archive/release-1.7.0.zip
+    wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
     unzip release-1.7.0.zip
     mv googletest-release-1.7.0 gtest && cd gtest
     cmake . && make
