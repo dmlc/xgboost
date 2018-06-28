cafc621914f6c7a05aa3842f32793b5615eb7f8f
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2018-06-28 04:10:39 +0000

    Do not unzip google test archive if exists (#3416)

--- tests/ci_build/build_via_cmake.sh
+++ tests/ci_build/build_via_cmake.sh
@@ -4,7 +4,7 @@ set -e
 # Build gtest via cmake
 rm -rf gtest
 wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
-unzip release-1.7.0.zip
+unzip -n release-1.7.0.zip
 mv googletest-release-1.7.0 gtest && cd gtest
 cmake . && make 
 mkdir lib && mv libgtest.a lib
--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -117,7 +117,7 @@ if [ ${TASK} == "cmake_test" ]; then
     set -e
     # Build gtest via cmake
     wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
-    unzip release-1.7.0.zip
+    unzip -n release-1.7.0.zip
     mv googletest-release-1.7.0 gtest && cd gtest
     cmake . && make
     mkdir lib && mv libgtest.a lib
