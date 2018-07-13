b24991088fcd65aa712ea4aa3bec9aae03f9c4d6
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2018-07-13 15:05:05 +1200

    Fix GPU linear tests

--- tests/python-gpu/test_gpu_linear.py
+++ tests/python-gpu/test_gpu_linear.py
@@ -9,6 +9,10 @@ import unittest
 class TestGPULinear(unittest.TestCase):
     def test_gpu_coordinate(self):
         tm._skip_if_no_sklearn()
-        variable_param = {'alpha': [.005, .1], 'lambda': [0.005],
+        variable_param = {'booster': ['gblinear'], 'updater': ['coord_descent'], 'eta': [0.5],
+                          'top_k': [10], 'tolerance': [1e-5], 'nthread': [2], 'alpha': [.005, .1], 'lambda': [0.005],
                           'coordinate_selection': ['cyclic', 'random', 'greedy'], 'n_gpus': [-1, 1]}
-        test_linear.assert_updater_accuracy('gpu_coord_descent', variable_param)
+        for param in test_linear.parameter_combinations(variable_param):
+            results = test_linear.run_suite(param, 200, None, scale_features=True)
+            test_linear.assert_regression_result(results, 1e-2)
+            test_linear.assert_classification_result(results)
