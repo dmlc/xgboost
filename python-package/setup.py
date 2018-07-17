054185e24f4b8b761bf683672762b6fb391441e9
pseudotensor <pseudotensor@gmail.com>
2018-07-17 10:50:12 -0700

    build system

--- python-package/setup.py
+++ python-package/setup.py
@@ -16,10 +16,16 @@ libpath_py = os.path.join(CURRENT_DIR, 'xgboost/libpath.py')
 libpath = {'__file__': libpath_py}
 exec(compile(open(libpath_py, "rb").read(), libpath_py, 'exec'), libpath, libpath)
 
-LIB_PATH = [os.path.relpath(libfile, CURRENT_DIR) for libfile in libpath['find_lib_path']()]
-print("Install libxgboost from: %s" % LIB_PATH)
+#LIB_PATH = [os.path.relpath(libfile, CURRENT_DIR) for libfile in libpath['find_lib_path']()]
+#print("Install libxgboost from: %s" % LIB_PATH)
 # Please use setup_pip.py for generating and deploying pip installation
 # detailed instruction in setup_pip.py
+
+package_data={'xgboost':'*'}
+#package_data={'':LIB_PATH}
+
+os.system("cp ../lib/libxgboost.so xgboost/")
+
 setup(name='xgboost',
       version=open(os.path.join(CURRENT_DIR, 'xgboost/VERSION')).read().strip(),
       description="XGBoost Python Package",
@@ -31,11 +37,11 @@ setup(name='xgboost',
       maintainer='Hyunsu Cho',
       maintainer_email='chohyu01@cs.washington.edu',
       zip_safe=False,
-      packages=find_packages(),
+      packages=['xgboost'],
       # this will use MANIFEST.in during install where we specify additional files,
       # this is the golden line
-      include_package_data=True,
-      data_files=[('xgboost', LIB_PATH)],
+      #include_package_data=True,
+      package_data=package_data,
       license='Apache-2.0',
       classifiers=['License :: OSI Approved :: Apache Software License'],
       url='https://github.com/dmlc/xgboost')
