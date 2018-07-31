29014bb55b175c0c4bed66b7a2652448217e9e8a
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2018-07-31 05:01:41 +0000

    Add regression test

--- tests/distributed/runtests.sh
+++ tests/distributed/runtests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "====== 1. Basic distributed test with Python ======"
 PYTHONPATH=../../python-package/ ../../dmlc-core/tracker/dmlc-submit  --cluster=local --num-workers=3\
   python test_basic.py
+
+echo "====== 2. Regression test for issue #3402 ======"
+../../dmlc-core/tracker/dmlc-submit --log-level DEBUG --cluster=local --num-workers=2 --worker-cores=1 ../../xgboost issue3402-example.conf
--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -147,3 +147,9 @@ if [ ${TASK} == "cpp_test" ]; then
     echo "GTEST_PATH="${CACHE_PREFIX} >> config.mk
     make cover
 fi
+
+if [ ${TASK} == "distributed_test" ]; then
+    set -e
+    cd tests/distributed
+    ./runtests.sh
+fi
