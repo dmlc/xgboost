eef5d639dae9b66d08361759f3d6970b8377268b
Nan Zhu <nanzhu@uber.com>
2018-08-04 11:21:42 -0700

    upload api doc

---
+++ doc/jvm/api_docs/xgboost4j_flink_scala/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"ml.dmlc.xgboost4j.scala.flink" : [{"object" : "ml\/dmlc\/xgboost4j\/scala\/flink\/XGBoost$.html", "name" : "ml.dmlc.xgboost4j.scala.flink.XGBoost"}, {"class" : "ml\/dmlc\/xgboost4j\/scala\/flink\/XGBoostModel.html", "name" : "ml.dmlc.xgboost4j.scala.flink.XGBoostModel"}], "ml.dmlc.xgboost4j.scala" : [], "ml.dmlc.xgboost4j" : [], "ml" : [], "ml.dmlc" : []};
