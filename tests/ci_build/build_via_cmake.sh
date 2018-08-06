55caad6e49e7b77158394e30961cbb19b0cd7d70
trivialfis <ybbs.daans@hotmail.com>
2018-08-07 06:08:08 +0800

    Remove redundant FindGTest.cmake. (#3533)
    During removal of FindGTest.cmake, also
    
    * Fix gtest include dirs.
    * Remove some blanks and use PWD for gtest dir.

--- tests/ci_build/build_via_cmake.sh
+++ tests/ci_build/build_via_cmake.sh
@@ -6,7 +6,7 @@ rm -rf gtest
 wget -nc https://github.com/google/googletest/archive/release-1.7.0.zip
 unzip -n release-1.7.0.zip
 mv googletest-release-1.7.0 gtest && cd gtest
-cmake . && make 
+cmake . && make
 mkdir lib && mv libgtest.a lib
 cd ..
 rm -rf release-1.7.0.zip*
@@ -14,7 +14,7 @@ rm -rf release-1.7.0.zip*
 rm -rf build
 mkdir build
 cd build
-cmake .. "$@" -DGOOGLE_TEST=ON -DGTEST_ROOT=../gtest
+cmake .. "$@" -DGOOGLE_TEST=ON -DGTEST_ROOT=$PWD/../gtest
 make clean
 make -j
 cd ..
