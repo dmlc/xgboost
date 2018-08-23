4ed8a882405dc5bbfb6cb099a075b8dcdeedecc2
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2018-08-22 18:59:30 -0700

    Update Python API doc (#3619)
    * Add XGBRanker to Python API doc
    
    * Show inherited members of XGBRegressor in API doc, since XGBRegressor uses default methods from XGBModel
    
    * Add table of contents to Python API doc
    
    * Skip JVM doc download if not available
    
    * Show inherited members for XGBRegressor and XGBRanker
    
    * Expose XGBRanker to Python XGBoost module directory
    
    * Add docstring to XGBRegressor.predict() and XGBRanker.predict()
    
    * Fix rendering errors in Python docstrings
    
    * Fix lint

--- python-package/xgboost/__init__.py
+++ python-package/xgboost/__init__.py
@@ -12,7 +12,7 @@ from .core import DMatrix, Booster
 from .training import train, cv
 from . import rabit                   # noqa
 try:
-    from .sklearn import XGBModel, XGBClassifier, XGBRegressor
+    from .sklearn import XGBModel, XGBClassifier, XGBRegressor, XGBRanker
     from .plotting import plot_importance, plot_tree, to_graphviz
 except ImportError:
     pass
@@ -23,5 +23,5 @@ with open(VERSION_FILE) as f:
 
 __all__ = ['DMatrix', 'Booster',
            'train', 'cv',
-           'XGBModel', 'XGBClassifier', 'XGBRegressor',
+           'XGBModel', 'XGBClassifier', 'XGBRegressor', 'XGBRanker',
            'plot_importance', 'plot_tree', 'to_graphviz']
