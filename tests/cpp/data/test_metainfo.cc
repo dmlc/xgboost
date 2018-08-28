dmlc/xgboost

commit 686e990ffc259b702de49ebe30232d079be9e5f1

GPU memory usage fixes + column sampling refactor (#3635)
* Remove thrust copy calls

* Fix  histogram memory usage

* Cap extreme histogram memory usage

* More efficient column sampling

* Use column sampler across updaters

* More efficient split evaluation on GPU with column sampling

src/common/host_device_vector.cu
@@ -77,7 +77,9 @@ struct HostDeviceVectorImpl {
 
     void LazySyncHost() {
       dh::safe_cuda(cudaSetDevice(device_));
-      thrust::copy(data_.begin(), data_.end(), vec_->data_h_.begin() + start_);
+      dh::safe_cuda(
+          cudaMemcpy(vec_->data_h_.data(), data_.data().get() + start_,
+                     data_.size() * sizeof(T), cudaMemcpyDeviceToHost));
       on_d_ = false;
     }
 
@@ -90,8 +92,9 @@ struct HostDeviceVectorImpl {
       size_t size_d = ShardSize(size_h, ndevices, index_);
       dh::safe_cuda(cudaSetDevice(device_));
       data_.resize(size_d);
-      thrust::copy(vec_->data_h_.begin() + start_,
-                   vec_->data_h_.begin() + start_ + size_d, data_.begin());
+      dh::safe_cuda(cudaMemcpy(data_.data().get(),
+                               vec_->data_h_.data() + start_,
+                               size_d * sizeof(T), cudaMemcpyHostToDevice));
       on_d_ = true;
       // this may cause a race condition if LazySyncDevice() is called
       // from multiple threads in parallel;
@@ -186,18 +189,22 @@ struct HostDeviceVectorImpl {
   void ScatterFrom(thrust::device_ptr<T> begin, thrust::device_ptr<T> end) {
     CHECK_EQ(end - begin, Size());
     if (on_h_) {
-      thrust::copy(begin, end, data_h_.begin());
+      dh::safe_cuda(cudaMemcpy(data_h_.data(), begin.get(),
+                               (end - begin) * sizeof(T),
+                               cudaMemcpyDeviceToHost));
     } else {
       dh::ExecuteShards(&shards_, [&](DeviceShard& shard) {
-          shard.ScatterFrom(begin.get());
-        });
+        shard.ScatterFrom(begin.get());
+      });
     }
   }
 
   void GatherTo(thrust::device_ptr<T> begin, thrust::device_ptr<T> end) {
     CHECK_EQ(end - begin, Size());
     if (on_h_) {
-      thrust::copy(data_h_.begin(), data_h_.end(), begin);
+      dh::safe_cuda(cudaMemcpy(begin.get(), data_h_.data(),
+                               data_h_.size() * sizeof(T),
+                               cudaMemcpyHostToDevice));
     } else {
       dh::ExecuteShards(&shards_, [&](DeviceShard& shard) { shard.GatherTo(begin); });
     }
@@ -400,5 +407,6 @@ void HostDeviceVector<T>::Resize(size_t new_size, T v) {
 template class HostDeviceVector<bst_float>;
 template class HostDeviceVector<GradientPair>;
 template class HostDeviceVector<unsigned int>;
+template class HostDeviceVector<int>;
 
 }  // namespace xgboost

src/common/hist_util.cc
@@ -402,7 +402,6 @@ void GHistIndexBlockMatrix::Init(const GHistIndexMatrix& gmat,
 void GHistBuilder::BuildHist(const std::vector<GradientPair>& gpair,
                              const RowSetCollection::Elem row_indices,
                              const GHistIndexMatrix& gmat,
-                             const std::vector<bst_uint>& feat_set,
                              GHistRow hist) {
   data_.resize(nbins_ * nthread_, GHistEntry());
   std::fill(data_.begin(), data_.end(), GHistEntry());
@@ -461,7 +460,6 @@ void GHistBuilder::BuildHist(const std::vector<GradientPair>& gpair,
 void GHistBuilder::BuildBlockHist(const std::vector<GradientPair>& gpair,
                                   const RowSetCollection::Elem row_indices,
                                   const GHistIndexBlockMatrix& gmatb,
-                                  const std::vector<bst_uint>& feat_set,
                                   GHistRow hist) {
   constexpr int kUnroll = 8;  // loop unrolling factor
   const size_t nblock = gmatb.GetNumBlock();

src/common/hist_util.h
@@ -266,13 +266,11 @@ class GHistBuilder {
   void BuildHist(const std::vector<GradientPair>& gpair,
                  const RowSetCollection::Elem row_indices,
                  const GHistIndexMatrix& gmat,
-                 const std::vector<bst_uint>& feat_set,
                  GHistRow hist);
   // same, with feature grouping
   void BuildBlockHist(const std::vector<GradientPair>& gpair,
                       const RowSetCollection::Elem row_indices,
                       const GHistIndexBlockMatrix& gmatb,
-                      const std::vector<bst_uint>& feat_set,
                       GHistRow hist);
   // construct a histogram via subtraction trick
   void SubtractionTrick(GHistRow self, GHistRow sibling, GHistRow parent);

src/common/host_device_vector.cc
@@ -102,6 +102,7 @@ void HostDeviceVector<T>::Reshard(GPUSet devices) { }
 template class HostDeviceVector<bst_float>;
 template class HostDeviceVector<GradientPair>;
 template class HostDeviceVector<unsigned int>;
+template class HostDeviceVector<int>;
 
 }  // namespace xgboost
 

src/common/random.h
@@ -7,8 +7,14 @@
 #ifndef XGBOOST_COMMON_RANDOM_H_
 #define XGBOOST_COMMON_RANDOM_H_
 
-#include <random>
+#include <xgboost/logging.h>
+#include <algorithm>
+#include <vector>
 #include <limits>
+#include <map>
+#include <numeric>
+#include <random>
+#include "host_device_vector.h"
 
 namespace xgboost {
 namespace common {
@@ -65,6 +71,78 @@ using GlobalRandomEngine = RandomEngine;
  *  only visible to current thread.
  */
 GlobalRandomEngine& GlobalRandom(); // NOLINT(*)
+
+/**
+ * \class ColumnSampler
+ *
+ * \brief Handles selection of columns due to colsample_bytree and
+ * colsample_bylevel parameters. Should be initialised before tree
+ * construction and to reset when tree construction is completed.
+ */
+
+class ColumnSampler {
+  HostDeviceVector<int> feature_set_tree_;
+  std::map<int, HostDeviceVector<int>> feature_set_level_;
+  float colsample_bylevel_{1.0f};
+  float colsample_bytree_{1.0f};
+
+  std::vector<int> ColSample(std::vector<int> features, float colsample) const {
+    if (colsample == 1.0f) return features;
+    CHECK_GT(features.size(), 0);
+    int n = std::max(1, static_cast<int>(colsample * features.size()));
+
+    std::shuffle(features.begin(), features.end(), common::GlobalRandom());
+    features.resize(n);
+    std::sort(features.begin(), features.end());
+
+    return features;
+  }
+
+ public:
+  /**
+   * \brief Initialise this object before use.
+   *
+   * \param num_col
+   * \param colsample_bylevel
+   * \param colsample_bytree
+   * \param skip_index_0      (Optional) True to skip index 0.
+   */
+  void Init(int64_t num_col, float colsample_bylevel, float colsample_bytree,
+            bool skip_index_0 = false) {
+    this->colsample_bylevel_ = colsample_bylevel;
+    this->colsample_bytree_ = colsample_bytree;
+    this->Reset();
+
+    int begin_idx = skip_index_0 ? 1 : 0;
+    auto& feature_set_h = feature_set_tree_.HostVector();
+    feature_set_h.resize(num_col - begin_idx);
+
+    std::iota(feature_set_h.begin(), feature_set_h.end(), begin_idx);
+    feature_set_h = ColSample(feature_set_h, this->colsample_bytree_);
+  }
+
+  /**
+   * \brief Resets this object.
+   */
+  void Reset() {
+    feature_set_tree_.HostVector().clear();
+    feature_set_level_.clear();
+  }
+
+  HostDeviceVector<int>& GetFeatureSet(int depth) {
+    if (this->colsample_bylevel_ == 1.0f) {
+      return feature_set_tree_;
+    }
+
+    if (feature_set_level_.count(depth) == 0) {
+      // Level sampling, level does not yet exist so generate it
+      auto& level = feature_set_level_[depth].HostVector();
+      level = ColSample(feature_set_tree_.HostVector(), this->colsample_bylevel_);
+    }
+    // Level sampling
+    return feature_set_level_[depth];
+  }
+};
 
 }  // namespace common
 }  // namespace xgboost

src/tree/updater_colmaker.cc
@@ -173,19 +173,8 @@ class ColMaker: public TreeUpdater {
         }
       }
       {
-        // initialize feature index
-        auto ncol = static_cast<unsigned>(fmat.Info().num_col_);
-        for (unsigned i = 0; i < ncol; ++i) {
-          if (fmat.GetColSize(i) != 0) {
-            feat_index_.push_back(i);
-          }
-        }
-        unsigned n = std::max(static_cast<unsigned>(1),
-                              static_cast<unsigned>(param_.colsample_bytree * feat_index_.size()));
-        std::shuffle(feat_index_.begin(), feat_index_.end(), common::GlobalRandom());
-        CHECK_GT(param_.colsample_bytree, 0U)
-            << "colsample_bytree cannot be zero.";
-        feat_index_.resize(n);
+        column_sampler_.Init(fmat.Info().num_col_, param_.colsample_bylevel,
+                             param_.colsample_bytree);
       }
       {
         // setup temp space for each thread
@@ -601,7 +590,7 @@ class ColMaker: public TreeUpdater {
 
     // update the solution candidate
     virtual void UpdateSolution(const SparsePage &batch,
-                                const std::vector<bst_uint> &feat_set,
+                                const std::vector<int> &feat_set,
                                 const std::vector<GradientPair> &gpair,
                                 const DMatrix &fmat) {
       const MetaInfo& info = fmat.Info();
@@ -643,15 +632,7 @@ class ColMaker: public TreeUpdater {
                           const std::vector<GradientPair> &gpair,
                           DMatrix *p_fmat,
                           RegTree *p_tree) {
-      std::vector<bst_uint> feat_set = feat_index_;
-      if (param_.colsample_bylevel != 1.0f) {
-        std::shuffle(feat_set.begin(), feat_set.end(), common::GlobalRandom());
-        unsigned n = std::max(static_cast<unsigned>(1),
-                              static_cast<unsigned>(param_.colsample_bylevel * feat_index_.size()));
-        CHECK_GT(param_.colsample_bylevel, 0U)
-            << "colsample_bylevel cannot be zero.";
-        feat_set.resize(n);
-      }
+      const std::vector<int> &feat_set = column_sampler_.GetFeatureSet(depth).HostVector();
       auto iter = p_fmat->ColIterator();
       while (iter->Next()) {
         this->UpdateSolution(iter->Value(), feat_set, gpair, *p_fmat);
@@ -770,8 +751,7 @@ class ColMaker: public TreeUpdater {
     const TrainParam& param_;
     // number of omp thread used during training
     const int nthread_;
-    // Per feature: shuffle index of each feature index
-    std::vector<bst_uint> feat_index_;
+    common::ColumnSampler column_sampler_;
     // Instance Data: current node position in the tree of each instance
     std::vector<int> position_;
     // PerThread x PerTreeNode: statistics for per thread construction

src/tree/updater_fast_hist.cc
@@ -170,7 +170,6 @@ class FastHistMaker: public TreeUpdater {
 
       tstart = dmlc::GetTime();
       this->InitData(gmat, gpair_h, *p_fmat, *p_tree);
-      std::vector<bst_uint> feat_set = feat_index_;
       time_init_data = dmlc::GetTime() - tstart;
 
       // FIXME(hcho3): this code is broken when param.num_roots > 1. Please fix it
@@ -179,7 +178,7 @@ class FastHistMaker: public TreeUpdater {
       for (int nid = 0; nid < p_tree->param.num_roots; ++nid) {
         tstart = dmlc::GetTime();
         hist_.AddHistRow(nid);
-        BuildHist(gpair_h, row_set_collection_[nid], gmat, gmatb, feat_set, hist_[nid]);
+        BuildHist(gpair_h, row_set_collection_[nid], gmat, gmatb, hist_[nid]);
         time_build_hist += dmlc::GetTime() - tstart;
 
         tstart = dmlc::GetTime();
@@ -187,7 +186,7 @@ class FastHistMaker: public TreeUpdater {
         time_init_new_node += dmlc::GetTime() - tstart;
 
         tstart = dmlc::GetTime();
-        this->EvaluateSplit(nid, gmat, hist_, *p_fmat, *p_tree, feat_set);
+        this->EvaluateSplit(nid, gmat, hist_, *p_fmat, *p_tree);
         time_evaluate_split += dmlc::GetTime() - tstart;
         qexpand_->push(ExpandEntry(nid, p_tree->GetDepth(nid),
                                    snode_[nid].best.loss_chg,
@@ -214,10 +213,10 @@ class FastHistMaker: public TreeUpdater {
           hist_.AddHistRow(cleft);
           hist_.AddHistRow(cright);
           if (row_set_collection_[cleft].Size() < row_set_collection_[cright].Size()) {
-            BuildHist(gpair_h, row_set_collection_[cleft], gmat, gmatb, feat_set, hist_[cleft]);
+            BuildHist(gpair_h, row_set_collection_[cleft], gmat, gmatb, hist_[cleft]);
             SubtractionTrick(hist_[cright], hist_[cleft], hist_[nid]);
           } else {
-            BuildHist(gpair_h, row_set_collection_[cright], gmat, gmatb, feat_set, hist_[cright]);
+            BuildHist(gpair_h, row_set_collection_[cright], gmat, gmatb, hist_[cright]);
             SubtractionTrick(hist_[cleft], hist_[cright], hist_[nid]);
           }
           time_build_hist += dmlc::GetTime() - tstart;
@@ -231,8 +230,8 @@ class FastHistMaker: public TreeUpdater {
           time_init_new_node += dmlc::GetTime() - tstart;
 
           tstart = dmlc::GetTime();
-          this->EvaluateSplit(cleft, gmat, hist_, *p_fmat, *p_tree, feat_set);
-          this->EvaluateSplit(cright, gmat, hist_, *p_fmat, *p_tree, feat_set);
+          this->EvaluateSplit(cleft, gmat, hist_, *p_fmat, *p_tree);
+          this->EvaluateSplit(cright, gmat, hist_, *p_fmat, *p_tree);
           time_evaluate_split += dmlc::GetTime() - tstart;
 
           qexpand_->push(ExpandEntry(cleft, p_tree->GetDepth(cleft),
@@ -296,12 +295,11 @@ class FastHistMaker: public TreeUpdater {
                           const RowSetCollection::Elem row_indices,
                           const GHistIndexMatrix& gmat,
                           const GHistIndexBlockMatrix& gmatb,
-                          const std::vector<bst_uint>& feat_set,
                           GHistRow hist) {
       if (fhparam_.enable_feature_grouping > 0) {
-        hist_builder_.BuildBlockHist(gpair, row_indices, gmatb, feat_set, hist);
+        hist_builder_.BuildBlockHist(gpair, row_indices, gmatb, hist);
       } else {
-        hist_builder_.BuildHist(gpair, row_indices, gmat, feat_set, hist);
+        hist_builder_.BuildHist(gpair, row_indices, gmat, hist);
       }
     }
 
@@ -427,23 +425,13 @@ class FastHistMaker: public TreeUpdater {
         // store a pointer to training data
         p_last_fmat_ = &fmat;
         // initialize feature index
-        auto ncol = static_cast<bst_uint>(info.num_col_);
-        feat_index_.clear();
         if (data_layout_ == kDenseDataOneBased) {
-          for (bst_uint i = 1; i < ncol; ++i) {
-            feat_index_.push_back(i);
-          }
+          column_sampler_.Init(info.num_col_, param_.colsample_bylevel,
+                               param_.colsample_bytree, true);
         } else {
-          for (bst_uint i = 0; i < ncol; ++i) {
-            feat_index_.push_back(i);
-          }
+          column_sampler_.Init(info.num_col_, param_.colsample_bylevel,
+                               param_.colsample_bytree, false);
         }
-        bst_uint n = std::max(static_cast<bst_uint>(1),
-                              static_cast<bst_uint>(param_.colsample_bytree * feat_index_.size()));
-        std::shuffle(feat_index_.begin(), feat_index_.end(), common::GlobalRandom());
-        CHECK_GT(param_.colsample_bytree, 0U)
-            << "colsample_bytree cannot be zero.";
-        feat_index_.resize(n);
       }
       if (data_layout_ == kDenseDataZeroBased || data_layout_ == kDenseDataOneBased) {
         /* specialized code for dense data:
@@ -481,11 +469,11 @@ class FastHistMaker: public TreeUpdater {
                               const GHistIndexMatrix& gmat,
                               const HistCollection& hist,
                               const DMatrix& fmat,
-                              const RegTree& tree,
-                              const std::vector<bst_uint>& feat_set) {
+                              const RegTree& tree) {
       // start enumeration
       const MetaInfo& info = fmat.Info();
-      const auto nfeature = static_cast<bst_uint>(feat_set.size());
+      const auto& feature_set = column_sampler_.GetFeatureSet(tree.GetDepth(nid)).HostVector();
+      const auto nfeature = static_cast<bst_uint>(feature_set.size());
       const auto nthread = static_cast<bst_omp_uint>(this->nthread_);
       best_split_tloc_.resize(nthread);
       #pragma omp parallel for schedule(static) num_threads(nthread)
@@ -494,7 +482,7 @@ class FastHistMaker: public TreeUpdater {
       }
       #pragma omp parallel for schedule(dynamic) num_threads(nthread)
       for (bst_omp_uint i = 0; i < nfeature; ++i) {
-        const bst_uint fid = feat_set[i];
+        const bst_uint fid = feature_set[i];
         const unsigned tid = omp_get_thread_num();
         this->EnumerateSplit(-1, gmat, hist[nid], snode_[nid], info,
           &best_split_tloc_[tid], fid, nid);
@@ -837,8 +825,7 @@ class FastHistMaker: public TreeUpdater {
     const FastHistParam& fhparam_;
     // number of omp thread used during training
     int nthread_;
-    // Per feature: shuffle index of each feature index
-    std::vector<bst_uint> feat_index_;
+    common::ColumnSampler column_sampler_;
     // the internal row sets
     RowSetCollection row_set_collection_;
     // the temp space for split

src/tree/updater_gpu_common.cuh
@@ -383,80 +383,5 @@ inline void SubsampleGradientPair(dh::DVec<GradientPair>* p_gpair, float subsamp
   });
 }
 
-inline std::vector<int> ColSample(std::vector<int> features, float colsample) {
-  CHECK_GT(features.size(), 0);
-  int n = std::max(1, static_cast<int>(colsample * features.size()));
-
-  std::shuffle(features.begin(), features.end(), common::GlobalRandom());
-  features.resize(n);
-  std::sort(features.begin(), features.end());
-
-  return features;
-}
-
-/**
- * \class ColumnSampler
- *
- * \brief Handles selection of columns due to colsample_bytree and
- * colsample_bylevel parameters. Should be initialised the before tree
- * construction and to reset When tree construction is completed.
- */
-
-class ColumnSampler {
-  std::vector<int> feature_set_tree_;
-  std::map<int, std::vector<int>> feature_set_level_;
-  TrainParam param_;
-
- public:
-  /**
-   * \fn  void Init(int64_t num_col, const TrainParam& param)
-   *
-   * \brief Initialise this object before use.
-   *
-   * \param num_col Number of cols.
-   * \param param   The parameter.
-   */
-
-  void Init(int64_t num_col, const TrainParam& param) {
-    this->Reset();
-    this->param_ = param;
-    feature_set_tree_.resize(num_col);
-    std::iota(feature_set_tree_.begin(), feature_set_tree_.end(), 0);
-    feature_set_tree_ = ColSample(feature_set_tree_, param.colsample_bytree);
-  }
-
-  /**
-   * \fn  void Reset()
-   *
-   * \brief Resets this object.
-   */
-
-  void Reset() {
-    feature_set_tree_.clear();
-    feature_set_level_.clear();
-  }
-
-  /**
-   * \fn  bool ColumnUsed(int column, int depth)
-   *
-   * \brief Whether the current column should be considered as a split.
-   *
-   * \param column  The column index.
-   * \param depth   The current tree depth.
-   *
-   * \return  True if it should be used, false if it should not be used.
-   */
-
-  bool ColumnUsed(int column, int depth) {
-    if (feature_set_level_.count(depth) == 0) {
-      feature_set_level_[depth] =
-          ColSample(feature_set_tree_, param_.colsample_bylevel);
-    }
-
-    return std::binary_search(feature_set_level_[depth].begin(),
-                              feature_set_level_[depth].end(), column);
-  }
-};
-
 }  // namespace tree
 }  // namespace xgboost

commit 78bea0d2041fc231be6d3d26be3037dbec6af54d

Add google test for a column sampling, restore metainfo tests (#3637)
* Add google test for a column sampling, restore metainfo tests

* Update metainfo test for visual studio

* Fix multi-GPU bug introduced in #3635

src/common/host_device_vector.cu
@@ -77,9 +77,9 @@ struct HostDeviceVectorImpl {
 
     void LazySyncHost() {
       dh::safe_cuda(cudaSetDevice(device_));
-      dh::safe_cuda(
-          cudaMemcpy(vec_->data_h_.data(), data_.data().get() + start_,
-                     data_.size() * sizeof(T), cudaMemcpyDeviceToHost));
+      dh::safe_cuda(cudaMemcpy(vec_->data_h_.data() + start_,
+                               data_.data().get(), data_.size() * sizeof(T),
+                               cudaMemcpyDeviceToHost));
       on_d_ = false;
     }
 

tests/cpp/common/test_random.cc
@@ -1,0 +1,37 @@
+#include "../../../src/common/random.h"
+#include "../helpers.h"
+#include "gtest/gtest.h"
+
+namespace xgboost {
+namespace common {
+TEST(ColumnSampler, Test) {
+  int n = 100;
+  ColumnSampler cs;
+  cs.Init(n, 0.5f, 0.5f);
+  auto &set0 = cs.GetFeatureSet(0).HostVector();
+  ASSERT_EQ(set0.size(), 25);
+
+  auto &set1 = cs.GetFeatureSet(0).HostVector();
+  ASSERT_EQ(set0, set1);
+
+  auto &set2 = cs.GetFeatureSet(1).HostVector();
+  ASSERT_NE(set1, set2);
+  ASSERT_EQ(set2.size(), 25);
+
+  // No level sampling, should be the same at different depth
+  cs.Init(n, 1.0f, 0.5f);
+  ASSERT_EQ(cs.GetFeatureSet(0).HostVector(), cs.GetFeatureSet(1).HostVector());
+
+  cs.Init(n, 1.0f, 1.0f);
+  auto &set3 = cs.GetFeatureSet(0).HostVector();
+  ASSERT_EQ(set3.size(), n);
+  cs.Init(n, 1.0f, 1.0f);
+  auto &set4 = cs.GetFeatureSet(0).HostVector();
+  ASSERT_EQ(set3, set4);
+
+  // Should always be a minimum of one feature
+  cs.Init(n, 1e-16f, 1e-16f);
+  ASSERT_EQ(cs.GetFeatureSet(0).HostVector().size(), 1);
+}
+}  // namespace common
+}  // namespace xgboost

tests/cpp/data/test_metainfo.cc
@@ -69,4 +69,66 @@ TEST(MetaInfo, SaveLoadBinary) {
 }
 
 TEST(MetaInfo, LoadQid) {
+  std::string tmp_file = TempFileName();
+  {
+    std::unique_ptr<dmlc::Stream> fs(
+      dmlc::Stream::Create(tmp_file.c_str(), "w"));
+    dmlc::ostream os(fs.get());
+    os << R"qid(3 qid:1 1:1 2:1 3:0 4:0.2 5:0
+                2 qid:1 1:0 2:0 3:1 4:0.1 5:1
+                1 qid:1 1:0 2:1 3:0 4:0.4 5:0
+                1 qid:1 1:0 2:0 3:1 4:0.3 5:0
+                1 qid:2 1:0 2:0 3:1 4:0.2 5:0
+                2 qid:2 1:1 2:0 3:1 4:0.4 5:0
+                1 qid:2 1:0 2:0 3:1 4:0.1 5:0
+                1 qid:2 1:0 2:0 3:1 4:0.2 5:0
+                2 qid:3 1:0 2:0 3:1 4:0.1 5:1
+                3 qid:3 1:1 2:1 3:0 4:0.3 5:0
+                4 qid:3 1:1 2:0 3:0 4:0.4 5:1
+                1 qid:3 1:0 2:1 3:1 4:0.5 5:0)qid";
+    os.set_stream(nullptr);
+  }
+  std::unique_ptr<xgboost::DMatrix> dmat(
+    xgboost::DMatrix::Load(tmp_file, true, false, "libsvm"));
+  std::remove(tmp_file.c_str());
+
+  const xgboost::MetaInfo& info = dmat->Info();
+  const std::vector<uint64_t> expected_qids{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  const std::vector<xgboost::bst_uint> expected_group_ptr{0, 4, 8, 12};
+  CHECK(info.qids_ == expected_qids);
+  CHECK(info.group_ptr_ == expected_group_ptr);
+  CHECK_GE(info.kVersion, info.kVersionQidAdded);
+
+  const std::vector<size_t> expected_offset{
+    0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
+  };
+  const std::vector<xgboost::Entry> expected_data{
+      xgboost::Entry(1, 1),   xgboost::Entry(2, 1),   xgboost::Entry(3, 0),
+      xgboost::Entry(4, 0.2), xgboost::Entry(5, 0),   xgboost::Entry(1, 0),
+      xgboost::Entry(2, 0),   xgboost::Entry(3, 1),   xgboost::Entry(4, 0.1),
+      xgboost::Entry(5, 1),   xgboost::Entry(1, 0),   xgboost::Entry(2, 1),
+      xgboost::Entry(3, 0),   xgboost::Entry(4, 0.4), xgboost::Entry(5, 0),
+      xgboost::Entry(1, 0),   xgboost::Entry(2, 0),   xgboost::Entry(3, 1),
+      xgboost::Entry(4, 0.3), xgboost::Entry(5, 0),   xgboost::Entry(1, 0),
+      xgboost::Entry(2, 0),   xgboost::Entry(3, 1),   xgboost::Entry(4, 0.2),
+      xgboost::Entry(5, 0),   xgboost::Entry(1, 1),   xgboost::Entry(2, 0),
+      xgboost::Entry(3, 1),   xgboost::Entry(4, 0.4), xgboost::Entry(5, 0),
+      xgboost::Entry(1, 0),   xgboost::Entry(2, 0),   xgboost::Entry(3, 1),
+      xgboost::Entry(4, 0.1), xgboost::Entry(5, 0),   xgboost::Entry(1, 0),
+      xgboost::Entry(2, 0),   xgboost::Entry(3, 1),   xgboost::Entry(4, 0.2),
+      xgboost::Entry(5, 0),   xgboost::Entry(1, 0),   xgboost::Entry(2, 0),
+      xgboost::Entry(3, 1),   xgboost::Entry(4, 0.1), xgboost::Entry(5, 1),
+      xgboost::Entry(1, 1),   xgboost::Entry(2, 1),   xgboost::Entry(3, 0),
+      xgboost::Entry(4, 0.3), xgboost::Entry(5, 0),   xgboost::Entry(1, 1),
+      xgboost::Entry(2, 0),   xgboost::Entry(3, 0),   xgboost::Entry(4, 0.4),
+      xgboost::Entry(5, 1),   xgboost::Entry(1, 0),   xgboost::Entry(2, 1),
+      xgboost::Entry(3, 1),   xgboost::Entry(4, 0.5), {5, 0}};
+  dmlc::DataIter<xgboost::SparsePage>* iter = dmat->RowIterator();
+  iter->BeforeFirst();
+  CHECK(iter->Next());
+  const xgboost::SparsePage& batch = iter->Value();
+  CHECK_EQ(batch.base_rowid, 0);
+  CHECK(batch.offset == expected_offset);
+  CHECK(batch.data == expected_data);
+  CHECK(!iter->Next());
 }