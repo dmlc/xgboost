8c76f41848ff304a181366c5a5b2dcaf51ae95a6
shiki <shiki.he@hotmail.com>
2018-09-04 01:03:00 -0400

    moved data processing to wgetdata.sh

--- demo/rank/wgetdata.sh
+++ demo/rank/wgetdata.sh
@@ -2,3 +2,9 @@
 wget https://s3-us-west-2.amazonaws.com/xgboost-examples/MQ2008.rar
 unrar x MQ2008.rar
 mv -f MQ2008/Fold1/*.txt .
+
+python trans_data.py train.txt mq2008.train mq2008.train.group
+
+python trans_data.py test.txt mq2008.test mq2008.test.group
+
+python trans_data.py vali.txt mq2008.vali mq2008.vali.group
