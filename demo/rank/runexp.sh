8f4acba34ba9df8e196be6bac65ad0da32be1f08
Shiki-H <shiki.he@hotmail.com>
2018-09-04 12:36:48 -0400

    moved data processing to wgetdata.sh (#3666)

--- demo/rank/runexp.sh
+++ demo/rank/runexp.sh
@@ -1,11 +1,5 @@
-python trans_data.py train.txt mq2008.train mq2008.train.group
-
-python trans_data.py test.txt mq2008.test mq2008.test.group
-
-python trans_data.py vali.txt mq2008.vali mq2008.vali.group
+#!/bin/bash
 
 ../../xgboost mq2008.conf
 
 ../../xgboost mq2008.conf task=pred model_in=0004.model
-
-
