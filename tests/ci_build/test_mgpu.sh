6c49d8b21a7d0d404e65959f07b35ca94bd848e2
Philip Cho <chohyu01@cs.washington.edu>
2018-09-28 22:55:46 -0700

    Consolidate Python tests. Use attributes to distinguish multi-GPU Python tests from single-CPU counterparts

--- tests/ci_build/test_mgpu.sh
+++ tests/ci_build/test_mgpu.sh
@@ -4,5 +4,5 @@ set -e
 cd python-package
 python setup.py install --user
 cd ..
-python -m nose -v --attr='!slow' tests/python-mgpu/
+python -m nose -v --eval-attr='(not slow) and mgpu' tests/python-gpu/
 ./testxgboost --gtest_filter=*.MGPU_*
