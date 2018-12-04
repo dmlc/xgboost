fb165bc0c8e35f1d77d560da58d954f2f392f224
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2018-12-04 17:18:21 +1300

    Allow single precision histogram summation in gpu_hist

--- src/common/device_helpers.cuh
+++ src/common/device_helpers.cuh
@@ -945,6 +945,32 @@ class AllReducer {
   }
 
   /**
+   * \brief Allreduce. Use in exactly the same way as NCCL but without needing
+   * streams or comms.
+   *
+   * \param communication_group_idx Zero-based index of the communication group.
+   * \param sendbuff                The sendbuff.
+   * \param recvbuff                The recvbuff.
+   * \param count                   Number of elements.
+   */
+
+  void AllReduceSum(int communication_group_idx, const float *sendbuff,
+                    float *recvbuff, int count) {
+#ifdef XGBOOST_USE_NCCL
+    CHECK(initialised_);
+    dh::safe_cuda(cudaSetDevice(device_ordinals.at(communication_group_idx)));
+    dh::safe_nccl(ncclAllReduce(sendbuff, recvbuff, count, ncclFloat, ncclSum,
+                                comms.at(communication_group_idx),
+                                streams.at(communication_group_idx)));
+    if(communication_group_idx == 0)
+    {
+      allreduce_bytes_ += count * sizeof(float);
+      allreduce_calls_ += 1;
+    }
+#endif
+  }
+
+  /**
    * \brief Allreduce. Use in exactly the same way as NCCL but without needing streams or comms.
    *
    * \param count Number of.
