09b174fa159468599e183027cdb546b7c62fbb0d
Nan Zhu <nanzhu@uber.com>
2018-12-10 11:30:05 -0800

    udpate rabit

--- src/tree/updater_quantile_hist.h
+++ src/tree/updater_quantile_hist.h
@@ -105,8 +105,7 @@ class QuantileHistMaker: public TreeUpdater {
       } else {
         hist_builder_.BuildHist(gpair, row_indices, gmat, hist);
       }
-
-      //this->histred_.Allreduce(hist.begin, hist.size);
+      this->histred_.Allreduce(hist.begin, hist.size);
     }
 
     inline void SubtractionTrick(GHistRow self, GHistRow sibling, GHistRow parent) {
