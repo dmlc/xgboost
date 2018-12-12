25b2a89a82761d353137a63db5c3c9cf4f951174
Nan Zhu <nanzhu@uber.com>
2018-12-11 23:26:14 -0800

    temp

--- src/tree/updater_quantile_hist.h
+++ src/tree/updater_quantile_hist.h
@@ -105,7 +105,7 @@ class QuantileHistMaker: public TreeUpdater {
       } else {
         hist_builder_.BuildHist(gpair, row_indices, gmat, hist);
       }
-      this->histred_.Allreduce(hist.begin, hist_builder_.GetNumBins());
+      this->histred_.Allreduce(hist.begin, hist.size);
     }
 
     inline void SubtractionTrick(GHistRow self, GHistRow sibling, GHistRow parent) {
