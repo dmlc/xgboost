2dd980bc5cb87ebe35f31bbb03288015d46a6e4f
Nan Zhu <nanzhu@uber.com>
2019-02-05 08:32:17 -0800

    fix lint issue

--- src/tree/updater_quantile_hist.h
+++ src/tree/updater_quantile_hist.h
@@ -209,7 +209,6 @@ class QuantileHistMaker: public TreeUpdater {
 
     // initialize temp data structure
     void InitData(const GHistIndexMatrix& gmat,
-
                   const std::vector<GradientPair>& gpair,
                   const DMatrix& fmat,
                   const RegTree& tree);
@@ -220,21 +219,21 @@ class QuantileHistMaker: public TreeUpdater {
                        const DMatrix& fmat,
                        const RegTree& tree);
 
-   void ApplySplit(int nid,
-                    const GHistIndexMatrix& gmat,
-                    const ColumnMatrix& column_matrix,
-                    const HistCollection& hist,
-                    const DMatrix& fmat,
-                    RegTree* p_tree);
+    void ApplySplit(int nid,
+                     const GHistIndexMatrix& gmat,
+                     const ColumnMatrix& column_matrix,
+                     const HistCollection& hist,
+                     const DMatrix& fmat,
+                     RegTree* p_tree);
 
-   void ApplySplitDenseData(const RowSetCollection::Elem rowset,
-                             const GHistIndexMatrix& gmat,
-                             std::vector<RowSetCollection::Split>* p_row_split_tloc,
-                             const Column& column,
-                             bst_int split_cond,
-                             bool default_left);
+    void ApplySplitDenseData(const RowSetCollection::Elem rowset,
+                              const GHistIndexMatrix& gmat,
+                              std::vector<RowSetCollection::Split>* p_row_split_tloc,
+                              const Column& column,
+                              bst_int split_cond,
+                              bool default_left);
 
-   void ApplySplitSparseData(const RowSetCollection::Elem rowset,
+    void ApplySplitSparseData(const RowSetCollection::Elem rowset,
                               const GHistIndexMatrix& gmat,
                               std::vector<RowSetCollection::Split>* p_row_split_tloc,
                               const Column& column,
@@ -243,14 +242,14 @@ class QuantileHistMaker: public TreeUpdater {
                               bst_int split_cond,
                               bool default_left);
 
-   void InitNewNode(int nid,
+    void InitNewNode(int nid,
                      const GHistIndexMatrix& gmat,
                      const std::vector<GradientPair>& gpair,
                      const DMatrix& fmat,
                      const RegTree& tree);
 
-   // enumerate the split values of specific feature
-   void EnumerateSplit(int d_step,
+    // enumerate the split values of specific feature
+    void EnumerateSplit(int d_step,
                         const GHistIndexMatrix& gmat,
                         const GHistRow& hist,
                         const NodeEntry& snode,
@@ -259,7 +258,7 @@ class QuantileHistMaker: public TreeUpdater {
                         bst_uint fid,
                         bst_uint nodeID);
 
-   void ExpandWithDepthWidth(const GHistIndexMatrix &gmat,
+    void ExpandWithDepthWidth(const GHistIndexMatrix &gmat,
                               const GHistIndexBlockMatrix &gmatb,
                               const ColumnMatrix &column_matrix,
                               DMatrix *p_fmat,
