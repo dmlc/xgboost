4886e92d441e8ae221ced15b66ad1ee6b0a04f03
Philip Cho <chohyu01@cs.washington.edu>
2019-02-14 17:33:45 -0800

    Symlink library path too

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -90,6 +90,8 @@ if [ ${TASK} == "r_test" ]; then
     if [ ${TRAVIS_OS_NAME} == "osx" ]; then
         # Work-around to fix "gfortran command not found" error
         sudo ln -s $(which gfortran-7) /usr/local/bin/gfortran
+        sudo mkdir -p /usr/local/gfortran/lib/gcc/x86_64-apple-darwin15
+        sudo ln -s /usr/local/lib/gcc/7/libgfortran /usr/local/gfortran/lib/gcc/x86_64-apple-darwin15/6.1.0
     fi
 
     curl -OL http://raw.github.com/craigcitro/r-travis/master/scripts/travis-tool.sh
