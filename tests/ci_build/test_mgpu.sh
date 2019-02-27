489e499b61211b45132ff0a56ddefac4c119af12
Matthew Jones <majones@nvidia.com>
2019-02-27 12:48:47 -0800

    fixing bad path in distributed test

--- tests/ci_build/test_mgpu.sh
+++ tests/ci_build/test_mgpu.sh
@@ -7,5 +7,5 @@ cd ..
 pytest -v -s --fulltrace -m "(not slow) and mgpu" tests/python-gpu
 ./testxgboost --gtest_filter=*.MGPU_*
 
-cd ../tests/distributed-gpu
+cd tests/distributed-gpu
 ./runtests-gpu.sh
