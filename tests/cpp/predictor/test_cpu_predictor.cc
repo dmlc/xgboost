d72ec29f8f6ff229716c390dcca6ac0f5c1d86af
Rong Ou <rong.ou@gmail.com>
2019-03-28 16:19:21 -0700

    add a test for cpu predictor using external memory

--- tests/cpp/predictor/test_cpu_predictor.cc
+++ tests/cpp/predictor/test_cpu_predictor.cc
@@ -1,4 +1,5 @@
 // Copyright by Contributors
+#include <dmlc/filesystem.h>
 #include <gtest/gtest.h>
 #include <xgboost/predictor.h>
 #include "../helpers.h"
@@ -55,6 +56,63 @@ TEST(cpu_predictor, Test) {
   cpu_predictor->PredictContribution((*dmat).get(), &out_contribution, model, true);
   for (auto const& contri : out_contribution) {
     ASSERT_EQ(contri, 1.5);
+  }
+
+  delete dmat;
+}
+
+TEST(cpu_predictor, ExternalMemoryTest) {
+  // Create sufficiently large data to make two row pages
+  dmlc::TemporaryDirectory tempdir;
+  const std::string tmp_file = tempdir.path + "/big.libsvm";
+  CreateBigTestData(tmp_file, 5000000);
+  xgboost::DMatrix *dmat = xgboost::DMatrix::Load(
+      tmp_file + "#" + tmp_file + ".cache", true, false);
+  EXPECT_TRUE(FileExists(tmp_file + ".cache.row.page"));
+
+  std::unique_ptr<Predictor> cpu_predictor =
+      std::unique_ptr<Predictor>(Predictor::Create("cpu_predictor"));
+
+  std::vector<std::unique_ptr<RegTree>> trees;
+  trees.push_back(std::unique_ptr<RegTree>(new RegTree));
+  (*trees.back())[0].SetLeaf(1.5f);
+  (*trees.back()).Stat(0).sum_hess = 1.0f;
+  gbm::GBTreeModel model(0.5);
+  model.CommitModel(std::move(trees), 0);
+  model.param.num_output_group = 1;
+  model.base_margin = 0;
+
+  // Test predict batch
+  HostDeviceVector<float> out_predictions;
+  cpu_predictor->PredictBatch(dmat, &out_predictions, model, 0);
+  std::vector<float> &out_predictions_h = out_predictions.HostVector();
+  EXPECT_EQ(out_predictions.Size(), dmat->Info().num_row_);
+  for (const auto& v : out_predictions_h) {
+    ASSERT_EQ(v, 1.5);
+  }
+
+  // Test predict leaf
+  std::vector<float> leaf_out_predictions;
+  cpu_predictor->PredictLeaf(dmat, &leaf_out_predictions, model);
+  EXPECT_EQ(leaf_out_predictions.size(), dmat->Info().num_row_);
+  for (const auto& v : leaf_out_predictions) {
+    ASSERT_EQ(v, 0);
+  }
+
+  // Test predict contribution
+  std::vector<float> out_contribution;
+  cpu_predictor->PredictContribution(dmat, &out_contribution, model);
+  EXPECT_EQ(out_contribution.size(), dmat->Info().num_row_);
+  for (const auto& v : out_contribution) {
+    ASSERT_EQ(v, 1.5);
+  }
+
+  // Test predict contribution (approximate method)
+  std::vector<float> out_contribution_approximate;
+  cpu_predictor->PredictContribution(dmat, &out_contribution_approximate, model, true);
+  EXPECT_EQ(out_contribution_approximate.size(), dmat->Info().num_row_);
+  for (const auto& v : out_contribution_approximate) {
+    ASSERT_EQ(v, 1.5);
   }
 
   delete dmat;
