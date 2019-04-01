7389d8ea75cbb218649af518c1a63c0b9ac30141
fis <jm.yuan@outlook.com>
2019-04-02 07:32:02 +0800

    Refactor CMake scripts.
    * Remove CMake CUDA wrapper.
    * Bump CMake version for CUDA.
    * Use CMake to handle Doxygen.
    * Split up CMakeList.
    * Export install target.
    * Use modern CMake.
    * Remove build.sh
    * Workaround for gpu_hist test.
    * Use cmake 3.12.

--- tests/ci_build/test_mgpu.sh
+++ tests/ci_build/test_mgpu.sh
@@ -6,6 +6,9 @@ python setup.py install --user
 cd ..
 pytest -v -s --fulltrace -m "(not slow) and mgpu" tests/python-gpu
 ./build/testxgboost --gtest_filter=*.MGPU_*
+cd ./build
+ctest --output-on-failure --tests-regex "TestXGBoostCLI"
+cd -
 
 cd tests/distributed
 ./runtests-gpu.sh
