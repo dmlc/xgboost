69b9356bcf2168ab2760111a80d350bcb5f88cc9
pseudotensor <pseudotensor@gmail.com>
2019-04-10 13:05:31 -0700

    Fix for hist2 and BinarySearchRow

--- include/xgboost/build_config.h
+++ include/xgboost/build_config.h
@@ -1,8 +1,11 @@
+/*!
+ * Copyright 2019 by Contributors
+ * \file build_config.h
+ */
 #ifndef XGBOOST_BUILD_CONFIG_H_
 #define XGBOOST_BUILD_CONFIG_H_
 
 #define XGBOOST_MM_PREFETCH_PRESENT
 #define XGBOOST_BUILTIN_PREFETCH_PRESENT
-#endif  // GUARDS
 
 #endif  // XGBOOST_BUILD_CONFIG_H_
