509ece935978550ca5d74cc2a75625b79e7b1400
fis <jm.yuan@outlook.com>
2019-04-12 22:26:28 +0800

    Move CLI test to gpu.

--- tests/ci_build/test_mgpu.sh
+++ tests/ci_build/test_mgpu.sh
@@ -6,9 +6,6 @@ python setup.py install --user
 cd ..
 pytest -v -s --fulltrace -m "(not slow) and mgpu" tests/python-gpu
 ./build/testxgboost --gtest_filter=*.MGPU_*
-cd ./build
-ctest --output-on-failure --tests-regex "TestXGBoostCLI"
-cd -
 
 cd tests/distributed
 ./runtests-gpu.sh
