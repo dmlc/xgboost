69421425142a3e682df56886bc5ba0d4881ba4e6
Nan Zhu <nanzhu@uber.com>
2019-04-18 21:30:14 -0700

    fix mc_metrics.cc

--- src/metric/multiclass_metric.cc
+++ src/metric/multiclass_metric.cc
@@ -3,12 +3,6 @@
  */
 // Dummy file to keep the CUDA conditional compile trick.
 
-
-#include <rabit/rabit.h>
-#include <xgboost/metric/multiclass_metric.h>
-#include <cmath>
-#include "../common/math.h"
-
 #if !defined(XGBOOST_USE_CUDA)
 #include "multiclass_metric.cu"
 #endif  // !defined(XGBOOST_USE_CUDA)
