92828345fb81e93be14da0d5d74ebdddc98b3270
Hyunsu Cho <chohyu01@cs.washington.edu>
2019-04-24 04:45:25 +0000

    Fix path of stashed JARs

--- tests/ci_build/test_jvm_cross.sh
+++ tests/ci_build/test_jvm_cross.sh
@@ -15,8 +15,8 @@ scala_version=$(mvn help:evaluate -Dexpression=scala.version -q -DforceStdout)
 scala_binary_version=$(mvn help:evaluate -Dexpression=scala.binary.version -q -DforceStdout)
 
 # Install XGBoost4J JAR into local Maven repository
-mvn install:install-file -Dfile=./jvm-packages/xgboost4j/target/xgboost4j-${xgboost4j_version}.jar -DgroupId=ml.dmlc -DartifactId=xgboost4j -Dversion=${xgboost4j_version} -Dpackaging=jar
-mvn install:install-file -Dfile=./jvm-packages/xgboost4j/target/xgboost4j-${xgboost4j_version}-tests.jar -DgroupId=ml.dmlc -DartifactId=xgboost4j -Dversion=${xgboost4j_version} -Dpackaging=test-jar -Dclassifier=tests
+mvn install:install-file -Dfile=./xgboost4j/target/xgboost4j-${xgboost4j_version}.jar -DgroupId=ml.dmlc -DartifactId=xgboost4j -Dversion=${xgboost4j_version} -Dpackaging=jar
+mvn install:install-file -Dfile=./xgboost4j/target/xgboost4j-${xgboost4j_version}-tests.jar -DgroupId=ml.dmlc -DartifactId=xgboost4j -Dversion=${xgboost4j_version} -Dpackaging=test-jar -Dclassifier=tests
 
 cd xgboost4j-tester
 # Generate pom.xml for XGBoost4J-tester, a dummy project to run XGBoost4J tests
