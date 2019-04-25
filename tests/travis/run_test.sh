9a4ee23bc5e6984c9699b97b2052e97023ca4787
Hyunsu Cho <chohyu01@cs.washington.edu>
2019-04-25 08:49:15 +0000

    Remove redundant tests from Travis CI

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-if [ ${TASK} == "lint" ]; then
-    source activate python3
-    conda install numpy scipy
-    python -m pip install cpplint pylint astroid
-    make lint || exit -1
-    echo "Check documentations..."
-
-    mkdir build_doc
-    cd build_doc
-    cmake .. -DBUILD_C_DOC=ON
-    make doc_doxygen 2> log.txt
-
-    (cat log.txt| grep -v ENABLE_PREPROCESSING |grep -v "unsupported tag") > logclean.txt
-    echo "---------Error Log----------"
-    cat logclean.txt
-    echo "----------------------------"
-    (cat logclean.txt|grep warning) && exit -1
-    (cat logclean.txt|grep error) && exit -1
-
-    cd -
-    rm -rf build_doc
-
-    exit 0
-fi
-
 cp make/travis.mk config.mk
 make -f dmlc-core/scripts/packages.mk lz4
-
 
 if [ ${TRAVIS_OS_NAME} == "osx" ]; then
     echo 'USE_OPENMP=0' >> config.mk
@@ -45,96 +19,13 @@ if [ ${TASK} == "python_test" ]; then
     python --version
     conda install numpy scipy pandas matplotlib scikit-learn
 
-    # Install data table from source
-    wget http://releases.llvm.org/5.0.2/clang+llvm-5.0.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    tar xf clang+llvm-5.0.2-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-    export LLVM5=$(pwd)/clang+llvm-5.0.2-x86_64-linux-gnu-ubuntu-14.04
-    python -m pip install datatable --no-binary datatable
-
     python -m pip install graphviz pytest pytest-cov codecov
+    python -m pip install https://h2o-release.s3.amazonaws.com/datatable/stable/datatable-0.7.0/datatable-0.7.0-cp37-cp37m-linux_x86_64.whl
     python -m pytest -v --fulltrace -s tests/python --cov=python-package/xgboost || exit -1
     codecov
-
-    source activate python2
-    echo "-------------------------------"
-    python --version
-    conda install numpy scipy pandas matplotlib scikit-learn
-    python -m pip install graphviz pytest
-    python -m pytest -v --fulltrace -s tests/python || exit -1
-    exit 0
-fi
-
-if [ ${TASK} == "python_lightweight_test" ]; then
-    make all || exit -1
-
-    echo "-------------------------------"
-    source activate python3
-    python --version
-    conda install numpy scipy
-    python -m pip install graphviz pytest pytest-cov codecov
-    python -m pytest -v --fulltrace -s tests/python --cov=python-package/xgboost || exit -1
-    codecov
-
-    source activate python2
-    echo "-------------------------------"
-    python --version
-    conda install numpy scipy pytest
-    python -m pip install graphviz
-    python -m pip install flake8==3.4.1
-    python -m pytest -v --fulltrace -s tests/python || exit -1
 
     flake8 --ignore E501 python-package || exit -1
     flake8 --ignore E501 tests/python || exit -1
-    exit 0
-fi
-
-if [ ${TASK} == "r_test" ]; then
-    set -e
-
-    make Rpack
-    cd ./xgboost
-
-    # Install package deps
-    Rscript -e "install.packages( \
-        c('devtools', 'testthat', 'lintr') \
-        , repos = 'http://cloud.r-project.org' \
-        , dependencies = c('Depends', 'Imports', 'LinkingTo') \
-    )"
-
-    Rscript -e \
-        "devtools::install_deps( \
-            repos = 'http://cloud.r-project.org' \
-            , upgrade = 'never' \
-            , dependencies = c('Depends', 'Imports', 'LinkingTo') \
-        )"
-
-    # install suggested packages separately to avoid huge build times
-    Rscript -e "install.packages( \
-        c('DiagrammeR', 'Ckmeans.1d.dp', 'vcd') \
-        , repos = 'https://cloud.r-project.org' \
-        , dependencies = c('Depends', 'Imports', 'LinkingTo') \
-    )"
-
-    # Run tests
-    echo "Building with R CMD build"
-    R CMD build \
-        --no-build-vignettes \
-        --no-manual \
-        .
-
-    echo "Running R tests"
-    R_PACKAGE_TARBALL=$(ls -1t *.tar.gz | head -n 1)
-
-    export _R_CHECK_TIMINGS_=0
-    export _R_CHECK_FORCE_SUGGESTS_=false
-    R CMD check \
-        ${R_PACKAGE_TARBALL} \
-        --no-vignettes \
-        --no-manual \
-        --as-cran \
-        --install-args=--build
-
-    exit 0
 fi
 
 if [ ${TASK} == "java_test" ]; then
@@ -165,15 +56,6 @@ if [ ${TASK} == "cmake_test" ]; then
     cd ..
     rm -rf build
 fi
-
-if [ ${TASK} == "cpp_test" ]; then
-    set -e
-    make -f dmlc-core/scripts/packages.mk gtest
-    echo "TEST_COVER=1" >> config.mk
-    echo "GTEST_PATH="${CACHE_PREFIX} >> config.mk
-    make cover
-fi
-
 
 if [ ${TASK} == "distributed_test" ]; then
     set -e
--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ${TASK} == "lint" ]; then
+if [ ${TASK} == "python_test" ] || [ ${TASK} ==  "distributed_test" ]; then
     if [ ${TRAVIS_OS_NAME} == "osx" ]; then
         wget -O conda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     else
@@ -14,22 +14,4 @@ if [ ${TASK} == "lint" ]; then
     # Useful for debugging any issues with conda
     conda info -a
     conda create -n python3 python=3.7
-fi
-
-
-if [ ${TASK} == "python_test" ] || [ ${TASK} == "python_lightweight_test" ] || [ ${TASK} ==  "distributed_test" ]; then
-    if [ ${TRAVIS_OS_NAME} == "osx" ]; then
-        wget -O conda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-    else
-        wget -O conda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    fi
-    bash conda.sh -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
-    hash -r
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
-    # Useful for debugging any issues with conda
-    conda info -a
-    conda create -n python3 python=3.7
-    conda create -n python2 python=2.7
 fi
