8bfb6ec11dc69838e4976869c87bad560fd51357
Sriram Chandramouli <sriram_chandramouli@hotmail.com>
2019-05-24 10:37:01 -0700

    - incorporate review comment to make the macro into a function

--- tests/cpp/common/test_column_matrix.cc
+++ tests/cpp/common/test_column_matrix.cc
@@ -51,22 +51,22 @@ TEST(DenseColumnWithMissing, Test) {
   delete dmat;
 }
 
-TEST(HistIndexCreationWithExternalMemory, Test) {
-#define TEST_HIST_IDX_MATRIX(NTHREADS) \
-  { \
-    /* This should create multiple sparse pages */ \
-    std::unique_ptr<DMatrix> dmat = CreateSparsePageDMatrix(1024, 1024); \
-    omp_set_num_threads(NTHREADS); \
-    GHistIndexMatrix gmat; \
-    gmat.Init(dmat.get(), 256); \
-  }
+void
+TestGHistIndexMatrixCreation(size_t nthreads) {
+  /* This should create multiple sparse pages */
+  std::unique_ptr<DMatrix> dmat = CreateSparsePageDMatrix(1024, 1024);
+  omp_set_num_threads(nthreads);
+  GHistIndexMatrix gmat;
+  gmat.Init(dmat.get(), 256);
+}
 
+TEST(HistIndexCreationWithExternalMemory, Test) {
   // Vary the number of threads to make sure that the last batch
   // is distributed properly to the available number of threads
   // in the thread pool
-  TEST_HIST_IDX_MATRIX(20)
-  TEST_HIST_IDX_MATRIX(30)
-  TEST_HIST_IDX_MATRIX(40)
+  TestGHistIndexMatrixCreation(20);
+  TestGHistIndexMatrixCreation(30);
+  TestGHistIndexMatrixCreation(40);
 }
 }  // namespace common
 }  // namespace xgboost
