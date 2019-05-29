a060eb51ad2ab0f8cd78892d1e6fac02480ffa36
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-05-29 12:19:39 +0200

    use absolute path to nccl

--- get_cuda.sh
+++ get_cuda.sh
@@ -6,9 +6,10 @@ export CUDA_MINOR=`echo ${CUDA_VERSION} | cut -d "." -f 2| sed 's/@//g'` && \
 export CUDA_SHORT=`echo ${CUDA_VERSION} | egrep -o '[0-9]\.[0-9]'` && \
 echo ${CUDA_SHORT} > cuda_short.txt ; \
 fi
-
+# full directory name of the script no matter where it is being called from
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 export cuda=`cat cuda_short.txt`
 export arch=`arch`
-export NCCL='-DUSE_NCCL=ON -DNCCL_ROOT=../nccl/build'
+export NCCL='-DUSE_NCCL=ON -DNCCL_ROOT="'$DIR'/../nccl/build"'
 
 echo ${NCCL}
