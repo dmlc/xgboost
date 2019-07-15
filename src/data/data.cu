cb1883c5209559c60fdb59c24ae5015f15e51b6f
Matthew Jones <mjones@dfscholars.org>
2019-07-15 15:27:13 -0600

    fixing DivRoundUp namespace

--- src/data/data.cu
+++ src/data/data.cu
@@ -47,7 +47,7 @@ void MetaInfo::SetCUDFInfo(const char* key, gdf_column** cols, size_t n_cols) {
   bst_float* data = field->DevicePointer(0);
   for (size_t i = 0; i < n_cols; ++i) {
     int block = 256;
-    unpack_cudf_column_k<<<dh::DivRoundUp(n_rows, block), block>>>
+    unpack_cudf_column_k<<<common::DivRoundUp(n_rows, block), block>>>
       (data + i, n_rows, n_cols, *cols[i]);
     dh::safe_cuda(cudaGetLastError());
   }
