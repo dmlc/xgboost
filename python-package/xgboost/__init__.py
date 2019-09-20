4b6c2139109d67a3de908c0b4d21c580ff1780e3
fis <jm.yuan@outlook.com>
2019-09-20 06:10:07 -0400

    Import dask in init.

--- python-package/xgboost/__init__.py
+++ python-package/xgboost/__init__.py
@@ -13,6 +13,7 @@ from .training import train, cv
 from . import rabit                   # noqa
 from . import tracker  # noqa
 from .tracker import RabitTracker  # noqa
+from . import dask
 try:
     from .sklearn import XGBModel, XGBClassifier, XGBRegressor, XGBRanker
     from .sklearn import XGBRFClassifier, XGBRFRegressor
@@ -29,4 +30,4 @@ __all__ = ['DMatrix', 'Booster',
            'RabitTracker',
            'XGBModel', 'XGBClassifier', 'XGBRegressor', 'XGBRanker',
            'XGBRFClassifier', 'XGBRFRegressor',
-           'plot_importance', 'plot_tree', 'to_graphviz']
+           'plot_importance', 'plot_tree', 'to_graphviz', 'dask']
