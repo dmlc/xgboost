607eb9860737f95ba3a3b7dcc1f72fbe02db0985
Chen Qin <qinnchen@gmail.com>
2019-09-23 21:25:22 -0700

    Update XGBoostRegressor.scala

--- jvm-packages/xgboost4j-spark/src/main/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostRegressor.scala
+++ jvm-packages/xgboost4j-spark/src/main/scala/ml/dmlc/xgboost4j/scala/spark/XGBoostRegressor.scala
@@ -270,8 +270,7 @@ class XGBoostRegressionModel private[ml] (
           if (batchCnt == 0) {
             val rabitEnv = Array(
               "DMLC_TASK_ID" -> TaskContext.getPartitionId().toString,
-              "DMLC_WORKER_STOP_PROCESS_ON_ERROR" -> "false"
-            ).toMap
+              "DMLC_WORKER_STOP_PROCESS_ON_ERROR" -> "false").toMap
             Rabit.init(ApplyRabitParams(rabitEnv).asJava)
           }
 
