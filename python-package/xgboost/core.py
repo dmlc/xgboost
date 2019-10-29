e656dab8b34204121d368b34f39a7e35f7bddb6f
Chen Qin <qinnchen@gmail.com>
2019-10-29 13:48:17 -0700

    Update core.py

--- python-package/xgboost/core.py
+++ python-package/xgboost/core.py
@@ -214,7 +214,7 @@ def ctypes2numpy(cptr, length, dtype):
     if not isinstance(cptr, ctypes.POINTER(ctype)):
         raise RuntimeError('expected {} pointer'.format(ctype))
     res = np.zeros(length, dtype=dtype)
-    # pylint: disable= unsubscriptable-object
+
     if not ctypes.memmove(res.ctypes.data, cptr, length * res.strides[0]):
         raise RuntimeError('memmove failed')
     return res
