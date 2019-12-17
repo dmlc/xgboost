d0669b2e492813b9091554ac784df6a2d0c031a5
fis <jm.yuan@outlook.com>
2019-12-17 13:48:57 +0800

    Don't make unnecessary changes.

--- demo/guide-python/boost_from_prediction.py
+++ demo/guide-python/boost_from_prediction.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import numpy as np
 import xgboost as xgb
 
 dtrain = xgb.DMatrix('../data/agaricus.txt.train')
@@ -7,15 +8,13 @@ watchlist = [(dtest, 'eval'), (dtrain, 'train')]
 ###
 # advanced: start from a initial base prediction
 #
-print('start running example to start from a initial prediction')
+print ('start running example to start from a initial prediction')
 # specify parameters via map, definition are same as c++ version
-param = {'max_depth': 2, 'eta': 1, 'verbosity': 1,
-         'objective': 'binary:logistic'}
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
 # train xgboost for 1 round
 bst = xgb.train(param, dtrain, 1, watchlist)
-# Note: we need the margin value instead of transformed prediction in
-# set_base_margin do predict with output_margin=True, will always give
-# you margin values before logistic transformation
+# Note: we need the margin value instead of transformed prediction in set_base_margin
+# do predict with output_margin=True, will always give you margin values before logistic transformation
 ptrain = bst.predict(dtrain, output_margin=True)
 ptest = bst.predict(dtest, output_margin=True)
 dtrain.set_base_margin(ptrain)
