298ebe68ac2cc87382c4263f45d4020af660e2cf
Jiaming Yuan <jm.yuan@outlook.com>
2019-12-24 14:25:48 +0800

    [Breaking] Remove `learning_rates` in Python. (#5155)
    * Remove `learning_rates`.
    
    It's been deprecated since we have callback.
    
    * Set `before_iteration` of `reset_learning_rate` to False to preserve 
      the initial learning rate, and comply to the term "reset".
    
    Closes #4709.
    
    * Tests for various `tree_method`.

--- python-package/xgboost/callback.py
+++ python-package/xgboost/callback.py
@@ -134,14 +134,16 @@ def reset_learning_rate(learning_rates):
 
         if context == 'train':
             bst, i, n = env.model, env.iteration, env.end_iteration
-            bst.set_param('learning_rate', get_learning_rate(i, n, learning_rates))
+            bst.set_param(
+                'learning_rate', get_learning_rate(i, n, learning_rates))
         elif context == 'cv':
             i, n = env.iteration, env.end_iteration
             for cvpack in env.cvfolds:
                 bst = cvpack.bst
-                bst.set_param('learning_rate', get_learning_rate(i, n, learning_rates))
+                bst.set_param(
+                    'learning_rate', get_learning_rate(i, n, learning_rates))
 
-    callback.before_iteration = True
+    callback.before_iteration = False
     return callback
 
 
