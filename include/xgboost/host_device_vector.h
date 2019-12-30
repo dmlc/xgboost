deaf41c57a7ab8b3e72de1e7877d7514a91d0de9
fis <jm.yuan@outlook.com>
2019-12-30 20:36:56 +0800

    Bring back move assignment operator.

--- include/xgboost/host_device_vector.h
+++ include/xgboost/host_device_vector.h
@@ -93,6 +93,7 @@ class HostDeviceVector {
   HostDeviceVector(HostDeviceVector<T>&&);
 
   HostDeviceVector<T>& operator=(const HostDeviceVector<T>&) = delete;
+  HostDeviceVector<T>& operator=(HostDeviceVector<T>&&);
 
   size_t Size() const;
   int DeviceIdx() const;
--- src/common/host_device_vector.cc
+++ src/common/host_device_vector.cc
@@ -8,6 +8,7 @@
 #include <xgboost/base.h>
 #include <xgboost/data.h>
 #include <cstdint>
+#include <memory>
 #include <utility>
 #include "xgboost/host_device_vector.h"
 
@@ -51,6 +52,17 @@ HostDeviceVector<T>::HostDeviceVector(const std::vector<T>& init, int device)
 template <typename T>
 HostDeviceVector<T>::HostDeviceVector(HostDeviceVector<T>&& that) {
   impl_ = new HostDeviceVectorImpl<T>(std::move(*that.impl_));
+}
+
+template <typename T>
+HostDeviceVector<T>& HostDeviceVector<T>::operator=(HostDeviceVector<T>&& that) {
+  if (this == &that) { return *this; }
+
+  std::unique_ptr<HostDeviceVectorImpl<T>> new_impl(
+      new HostDeviceVectorImpl<T>(std::move(*that.impl_)));
+  delete impl_;
+  impl_ = new_impl.release();
+  return *this;
 }
 
 template <typename T>
