b5bef37db16fdf820d10be4de78f65b058fb00ae
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-01-24 00:41:54 -0800

    Remove dup atomicAdd

--- src/tree/updater_gpu_common.cuh
+++ src/tree/updater_gpu_common.cuh
@@ -12,27 +12,6 @@
 #include "../common/random.h"
 #include "param.h"
 
-#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 600
-
-#else  // In device code and CUDA < 600
-XGBOOST_DEVICE __forceinline__ double atomicAdd(double* address, double val) {
-  unsigned long long int* address_as_ull =
-      (unsigned long long int*)address;                   // NOLINT
-  unsigned long long int old = *address_as_ull, assumed;  // NOLINT
-
-  do {
-    assumed = old;
-    old = atomicCAS(address_as_ull, assumed,
-                    __double_as_longlong(val + __longlong_as_double(assumed)));
-
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN !=
-    // NaN)
-  } while (assumed != old);
-
-  return __longlong_as_double(old);
-}
-#endif
-
 namespace xgboost {
 namespace tree {
 
