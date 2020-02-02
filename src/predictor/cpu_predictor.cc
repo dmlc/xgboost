13a6da5df13c2295a1f92a8e4701579ba74fc3ff
Nan Zhu <nanzhu@uber.com>
2020-02-01 23:08:49 -0800

    try to compile

--- src/predictor/cpu_predictor.cc
+++ src/predictor/cpu_predictor.cc
@@ -179,8 +179,7 @@ class CPUPredictor : public Predictor {
     if (!generic_param_->adding_all_to_cache && cache_entry == cache_->cend()) {
       return;
     } else if (generic_param_->adding_all_to_cache) {
-      auto * new_cache_entry = new PredictionCacheEntry();
-      (*cache_)[dmat] = (*new_cache_entry);
+      (*cache_)[dmat].data = std::make_shared<DMatrix>(dmat);
     }
     if (cache_entry->second.predictions.Size() == 0) {
       // See comment in GPUPredictor::PredictBatch.
