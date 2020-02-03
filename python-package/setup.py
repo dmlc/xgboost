c74216f22cf4ee96833866ea833a430fb62846da
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2020-02-03 10:38:52 -0800

    Declare Python 3.8 support in setup.py (#5274)

--- python-package/setup.py
+++ python-package/setup.py
@@ -62,6 +62,7 @@ setup(name='xgboost',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7'],
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8'],
       python_requires='>=3.5',
       url='https://github.com/dmlc/xgboost')
