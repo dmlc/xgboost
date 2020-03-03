4ef1f8b2e5b9c6cbde0a740eec15b45b2ea12d9b
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2020-03-04 12:01:52 +1300

    mingw fix

--- src/data/ellpack_page_source.cc
+++ src/data/ellpack_page_source.cc
@@ -2,20 +2,23 @@
  * Copyright 2019 XGBoost contributors
  */
 #ifndef XGBOOST_USE_CUDA
+#include <dmlc/base.h>
+#if DMLC_ENABLE_STD_THREAD
 
-#include <xgboost/data.h>
 #include "ellpack_page_source.h"
+#include <xgboost/data.h>
 namespace xgboost {
 namespace data {
 
 EllpackPageSource::EllpackPageSource(DMatrix* dmat,
                                      const std::string& cache_info,
                                      const BatchParam& param) noexcept(false) {
-  LOG(FATAL) << "Internal Error: "
-                "XGBoost is not compiled with CUDA but EllpackPageSource is required";
+  LOG(FATAL)
+      << "Internal Error: "
+         "XGBoost is not compiled with CUDA but EllpackPageSource is required";
 }
 
 }  // namespace data
 }  // namespace xgboost
-
+#endif  // DMLC_ENABLE_STD_THREAD
 #endif  // XGBOOST_USE_CUDA
