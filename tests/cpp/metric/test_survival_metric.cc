d2c5c56e4a215efb684be8cfa579ac2d98357c5b
Philip Cho <chohyu01@cs.washington.edu>
2020-03-17 03:38:44 -0700

    Add a dummy test for AFT loss for uncensored case

--- tests/cpp/metric/test_survival_metric.cc
+++ tests/cpp/metric/test_survival_metric.cc
@@ -3,10 +3,13 @@
  */
 #include <gtest/gtest.h>
 #include <memory>
+#include <numeric>
+#include <cmath>
 
 #include "xgboost/metric.h"
 #include "xgboost/logging.h"
 #include "../helpers.h"
+#include "../../../src/common/survival_util.h"
 
 namespace xgboost {
 namespace common {
@@ -22,6 +25,27 @@ TEST(AFTMetric, Configuration) {
   auto aft_param_json = j_obj["aft_loss_param"];
   ASSERT_EQ(get<String>(aft_param_json["aft_loss_distribution"]), "normal");
   ASSERT_EQ(get<String>(aft_param_json["aft_loss_distribution_scale"]), "10");
+}
+
+TEST(AFTLoss, Uncensored) {
+  std::unique_ptr<AFTLoss> loss(new AFTLoss(AFTDistributionType::kNormal));
+
+  // Given true label 100, compute the AFT loss for various prediction values
+  const int num_point = 20;
+  const double true_label = 100.0;
+  std::vector<double> y_lower(num_point, true_label);
+  std::vector<double> y_higher(y_lower);
+  std::vector<double> y_pred(num_point);
+  std::vector<double> loss_val(num_point);
+
+  // Generate prediction value ranging from 2**1 to 2**15, using grid points in log scale
+  const double log_y_low = 1.0;
+  const double log_y_high = 15.0;
+  for (int i = 0; i < num_point; ++i) {
+    y_pred[i] = std::pow(2.0, i * (log_y_high - log_y_low) / (num_point - 1) + log_y_low);
+    loss_val[i] = loss->Loss(y_lower[i], y_higher[i], std::log(y_pred[i]), 1.0);
+    LOG(INFO) << y_pred[i] << ", " << loss_val[i];
+  }
 }
 
 } // namespace common
