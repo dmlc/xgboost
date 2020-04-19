67d9b52fa39b71d74f0c14555b6301b9d1619fbc
fis <jm.yuan@outlook.com>
2020-04-20 07:23:51 +0800

    Fix metainfo copy ctor.

--- include/xgboost/data.h
+++ include/xgboost/data.h
@@ -87,8 +87,15 @@ class MetaInfo {
 
     this->weights_.Resize(that.weights_.Size());
     this->weights_.Copy(that.weights_);
+
     this->base_margin_.Resize(that.base_margin_.Size());
     this->base_margin_.Copy(that.base_margin_);
+
+    this->labels_lower_bound_.Resize(that.labels_lower_bound_.Size());
+    this->labels_lower_bound_.Copy(that.labels_lower_bound_);
+
+    this->labels_upper_bound_.Resize(that.labels_upper_bound_.Size());
+    this->labels_upper_bound_.Copy(that.labels_upper_bound_);
     return *this;
   }
 
