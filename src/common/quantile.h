577054a086d20e09f635ae5953eea6e76b299064
fis <jm.yuan@outlook.com>
2020-06-21 18:37:15 +0800

    Move set combined back.

--- src/common/quantile.h
+++ src/common/quantile.h
@@ -241,7 +241,77 @@ struct WQSummary {
    * \param sa first input summary to be merged
    * \param sb second input summary to be merged
    */
-  void SetCombine(const WQSummary &sa, const WQSummary &sb);
+  void SetCombine(const WQSummary &sa, const WQSummary &sb) {
+    // this->DeviceSetCombined(sa, sb);
+    // return;
+
+    if (sa.size == 0) {
+      this->CopyFrom(sb);
+      return;
+    }
+    if (sb.size == 0) {
+      this->CopyFrom(sa);
+      return;
+    }
+    CHECK(sa.size > 0 && sb.size > 0);
+    const Entry *a = sa.data, *a_end = sa.data + sa.size;
+    const Entry *b = sb.data, *b_end = sb.data + sb.size;
+    // extended rmin value
+    RType aprev_rmin = 0, bprev_rmin = 0;
+    Entry *dst = this->data;
+    while (a != a_end && b != b_end) {
+      // duplicated value entry
+      if (a->value == b->value) {
+        *dst = Entry(a->rmin + b->rmin, a->rmax + b->rmax, a->wmin + b->wmin,
+                     a->value);
+        aprev_rmin = a->RMinNext();
+        bprev_rmin = b->RMinNext();
+        ++dst;
+        ++a;
+        ++b;
+      } else if (a->value < b->value) {
+        *dst = Entry(a->rmin + bprev_rmin, a->rmax + b->RMaxPrev(), a->wmin,
+                     a->value);
+        aprev_rmin = a->RMinNext();
+        ++dst;
+        ++a;
+      } else {
+        *dst = Entry(b->rmin + aprev_rmin, b->rmax + a->RMaxPrev(), b->wmin,
+                     b->value);
+        bprev_rmin = b->RMinNext();
+        ++dst;
+        ++b;
+      }
+    }
+    if (a != a_end) {
+      RType brmax = (b_end - 1)->rmax;
+      do {
+        *dst = Entry(a->rmin + bprev_rmin, a->rmax + brmax, a->wmin, a->value);
+        ++dst;
+        ++a;
+      } while (a != a_end);
+    }
+    if (b != b_end) {
+      RType armax = (a_end - 1)->rmax;
+      do {
+        *dst = Entry(b->rmin + aprev_rmin, b->rmax + armax, b->wmin, b->value);
+        ++dst;
+        ++b;
+      } while (b != b_end);
+    }
+
+    this->size = dst - data;
+
+    const RType tol = 10;
+    RType err_mingap, err_maxgap, err_wgap;
+    this->FixError(&err_mingap, &err_maxgap, &err_wgap);
+    if (err_mingap > tol || err_maxgap > tol || err_wgap > tol) {
+      LOG(INFO) << "mingap=" << err_mingap << ", maxgap=" << err_maxgap
+                << ", wgap=" << err_wgap;
+    }
+    CHECK(size <= sa.size + sb.size) << "bug in combine";
+  }
+
   // helper function to print the current content of sketch
   inline void Print() const {
     for (size_t i = 0; i < this->size; ++i) {
