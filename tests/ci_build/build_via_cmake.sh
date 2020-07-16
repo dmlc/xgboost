3088da4f494315adc2da4f9663e6d1d2b9e86241
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-07-15 19:36:17 -0700

    Add option to enable all compiler warnings in GCC/Clang

--- tests/ci_build/build_via_cmake.sh
+++ tests/ci_build/build_via_cmake.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf build
 mkdir build
 cd build
-cmake .. "$@" -DGOOGLE_TEST=ON -DUSE_DMLC_GTEST=ON -DCMAKE_VERBOSE_MAKEFILE=ON
+cmake .. "$@" -DGOOGLE_TEST=ON -DUSE_DMLC_GTEST=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DENABLE_ALL_WARNINGS=ON
 make clean
 make -j$(nproc)
 cd ..
