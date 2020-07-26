5a4592d10f26a5083b07e7a0e66a705913c18599
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-07-25 19:04:55 -0700

    Add a big warning about compatibility in QuantileHistMaker::LoadConfig()

--- src/tree/updater_quantile_hist.h
+++ src/tree/updater_quantile_hist.h
@@ -132,6 +132,11 @@ class QuantileHistMaker: public TreeUpdater {
       // We add this compatibility check because it's just recently that we (developers) began
       // persuade R users away from using saveRDS() for model serialization. Hopefully, one day,
       // everyone will beg using xgb.save().
+      LOG(WARNING) << "Attempted to load interal configuration for a model file that's generated "
+        << "by a previous version of XGBoost. A likely cause for this warning is that the model "
+        << "was saved with saveRDS() in R or pickle.dump() in Python. We strongly ADVISE AGAINST "
+        << "using saveRDS() or pickle.dump() so that the model remains accessible in current and "
+        << "upcoming XGBoost releases. Please use xgb.save() instead.";
       this->hist_maker_param_.UpdateAllowUnknown(Args{});
     }
   }
