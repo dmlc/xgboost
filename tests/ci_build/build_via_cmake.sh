2ee04b3eac05cf26af1942bd0e78d9fd009d8268
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2020-08-07 19:35:29 -0700

    Set CMake prefix path to use Conda env

--- tests/ci_build/build_via_cmake.sh
+++ tests/ci_build/build_via_cmake.sh
@@ -8,14 +8,16 @@ then
   shift 1
   cmake_args="$@"
   source activate ${conda_env}
+  cmake_prefix_flag="-DCMAKE_PREFIX_PATH=$CONDA_PREFIX"
 else
   cmake_args="$@"
+  cmake_prefix_flag=''
 fi
 
 rm -rf build
 mkdir build
 cd build
-cmake .. ${cmake_args} -DGOOGLE_TEST=ON -DUSE_DMLC_GTEST=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DENABLE_ALL_WARNINGS=ON -GNinja
+cmake .. ${cmake_args} -DGOOGLE_TEST=ON -DUSE_DMLC_GTEST=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DENABLE_ALL_WARNINGS=ON -GNinja ${cmake_prefix_flag}
 ninja clean
 time ninja -v
 cd ..
