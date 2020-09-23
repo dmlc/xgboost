32bfe718e3b2044b75f58ee276e22a499e6d9d86
fis <jm.yuan@outlook.com>
2020-09-23 23:16:42 +0800

    Add DaskDeviceQuantileDMatrix demo.

--- tests/python-gpu/test_gpu_demos.py
+++ tests/python-gpu/test_gpu_demos.py
@@ -6,8 +6,21 @@ sys.path.append("tests/python")
 import testing as tm
 import test_demos as td         # noqa
 
+
 @pytest.mark.skipif(**tm.no_cupy())
 def test_data_iterator():
     script = os.path.join(td.PYTHON_DEMO_DIR, 'data_iterator.py')
     cmd = ['python', script]
+    subprocess.check_call(cmd)
+
+
+@pytest.mark.skipif(**tm.no_dask())
+@pytest.mark.skipif(**tm.no_dask_cuda())
+@pytest.mark.skipif(**tm.no_cupy())
+def test_dask_training():
+    script = os.path.join(tm.PROJECT_ROOT, 'demo', 'dask', 'gpu_training.py')
+    cmd = ['python', script, '--ddqdm=1']
+    subprocess.check_call(cmd)
+
+    cmd = ['python', script, '--ddqdm=0']
     subprocess.check_call(cmd)
