f83de4894c4418d1056c1d513a855cb9883892a6
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-09-24 14:03:51 -0700

    Add back support for scipy.sparse.coo_matrix

--- tests/python/test_dmatrix.py
+++ tests/python/test_dmatrix.py
@@ -76,6 +76,15 @@ class TestDMatrix(unittest.TestCase):
         assert dtrain.num_row() == 3
         assert dtrain.num_col() == 3
 
+    def test_coo(self):
+        row = np.array([0, 2, 2, 0, 1, 2])
+        col = np.array([0, 0, 1, 2, 2, 2])
+        data = np.array([1, 2, 3, 4, 5, 6])
+        X = scipy.sparse.coo_matrix((data, (row, col)), shape=(3, 3))
+        dtrain = xgb.DMatrix(X)
+        assert dtrain.num_row() == 3
+        assert dtrain.num_col() == 3
+
     def test_np_view(self):
         # Sliced Float32 array
         y = np.array([12, 34, 56], np.float32)[::2]
