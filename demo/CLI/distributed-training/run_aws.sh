a64fc149ca6b4a91380353b86f2f5e869ad5fb32
fis <jm.yuan@outlook.com>
2020-10-19 20:16:08 +0800

    fix aws demo.

--- demo/CLI/distributed-training/run_aws.sh
+++ demo/CLI/distributed-training/run_aws.sh
@@ -4,8 +4,8 @@
 export BUCKET=mybucket
 
 # submit the job to YARN
-../../dmlc-core/tracker/dmlc-submit --cluster=yarn --num-workers=2 --worker-cores=2\
-    ../../xgboost mushroom.aws.conf nthread=2\
-    data=s3://${BUCKET}/xgb-demo/train\
-    eval[test]=s3://${BUCKET}/xgb-demo/test\
-    model_dir=s3://${BUCKET}/xgb-demo/model
+../../../dmlc-core/tracker/dmlc-submit --cluster=yarn --num-workers=2 --worker-cores=2\
+				       ../../../xgboost mushroom.aws.conf nthread=2\
+				       data=s3://${BUCKET}/xgb-demo/train\
+				       eval[test]=s3://${BUCKET}/xgb-demo/test\
+				       model_dir=s3://${BUCKET}/xgb-demo/model
