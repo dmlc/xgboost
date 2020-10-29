5761cb7f5e982d42e5f5fd19bf1184351c185367
fis <jm.yuan@outlook.com>
2020-10-29 20:36:10 +0800

    Move to cc.

--- src/data/sparse_page_source.cc
+++ src/data/sparse_page_source.cc
@@ -76,5 +76,49 @@ void SplitWritePage(std::shared_ptr<SparsePage> page, size_t page_size,
   }
   CHECK_EQ(rows_written, total);
 }
+
+void DataPool::Slice(std::shared_ptr<SparsePage> out, size_t offset,
+                     size_t n_rows, size_t entry_offset) const {
+  auto const &in_offset = pool_.offset.HostVector();
+  auto const &in_data = pool_.data.HostVector();
+  auto &h_offset = out->offset.HostVector();
+  CHECK_LE(offset + n_rows + 1, in_offset.size());
+  h_offset.resize(n_rows + 1, 0);
+  std::transform(in_offset.cbegin() + offset,
+                 in_offset.cbegin() + offset + n_rows + 1, h_offset.begin(),
+                 [=](size_t ptr) { return ptr - entry_offset; });
+
+  auto &h_data = out->data.HostVector();
+  CHECK_GT(h_offset.size(), 0);
+  size_t n_entries = h_offset.back();
+  h_data.resize(n_entries);
+
+  CHECK_EQ(n_entries, in_offset.at(offset + n_rows) - in_offset.at(offset));
+  std::copy_n(in_data.cbegin() + in_offset.at(offset), n_entries,
+              h_data.begin());
+}
+
+void DataPool::SplitWritePage() {
+  size_t total = pool_.Size();
+  size_t offset = 0;
+  size_t entry_offset = 0;
+  do {
+    size_t n_rows = std::min(page_size_, total - offset);
+    std::shared_ptr<SparsePage> out;
+    writer_->Alloc(&out);
+    out->Clear();
+    out->SetBaseRowId(inferred_num_rows_);
+    this->Slice(out, offset, n_rows, entry_offset);
+    inferred_num_rows_ += out->Size();
+    offset += n_rows;
+    entry_offset += out->data.Size();
+    writer_->PushWrite(std::move(out));
+  } while (total - offset >= page_size_);
+
+  auto out = std::make_shared<SparsePage>();
+  this->Slice(out, offset, total - offset, entry_offset);
+  pool_.Clear();
+  pool_.Push(*out);
+}
 }  // namespace data
 }  // namespace xgboost
--- src/data/sparse_page_source.h
+++ src/data/sparse_page_source.h
@@ -240,48 +240,8 @@ class DataPool {
   SparsePageWriter<SparsePage> *writer_;
 
   void Slice(std::shared_ptr<SparsePage> out, size_t offset, size_t n_rows,
-             size_t entry_offset) const {
-    auto const &in_offset = pool_.offset.HostVector();
-    auto const &in_data = pool_.data.HostVector();
-    auto &h_offset = out->offset.HostVector();
-    CHECK_LE(offset + n_rows + 1, in_offset.size());
-    h_offset.resize(n_rows + 1, 0);
-    std::transform(in_offset.cbegin() + offset,
-                   in_offset.cbegin() + offset + n_rows + 1, h_offset.begin(),
-                   [=](size_t ptr) { return ptr - entry_offset; });
-
-    auto &h_data = out->data.HostVector();
-    CHECK_GT(h_offset.size(), 0);
-    size_t n_entries = h_offset.back();
-    h_data.resize(n_entries);
-
-    CHECK_EQ(n_entries, in_offset.at(offset + n_rows) - in_offset.at(offset));
-    std::copy_n(in_data.cbegin() + in_offset.at(offset), n_entries,
-                h_data.begin());
-  }
-
-  void SplitWritePage() {
-    size_t total = pool_.Size();
-    size_t offset = 0;
-    size_t entry_offset = 0;
-    do {
-      size_t n_rows = std::min(page_size_, total - offset);
-      std::shared_ptr<SparsePage> out;
-      writer_->Alloc(&out);
-      out->Clear();
-      out->SetBaseRowId(inferred_num_rows_);
-      this->Slice(out, offset, n_rows, entry_offset);
-      inferred_num_rows_ += out->Size();
-      offset += n_rows;
-      entry_offset += out->data.Size();
-      writer_->PushWrite(std::move(out));
-    } while(total - offset >= page_size_);
-
-    auto out = std::make_shared<SparsePage>();
-    this->Slice(out, offset, total - offset, entry_offset);
-    pool_.Clear();
-    pool_.Push(*out);
-  }
+             size_t entry_offset) const;
+  void SplitWritePage();
 
  public:
   DataPool(MetaInfo *info, size_t page_size,
@@ -290,6 +250,7 @@ class DataPool {
         page_size_{page_size}, writer_{writer} {}
 
   void Push(std::shared_ptr<SparsePage> page) {
+    info_->num_nonzero_ += page->data.Size();
     pool_.Push(*page);
     if (pool_.Size() > page_size_) {
       this->SplitWritePage();
