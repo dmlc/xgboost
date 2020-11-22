1c3f49ed803eb8ffa34084a34338d9ce845c5412
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-11-22 05:30:28 -0500

    [R] Fix R package installation via CMake

--- cmake/RPackageInstallTargetSetup.cmake
+++ cmake/RPackageInstallTargetSetup.cmake
@@ -6,8 +6,8 @@ function(setup_rpackage_install_target rlib_target build_dir)
   install(
     DIRECTORY "${xgboost_SOURCE_DIR}/R-package"
     DESTINATION "${build_dir}"
-    REGEX "src/*" EXCLUDE
-    REGEX "R-package/configure" EXCLUDE
+    PATTERN "src/*" EXCLUDE
+    PATTERN "R-package/configure" EXCLUDE
   )
   install(TARGETS ${rlib_target}
     LIBRARY DESTINATION "${build_dir}/R-package/src/"
