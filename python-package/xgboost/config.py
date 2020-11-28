0ea7e0120b0fa8f199bcbe172972a8e75be50e55
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-11-28 12:16:10 -0800

    Document behavior of set_config w.r.t Dask

--- python-package/xgboost/config.py
+++ python-package/xgboost/config.py
@@ -2,22 +2,57 @@
 import ctypes
 import json
 from contextlib import contextmanager
+from functools import wraps
 
 from .core import _LIB, _check_call, c_str, py_str
 
 
-def set_config(**new_config):
-    """Set global configuration, collection of parameters that can be applied in the global scope.
-    See https://xgboost.readthedocs.io/en/latest/parameter.html for the full list of parameters
-    supported in the global configuration.
-
-    .. versionadded:: 1.3.0
+def config_doc(*, header=None, extra_note=None, parameters=None, returns=None, see_also=None):
+    """Decorator to format docstring for config functions.
 
     Parameters
     ----------
-    new_config: Dict[str, Any]
-        Keyword arguments representing the parameters and their values
+    header: str
+        An introducion to the function
+    extra_note: str
+        Additional notes
+    parameters: str
+        Parameters of the function
+    returns: str
+        Return value
+    see_also: str
+        Related functions
+    """
 
+    doc_template = """
+    {header}
+    Global configuration consists of a collection of parameters that can be applied in the global
+    scope. See https://xgboost.readthedocs.io/en/latest/parameter.html for the full list of
+    parameters supported in the global configuration.
+
+    .. note:: Setting global configuration in a Dask cluster
+
+        By default, the global configuration of the scheduler process is not shared with the worker
+        processes:
+
+        .. code:: python
+
+            xgb.set_config(verbosity=0)  # has no effect on Dask worker processes
+
+        To set global configuration for worker processes, make sure to call ``set_config()`` with
+        ``client.run()``:
+
+        .. code:: python
+
+            with Client(cluster) as client:
+                # Set verbosity config in worker processes
+                client.run(xgb.set_config, verbosity=0)
+    {extra_note}
+
+    .. versionadded:: 1.3.0
+    """
+
+    common_example = """
     Example
     -------
 
@@ -25,28 +60,69 @@ def set_config(**new_config):
 
         import xgboost as xgb
 
-        # Silence all messages
-        xgb.set_config(verbosity=0)
+        # Show all messages, including ones pertaining to debugging
+        xgb.set_config(verbosity=2)
+
+        # Get current value of global configuration
+        # This is a dict containing all parameters in the global configuration,
+        # including 'verbosity'
+        config = xgb.get_config()
+        assert config['verbosity'] == 2
+
+        # Example of using the context manager xgb.config_context().
+        # The context manager will restore the previous value of the global
+        # configuration upon exiting.
+        with xgb.config_context(verbosity=0):
+            # Suppress warning caused by model generated with XGBoost version < 1.0.0
+            bst = xgb.Booster(model_file='./old_model.bin')
+        assert xgb.get_config()['verbosity'] == 2  # old value restored
     """
+    config_doc.header = header
+    config_doc.extra_note = extra_note
+    config_doc.parameters = parameters
+    config_doc.returns = returns
+    config_doc.see_also = see_also
+
+    def config_doc_decorator(func):
+        header = '' if config_doc.header is None else config_doc.header
+        extra_note = '' if config_doc.extra_note is None else config_doc.extra_note
+        parameters = '' if config_doc.parameters is None else config_doc.parameters
+        returns = '' if config_doc.returns is None else config_doc.returns
+        see_also = '' if config_doc.see_also is None else config_doc.see_also
+        func.__doc__ = (doc_template.format(header=header, extra_note=extra_note)
+                        + parameters + returns + common_example + see_also)
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrap
+    return config_doc_decorator
+
+
+@config_doc(header="""
+    Set global configuration.
+    """,
+            parameters="""
+    Parameters
+    ----------
+    new_config: Dict[str, Any]
+        Keyword arguments representing the parameters and their values
+            """)
+def set_config(**new_config):
     config_str = json.dumps(new_config)
 
     _check_call(_LIB.XGBSetGlobalConfig(c_str(config_str)))
 
 
-def get_config():
-    """
-    Get current values of the global configuration. Global configuration consists of a collection
-    of parameters that can be applied in the global scope.
-    See https://xgboost.readthedocs.io/en/latest/parameter.html for the full list of parameters
-    supported in the global configuration.
-
-    .. versionadded:: 1.3.0
-
+@config_doc(header="""
+    Get current values of the global configuration.
+    """,
+            returns="""
     Returns
     -------
     args: Dict[str, Any]
         The list of global parameters and their values
-    """
+            """)
+def get_config():
     config_str = ctypes.c_char_p()
     _check_call(_LIB.XGBGetGlobalConfig(ctypes.byref(config_str)))
     config = json.loads(py_str(config_str.value))
@@ -55,41 +131,28 @@ def get_config():
 
 
 @contextmanager
-def config_context(**new_config):
-    """
-    Context manager for global XGBoost configuration. Global configuration consists of a collection
-    of parameters that can be applied in the global scope. See
-    https://xgboost.readthedocs.io/en/latest/parameter.html for the full list of parameters
-    supported in the global configuration.
-
-    .. note::
-
-        All settings, not just those presently modified, will be returned to their previous values
-        when the context manager is exited. This is not thread-safe.
-
-    .. versionadded:: 1.3.0
-
+@config_doc(header="""
+    Context manager for global XGBoost configuration.
+    """,
+            parameters="""
     Parameters
     ----------
     new_config: Dict[str, str]
         Keyword arguments representing the parameters and their values
+            """,
+            extra_note="""
+    .. note::
 
-    Example
-    -------
-
-    .. code-block:: python
-
-        import xgboost as xgb
-
-        # Suppress warning caused by model generated with XGBoost version < 1.0.0
-        with xgb.config_context(verbosity=0):
-            bst = xgb.Booster(model_file='./old_model.bin')
-
+        All settings, not just those presently modified, will be returned to their previous values
+        when the context manager is exited. This is not thread-safe.
+            """,
+            see_also="""
     See Also
     --------
     set_config: Set global XGBoost configuration
     get_config: Get current values of the global configuration
-    """
+            """)
+def config_context(**new_config):
     old_config = get_config().copy()
     set_config(**new_config)
 
