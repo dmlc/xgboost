25afaa4da8a540c792df319e5ddb473bbae9137b
fis <jm.yuan@outlook.com>
2020-12-01 20:56:14 +0800

    Test dask config.

--- tests/python/test_config.py
+++ tests/python/test_config.py
@@ -14,22 +14,3 @@ def test_global_config_verbosity(verbosity_level):
         new_verbosity = get_current_verbosity()
         assert new_verbosity == verbosity_level
     assert old_verbosity == get_current_verbosity()
-
-
-@pytest.mark.skipif(**tm.no_dask())
-def test_global_config_with_dask():
-    from distributed import Client, LocalCluster
-
-    xgb.set_config(verbosity=0)
-    config = xgb.get_config()
-    assert config['verbosity'] == 0
-    with LocalCluster(n_workers=4) as cluster:
-        with Client(cluster) as client:
-            # By default, the global configuration of the scheduler process is not shared with the
-            # the worker processes.
-            for config in client.run(xgb.get_config).values():
-                assert config['verbosity'] != 0
-            # Use client.run() to configure global configuration for the worker processes:
-            client.run(xgb.set_config, verbosity=0)
-            for config in client.run(xgb.get_config).values():
-                assert config['verbosity'] == 0
