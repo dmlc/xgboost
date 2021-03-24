f3fe1d919564b6b63aca93a224a1dcef35644c9a
fis <jm.yuan@outlook.com>
2021-03-24 11:06:44 +0800

    Check.

--- src/data/array_interface.cu
+++ src/data/array_interface.cu
@@ -6,6 +6,16 @@
 
 namespace xgboost {
 void ArrayInterfaceHandler::SyncCudaStream(int64_t stream) {
-  dh::safe_cuda(cudaStreamSynchronize(reinterpret_cast<cudaStream_t>(stream)));
+  switch (stream) {
+  case 0:
+    LOG(FATAL) << "Invalid stream ID in array interface: " << stream;
+  case 1:
+    // default legacy stream
+    break;
+  case 2:
+    // default per-thread stream
+  default:
+    dh::safe_cuda(cudaStreamSynchronize(reinterpret_cast<cudaStream_t>(stream)));
+  }
 }
 }  // namespace xgboost
