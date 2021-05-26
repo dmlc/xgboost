aa4c0603483740c7150914883718344c6db35762
fis <jm.yuan@outlook.com>
2021-05-26 17:05:37 +0800

    Add `enable_categorical` to sklearn.

--- python-package/xgboost/sklearn.py
+++ python-package/xgboost/sklearn.py
@@ -164,6 +164,14 @@ __model_doc = f'''
     validate_parameters : Optional[bool]
         Give warnings for unknown parameter.
 
+    enable_categorical : bool
+
+        .. versionadded:: 1.5.0
+
+        Experimental support for categorical data.  Do not set to true unless you are
+        interested in development. Only valid when `gpu_hist` and pandas dataframe are
+        used.
+
     kwargs : dict, optional
         Keyword arguments for XGBoost Booster object.  Full documentation of
         parameters can be found here:
@@ -257,6 +265,7 @@ def _wrap_evaluation_matrices(
     eval_group: Optional[List[Any]],
     eval_qid: Optional[List[Any]],
     create_dmatrix: Callable,
+    enable_categorical: bool,
     label_transform: Callable = lambda x: x,
 ) -> Tuple[Any, Optional[List[Tuple[Any, str]]]]:
     """Convert array_like evaluation matrices into DMatrix.  Perform validation on the way.
@@ -271,6 +280,7 @@ def _wrap_evaluation_matrices(
         base_margin=base_margin,
         feature_weights=feature_weights,
         missing=missing,
+        enable_categorical=enable_categorical,
     )
 
     n_validation = 0 if eval_set is None else len(eval_set)
@@ -317,6 +327,7 @@ def _wrap_evaluation_matrices(
                     qid=eval_qid[i],
                     base_margin=base_margin_eval_set[i],
                     missing=missing,
+                    enable_categorical=enable_categorical,
                 )
                 evals.append(m)
         nevals = len(evals)
@@ -375,6 +386,7 @@ class XGBModel(XGBModelBase):
         gpu_id: Optional[int] = None,
         validate_parameters: Optional[bool] = None,
         predictor: Optional[str] = None,
+        enable_categorical: bool = False,
         **kwargs: Any
     ) -> None:
         if not SKLEARN_INSTALLED:
@@ -411,6 +423,7 @@ class XGBModel(XGBModelBase):
         self.gpu_id = gpu_id
         self.validate_parameters = validate_parameters
         self.predictor = predictor
+        self.enable_categorical = enable_categorical
 
     def _more_tags(self) -> Dict[str, bool]:
         '''Tags used for scikit-learn data validation.'''
@@ -514,7 +527,9 @@ class XGBModel(XGBModelBase):
         params = self.get_params()
         # Parameters that should not go into native learner.
         wrapper_specific = {
-            'importance_type', 'kwargs', 'missing', 'n_estimators', 'use_label_encoder'}
+            'importance_type', 'kwargs', 'missing', 'n_estimators', 'use_label_encoder',
+            "enable_categorical"
+        }
         filtered = dict()
         for k, v in params.items():
             if k not in wrapper_specific and not callable(v):
@@ -735,6 +750,7 @@ class XGBModel(XGBModelBase):
             eval_group=None,
             eval_qid=None,
             create_dmatrix=lambda **kwargs: DMatrix(nthread=self.n_jobs, **kwargs),
+            enable_categorical=self.enable_categorical,
         )
         params = self.get_xgb_params()
 
@@ -1202,6 +1218,7 @@ class XGBClassifier(XGBModel, XGBClassifierBase):
             eval_group=None,
             eval_qid=None,
             create_dmatrix=lambda **kwargs: DMatrix(nthread=self.n_jobs, **kwargs),
+            enable_categorical=self.enable_categorical,
             label_transform=label_transform,
         )
 
@@ -1628,6 +1645,7 @@ class XGBRanker(XGBModel, XGBRankerMixIn):
             eval_group=eval_group,
             eval_qid=eval_qid,
             create_dmatrix=lambda **kwargs: DMatrix(nthread=self.n_jobs, **kwargs),
+            enable_categorical=self.enable_categorical,
         )
 
         evals_result: TrainingCallback.EvalsLog = {}
