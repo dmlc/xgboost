9710507b04e446549e72c0505d2d921b189a7ec5
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-06-02 10:52:38 -0700

    Use black for formatting

--- demo/dask/callbacks.py
+++ demo/dask/callbacks.py
@@ -17,8 +17,9 @@ def probability_for_going_backward(epoch):
 # All callback functions must inherit from TrainingCallback
 class CustomEarlyStopping(xgb.callback.TrainingCallback):
     """A custom early stopping class where early stopping is determined stochastically.
-       In the beginning, allow the metric to become worse with a probability of 0.999.
-       As boosting progresses, the probability should be adjusted downward"""
+    In the beginning, allow the metric to become worse with a probability of 0.999.
+    As boosting progresses, the probability should be adjusted downward"""
+
     def __init__(self, *, validation_set, target_metric, maximize, seed):
         self.validation_set = validation_set
         self.target_metric = target_metric
@@ -32,16 +33,22 @@ class CustomEarlyStopping(xgb.callback.TrainingCallback):
 
     def after_iteration(self, model, epoch, evals_log):
         metric_history = evals_log[self.validation_set][self.target_metric]
-        if len(metric_history) < 2 or self.better(metric_history[-1], metric_history[-2]):
+        if len(metric_history) < 2 or self.better(
+            metric_history[-1], metric_history[-2]
+        ):
             return False  # continue training
         p = probability_for_going_backward(epoch)
-        go_backward = self.rng.choice(2, size=(1,), replace=True, p=[1 - p, p]).astype(np.bool)[0]
-        print('The validation metric went into the wrong direction. '
-              + f'Stopping training with probability {1 - p}...')
+        go_backward = self.rng.choice(2, size=(1,), replace=True, p=[1 - p, p]).astype(
+            np.bool
+        )[0]
+        print(
+            "The validation metric went into the wrong direction. "
+            + f"Stopping training with probability {1 - p}..."
+        )
         if go_backward:
             return False  # continue training
         else:
-            return True   # stop training
+            return True  # stop training
 
 
 def main(client):
@@ -53,24 +60,28 @@ def main(client):
     dtrain = DaskDMatrix(client, X_train, y_train)
     dtest = DaskDMatrix(client, X_test, y_test)
 
-    output = xgb.dask.train(client,
-                            {'verbosity': 1,
-                             'tree_method': 'hist',
-                             'objective': 'reg:squarederror',
-                             'eval_metric': 'rmse',
-                             'max_depth': 6,
-                             'learning_rate': 1.0},
-                            dtrain,
-                            num_boost_round=1000,
-                            evals=[(dtrain, 'train'), (dtest, 'test')],
-                            callbacks=[CustomEarlyStopping(
-                                validation_set='test',
-                                target_metric='rmse',
-                                maximize=False,
-                                seed=0)])
+    output = xgb.dask.train(
+        client,
+        {
+            "verbosity": 1,
+            "tree_method": "hist",
+            "objective": "reg:squarederror",
+            "eval_metric": "rmse",
+            "max_depth": 6,
+            "learning_rate": 1.0,
+        },
+        dtrain,
+        num_boost_round=1000,
+        evals=[(dtrain, "train"), (dtest, "test")],
+        callbacks=[
+            CustomEarlyStopping(
+                validation_set="test", target_metric="rmse", maximize=False, seed=0
+            )
+        ],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # or use other clusters for scaling
     with LocalCluster(n_workers=4, threads_per_worker=1) as cluster:
         with Client(cluster) as client:
