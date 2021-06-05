21bb93d2c2cb95cd8fd09f69568e3299b5e12665
fis <jm.yuan@outlook.com>
2021-06-05 20:29:10 +0800

    Lint.

--- src/predictor/predict_fn.h
+++ src/predictor/predict_fn.h
@@ -3,27 +3,29 @@
  */
 #ifndef XGBOOST_PREDICTOR_PREDICT_FN_H_
 #define XGBOOST_PREDICTOR_PREDICT_FN_H_
-#include "xgboost/tree_model.h"
 #include "../common/categorical.h"
+#include "xgboost/tree_model.h"
 
 namespace xgboost {
 namespace predictor {
 template <bool has_missing, bool has_categorical>
-inline XGBOOST_DEVICE bst_node_t GetNextNode(
-    const RegTree::Node& node, const bst_node_t nid, float fvalue,
-    bool is_missing, RegTree::CategoricalSplitMatrix const& cats) {
+inline XGBOOST_DEVICE bst_node_t
+GetNextNode(const RegTree::Node &node, const bst_node_t nid, float fvalue,
+            bool is_missing, RegTree::CategoricalSplitMatrix const &cats) {
   if (has_missing && is_missing) {
     return node.DefaultChild();
   } else {
     if (has_categorical && common::IsCat(cats.split_type, nid)) {
       auto node_categories = cats.categories.subspan(cats.node_ptr[nid].beg,
                                                      cats.node_ptr[nid].size);
-      return Decision(node_categories, common::AsCat(fvalue)) ? node.LeftChild() : node.RightChild();
+      return Decision(node_categories, common::AsCat(fvalue))
+                 ? node.LeftChild()
+                 : node.RightChild();
     } else {
       return node.LeftChild() + !(fvalue < node.SplitCond());
     }
   }
 }
-}  // namespace predictor
-}  // namespace xgboost
+}      // namespace predictor
+}      // namespace xgboost
 #endif  // XGBOOST_PREDICTOR_PREDICT_FN_H_
