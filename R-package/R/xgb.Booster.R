35a43be868ce643e5a5581403c7ec3ad0ba1d819
fis <jm.yuan@outlook.com>
2021-06-07 20:32:23 +0800

    Fix index.

--- R-package/R/xgb.Booster.R
+++ R-package/R/xgb.Booster.R
@@ -356,7 +356,7 @@ predict.xgb.Booster <- function(object, newdata, missing = NA, outputmargin = FA
     if (is.null(object$best_iteration)) {
       iterationrange <- c(0, 0)
     } else {
-      iterationrange <- c(0, as.integer(object$best_iteration) + 1)
+      iterationrange <- c(0, as.integer(object$best_iteration))
     }
   }
   ## Handle the 0 length values.
