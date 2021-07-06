f937f514aafdc05515355e35158532d7e593ae74
Jiaming Yuan <jm.yuan@outlook.com>
2021-07-06 14:46:43 +0800

    Remove lz4 compression with external memory. (#7076)

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://travis-ci.community/t/macos-build-fails-because-of-homebrew-bundle-unknown-command/7296/27
-brew install cmake libomp lz4
+brew install cmake libomp
 
 
 if [ ${TASK} == "python_test" ] || [ ${TASK} == "python_sdist_test" ]; then
