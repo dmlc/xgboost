983f2d1cb9bf30716d7687fafb3e9b76b4085f52
naveenkb <veenkb234@gmail.com>
2021-07-08 19:42:36 +0530

    Expose bestIteration and bestScore attributes

--- jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoost.java
+++ jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoost.java
@@ -242,11 +242,15 @@ public class XGBoost {
           if (score > bestScore) {
             bestScore = score;
             bestIteration = iter;
+            booster.setAttr("bestIteration", String.valueOf(bestIteration));
+            booster.setAttr("bestScore", String.valueOf(bestScore));
           }
         } else {
           if (score < bestScore) {
             bestScore = score;
             bestIteration = iter;
+            booster.setAttr("bestIteration", String.valueOf(bestIteration));
+            booster.setAttr("bestScore", String.valueOf(bestScore));
           }
         }
         if (earlyStoppingRounds > 0) {
