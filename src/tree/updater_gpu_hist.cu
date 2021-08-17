441c115c6e7a9d28ffab6cb1f2edc11db2ce5792
fis <jm.yuan@outlook.com>
2021-08-18 03:51:10 +0800

    Revert "Mixed f32 and f64."
    This reverts commit 8f45847d0a9b47d12f88d8f7fcb4d1c02bd6ba4d.

--- src/tree/updater_gpu_hist.cu
+++ src/tree/updater_gpu_hist.cu
@@ -230,9 +230,9 @@ struct GPUHistMakerDevice {
     // feature_groups.reset(new FeatureGroups(
     //     page->Cuts(), page->is_dense, dh::MaxSharedMemoryOptin(device_id),
     //     sizeof(GradientSumT)));
-    feature_groups.reset(new FeatureGroups(page->Cuts(), page->is_dense,
-                                           dh::MaxSharedMemoryOptin(device_id),
-                                           sizeof(GradientPair)));
+    feature_groups.reset(new FeatureGroups(
+        page->Cuts(), page->is_dense, dh::MaxSharedMemoryOptin(device_id),
+        sizeof(GradientPairInt32)));
   }
 
   ~GPUHistMakerDevice() {  // NOLINT
