6272a721526bd7a60f75147cc116694d33d730bf
nicovdijk <nicovdijk@gmail.com>
2021-10-15 09:58:50 +0200

    scalastyle changes

--- jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/PersistenceSuite.scala
+++ jvm-packages/xgboost4j-spark/src/test/scala/ml/dmlc/xgboost4j/scala/spark/PersistenceSuite.scala
@@ -140,7 +140,7 @@ class PersistenceSuite extends FunSuite with TmpFolderPerSuite with PerTest {
     val paramMap = Map("eta" -> "0.1", "max_depth" -> "6", "silent" -> "1",
       "custom_eval" -> new EvalError, "custom_obj" -> new CustomObj(1),
       "num_round" -> "10", "num_workers" -> numWorkers)
-    
+
     val xgbc = new XGBoostClassifier(paramMap)
     val xgbcPath = new File(tempDir.toFile, "xgbc").getPath
     xgbc.write.overwrite().save(xgbcPath)
