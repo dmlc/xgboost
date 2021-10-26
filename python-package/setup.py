0e2f2e4bb4119bf3b127d18f163c82407727dd97
fis <jm.yuan@outlook.com>
2021-10-27 02:36:16 +0800

    Require Python 3.7.
    * Update setup.py.
    
    All our current tests are running on 3.7+.

--- python-package/setup.py
+++ python-package/setup.py
@@ -360,12 +360,11 @@ if __name__ == '__main__':
                        'Operating System :: OS Independent',
                        'Programming Language :: Python',
                        'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
                        'Programming Language :: Python :: 3.8',
                        'Programming Language :: Python :: 3.9',
                        'Programming Language :: Python :: 3.10'],
-          python_requires='>=3.6',
+          python_requires=">=3.7",
           url='https://github.com/dmlc/xgboost')
 
     clean_up()
