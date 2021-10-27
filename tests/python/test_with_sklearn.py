76372a424bce210deb5d8c294860ba83c8c81762
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-10-27 14:51:31 -0700

    Update assertion

--- tests/python/test_with_sklearn.py
+++ tests/python/test_with_sklearn.py
@@ -409,7 +409,7 @@ def test_parameter_tuning():
                        cv=3, verbose=1)
     clf.fit(X, y)
     assert clf.best_score_ < 0.7
-    assert clf.best_params_ == {'n_estimators': 100, 'max_depth': 4}
+    assert clf.best_params_ == {'n_estimators': 200, 'max_depth': 4}
 
 
 def test_regression_with_custom_objective():
