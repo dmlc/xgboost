a900f8fd21fd09f32909cc529c6d70ccbeef4632
fis <jm.yuan@outlook.com>
2021-11-02 21:03:10 +0800

    Test.

--- tests/python/test_openmp.py
+++ tests/python/test_openmp.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
+from multiprocessing import Process, Queue
+import os
+
 import xgboost as xgb
 import numpy as np
+import pytest
+
+import testing as tm
 
 
 class TestOMP:
@@ -71,3 +76,29 @@ class TestOMP:
         assert auc_1 == auc_2 == auc_3
         assert np.array_equal(auc_1, auc_2)
         assert np.array_equal(auc_1, auc_3)
+
+    @pytest.mark.skipif(**tm.no_sklearn())
+    def test_with_omp_thread_limit(self):
+        def test_fn(q: Queue, limit: int):
+            from sklearn.datasets import load_iris
+            from sklearn.metrics import roc_auc_score
+            os.environ["OMP_THREAD_LIMIT"] = str(limit)
+            X, y = load_iris(return_X_y=True)
+            Xy = xgb.DMatrix(X, y, nthread=16)
+            booster = xgb.train({"num_class": 3, "objective": "multi:softprob"}, Xy, num_boost_round=8)
+            score = booster.predict(Xy)
+            print(y.shape, score.shape)
+            auc = roc_auc_score(y, score, average="weighted", multi_class="ovr")
+            q.put(auc)
+
+        queue = Queue()
+        processes = []
+        for i in (1, 2, 16):
+            p = Process(target=test_fn, args=(queue, i))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+            score = queue.get()
+            print("score:", score)
