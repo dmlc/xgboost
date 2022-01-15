313afeef2e0724cf73335ad768639b8d524cbda5
fis <jm.yuan@outlook.com>
2022-01-16 02:26:58 +0800

    year.

--- jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoostJNI.java
+++ jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoostJNI.java
@@ -1,5 +1,6 @@
 /*
- Copyright (c) 2014 by Contributors
+
+ Copyright (c) 2014-2022 by Contributors
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
