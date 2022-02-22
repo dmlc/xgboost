91be0debd48b76ab81955e8c26457f08ba84d27c
fis <jm.yuan@outlook.com>
2022-02-23 01:48:45 +0800

    Revert "Rename."
    This reverts commit 820dbf166796ca74c0cc0d11a11a10aaffef4a3c.

--- R-package/R/xgb.load.raw.R
+++ R-package/R/xgb.load.raw.R
@@ -3,16 +3,16 @@
 #' User can generate raw memory buffer by calling xgb.save.raw
 #'
 #' @param buffer the buffer returned by xgb.save.raw
-#' @param as.booster Return the loaded model as xgb.Booster instead of xgb.Booster.handle.
+#' @param as_booster Return the loaded model as xgb.Booster instead of xgb.Booster.handle.
 #'
 #' @export
-xgb.load.raw <- function(buffer, as.booster = FALSE) {
+xgb.load.raw <- function(buffer, as_booster = FALSE) {
   cachelist <- list()
   handle <- .Call(XGBoosterCreate_R, cachelist)
   .Call(XGBoosterLoadModelFromRaw_R, handle, buffer)
   class(handle) <- "xgb.Booster.handle"
 
-  if (as.booster) {
+  if (as_booster) {
     booster <- list(handle = handle, raw = NULL)
     class(booster) <- "xgb.Booster"
     booster <- xgb.Booster.complete(booster, saveraw = TRUE)
