fb5f53055e19a1c11f5e72db3e19d98cd36ba739
fis <jm.yuan@outlook.com>
2022-02-24 07:20:51 +0800

    Fix.

--- src/common/hist_util.cuh
+++ src/common/hist_util.cuh
@@ -126,7 +126,11 @@ void MakeEntriesFromAdapter(AdapterBatch const& batch, BatchIter batch_iter, Ran
   dh::XGBCachingDeviceAllocator<char> alloc;
   auto entry_iter = thrust::make_counting_iterator<uint32_t>(0u);
   sorted_idx->resize(num_valid);
-  dh::CopyIf(entry_iter + range.begin(), entry_iter + range.end(), sorted_idx->begin(), is_valid);
+  auto copyif_op = [=] XGBOOST_DEVICE(uint32_t idx) {
+    auto check = data::IsValidFunctor{missing};
+    return check(batch.GetElement(idx).value);
+  };
+  dh::CopyIf(entry_iter + range.begin(), entry_iter + range.end(), sorted_idx->begin(), copyif_op);
 }
 
 template <typename Batch>
