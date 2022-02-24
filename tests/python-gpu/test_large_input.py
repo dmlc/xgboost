f14079bc906b99f078628a451e11d55c814f3172
fis <jm.yuan@outlook.com>
2022-02-24 09:34:58 +0800

    Check.

--- tests/python-gpu/test_large_input.py
+++ tests/python-gpu/test_large_input.py
@@ -1,8 +1,11 @@
+import sys
 import numpy as np
 import xgboost as xgb
 import cupy as cp
-import time
 import pytest
+
+sys.path.append("tests/python")
+import testing as tm
 
 
 # Test for integer overflow or out of memory exceptions
@@ -12,10 +15,20 @@ def test_large_input():
     required_bytes = 1.5e+10
     if available_bytes < required_bytes:
         pytest.skip("Not enough memory on this device")
+
     n = 1000
     m = ((1 << 31) + n - 1) // n
     assert (np.log2(m * n) > 31)
     X = cp.ones((m, n), dtype=np.float32)
     y = cp.ones(m)
     dmat = xgb.DeviceQuantileDMatrix(X, y)
-    xgb.train({"tree_method": "gpu_hist", "max_depth": 1}, dmat, 1)
+    result = {}
+    xgb.train(
+        {"tree_method": "gpu_hist", "max_depth": 2},
+        dmat,
+        4,
+        evals=[(dmat, "Train")],
+        evals_result=result
+    )
+
+    tm.non_increasing(result["Train"]["rmse"])
