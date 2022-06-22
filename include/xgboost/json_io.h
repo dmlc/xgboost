c769ab7bc90073b3026cbdd8f8cd0539c6bec7b9
fis <jm.yuan@outlook.com>
2022-06-22 17:32:15 +0800

    Fix arm build.

--- include/xgboost/json_io.h
+++ include/xgboost/json_io.h
@@ -89,7 +89,7 @@ class JsonReader {
     } else if (got == 0) {
       msg += "\\0\"";
     } else {
-      msg += std::string {got} + " \"";
+      msg += (got <= static_cast<char>(127) ? std::string{got} : std::to_string(got)) + " \"";
     }
     Error(msg);
   }
