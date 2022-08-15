89236093eb604dd26f7eb8991331f862532704d0
Rong Ou <rong.ou@gmail.com>
2022-08-15 13:43:22 -0700

    fix lint errors

--- tests/cpp/collective/test_communicator_factory.cc
+++ tests/cpp/collective/test_communicator_factory.cc
@@ -26,19 +26,20 @@ TEST(CommunicatorFactory, TypeFromEnv) {
 }
 
 TEST(CommunicatorFactory, TypeFromArgs) {
-  char *args0[] = {(char *)("foo=bar")};
+  char *args0[] = {(char *)("foo=bar")};  // NOLINT(google-readability-casting)
   EXPECT_EQ(CommunicatorType::kUnknown, CommunicatorFactory::GetTypeFromArgs(1, args0));
 
-  char *args1[] = {(char *)("xgboost_communicator=rabit")};
+  char *args1[] = {(char *)("xgboost_communicator=rabit")};  // NOLINT(google-readability-casting)
   EXPECT_EQ(CommunicatorType::kRabit, CommunicatorFactory::GetTypeFromArgs(1, args1));
 
-  char *args2[] = {(char *)("xgboost_communicator=MPI")};
+  char *args2[] = {(char *)("xgboost_communicator=MPI")};  // NOLINT(google-readability-casting)
   EXPECT_EQ(CommunicatorType::kMPI, CommunicatorFactory::GetTypeFromArgs(1, args2));
 
-  char *args3[] = {(char *)("xgboost_communicator=federated")};
+  char *args3[] = {
+      (char *)("xgboost_communicator=federated")};  // NOLINT(google-readability-casting)
   EXPECT_EQ(CommunicatorType::kFederated, CommunicatorFactory::GetTypeFromArgs(1, args3));
 
-  char *args4[] = {(char *)("xgboost_communicator=foo")};
+  char *args4[] = {(char *)("xgboost_communicator=foo")};  // NOLINT(google-readability-casting)
   EXPECT_THROW(CommunicatorFactory::GetTypeFromArgs(1, args4), dmlc::Error);
 }
 
