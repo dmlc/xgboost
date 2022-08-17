3f12f4a7b7e963cb6308e1e73dbe873afab75b7d
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-08-17 15:45:24 -0700

    Put pruning logic in CI directory

--- tests/ci_build/prune_libnccl.sh
+++ tests/ci_build/prune_libnccl.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf build
-mkdir build
-cd build
+rm -rf tmp_nccl
+
+mkdir tmp_nccl
+pushd tmp_nccl
 
 set -x
 
-cmake .. -DUSE_CUDA=ON -DUSE_NCCL=ON -GNinja
-ninja -v PruneLibNccl
-mv libnccl_static.a ..
+cat << EOF > test.cu
+int main(void) { return 0; }
+EOF
+
+cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION 3.18 FATAL_ERROR)
+project(gencode_extractor CXX C)
+cmake_policy(SET CMP0104 NEW)
+set(CMAKE_CUDA_HOST_COMPILER \${CMAKE_CXX_COMPILER})
+enable_language(CUDA)
+include(../cmake/Utils.cmake)
+set(GEN_CODE "")
+format_gencode_flags("" GEN_CODE)
+add_library(test OBJECT test.cu)
+set_property(TARGET test PROPERTY CUDA_ARCHITECTURES \${CMAKE_CUDA_ARCHITECTURES})
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+EOF
+
+cmake . -GNinja
+gen_code=$(grep --color -o -- '--generate-code=\S*' compile_commands.json | paste -sd ' ')
+
+nvprune ${gen_code} /usr/lib64/libnccl_static.a -o ../libnccl_static.a
+
+popd
+rm -rf tmp_nccl
+
+set +x
