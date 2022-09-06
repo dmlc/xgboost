fb4bba43a29734f055ee11d518689e3d9ec4e9ba
fis <jm.yuan@outlook.com>
2022-09-06 17:12:10 +0800

    Add tests from Rory.

--- tests/cpp/tree/gpu_hist/test_evaluate_splits.cu
+++ tests/cpp/tree/gpu_hist/test_evaluate_splits.cu
@@ -52,6 +52,242 @@ TEST_F(TestCategoricalSplitWithMissing, GPUHistEvaluator) {
                     result.dir == kLeftDir, result.left_sum, result.right_sum);
 }
 
+TEST(GpuHist, PartitionBasic) {
+  TrainParam tparam = ZeroParam();
+  tparam.max_cat_to_onehot = 0;
+  GPUTrainingParam param{tparam};
+
+  common::HistogramCuts cuts;
+  cuts.cut_values_.HostVector() = std::vector<float>{0.0, 1.0, 2.0};
+  cuts.cut_ptrs_.HostVector() = std::vector<uint32_t>{0, 3};
+  cuts.min_vals_.HostVector() = std::vector<float>{0.0};
+  cuts.cut_ptrs_.SetDevice(0);
+  cuts.cut_values_.SetDevice(0);
+  cuts.min_vals_.SetDevice(0);
+  thrust::device_vector<bst_feature_t> feature_set = std::vector<bst_feature_t>{0};
+
+  thrust::device_vector<int> monotonic_constraints(feature_set.size(), 0);
+  dh::device_vector<FeatureType> feature_types(feature_set.size(), FeatureType::kCategorical);
+  common::Span<FeatureType> d_feature_types;
+  auto max_cat =
+      *std::max_element(cuts.cut_values_.HostVector().begin(), cuts.cut_values_.HostVector().end());
+  cuts.SetCategorical(true, max_cat);
+  d_feature_types = dh::ToSpan(feature_types);
+
+  EvaluateSplitSharedInputs shared_inputs{
+      param,
+      d_feature_types,
+      cuts.cut_ptrs_.ConstDeviceSpan(),
+      cuts.cut_values_.ConstDeviceSpan(),
+      cuts.min_vals_.ConstDeviceSpan(),
+  };
+
+  GPUHistEvaluator evaluator{tparam, static_cast<bst_feature_t>(feature_set.size()), 0};
+  evaluator.Reset(cuts, dh::ToSpan(feature_types), feature_set.size(), tparam, 0);
+
+  {
+    // -1.0s go left
+    // -3.0s go right
+    GradientPairPrecise parent_sum(-5.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-1.0, 1.0}, {-3.0, 1.0}};
+    EvaluateSplitInputs input{0, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(cats, std::bitset<32>("11000000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+
+  {
+    // -1.0s go left
+    // -3.0s go right
+    GradientPairPrecise parent_sum(-7.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-3.0, 1.0}, {-3.0, 1.0}};
+    EvaluateSplitInputs input{1, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(cats, std::bitset<32>("10000000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+  {
+    // All -1.0, gain from splitting should be 0.0
+    GradientPairPrecise parent_sum(-3.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-1.0, 1.0}, {-1.0, 1.0}};
+    EvaluateSplitInputs input{2, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    EXPECT_FLOAT_EQ(result.loss_chg, 0.0f);
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+  // With 3.0/3.0 missing values
+  // All categories go left
+  // missing values go right
+  {
+    GradientPairPrecise parent_sum(0.0, 6.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-1.0, 1.0}, {-1.0, 1.0}};
+    EvaluateSplitInputs input{3, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(cats, std::bitset<32>("11000000000000000000000000000000"));
+    EXPECT_EQ(result.dir, kLeftDir);
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+  {
+    // -1.0s go left
+    // -3.0s go right
+    GradientPairPrecise parent_sum(-5.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-3.0, 1.0}, {-1.0, 1.0}};
+    EvaluateSplitInputs input{4, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(cats, std::bitset<32>("10100000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+  {
+    // -1.0s go left
+    // -3.0s go right
+    GradientPairPrecise parent_sum(-5.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram =
+        std::vector<GradientPairPrecise>{{-3.0, 1.0}, {-1.0, 1.0}, {-3.0, 1.0}};
+    EvaluateSplitInputs input{5, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(cats, std::bitset<32>("01000000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+}
+
+TEST(GpuHist, PartitionTwoFeatures) {
+  TrainParam tparam = ZeroParam();
+  tparam.max_cat_to_onehot = 0;
+  GPUTrainingParam param{tparam};
+
+  common::HistogramCuts cuts;
+  cuts.cut_values_.HostVector() = std::vector<float>{0.0, 1.0, 2.0, 0.0, 1.0, 2.0};
+  cuts.cut_ptrs_.HostVector() = std::vector<uint32_t>{0, 3, 6};
+  cuts.min_vals_.HostVector() = std::vector<float>{0.0, 0.0};
+  cuts.cut_ptrs_.SetDevice(0);
+  cuts.cut_values_.SetDevice(0);
+  cuts.min_vals_.SetDevice(0);
+  thrust::device_vector<bst_feature_t> feature_set = std::vector<bst_feature_t>{0, 1};
+
+  thrust::device_vector<int> monotonic_constraints(feature_set.size(), 0);
+  dh::device_vector<FeatureType> feature_types(feature_set.size(), FeatureType::kCategorical);
+  common::Span<FeatureType> d_feature_types(dh::ToSpan(feature_types));
+  auto max_cat =
+      *std::max_element(cuts.cut_values_.HostVector().begin(), cuts.cut_values_.HostVector().end());
+  cuts.SetCategorical(true, max_cat);
+
+  EvaluateSplitSharedInputs shared_inputs{
+      param,
+      d_feature_types,
+      cuts.cut_ptrs_.ConstDeviceSpan(),
+      cuts.cut_values_.ConstDeviceSpan(),
+      cuts.min_vals_.ConstDeviceSpan(),
+  };
+
+  GPUHistEvaluator evaluator{tparam, static_cast<bst_feature_t>(feature_set.size()), 0};
+  evaluator.Reset(cuts, dh::ToSpan(feature_types), feature_set.size(), tparam, 0);
+
+  {
+    GradientPairPrecise parent_sum(-6.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram = std::vector<GradientPairPrecise>{
+        {-2.0, 1.0}, {-2.0, 1.0}, {-2.0, 1.0}, {-1.0, 1.0}, {-1.0, 1.0}, {-4.0, 1.0}};
+    EvaluateSplitInputs input{0, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(result.findex, 1);
+    EXPECT_EQ(cats, std::bitset<32>("11000000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+
+  {
+    GradientPairPrecise parent_sum(-6.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram = std::vector<GradientPairPrecise>{
+        {-2.0, 1.0}, {-2.0, 1.0}, {-2.0, 1.0}, {-1.0, 1.0}, {-2.5, 1.0}, {-2.5, 1.0}};
+    EvaluateSplitInputs input{1, 0, parent_sum, dh::ToSpan(feature_set),
+                              dh::ToSpan(feature_histogram)};
+    DeviceSplitCandidate result = evaluator.EvaluateSingleSplit(input, shared_inputs).split;
+    auto cats = std::bitset<32>(evaluator.GetHostNodeCats(input.nidx)[0]);
+    EXPECT_EQ(result.findex, 1);
+    EXPECT_EQ(cats, std::bitset<32>("10000000000000000000000000000000"));
+    EXPECT_FLOAT_EQ(result.left_sum.GetGrad() + result.right_sum.GetGrad(), parent_sum.GetGrad());
+    EXPECT_FLOAT_EQ(result.left_sum.GetHess() + result.right_sum.GetHess(), parent_sum.GetHess());
+  }
+}
+
+TEST(GpuHist, PartitionTwoNodes) {
+  TrainParam tparam = ZeroParam();
+  tparam.max_cat_to_onehot = 0;
+  GPUTrainingParam param{tparam};
+
+  common::HistogramCuts cuts;
+  cuts.cut_values_.HostVector() = std::vector<float>{0.0, 1.0, 2.0};
+  cuts.cut_ptrs_.HostVector() = std::vector<uint32_t>{0, 3};
+  cuts.min_vals_.HostVector() = std::vector<float>{0.0};
+  cuts.cut_ptrs_.SetDevice(0);
+  cuts.cut_values_.SetDevice(0);
+  cuts.min_vals_.SetDevice(0);
+  thrust::device_vector<bst_feature_t> feature_set = std::vector<bst_feature_t>{0};
+
+  thrust::device_vector<int> monotonic_constraints(feature_set.size(), 0);
+  dh::device_vector<FeatureType> feature_types(feature_set.size(), FeatureType::kCategorical);
+  common::Span<FeatureType> d_feature_types(dh::ToSpan(feature_types));
+  auto max_cat =
+      *std::max_element(cuts.cut_values_.HostVector().begin(), cuts.cut_values_.HostVector().end());
+  cuts.SetCategorical(true, max_cat);
+
+  EvaluateSplitSharedInputs shared_inputs{
+      param,
+      d_feature_types,
+      cuts.cut_ptrs_.ConstDeviceSpan(),
+      cuts.cut_values_.ConstDeviceSpan(),
+      cuts.min_vals_.ConstDeviceSpan(),
+  };
+
+  GPUHistEvaluator evaluator{tparam, static_cast<bst_feature_t>(feature_set.size()), 0};
+  evaluator.Reset(cuts, dh::ToSpan(feature_types), feature_set.size(), tparam, 0);
+
+  {
+    GradientPairPrecise parent_sum(-6.0, 3.0);
+    thrust::device_vector<GradientPairPrecise> feature_histogram_a =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-2.5, 1.0}, {-2.5, 1.0},
+                                         {-1.0, 1.0}, {-1.0, 1.0}, {-4.0, 1.0}};
+    thrust::device_vector<EvaluateSplitInputs> inputs(2);
+    inputs[0] = EvaluateSplitInputs{0, 0, parent_sum, dh::ToSpan(feature_set),
+                                    dh::ToSpan(feature_histogram_a)};
+    thrust::device_vector<GradientPairPrecise> feature_histogram_b =
+        std::vector<GradientPairPrecise>{{-1.0, 1.0}, {-1.0, 1.0}, {-4.0, 1.0}};
+    inputs[1] = EvaluateSplitInputs{1, 0, parent_sum, dh::ToSpan(feature_set),
+                                    dh::ToSpan(feature_histogram_b)};
+    thrust::device_vector<GPUExpandEntry> results(2);
+    evaluator.EvaluateSplits({0, 1}, 1, dh::ToSpan(inputs), shared_inputs, dh::ToSpan(results));
+    GPUExpandEntry result_a = results[0];
+    GPUExpandEntry result_b = results[1];
+    EXPECT_EQ(std::bitset<32>(evaluator.GetHostNodeCats(0)[0]),
+              std::bitset<32>("10000000000000000000000000000000"));
+    EXPECT_EQ(std::bitset<32>(evaluator.GetHostNodeCats(1)[0]),
+              std::bitset<32>("11000000000000000000000000000000"));
+  }
+}
+
 void TestEvaluateSingleSplit(bool is_categorical) {
   GradientPairPrecise parent_sum(0.0, 1.0);
   TrainParam tparam = ZeroParam();
