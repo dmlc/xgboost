b2bbf49015e7c9b34524450755249ae23b519a89
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-10-04 04:03:38 -0700

    Additional improvements to CI (#8303)
    * Wait until budget check is complete
    
    * Ensure that multi-GPU tests run for the master branch
    
    * Fix

--- tests/buildkite/conftest.ps1
+++ tests/buildkite/conftest.ps1
@@ -6,6 +6,8 @@ if ( $Env:BUILDKITE_PULL_REQUEST -and ($Env:BUILDKITE_PULL_REQUEST -ne "false") 
 
 if ( ($Env:BUILDKITE_BRANCH -eq "master") -or ($Env:BUILDKITE_BRANCH -match "release_.+") ) {
   $is_release_branch = 1
+  $enforce_daily_budget = 0
 } else {
   $is_release_branch = 0
+  $enforce_daily_budget = 1
 }
