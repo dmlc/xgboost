6e5741d32e13a8c213c6b435424a7d723f63e391
fis <jm.yuan@outlook.com>
2022-10-13 16:17:09 +0800

    Fix formatting.

--- tests/python-gpu/test_gpu_spark/test_gpu_spark.py
+++ tests/python-gpu/test_gpu_spark/test_gpu_spark.py
@@ -1,16 +1,15 @@
-import logging
-import sys
 import json
+import logging
+import subprocess
+import sys
 
 import pytest
 import sklearn
-import subprocess
-from io import StringIO
 
 sys.path.append("tests/python")
 import testing as tm
 
-if tm.no_dask()["condition"]:
+if tm.no_spark()["condition"]:
     pytest.skip(msg=tm.no_spark()["reason"], allow_module_level=True)
 if sys.platform.startswith("win"):
     pytest.skip("Skipping PySpark tests on Windows", allow_module_level=True)
@@ -26,7 +25,7 @@ gpu_discovery_script_path = "tests/python-gpu/test_gpu_spark/discover_gpu.sh"
 def get_devices():
     """This works only if driver is the same machine of worker."""
     completed = subprocess.run(gpu_discovery_script_path, stdout=subprocess.PIPE)
-    assert completed.returncode == 0, "Failed to execute"
+    assert completed.returncode == 0, "Failed to execute discovery script."
     msg = completed.stdout.decode("utf-8")
     result = json.loads(msg)
     addresses = result["addresses"]
