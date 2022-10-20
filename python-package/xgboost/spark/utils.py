7225fb4006d94fedeb2d4735c233e10a7520c396
fis <jm.yuan@outlook.com>
2022-10-20 18:56:09 +0800

    [pyspark] Add type hint to basic utilities.

--- python-package/xgboost/spark/utils.py
+++ python-package/xgboost/spark/utils.py
@@ -1,29 +1,32 @@
-# type: ignore
 """Xgboost pyspark integration submodule for helper functions."""
 import inspect
 import json
 import logging
 import sys
 from threading import Thread
+from typing import Any, Callable, Dict, List, Set, Type
 
 import pyspark
+from pyspark import BarrierTaskContext, SparkContext
 from pyspark.sql.session import SparkSession
 from xgboost.tracker import RabitTracker
 
 from xgboost import collective
 
 
-def get_class_name(cls):
-    """
-    Return the class name.
+def get_class_name(cls: Type) -> str:
+    """Return the class name.
+
     """
     return f"{cls.__module__}.{cls.__name__}"
 
 
-def _get_default_params_from_func(func, unsupported_set):
-    """
-    Returns a dictionary of parameters and their default value of function fn.
-    Only the parameters with a default value will be included.
+def _get_default_params_from_func(
+    func: Callable, unsupported_set: Set[str]
+) -> Dict[str, Any]:
+    """Returns a dictionary of parameters and their default value of function fn.  Only
+    the parameters with a default value will be included.
+
     """
     sig = inspect.signature(func)
     filtered_params_dict = {}
@@ -38,27 +41,28 @@ def _get_default_params_from_func(func, unsupported_set):
 
 
 class CommunicatorContext:
-    """
-    A context controlling collective communicator initialization and finalization.
-    This isn't specificially necessary (note Part 3), but it is more understandable coding-wise.
+    """A context controlling collective communicator initialization and finalization.
+    This isn't specificially necessary (note Part 3), but it is more understandable
+    coding-wise.
+
     """
 
-    def __init__(self, context, **args):
+    def __init__(self, context: BarrierTaskContext, **args: Any) -> None:
         self.args = args
         self.args["DMLC_TASK_ID"] = str(context.partitionId())
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         collective.init(**self.args)
 
-    def __exit__(self, *args):
+    def __exit__(self, *args: Any) -> None:
         collective.finalize()
 
 
-def _start_tracker(context, n_workers):
+def _start_tracker(context: BarrierTaskContext, n_workers: int) -> Dict[str, Any]:
+    """Start Rabit tracker with n_workers
+
     """
-    Start Rabit tracker with n_workers
-    """
-    env = {"DMLC_NUM_WORKER": n_workers}
+    env: Dict[str, Any] = {"DMLC_NUM_WORKER": n_workers}
     host = _get_host_ip(context)
     rabit_context = RabitTracker(host_ip=host, n_workers=n_workers)
     env.update(rabit_context.worker_envs())
@@ -69,26 +73,25 @@ def _start_tracker(context, n_workers):
     return env
 
 
-def _get_rabit_args(context, n_workers):
+def _get_rabit_args(context: BarrierTaskContext, n_workers: int) -> Dict[str, Any]:
+    """Get rabit context arguments to send to each worker.
+
     """
-    Get rabit context arguments to send to each worker.
-    """
-    # pylint: disable=consider-using-f-string
     env = _start_tracker(context, n_workers)
     return env
 
 
-def _get_host_ip(context):
-    """
-    Gets the hostIP for Spark. This essentially gets the IP of the first worker.
+def _get_host_ip(context: BarrierTaskContext) -> str:
+    """Gets the hostIP for Spark. This essentially gets the IP of the first worker.
+
     """
     task_ip_list = [info.address.split(":")[0] for info in context.getTaskInfos()]
     return task_ip_list[0]
 
 
-def _get_args_from_message_list(messages):
-    """
-    A function to send/recieve messages in barrier context mode
+def _get_args_from_message_list(messages: List[str]) -> Dict[str, Any]:
+    """A function to send/recieve messages in barrier context mode
+
     """
     output = ""
     for message in messages:
@@ -98,8 +101,11 @@ def _get_args_from_message_list(messages):
     return json.loads(output)
 
 
-def _get_spark_session():
-    """Get or create spark session. Note: This function can only be invoked from driver side."""
+def _get_spark_session() -> SparkSession:
+    """Get or create spark session. Note: This function can only be invoked from driver
+    side.
+
+    """
     if pyspark.TaskContext.get() is not None:
         # This is a safety check.
         raise RuntimeError(
@@ -108,7 +114,7 @@ def _get_spark_session():
     return SparkSession.builder.getOrCreate()
 
 
-def get_logger(name, level="INFO"):
+def get_logger(name: str, level: str = "INFO") -> logging.Logger:
     """Gets a logger by name, or creates and configures it for the first time."""
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -119,7 +125,7 @@ def get_logger(name, level="INFO"):
     return logger
 
 
-def _get_max_num_concurrent_tasks(spark_context):
+def _get_max_num_concurrent_tasks(spark_context: SparkContext) -> int:
     """Gets the current max number of concurrent tasks."""
     # pylint: disable=protected-access
     # spark 3.1 and above has a different API for fetching max concurrent tasks
@@ -130,13 +136,13 @@ def _get_max_num_concurrent_tasks(spark_context):
     return spark_context._jsc.sc().maxNumConcurrentTasks()
 
 
-def _is_local(spark_context) -> bool:
+def _is_local(spark_context: SparkContext) -> bool:
     """Whether it is Spark local mode"""
     # pylint: disable=protected-access
     return spark_context._jsc.sc().isLocal()
 
 
-def _get_gpu_id(task_context) -> int:
+def _get_gpu_id(task_context: BarrierTaskContext) -> int:
     """Get the gpu id from the task resources"""
     if task_context is None:
         # This is a safety check.
