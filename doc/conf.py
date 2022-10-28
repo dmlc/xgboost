a4b9bff29f217fe0ad3624d1f1102ae2e7f9e7ff
fis <jm.yuan@outlook.com>
2022-10-28 15:49:49 +0800

    Revert.

--- doc/conf.py
+++ doc/conf.py
@@ -144,6 +144,7 @@ autoclass_content = 'both'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build']
+html_extra_path = ['./tmp']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
