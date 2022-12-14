c923d3088dee87af6abc6ea64f095a4b69d9e329
Jiaming Yuan <jm.yuan@outlook.com>
2022-12-15 03:32:20 +0800

    Cleanup.

--- src/tree/fit_stump.cc
+++ src/tree/fit_stump.cc
@@ -22,30 +22,22 @@ namespace obj {
 namespace cpu_impl {
 void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpair,
               linalg::VectorView<float> out) {
-  // 2 rows, first one is gradient, sencond one is hessian. Number of columns equal to
-  // number of targets.
   auto n_targets = out.Size();
   CHECK_EQ(n_targets, gpair.Shape(1));
-  linalg::Tensor<double, 2> sum = linalg::Zeros<double>(ctx, 2, n_targets);
-  CHECK(sum.HostView().CContiguous());
-  auto sum_grad = sum.HostView().Slice(0, linalg::All());
-  auto sum_hess = sum.HostView().Slice(1, linalg::All());
-
+  linalg::Vector<GradientPairPrecise> sum = linalg::Constant(ctx, GradientPairPrecise{}, n_targets);
+  auto h_sum = sum.HostView();
   // first dim for gpair is samples, second dim is target.
   // Reduce by column
   common::ParallelFor(gpair.Shape(1), 1, [&](auto j) {
     for (std::size_t i = 0; i < gpair.Shape(0); ++i) {
-      sum_grad(j) += gpair(i, j).GetGrad();
-      sum_hess(j) += gpair(i, j).GetHess();
+      h_sum(j) += GradientPairPrecise{gpair(i, j)};
     }
   });
-  CHECK(sum_grad.CContiguous());
-  collective::Allreduce<collective::Operation::kSum>(sum_grad.Values().data(), sum_grad.Size());
-  CHECK(sum_hess.CContiguous());
-  collective::Allreduce<collective::Operation::kSum>(sum_hess.Values().data(), sum_hess.Size());
+  collective::Allreduce<collective::Operation::kSum>(
+      reinterpret_cast<double*>(h_sum.Values().data()), h_sum.Size() * 2);
 
-  for (std::size_t i = 0; i < sum_hess.Size(); ++i) {
-    out(i) = static_cast<float>(CalcUnregulatedWeight(sum_grad(i), sum_hess(i)));
+  for (std::size_t i = 0; i < h_sum.Size(); ++i) {
+    out(i) = static_cast<float>(CalcUnregulatedWeight(h_sum(i).GetGrad(), h_sum(i).GetHess()));
   }
 }
 }  // namespace cpu_impl
--- src/tree/fit_stump.cu
+++ src/tree/fit_stump.cu
@@ -26,14 +26,10 @@ namespace obj {
 namespace cuda_impl {
 void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpair,
               linalg::VectorView<float> out) {
-  // 2 rows, first one is gradient, sencond one is hessian. Number of columns equal to
-  // number of targets.
   auto n_targets = out.Size();
   CHECK_EQ(n_targets, gpair.Shape(1));
-  linalg::Tensor<double, 2> sum = linalg::Zeros<double>(ctx, 2, n_targets);
+  linalg::Vector<GradientPairPrecise> sum = linalg::Constant(ctx, GradientPairPrecise{}, n_targets);
   CHECK(out.Contiguous());
-  auto sum_grad = sum.View(ctx->gpu_id).Slice(0, linalg::All());
-  auto sum_hess = sum.View(ctx->gpu_id).Slice(1, linalg::All());
 
   // Reduce by column
   auto key_it = dh::MakeTransformIterator<bst_target_t>(
@@ -41,41 +37,28 @@ void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpai
         return i / gpair.Shape(0);
         return std::get<1>(linalg::UnravelIndex(i, gpair.Shape()));
       });
-  auto grad_it = dh::MakeTransformIterator<double>(thrust::make_counting_iterator(0ul),
-                                                   [=] XGBOOST_DEVICE(std::size_t i) -> double {
-                                                     auto target = i / gpair.Shape(0);
-                                                     auto sample = i % gpair.Shape(0);
-                                                     return gpair(sample, target).GetGrad();
-                                                   });
-  auto hess_it = dh::MakeTransformIterator<double>(thrust::make_counting_iterator(0ul),
-                                                   [=] XGBOOST_DEVICE(std::size_t i) -> double {
-                                                     auto target = i / gpair.Shape(0);
-                                                     auto sample = i % gpair.Shape(0);
-                                                     return gpair(sample, target).GetHess();
-                                                   });
-  auto val_it = thrust::make_zip_iterator(grad_it, hess_it);
-  CHECK(sum_grad.CContiguous());
-  CHECK(sum_hess.CContiguous());
-  auto out_it =
-      thrust::make_zip_iterator(dh::tbegin(sum_grad.Values()), dh::tbegin(sum_hess.Values()));
+  auto grad_it = dh::MakeTransformIterator<GradientPairPrecise>(
+      thrust::make_counting_iterator(0ul),
+      [=] XGBOOST_DEVICE(std::size_t i) -> GradientPairPrecise {
+        auto target = i / gpair.Shape(0);
+        auto sample = i % gpair.Shape(0);
+        return GradientPairPrecise{gpair(sample, target)};
+      });
+  auto d_sum = sum.View(ctx->gpu_id);
 
   dh::XGBCachingDeviceAllocator<char> alloc;
   auto policy = thrust::cuda::par(alloc);
-  thrust::reduce_by_key(policy, key_it, key_it + gpair.Size(), val_it,
-                        dh::TypedDiscard<bst_target_t>{}, out_it, thrust::equal_to<bst_target_t>{},
-                        [=] __device__(auto lhs, auto rhs) {
-                          return thrust::make_tuple(thrust::get<0>(lhs) + thrust::get<0>(rhs),
-                                                    thrust::get<1>(lhs) + thrust::get<1>(rhs));
-                        });
+  thrust::reduce_by_key(policy, key_it, key_it + gpair.Size(), grad_it,
+                        dh::TypedDiscard<bst_target_t>{}, dh::tbegin(d_sum.Values()));
 
   collective::DeviceCommunicator* communicator = collective::Communicator::GetDevice(ctx->gpu_id);
-  communicator->AllReduceSum(sum_grad.Values().data(), sum_grad.Size());
-  communicator->AllReduceSum(sum_hess.Values().data(), sum_hess.Size());
+  communicator->AllReduceSum(reinterpret_cast<double*>(d_sum.Values().data()), d_sum.Size() * 2);
 
-  thrust::for_each_n(policy, thrust::make_counting_iterator(0ul), n_targets,
-                     [=] XGBOOST_DEVICE(std::size_t i) mutable {
-                       out(i) = static_cast<float>(CalcUnregulatedWeight(sum_grad(i), sum_hess(i)));
-                     });
+  thrust::for_each_n(
+      policy, thrust::make_counting_iterator(0ul), n_targets,
+      [=] XGBOOST_DEVICE(std::size_t i) mutable {
+        out(i) = static_cast<float>(CalcUnregulatedWeight(d_sum(i).GetGrad(), d_sum(i).GetHess()));
+      });
 }
 }  // namespace cuda_impl
 }  // namespace obj
