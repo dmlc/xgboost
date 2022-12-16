8cc70d346a36e8910ddea399f568c715e67b37ff
jiamingy <jm.yuan@outlook.com>
2022-12-17 00:44:33 +0800

    Cleanup.

--- src/objective/regression_obj.cu
+++ src/objective/regression_obj.cu
@@ -42,7 +42,7 @@ namespace obj {
 namespace {
 void CheckInitInputs(MetaInfo const& info) {
   CHECK_EQ(info.labels.Shape(0), info.num_row_) << "Invalid shape of labels.";
-  if (!info.weights_.Empty() && info.group_ptr_.empty()) {
+  if (!info.weights_.Empty()) {
     CHECK_EQ(info.weights_.Size(), info.num_row_)
         << "Number of weights should be equal to number of data points.";
   }
