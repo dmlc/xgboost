53e6e327182c4aa4e5aaafa024156befad96064a
James Lamb <jaylamb20@gmail.com>
2022-12-16 11:22:41 -0600

    [R] resolve assignment_linter warnings (#8599)

--- demo/kaggle-higgs/higgs-train.R
+++ demo/kaggle-higgs/higgs-train.R
@@ -23,9 +23,9 @@ param <- list("objective" = "binary:logitraw",
               "eval_metric" = "ams@0.15",
               "nthread" = 16)
 watchlist <- list("train" = xgmat)
-nrounds = 120
+nrounds <- 120
 print ("loading data end, start to boost trees")
-bst = xgb.train(param, xgmat, nrounds, watchlist );
+bst <- xgb.train(param, xgmat, nrounds, watchlist );
 # save out model
 xgb.save(bst, "higgs.model")
 print ('finish training')
