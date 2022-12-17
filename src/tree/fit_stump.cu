7a2abb96246728ca9cf37fd9a1624adf0ca3d558
Jiaming Yuan <jm.yuan@outlook.com>
2022-12-17 18:37:30 +0800

    Cleanup.

--- src/tree/fit_stump.cu
+++ src/tree/fit_stump.cu
@@ -12,7 +12,7 @@
 #include <cstddef>                                // std::size_t
 
 #include "../collective/device_communicator.cuh"  // DeviceCommunicator
-#include "../common/device_helpers.cuh"           // dh::MakeTransformIterator,Reduce
+#include "../common/device_helpers.cuh"           // dh::MakeTransformIterator
 #include "fit_stump.h"
 #include "xgboost/base.h"     // GradientPairPrecise, GradientPair, XGBOOST_DEVICE
 #include "xgboost/context.h"  // Context
@@ -32,9 +32,8 @@ void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpai
 
   // Reduce by column
   auto key_it = dh::MakeTransformIterator<bst_target_t>(
-      thrust::make_counting_iterator(0ul), [=] XGBOOST_DEVICE(std::size_t i) -> bst_target_t {
-        return i / gpair.Shape(0);
-      });
+      thrust::make_counting_iterator(0ul),
+      [=] XGBOOST_DEVICE(std::size_t i) -> bst_target_t { return i / gpair.Shape(0); });
   auto grad_it = dh::MakeTransformIterator<GradientPairPrecise>(
       thrust::make_counting_iterator(0ul),
       [=] XGBOOST_DEVICE(std::size_t i) -> GradientPairPrecise {
@@ -43,6 +42,7 @@ void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpai
         return GradientPairPrecise{gpair(sample, target)};
       });
   auto d_sum = sum.View(ctx->gpu_id);
+  CHECK(d_sum.CContiguous());
 
   dh::XGBCachingDeviceAllocator<char> alloc;
   auto policy = thrust::cuda::par(alloc);
@@ -52,11 +52,11 @@ void FitStump(Context const* ctx, linalg::TensorView<GradientPair const, 2> gpai
   collective::DeviceCommunicator* communicator = collective::Communicator::GetDevice(ctx->gpu_id);
   communicator->AllReduceSum(reinterpret_cast<double*>(d_sum.Values().data()), d_sum.Size() * 2);
 
-  thrust::for_each_n(
-      policy, thrust::make_counting_iterator(0ul), n_targets,
-      [=] XGBOOST_DEVICE(std::size_t i) mutable {
-        out(i) = static_cast<float>(CalcUnregularizedWeight(d_sum(i).GetGrad(), d_sum(i).GetHess()));
-      });
+  thrust::for_each_n(policy, thrust::make_counting_iterator(0ul), n_targets,
+                     [=] XGBOOST_DEVICE(std::size_t i) mutable {
+                       out(i) = static_cast<float>(
+                           CalcUnregularizedWeight(d_sum(i).GetGrad(), d_sum(i).GetHess()));
+                     });
 }
 }  // namespace cuda_impl
 }  // namespace tree
