5bf9e794131d6810f708e32b980bb73d65c86679
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-12-21 01:41:34 -0800

    [CI] Disable gtest with RMM (#8620)

--- tests/buildkite/test-cpp-gpu.sh
+++ tests/buildkite/test-cpp-gpu.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source tests/buildkite/conftest.sh
 
-echo "--- Run Google Tests with CUDA, using 4 GPUs"
+echo "--- Run Google Tests with CUDA, using a GPU"
 buildkite-agent artifact download "build/testxgboost" . --step build-cuda
 chmod +x build/testxgboost
 tests/ci_build/ci_build.sh gpu nvidia-docker \
@@ -12,11 +12,12 @@ tests/ci_build/ci_build.sh gpu nvidia-docker \
   --build-arg RAPIDS_VERSION_ARG=$RAPIDS_VERSION \
   build/testxgboost
 
-echo "--- Run Google Tests with CUDA, using 4 GPUs, RMM enabled"
-rm -rfv build/
-buildkite-agent artifact download "build/testxgboost" . --step build-cuda-with-rmm
-chmod +x build/testxgboost
-tests/ci_build/ci_build.sh rmm nvidia-docker \
-  --build-arg CUDA_VERSION_ARG=$CUDA_VERSION \
-  --build-arg RAPIDS_VERSION_ARG=$RAPIDS_VERSION bash -c \
-  "source activate gpu_test && build/testxgboost --use-rmm-pool"
+# Disabled until https://github.com/dmlc/xgboost/issues/8619 is resolved
+# echo "--- Run Google Tests with CUDA, using a GPU, RMM enabled"
+# rm -rfv build/
+# buildkite-agent artifact download "build/testxgboost" . --step build-cuda-with-rmm
+# chmod +x build/testxgboost
+# tests/ci_build/ci_build.sh rmm nvidia-docker \
+#   --build-arg CUDA_VERSION_ARG=$CUDA_VERSION \
+#   --build-arg RAPIDS_VERSION_ARG=$RAPIDS_VERSION bash -c \
+#   "source activate gpu_test && build/testxgboost --use-rmm-pool"
