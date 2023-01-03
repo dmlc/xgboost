00038d10d6b50b09098ce7f3490484c9d592711b
jiamingy <jm.yuan@outlook.com>
2023-01-03 21:13:21 +0800

    [CI] Fix CI with updated dependencies.
    - Fix jvm package get iris.

--- jvm-packages/xgboost4j-tester/get_iris.py
+++ jvm-packages/xgboost4j-tester/get_iris.py
@@ -1,9 +1,9 @@
-from sklearn.datasets import load_iris
 import numpy as np
 import pandas
+from sklearn.datasets import load_iris
 
 X, y = load_iris(return_X_y=True)
-y = y.astype(np.int)
+y = y.astype(np.int32)
 df = pandas.DataFrame(data=X, columns=['sepal length', 'sepal width', 'petal length', 'petal width'])
 class_id_to_name = {0:'Iris-setosa', 1:'Iris-versicolor', 2:'Iris-virginica'}
 df['class'] = np.vectorize(class_id_to_name.get)(y)
