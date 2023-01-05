4990d104c7d26c2790c7f11ba89ef55f89cfcb70
jiamingy <jm.yuan@outlook.com>
2023-01-05 23:37:20 +0800

    debug CI.

--- tests/python/test_with_shap.py
+++ tests/python/test_with_shap.py
@@ -9,18 +9,25 @@ except Exception:
     shap = None
     pass
 
+
 pytestmark = pytest.mark.skipif(shap is None, reason="Requires shap package")
+print("\nSHAP:", shap.__version__, "NUMPY:", np.__version__)
 
 
 # Check integration is not broken from xgboost side
 # Changes in binary format may cause problems
 def test_with_shap():
     from sklearn.datasets import fetch_california_housing
+
     X, y = fetch_california_housing(return_X_y=True)
     dtrain = xgb.DMatrix(X, label=y)
     model = xgb.train({"learning_rate": 0.01}, dtrain, 10)
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X)
     margin = model.predict(dtrain, output_margin=True)
-    assert np.allclose(np.sum(shap_values, axis=len(shap_values.shape) - 1),
-                       margin - explainer.expected_value, 1e-3, 1e-3)
+    assert np.allclose(
+        np.sum(shap_values, axis=len(shap_values.shape) - 1),
+        margin - explainer.expected_value,
+        1e-3,
+        1e-3,
+    )
