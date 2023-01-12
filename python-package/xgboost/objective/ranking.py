e53cbca8ceea95fe60dc3cb2cecb79051c1853db
jiamingy <jm.yuan@outlook.com>
2023-01-13 05:47:32 +0800

    python binding.

---
+++ python-package/xgboost/objective/ranking.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict
+
+from xgboost._typing import FloatCompatible
+
+from .base import _BuiltinObjFunction, objective_doc
+
+
+@objective_doc
+class LambdaMartNDCG(_BuiltinObjFunction):
+    # fixme: maybe sequence of truncations?
+    def __init__(self, truncation: FloatCompatible) -> None:
+        self.truncation = truncation
+
+    @staticmethod
+    def name() -> str:
+        return "lambdamart:ndcg"
+
+    def _save_config(self) -> Dict[str, Any]:
+        return {"truncation": self.truncation}
