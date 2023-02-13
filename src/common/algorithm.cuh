b40c7d92ed213dd6469b1a3c09139f30a8f73efe
Jiaming Yuan <jm.yuan@outlook.com>
2023-02-13 16:11:11 +0800

    tidy.

--- src/common/algorithm.cuh
+++ src/common/algorithm.cuh
@@ -1,20 +1,26 @@
 /**
  * Copyright 2022-2023 by XGBoost Contributors
  */
-#pragma once
+#ifndef XGBOOST_COMMON_ALGORITHM_CUH_
+#define XGBOOST_COMMON_ALGORITHM_CUH_
 
 #include <thrust/copy.h>       // copy
 #include <thrust/sort.h>       // stable_sort_by_key
+#include <thrust/tuple.h>      // tuple,get
 
+#include <cstddef>             // size_t
+#include <cstdint>             // int32_t
 #include <cub/cub.cuh>         // DispatchSegmentedRadixSort,NullType,DoubleBuffer
 #include <iterator>            // distance
 #include <limits>              // numeric_limits
 #include <type_traits>         // conditional_t,remove_const_t
 
+#include "common.h"            // safe_cuda
 #include "cuda_context.cuh"    // CUDAContext
-#include "device_helpers.cuh"  // TemporaryArray
-#include "xgboost/base.h"
+#include "device_helpers.cuh"  // TemporaryArray,SegmentId,LaunchN,Iota,device_vector
+#include "xgboost/base.h"      // XGBOOST_DEVICE
 #include "xgboost/context.h"   // Context
+#include "xgboost/logging.h"   // CHECK
 #include "xgboost/span.h"      // Span,byte
 
 namespace xgboost {
@@ -24,13 +30,13 @@ namespace detail {
 template <bool IS_DESCENDING, typename KeyT, typename BeginOffsetIteratorT,
           typename EndOffsetIteratorT>
 static void DeviceSegmentedRadixSortKeys(CUDAContext const *ctx, void *d_temp_storage,
-                                         size_t &temp_storage_bytes, const KeyT *d_keys_in,
-                                         KeyT *d_keys_out, int num_items, int num_segments,
-                                         BeginOffsetIteratorT d_begin_offsets,
+                                         std::size_t &temp_storage_bytes,  // NOLINT
+                                         const KeyT *d_keys_in, KeyT *d_keys_out, int num_items,
+                                         int num_segments, BeginOffsetIteratorT d_begin_offsets,
                                          EndOffsetIteratorT d_end_offsets, int begin_bit = 0,
                                          int end_bit = sizeof(KeyT) * 8,
                                          bool debug_synchronous = false) {
-  typedef int OffsetT;
+  using OffsetT = int;
 
   // Null value type
   cub::DoubleBuffer<KeyT> d_keys(const_cast<KeyT *>(d_keys_in), d_keys_out);
@@ -46,18 +52,19 @@ static void DeviceSegmentedRadixSortKeys(CUDAContext const *ctx, void *d_temp_st
 // Wrapper around cub sort for easier `descending` sort.
 template <bool descending, typename KeyT, typename ValueT, typename BeginOffsetIteratorT,
           typename EndOffsetIteratorT>
-void DeviceSegmentedRadixSortPair(void *d_temp_storage, size_t &temp_storage_bytes,
-                                  const KeyT *d_keys_in,  // NOLINT
-                                  KeyT *d_keys_out, const ValueT *d_values_in, ValueT *d_values_out,
-                                  size_t num_items, size_t num_segments,
+void DeviceSegmentedRadixSortPair(void *d_temp_storage,
+                                  std::size_t &temp_storage_bytes,  // NOLINT
+                                  const KeyT *d_keys_in, KeyT *d_keys_out,
+                                  const ValueT *d_values_in, ValueT *d_values_out,
+                                  std::size_t num_items, std::size_t num_segments,
                                   BeginOffsetIteratorT d_begin_offsets,
                                   EndOffsetIteratorT d_end_offsets, dh::CUDAStreamView stream,
                                   int begin_bit = 0, int end_bit = sizeof(KeyT) * 8) {
   cub::DoubleBuffer<KeyT> d_keys(const_cast<KeyT *>(d_keys_in), d_keys_out);
   cub::DoubleBuffer<ValueT> d_values(const_cast<ValueT *>(d_values_in), d_values_out);
   // In old version of cub, num_items in dispatch is also int32_t, no way to change.
-  using OffsetT =
-      std::conditional_t<dh::BuildWithCUDACub() && dh::HasThrustMinorVer<13>(), size_t, int32_t>;
+  using OffsetT = std::conditional_t<dh::BuildWithCUDACub() && dh::HasThrustMinorVer<13>(),
+                                     std::size_t, std::int32_t>;
   CHECK_LE(num_items, std::numeric_limits<OffsetT>::max());
   // For Thrust >= 1.12 or CUDA >= 11.4, we require system cub installation
 
@@ -87,7 +94,7 @@ void DeviceSegmentedRadixSortPair(void *d_temp_storage, size_t &temp_storage_byt
 template <typename U, typename V>
 void SegmentedSequence(Context const *ctx, Span<U> d_offset_ptr, Span<V> out_sequence) {
   dh::LaunchN(out_sequence.size(), ctx->CUDACtx()->Stream(),
-              [out_sequence, d_offset_ptr] __device__(size_t idx) {
+              [out_sequence, d_offset_ptr] __device__(std::size_t idx) {
                 auto group = dh::SegmentId(d_offset_ptr, idx);
                 out_sequence[idx] = idx - d_offset_ptr[group];
               });
@@ -97,8 +104,8 @@ template <bool descending, typename U, typename V>
 inline void SegmentedSortKeys(Context const *ctx, Span<V const> group_ptr,
                               Span<U> out_sorted_values) {
   CHECK_GE(group_ptr.size(), 1ul);
-  size_t n_groups = group_ptr.size() - 1;
-  size_t bytes = 0;
+  std::size_t n_groups = group_ptr.size() - 1;
+  std::size_t bytes = 0;
   auto const *cuctx = ctx->CUDACtx();
   CHECK(cuctx);
   detail::DeviceSegmentedRadixSortKeys<descending>(
@@ -121,8 +128,8 @@ template <bool accending, bool per_seg_index, typename U, typename V, typename I
 void SegmentedArgSort(Context const *ctx, Span<U> values, Span<V> group_ptr,
                       Span<IdxT> sorted_idx) {
   CHECK_GE(group_ptr.size(), 1ul);
-  size_t n_groups = group_ptr.size() - 1;
-  size_t bytes = 0;
+  std::size_t n_groups = group_ptr.size() - 1;
+  std::size_t bytes = 0;
   if (per_seg_index) {
     SegmentedSequence(ctx, group_ptr, sorted_idx);
   } else {
@@ -151,15 +158,15 @@ void SegmentedArgSort(Context const *ctx, Span<U> values, Span<V> group_ptr,
  */
 template <typename SegIt, typename ValIt>
 void SegmentedArgMergeSort(Context const *ctx, SegIt seg_begin, SegIt seg_end, ValIt val_begin,
-                           ValIt val_end, dh::device_vector<size_t> *p_sorted_idx) {
-  using Tup = thrust::tuple<int32_t, float>;
+                           ValIt val_end, dh::device_vector<std::size_t> *p_sorted_idx) {
+  using Tup = thrust::tuple<std::int32_t, float>;
   auto &sorted_idx = *p_sorted_idx;
-  size_t n = std::distance(val_begin, val_end);
+  std::size_t n = std::distance(val_begin, val_end);
   sorted_idx.resize(n);
   dh::Iota(dh::ToSpan(sorted_idx));
   dh::device_vector<Tup> keys(sorted_idx.size());
   auto key_it = dh::MakeTransformIterator<Tup>(thrust::make_counting_iterator(0ul),
-                                               [=] XGBOOST_DEVICE(size_t i) -> Tup {
+                                               [=] XGBOOST_DEVICE(std::size_t i) -> Tup {
                                                  int32_t leaf_idx;
                                                  if (i < *seg_begin) {
                                                    leaf_idx = -1;
@@ -170,8 +177,6 @@ void SegmentedArgMergeSort(Context const *ctx, SegIt seg_begin, SegIt seg_end, V
                                                  return thrust::make_tuple(leaf_idx, residue);
                                                });
   thrust::copy(ctx->CUDACtx()->CTP(), key_it, key_it + keys.size(), keys.begin());
-
-  dh::XGBDeviceAllocator<char> alloc;
   thrust::stable_sort_by_key(ctx->CUDACtx()->TP(), keys.begin(), keys.end(), sorted_idx.begin(),
                              [=] XGBOOST_DEVICE(Tup const &l, Tup const &r) {
                                if (thrust::get<0>(l) != thrust::get<0>(r)) {
@@ -182,3 +187,4 @@ void SegmentedArgMergeSort(Context const *ctx, SegIt seg_begin, SegIt seg_end, V
 }
 }  // namespace common
 }  // namespace xgboost
+#endif  // XGBOOST_COMMON_ALGORITHM_CUH_
