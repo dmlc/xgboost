89933b3898d28cef82ffa0ece4cee9fd4e581717
Rong Ou <rong.ou@gmail.com>
2023-02-17 11:44:24 -0800

    clean up tests

--- tests/cpp/tree/test_approx.cc
+++ tests/cpp/tree/test_approx.cc
@@ -10,22 +10,29 @@
 
 namespace xgboost {
 namespace tree {
+
+namespace {
+std::vector<float> GenerateHess(size_t n_samples) {
+  auto grad = GenerateRandomGradients(n_samples);
+  std::vector<float> hess(grad.Size());
+  std::transform(grad.HostVector().cbegin(), grad.HostVector().cend(), hess.begin(),
+                 [](auto gpair) { return gpair.GetHess(); });
+  return hess;
+}
+}  // anonymous namespace
+
 TEST(Approx, Partitioner) {
   size_t n_samples = 1024, n_features = 1, base_rowid = 0;
   Context ctx;
+  ctx.InitAllowUnknown(Args{});
   CommonRowPartitioner partitioner{&ctx, n_samples, base_rowid, false};
   ASSERT_EQ(partitioner.base_rowid, base_rowid);
   ASSERT_EQ(partitioner.Size(), 1);
   ASSERT_EQ(partitioner.Partitions()[0].Size(), n_samples);
 
-  auto Xy = RandomDataGenerator{n_samples, n_features, 0}.GenerateDMatrix(true);
-  ctx.InitAllowUnknown(Args{});
+  auto const Xy = RandomDataGenerator{n_samples, n_features, 0}.GenerateDMatrix(true);
+  auto hess = GenerateHess(n_samples);
   std::vector<CPUExpandEntry> candidates{{0, 0, 0.4}};
-
-  auto grad = GenerateRandomGradients(n_samples);
-  std::vector<float> hess(grad.Size());
-  std::transform(grad.HostVector().cbegin(), grad.HostVector().cend(), hess.begin(),
-                 [](auto gpair) { return gpair.GetHess(); });
 
   for (auto const& page : Xy->GetBatches<GHistIndexMatrix>({64, hess, true})) {
     bst_feature_t const split_ind = 0;
@@ -67,21 +74,15 @@ TEST(Approx, Partitioner) {
 }
 
 namespace {
-void TestColumnSplitPartitioner(size_t n_samples, size_t n_features, size_t base_rowid,
-                                float min_value, float mid_value,
+void TestColumnSplitPartitioner(size_t n_samples, size_t base_rowid, std::shared_ptr<DMatrix> Xy,
+                                std::vector<float>* hess, float min_value, float mid_value,
                                 CommonRowPartitioner const& expected_mid_partitioner) {
-  auto Xy = RandomDataGenerator{n_samples, n_features, 0}.GenerateDMatrix(true);
-  Xy = std::shared_ptr<DMatrix>{Xy->SliceCol(collective::GetWorldSize(), collective::GetRank())};
+  auto dmat =
+      std::unique_ptr<DMatrix>{Xy->SliceCol(collective::GetWorldSize(), collective::GetRank())};
   std::vector<CPUExpandEntry> candidates{{0, 0, 0.4}};
-
-  auto grad = GenerateRandomGradients(n_samples);
-  std::vector<float> hess(grad.Size());
-  std::transform(grad.HostVector().cbegin(), grad.HostVector().cend(), hess.begin(),
-                 [](auto gpair) { return gpair.GetHess(); });
-
   Context ctx;
   ctx.InitAllowUnknown(Args{});
-  for (auto const& page : Xy->GetBatches<GHistIndexMatrix>({64, hess, true})) {
+  for (auto const& page : dmat->GetBatches<GHistIndexMatrix>({64, *hess, true})) {
     {
       RegTree tree;
       CommonRowPartitioner partitioner{&ctx, n_samples, base_rowid, true};
@@ -121,63 +122,28 @@ void TestColumnSplitPartitioner(size_t n_samples, size_t n_features, size_t base
 
 TEST(Approx, PartitionerColSplit) {
   size_t n_samples = 1024, n_features = 16, base_rowid = 0;
-  Context ctx;
-  CommonRowPartitioner partitioner{&ctx, n_samples, base_rowid, false};
-  ASSERT_EQ(partitioner.base_rowid, base_rowid);
-  ASSERT_EQ(partitioner.Size(), 1);
-  ASSERT_EQ(partitioner.Partitions()[0].Size(), n_samples);
-
-  auto Xy = RandomDataGenerator{n_samples, n_features, 0}.GenerateDMatrix(true);
-  ctx.InitAllowUnknown(Args{});
+  auto const Xy = RandomDataGenerator{n_samples, n_features, 0}.GenerateDMatrix(true);
+  auto hess = GenerateHess(n_samples);
   std::vector<CPUExpandEntry> candidates{{0, 0, 0.4}};
 
-  auto grad = GenerateRandomGradients(n_samples);
-  std::vector<float> hess(grad.Size());
-  std::transform(grad.HostVector().cbegin(), grad.HostVector().cend(), hess.begin(),
-                 [](auto gpair) { return gpair.GetHess(); });
-
   float min_value, mid_value;
+  Context ctx;
+  ctx.InitAllowUnknown(Args{});
   CommonRowPartitioner mid_partitioner{&ctx, n_samples, base_rowid, false};
   for (auto const& page : Xy->GetBatches<GHistIndexMatrix>({64, hess, true})) {
     bst_feature_t const split_ind = 0;
-    {
-      min_value = page.cut.MinValues()[split_ind];
-      RegTree tree;
-      CommonRowPartitioner min_partitioner{&ctx, n_samples, base_rowid, false};
-      GetSplit(&tree, min_value, &candidates);
-      min_partitioner.UpdatePosition(&ctx, page, candidates, &tree);
-      ASSERT_EQ(min_partitioner.Size(), 3);
-      ASSERT_EQ(min_partitioner[1].Size(), 0);
-      ASSERT_EQ(min_partitioner[2].Size(), n_samples);
-    }
-    {
-      auto ptr = page.cut.Ptrs()[split_ind + 1];
-      mid_value = page.cut.Values().at(ptr / 2);
-      RegTree tree;
-      GetSplit(&tree, mid_value, &candidates);
-      mid_partitioner.UpdatePosition(&ctx, page, candidates, &tree);
+    min_value = page.cut.MinValues()[split_ind];
 
-      auto left_nidx = tree[RegTree::kRoot].LeftChild();
-      auto elem = mid_partitioner[left_nidx];
-      ASSERT_LT(elem.Size(), n_samples);
-      ASSERT_GT(elem.Size(), 1);
-      for (auto it = elem.begin; it != elem.end; ++it) {
-        auto value = page.cut.Values().at(page.index[*it * n_features]);
-        ASSERT_LE(value, mid_value);
-      }
-
-      auto right_nidx = tree[RegTree::kRoot].RightChild();
-      elem = mid_partitioner[right_nidx];
-      for (auto it = elem.begin; it != elem.end; ++it) {
-        auto value = page.cut.Values().at(page.index[*it * n_features]);
-        ASSERT_GT(value, mid_value) << *it;
-      }
-    }
+    auto ptr = page.cut.Ptrs()[split_ind + 1];
+    mid_value = page.cut.Values().at(ptr / 2);
+    RegTree tree;
+    GetSplit(&tree, mid_value, &candidates);
+    mid_partitioner.UpdatePosition(&ctx, page, candidates, &tree);
   }
 
   auto constexpr kWorkers = 4;
-  RunWithInMemoryCommunicator(kWorkers, TestColumnSplitPartitioner, n_samples, n_features,
-                              base_rowid, min_value, mid_value, mid_partitioner);
+  RunWithInMemoryCommunicator(kWorkers, TestColumnSplitPartitioner, n_samples, base_rowid, Xy,
+                              &hess, min_value, mid_value, mid_partitioner);
 }
 
 namespace {
