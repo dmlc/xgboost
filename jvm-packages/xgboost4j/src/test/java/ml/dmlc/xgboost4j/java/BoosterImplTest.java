b3796b74b64c70862b1f4f64e3e902ce70e28e18
Jon Yoquinto <jyoquinto@sovrn.com>
2023-03-23 13:40:37 -0600

    a dMatrix for testing

--- jvm-packages/xgboost4j/src/test/java/ml/dmlc/xgboost4j/java/BoosterImplTest.java
+++ jvm-packages/xgboost4j/src/test/java/ml/dmlc/xgboost4j/java/BoosterImplTest.java
@@ -95,16 +95,18 @@ class InplacePredictThread extends Thread {
   float[][] testX;
   int test_rows;
   int features;
+  DMatrix dMatrix;
   float[][] true_predicts;
   Booster booster;
   Random rng = new Random();
   int n_preds = 100;
 
-  public InplacePredictThread(int n, Booster booster, float[][] testX, int test_rows, int features, float[][] true_predicts) {
+  public InplacePredictThread(int n, Booster booster, float[][] testX, int test_rows, int features, DMatrix dMatrix, float[][] true_predicts) {
     this.thread_num = n;
     this.booster = booster;
     this.testX = testX;
     this.test_rows = test_rows;
+    this.dMatrix = dMatrix;
     this.features = features;
     this.true_predicts = true_predicts;
   }
@@ -120,7 +122,7 @@ class InplacePredictThread extends Thread {
         int r = this.rng.nextInt(this.test_rows);
 
         // In-place predict a single random row
-        float[][] predictions = booster.inplace_predict(this.testX[r], 1, this.features);
+        float[][] predictions = booster.inplace_predict(this.testX[r], 1, this.features, this.dMatrix);
 
         // Confirm results as expected
         if (predictions[0][0] != this.true_predicts[r][0]) {
