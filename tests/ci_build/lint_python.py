8c8e8c0d63a1fe7860f63ad995c21a9159e7d337
Jiaming Yuan <jm.yuan@outlook.com>
2023-03-24 02:44:43 +0800

    Run linters with the multi output demo.

--- tests/ci_build/lint_python.py
+++ tests/ci_build/lint_python.py
@@ -161,6 +161,7 @@ def main(args: argparse.Namespace) -> None:
                 "demo/guide-python/spark_estimator_examples.py",
                 "demo/guide-python/individual_trees.py",
                 "demo/guide-python/quantile_regression.py",
+                "demo/guide-python/multioutput_regression.py",
                 # CI
                 "tests/ci_build/lint_python.py",
                 "tests/ci_build/test_r_package.py",
@@ -204,6 +205,7 @@ def main(args: argparse.Namespace) -> None:
                 "demo/guide-python/feature_weights.py",
                 "demo/guide-python/individual_trees.py",
                 "demo/guide-python/quantile_regression.py",
+                "demo/guide-python/multioutput_regression.py",
                 # tests
                 "tests/python/test_dt.py",
                 "tests/python/test_data_iterator.py",
