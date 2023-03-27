148e29ec4c29a516a3b45101c1e23858229adf71
Jon Yoquinto <jyoquinto@sovrn.com>
2023-03-27 14:41:13 -0600

    undo static cast

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -1050,6 +1050,7 @@ XGB_DLL int XGBoosterInplacePredict(BoosterHandle handle,
     }
   }
   fprintf (stdout, reinterpret_cast<const char *>(p_m.get()));
+  p_m.reset(new data::DMatrixProxy);
   auto stuff = dynamic_cast<data::DMatrixProxy *>(p_m.get());
   auto proxy = new std::shared_ptr<xgboost::data::DMatrixProxy>(new xgboost::data::DMatrixProxy);
   printf ("stuff is %s", typeid(stuff).name());
