7a15e7cfcec3df42c1e6bc86d0626866306ece29
Jiaming Yuan <jm.yuan@outlook.com>
2023-04-26 17:07:46 +0800

    ser.

--- python-package/xgboost/dask.py
+++ python-package/xgboost/dask.py
@@ -73,7 +73,7 @@ from .core import (
     _deprecate_positional_args,
     _expect,
 )
-from .data import _is_cudf_df, _is_cupy_array
+from .data import _is_cudf_ser, _is_cupy_array
 from .sklearn import (
     XGBClassifier,
     XGBClassifierBase,
@@ -1899,7 +1899,7 @@ class DaskXGBClassifier(DaskScikitLearnBase, XGBClassifierMixIn, XGBClassifierBa
             self.classes_ = await self.client.compute(da.unique(y))
         else:
             self.classes_ = await self.client.compute(y.drop_duplicates())
-        if _is_cudf_df(self.classes_):
+        if _is_cudf_ser(self.classes_):
             self.classes_ = self.classes_.to_cupy()
         if _is_cupy_array(self.classes_):
             self.classes_ = self.classes_.get()
