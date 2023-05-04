939b117b0a8695267c643fb7b761259fd9b88da0
Jiaming Yuan <jm.yuan@outlook.com>
2023-05-04 22:14:42 +0800

    Fix monotone constraints on CPU.

--- tests/cpp/tree/test_constraints.cc
+++ tests/cpp/tree/test_constraints.cc
@@ -6,6 +6,8 @@
 #include <string>
 
 #include "../../../src/tree/constraints.h"
+#include "../../../src/tree/hist/evaluate_splits.h"
+#include "../helpers.h"
 
 namespace xgboost {
 namespace tree {
@@ -56,5 +58,37 @@ TEST(CPUFeatureInteractionConstraint, Basic) {
   ASSERT_FALSE(constraints.Query(1, 5));
 }
 
+TEST(CPUMonoConstraint, Basic) {
+  std::size_t kRows{64}, kCols{16};
+  Context ctx;
+
+  TrainParam param;
+  std::vector<std::int32_t> mono(kCols, 1);
+  I32Array arr;
+  for (std::size_t i = 0; i < kCols; ++i) {
+    arr.GetArray().push_back(mono[i]);
+  }
+  Json jarr{std::move(arr)};
+  std::string str_mono;
+  Json::Dump(jarr, &str_mono);
+  str_mono.front() = '(';
+  str_mono.back() = ')';
+
+  param.UpdateAllowUnknown(Args{{"monotone_constraints", str_mono}});
+
+  auto Xy = RandomDataGenerator{kRows, kCols, 0.0}.GenerateDMatrix(true);
+  auto sampler = std::make_shared<common::ColumnSampler>();
+
+  HistEvaluator<CPUExpandEntry> evalutor{&ctx, &param, Xy->Info(), sampler};
+  evalutor.InitRoot(GradStats{2.0, 2.0});
+
+  SplitEntry split;
+  split.Update(1.0f, 0, 3.0, false, false, GradStats{1.0, 1.0}, GradStats{1.0, 1.0});
+  CPUExpandEntry entry{0, 0, split};
+  RegTree tree{1, static_cast<bst_feature_t>(kCols)};
+  evalutor.ApplyTreeSplit(entry, &tree);
+
+  ASSERT_TRUE(evalutor.Evaluator().has_constraint);
+}
 }  // namespace tree
 }  // namespace xgboost
