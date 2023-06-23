3ad467924d06748fc26565bf4312d4927809d541
Jiaming Yuan <jm.yuan@outlook.com>
2023-06-24 02:24:36 +0800

    Use read vec.

--- src/data/ellpack_page_raw_format.cu
+++ src/data/ellpack_page_raw_format.cu
@@ -2,18 +2,17 @@
  * Copyright 2019-2023, XGBoost contributors
  */
 #include <dmlc/registry.h>
-#include <xgboost/data.h>
 
 #include <cstddef>  // for size_t
 
-#include "../common/io.h"  // for AlignedResourceReadStream, AlignedFileWriteStream
+#include "../common/io.h"                 // for AlignedResourceReadStream, AlignedFileWriteStream
+#include "../common/ref_resource_view.h"  // for ReadVec, WriteVec
 #include "ellpack_page.cuh"
-#include "histogram_cut_format.h"
-#include "sparse_page_writer.h"
+#include "histogram_cut_format.h"  // for ReadHistogramCuts, WriteHistogramCuts
+#include "sparse_page_writer.h"    // for SparsePageFormat
 
 namespace xgboost::data {
 DMLC_REGISTRY_FILE_TAG(ellpack_page_raw_format);
-
 
 class EllpackPageRawFormat : public SparsePageFormat<EllpackPage> {
  public:
@@ -31,7 +30,7 @@ class EllpackPageRawFormat : public SparsePageFormat<EllpackPage> {
     if (!fi->Read(&impl->row_stride)) {
       return false;
     }
-    if (!fi->Read(&impl->gidx_buffer.HostVector())) {
+    if (!common::ReadVec(fi, &impl->gidx_buffer.HostVector())) {
       return false;
     }
     if (!fi->Read(&impl->base_rowid)) {
@@ -56,7 +55,5 @@ class EllpackPageRawFormat : public SparsePageFormat<EllpackPage> {
 
 XGBOOST_REGISTER_ELLPACK_PAGE_FORMAT(raw)
     .describe("Raw ELLPACK binary data format.")
-    .set_body([]() {
-      return new EllpackPageRawFormat();
-    });
+    .set_body([]() { return new EllpackPageRawFormat(); });
 }  // namespace xgboost::data
