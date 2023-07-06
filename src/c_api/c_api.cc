4589f29531313a0200dfcf8b8de48f449c2b5955
Eduardo Múgica <eduardo@acuratio.com>
2023-07-06 13:32:53 +0200

    Read data_split_mode  and pass it to DMatrix

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -464,7 +464,9 @@ XGB_DLL int XGDMatrixCreateFromDense(char const *data,
   float missing = GetMissing(config);
   auto n_threads = OptionalArg<Integer, int64_t>(config, "nthread", 0);
   xgboost_CHECK_C_ARG_PTR(out);
-  *out = new std::shared_ptr<DMatrix>(DMatrix::Create(&adapter, missing, n_threads));
+  auto data_split_mode =
+      static_cast<DataSplitMode>(OptionalArg<Integer, int64_t>(config, "data_split_mode", 0));
+  *out = new std::shared_ptr<DMatrix>(DMatrix::Create(&adapter, missing, n_threads, std::string(""), data_split_mode));
   API_END();
 }
 
