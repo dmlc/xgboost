3da64e11770aafae9ca49111746f2d65a3943b8b
Rong Ou <rong.ou@gmail.com>
2023-07-07 18:12:46 -0700

    Support column split with GPU quantile

--- src/data/iterative_dmatrix.cu
+++ src/data/iterative_dmatrix.cu
@@ -106,7 +106,7 @@ void IterativeDMatrix::InitFromCUDA(Context const* ctx, BatchParam const& p,
     sketch_containers.clear();
     sketch_containers.shrink_to_fit();
 
-    final_sketch.MakeCuts(&cuts);
+    final_sketch.MakeCuts(&cuts, this->info_.IsColumnSplit());
   } else {
     GetCutsFromRef(ctx, ref, Info().num_col_, p, &cuts);
   }
