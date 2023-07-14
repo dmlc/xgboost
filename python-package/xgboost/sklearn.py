a75accf0860f84556dbb684f047981d0efbf563d
Jiaming Yuan <jm.yuan@outlook.com>
2023-07-14 12:25:38 +0800

    Fix.

--- python-package/xgboost/sklearn.py
+++ python-package/xgboost/sklearn.py
@@ -943,7 +943,7 @@ class XGBModel(XGBModelBase):
 
     def _create_dmatrix(self, ref: Optional[DMatrix], **kwargs: Any) -> DMatrix:
         # Use `QuantileDMatrix` to save memory.
-        if _can_use_qdm(self.tree_method):
+        if _can_use_qdm(self.tree_method) and self.booster != "gblinear":
             try:
                 return QuantileDMatrix(
                     **kwargs, ref=ref, nthread=self.n_jobs, max_bin=self.max_bin
