20676a18900408f2cc983d3e674cfde97194861e
Jiaming Yuan <jm.yuan@outlook.com>
2023-08-08 20:39:56 +0800

    tests.

--- python-package/xgboost/data.py
+++ python-package/xgboost/data.py
@@ -1268,7 +1268,7 @@ def _proxy_transform(
     feature_names: Optional[FeatureNames],
     feature_types: Optional[FeatureTypes],
     enable_categorical: bool,
-) -> Tuple[Any, Optional[list], Optional[FeatureNames], Optional[FeatureTypes],]:
+) -> Tuple[Any, Optional[list], Optional[FeatureNames], Optional[FeatureTypes]]:
     if _is_cudf_df(data) or _is_cudf_ser(data):
         return _transform_cudf_df(
             data, feature_names, feature_types, enable_categorical
