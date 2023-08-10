718f739f6250f55f8fd41bbbb3e0ec0068e2d9de
James Lamb <jaylamb20@gmail.com>
2023-08-09 21:19:50 -0500

    [R] [CI] use lintr 3.1.0

--- tests/ci_build/lint_r.R
+++ tests/ci_build/lint_r.R
@@ -20,15 +20,23 @@ my_linters <- list(
   any_duplicated = lintr::any_duplicated_linter(),
   any_is_na = lintr::any_is_na_linter(),
   assignment_linter = lintr::assignment_linter(),
+  boolean_arithmetic = lintr::boolean_arithmetic_linter(),
   brace_linter = lintr::brace_linter(),
+  class_equals = lintr::class_equals_linter(),
   commas_linter = lintr::commas_linter(),
+  empty_assignment = lintr::empty_assignment_linter(),
   equals_na = lintr::equals_na_linter(),
   fixed_regex = lintr::fixed_regex_linter(),
+  for_loop_index = lintr::for_loop_index_linter(),
+  function_return = lintr::function_return_linter(),
   infix_spaces_linter = lintr::infix_spaces_linter(),
+  is_numeric = lintr::is_numeric_linter(),
   line_length_linter = lintr::line_length_linter(length = 150L),
-  no_tab_linter = lintr::no_tab_linter(),
+  lengths = lintr::lengths_linter(),
+  matrix = lintr::matrix_apply_linter(),
   object_usage_linter = lintr::object_usage_linter(),
   object_length_linter = lintr::object_length_linter(),
+  routine_registration = lintr::routine_registration_linter(),
   semicolon = lintr::semicolon_linter(),
   seq = lintr::seq_linter(),
   spaces_inside_linter = lintr::spaces_inside_linter(),
@@ -37,9 +45,10 @@ my_linters <- list(
   trailing_blank_lines_linter = lintr::trailing_blank_lines_linter(),
   trailing_whitespace_linter = lintr::trailing_whitespace_linter(),
   true_false = lintr::T_and_F_symbol_linter(),
-  unneeded_concatenation = lintr::unneeded_concatenation_linter(),
+  unnecessary_concatenation = lintr::unnecessary_concatenation_linter(),
   unreachable_code = lintr::unreachable_code_linter(),
-  vector_logic = lintr::vector_logic_linter()
+  vector_logic = lintr::vector_logic_linter(),
+  whitespace = lintr::whitespace_linter()
 )
 
 noquote(paste0(length(FILES_TO_LINT), " R files need linting"))
