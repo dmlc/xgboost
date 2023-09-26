1b2d7cc95b5e5151aaad7bcb88293c7746f88c2f
Rong Ou <rong.ou@gmail.com>
2023-09-26 11:28:29 -0700

    refactor colsample tests

--- tests/cpp/test_learner.cc
+++ tests/cpp/test_learner.cc
@@ -720,48 +720,43 @@ INSTANTIATE_TEST_SUITE_P(ColumnSplitObjective, TestColumnSplit,
                          });
 
 namespace {
+Json GetColumnSamplerModel(std::shared_ptr<DMatrix> dmat, std::string const& tree_method,
+                           std::string const& objective, std::string const& device) {
+  std::unique_ptr<Learner> learner{Learner::Create({dmat})};
+  learner->SetParam("tree_method", tree_method);
+  learner->SetParam("device", device);
+  learner->SetParam("objective", objective);
+  learner->SetParam("colsample_bytree", "0.5");
+  learner->SetParam("colsample_bylevel", "0.6");
+  learner->SetParam("colsample_bynode", "0.7");
+  learner->UpdateOneIter(0, dmat);
+  Json model{Object{}};
+  learner->SaveModel(&model);
+  return model;
+}
+
 void VerifyColumnSplitColumnSampler(std::string const& tree_method, bool use_gpu,
                                     Json const& expected_model) {
-  Json model{Object{}};
-  {
-    auto const world_size = collective::GetWorldSize();
-    auto const rank = collective::GetRank();
-    auto const objective = "reg:logistic";
-    auto p_fmat = MakeFmatForObjTest(objective);
-    std::shared_ptr<DMatrix> sliced{p_fmat->SliceCol(world_size, rank)};
-    std::unique_ptr<Learner> learner{Learner::Create({sliced})};
-    learner->SetParam("tree_method", tree_method);
-    if (use_gpu) {
-      auto gpu_id = common::AllVisibleGPUs() == 1 ? 0 : rank;
-      learner->SetParam("device", "cuda:" + std::to_string(gpu_id));
-    }
-    learner->SetParam("objective", objective);
-    learner->SetParam("colsample_bytree", "0.5");
-    learner->SetParam("colsample_bylevel", "0.6");
-    learner->SetParam("colsample_bynode", "0.7");
-    learner->UpdateOneIter(0, sliced);
-    learner->SaveModel(&model);
+  auto const world_size = collective::GetWorldSize();
+  auto const rank = collective::GetRank();
+  auto const objective = "reg:logistic";
+  auto p_fmat = MakeFmatForObjTest(objective);
+  std::shared_ptr<DMatrix> sliced{p_fmat->SliceCol(world_size, rank)};
+  std::string device = "cpu";
+  if (use_gpu) {
+    auto gpu_id = common::AllVisibleGPUs() == 1 ? 0 : rank;
+    device = "cuda:" + std::to_string(gpu_id);
   }
+  auto model = GetColumnSamplerModel(sliced, tree_method, objective, device);
   ASSERT_EQ(model, expected_model);
 }
 
 void TestColumnSplitColumnSampler(std::string const& tree_method, bool use_gpu) {
-  Json model{Object{}};
-  {
-    auto objective = "reg:logistic";
-    auto p_fmat = MakeFmatForObjTest(objective);
-    std::unique_ptr<Learner> learner{Learner::Create({p_fmat})};
-    learner->SetParam("tree_method", tree_method);
-    if (use_gpu) {
-      learner->SetParam("device", "cuda:0");
-    }
-    learner->SetParam("objective", objective);
-    learner->SetParam("colsample_bytree", "0.5");
-    learner->SetParam("colsample_bylevel", "0.6");
-    learner->SetParam("colsample_bynode", "0.7");
-    learner->UpdateOneIter(0, p_fmat);
-    learner->SaveModel(&model);
-  }
+  auto objective = "reg:logistic";
+  auto p_fmat = MakeFmatForObjTest(objective);
+  std::string device = use_gpu ? "cuda:0" : "cpu";
+  auto model = GetColumnSamplerModel(p_fmat, tree_method, objective, device);
+
   auto world_size{3};
   if (use_gpu) {
     world_size = common::AllVisibleGPUs();
