386814c83a8fbf0cfc56e4962d96fb70ea090cc1
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-21 23:01:42 -0700

    Reduce verbosity further

--- tests/buildkite/test-macos-m1-clang11.sh
+++ tests/buildkite/test-macos-m1-clang11.sh
@@ -15,13 +15,14 @@ set +x
 # Create new Conda env
 . $HOME/mambaforge/etc/profile.d/conda.sh
 . $HOME/mambaforge/etc/profile.d/mamba.sh
-set -x
 conda_env=xgboost_dev_$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
-conda activate
-mamba env create -n ${conda_env} --file=tests/ci_build/conda_env/macos_cpu_test.yml
+mamba create -n ${conda_env} python=3.8
+conda activate ${conda_env}
+mamba env update -n ${conda_env} --file tests/ci_build/conda_env/macos_cpu_test.yml
 
 # Ensure that XGBoost can be built with Clang 11
 echo "--- Build and Test XGBoost with MacOS M1, Clang 11"
+set -x
 LLVM11_PATH=$(brew --prefix llvm\@11)
 mkdir build
 pushd build
