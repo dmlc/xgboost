641f2666cb359d3b4c0b1e00af53a9b2ce620f48
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-21 22:42:19 -0700

    Fix conda set up

--- tests/buildkite/test-macos-m1-clang11.sh
+++ tests/buildkite/test-macos-m1-clang11.sh
@@ -11,6 +11,11 @@ system_profiler SPSoftwareDataType
 sysctl -n machdep.cpu.brand_string
 uname -m
 
+# Create new Conda env
+conda_env=xgboost_dev_$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
+conda activate
+mamba env create -n ${conda_env} --file=tests/ci_build/conda_env/macos_cpu_test.yml
+
 # Ensure that XGBoost can be built with Clang 11
 echo "--- Build and Test XGBoost with MacOS M1, Clang 11"
 LLVM11_PATH=$(brew --prefix llvm\@11)
@@ -20,7 +25,3 @@ cmake .. -GNinja -DCMAKE_C_COMPILER=${LLVM11_PATH}/bin/clang \
   -DCMAKE_CXX_COMPILER=${LLVM11_PATH}/bin/clang++ -DGOOGLE_TEST=ON \
   -DUSE_DMLC_GTEST=ON
 ninja -v
-
-# Create new Conda env
-conda_env=xgboost_dev_$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
-mamba env create -n ${conda_env} --file=tests/ci_build/conda_env/macos_cpu_test.yml
