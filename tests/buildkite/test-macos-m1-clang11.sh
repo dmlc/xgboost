c3800c3e263e455bb36626cdfa45477152c1cf5f
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-21 23:07:05 -0700

    Add -y flag

--- tests/buildkite/test-macos-m1-clang11.sh
+++ tests/buildkite/test-macos-m1-clang11.sh
@@ -16,9 +16,9 @@ set +x
 . $HOME/mambaforge/etc/profile.d/conda.sh
 . $HOME/mambaforge/etc/profile.d/mamba.sh
 conda_env=xgboost_dev_$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
-mamba create -n ${conda_env} python=3.8
+mamba create -y -n ${conda_env} python=3.8
 conda activate ${conda_env}
-mamba env update -n ${conda_env} --file tests/ci_build/conda_env/macos_cpu_test.yml
+mamba env update -y -n ${conda_env} --file tests/ci_build/conda_env/macos_cpu_test.yml
 
 # Ensure that XGBoost can be built with Clang 11
 echo "--- Build and Test XGBoost with MacOS M1, Clang 11"
