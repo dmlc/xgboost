9db789b28348a3a8f5f62be6c64ebb97bfcade56
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-11-01 09:36:24 -0700

    [CI] Improve CI for Mac M1

--- tests/buildkite/test-macos-m1-clang11.sh
+++ tests/buildkite/test-macos-m1-clang11.sh
@@ -12,14 +12,17 @@ sysctl -n machdep.cpu.brand_string
 uname -m
 set +x
 
-# Create new Conda env
-echo "--- Set up Conda env"
-. $HOME/mambaforge/etc/profile.d/conda.sh
-. $HOME/mambaforge/etc/profile.d/mamba.sh
-conda_env=xgboost_dev_$(uuidgen | tr '[:upper:]' '[:lower:]' | tr -d '-')
-mamba create -y -n ${conda_env} python=3.8
-conda activate ${conda_env}
-mamba env update -n ${conda_env} --file tests/ci_build/conda_env/macos_cpu_test.yml
+# Build XGBoost4J binary
+echo "--- Build libxgboost4j.dylib"
+set -x
+mkdir build
+pushd build
+export JAVA_HOME=$(/usr/libexec/java_home)
+cmake .. -GNinja -DJVM_BINDINGS=ON
+ninja
+popd
+rm -rf build
+set +x
 
 # Ensure that XGBoost can be built with Clang 11
 echo "--- Build and Test XGBoost with MacOS M1, Clang 11"
