4f6f7a942d988eb56588d59e47ae6ef1f1739299
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-11-08 00:13:04 -0800

    Bump version to 2.0.2; revamp pom.xml

--- include/xgboost/version_config.h
+++ include/xgboost/version_config.h
@@ -6,6 +6,6 @@
 
 #define XGBOOST_VER_MAJOR 2  /* NOLINT */
 #define XGBOOST_VER_MINOR 0  /* NOLINT */
-#define XGBOOST_VER_PATCH 1  /* NOLINT */
+#define XGBOOST_VER_PATCH 2  /* NOLINT */
 
 #endif  // XGBOOST_VERSION_CONFIG_H_
