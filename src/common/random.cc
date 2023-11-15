e4bfc88e0ac02794487a5e2b5b18a8668a1677f6
Jiaming Yuan <jm.yuan@outlook.com>
2023-11-15 17:10:16 +0800

    cpu build.

--- src/common/random.cc
+++ src/common/random.cc
@@ -19,9 +19,14 @@ std::shared_ptr<HostDeviceVector<bst_feature_t>> ColumnSampler::ColSample(
   auto p_new_features = std::make_shared<HostDeviceVector<bst_feature_t>>();
 
   if (ctx_->IsCUDA()) {
+#if defined(XGBOOST_USE_CUDA)
     cuda_impl::SampleFeature(ctx_, n, p_features, p_new_features, this->feature_weights_,
                              &this->weight_buffer_, &this->idx_buffer_, rng_);
     return p_new_features;
+#else
+    AssertGPUSupport();
+    return nullptr;
+#endif  // defined(XGBOOST_USE_CUDA)
   }
 
   const auto &features = p_features->HostVector();
