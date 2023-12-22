e07684d1063a770f1deb229c11a9c2ea2bb63e05
Jiaming Yuan <jm.yuan@outlook.com>
2023-12-22 16:59:12 +0800

    Add timeout for distributed GPU tests.

--- tests/test_distributed/test_gpu_with_dask/test_gpu_demos.py
+++ tests/test_distributed/test_gpu_with_dask/test_gpu_demos.py
@@ -5,9 +5,13 @@ import pytest
 
 from xgboost import testing as tm
 
+pytestmark = [
+    pytest.mark.skipif(**tm.no_dask()),
+    pytest.mark.skipif(**tm.no_dask_cuda()),
+    tm.timeout(60),
+]
 
-@pytest.mark.skipif(**tm.no_dask())
-@pytest.mark.skipif(**tm.no_dask_cuda())
+
 @pytest.mark.skipif(**tm.no_cupy())
 @pytest.mark.mgpu
 def test_dask_training():
@@ -16,8 +20,6 @@ def test_dask_training():
     subprocess.check_call(cmd)
 
 
-@pytest.mark.skipif(**tm.no_dask_cuda())
-@pytest.mark.skipif(**tm.no_dask())
 @pytest.mark.mgpu
 def test_dask_sklearn_demo():
     script = os.path.join(tm.demo_dir(__file__), "dask", "sklearn_gpu_training.py")
