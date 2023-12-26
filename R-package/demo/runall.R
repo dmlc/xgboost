147e1cd31f35dff03ee15116bd7c104fb5ad6f67
david-cortes <david.cortes.rivera@gmail.com>
2023-12-26 14:12:38 +0100

    remove all references to caret package

--- R-package/demo/runall.R
+++ R-package/demo/runall.R
@@ -9,6 +9,5 @@ demo(create_sparse_matrix, package = 'xgboost')
 demo(predict_leaf_indices, package = 'xgboost')
 demo(early_stopping, package = 'xgboost')
 demo(poisson_regression, package = 'xgboost')
-demo(caret_wrapper, package = 'xgboost')
 demo(tweedie_regression, package = 'xgboost')
 #demo(gpu_accelerated, package = 'xgboost')  # can only run when built with GPU support
