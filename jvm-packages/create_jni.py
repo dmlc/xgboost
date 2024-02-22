2b6022da0eeade8fbdbd4f6b1a78a046c010405a
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-02-22 13:38:56 -0800

    Enforce 64-bit

--- jvm-packages/create_jni.py
+++ jvm-packages/create_jni.py
@@ -115,9 +115,9 @@ def native_build(args):
             if sys.platform == "win32":
                 supported_generators = (
                     "",  # empty, decided by cmake
-                    '-G"Visual Studio 17 2022"',
-                    '-G"Visual Studio 16 2019"',
-                    '-G"Visual Studio 15 2017"',
+                    '-G"Visual Studio 17 2022" -A x64',
+                    '-G"Visual Studio 16 2019" -A x64',
+                    '-G"Visual Studio 15 2017" -A x64',
                 )
                 for generator in supported_generators:
                     try:
