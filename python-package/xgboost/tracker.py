c3d73a0e3601b2b7609612f423792c5978c53b50
Jiaming Yuan <jm.yuan@outlook.com>
2024-03-27 21:22:50 +0800

    take down the jvm package for now.

--- python-package/xgboost/tracker.py
+++ python-package/xgboost/tracker.py
@@ -1,21 +1,10 @@
-"""
-This script is a variant of dmlc-core/dmlc_tracker/tracker.py,
-which is a specialized version for xgboost tasks.
-"""
-
-import argparse
+"""Tracker for XGBoost collective."""
 import ctypes
 import json
-import logging
-import socket
-import sys
 from enum import IntEnum, unique
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple, Union
 
 from .core import _LIB, _check_call, make_jcargs
-
-_RingMap = Dict[int, Tuple[int, int]]
-_TreeMap = Dict[int, List[int]]
 
 
 class RabitTracker:
@@ -95,75 +84,3 @@ class RabitTracker:
         assert c_env.value is not None
         env = json.loads(c_env.value)
         return env
-
-def start_rabit_tracker(args: argparse.Namespace) -> None:
-    """Standalone function to start rabit tracker.
-
-    Parameters
-    ----------
-    args: arguments to start the rabit tracker.
-    """
-    envs = {"n_workers": args.num_workers}
-    rabit = RabitTracker(host_ip=get_host_ip(args.host_ip), n_workers=args.num_workers)
-    envs.update(rabit.worker_envs())
-    rabit.start()
-    sys.stdout.write("DMLC_TRACKER_ENV_START\n")
-    # simply write configuration to stdout
-    for k, v in envs.items():
-        sys.stdout.write(f"{k}={v}\n")
-    sys.stdout.write("DMLC_TRACKER_ENV_END\n")
-    sys.stdout.flush()
-    rabit.join()
-
-
-def main() -> None:
-    """Main function if tracker is executed in standalone mode."""
-    parser = argparse.ArgumentParser(description="Rabit Tracker start.")
-    parser.add_argument(
-        "--num-workers",
-        required=True,
-        type=int,
-        help="Number of worker process to be launched.",
-    )
-    parser.add_argument(
-        "--num-servers",
-        default=0,
-        type=int,
-        help="Number of server process to be launched. Only used in PS jobs.",
-    )
-    parser.add_argument(
-        "--host-ip",
-        default=None,
-        type=str,
-        help=(
-            "Host IP addressed, this is only needed "
-            + "if the host IP cannot be automatically guessed."
-        ),
-    )
-    parser.add_argument(
-        "--log-level",
-        default="INFO",
-        type=str,
-        choices=["INFO", "DEBUG"],
-        help="Logging level of the logger.",
-    )
-    args = parser.parse_args()
-
-    fmt = "%(asctime)s %(levelname)s %(message)s"
-    if args.log_level == "INFO":
-        level = logging.INFO
-    elif args.log_level == "DEBUG":
-        level = logging.DEBUG
-    else:
-        raise RuntimeError(f"Unknown logging level {args.log_level}")
-
-    logging.basicConfig(format=fmt, level=level)
-
-    if args.num_servers == 0:
-        start_rabit_tracker(args)
-    else:
-        raise RuntimeError("Do not yet support start ps tracker in standalone mode.")
-
-
-if __name__ == "__main__":
-    main()
