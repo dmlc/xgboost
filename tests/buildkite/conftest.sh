a799a89697d58d20df670ffdb5c1bda7d3567325
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-04-03 02:02:11 -0700

    Remove all slashes in branch name

--- tests/buildkite/conftest.sh
+++ tests/buildkite/conftest.sh
@@ -39,11 +39,12 @@ fi
 if [[ -n $BUILDKITE_PULL_REQUEST && $BUILDKITE_PULL_REQUEST != "false" ]]
 then
   is_pull_request=1
-  export BRANCH_NAME=PR-$BUILDKITE_PULL_REQUEST
+  BRANCH_NAME=PR-$BUILDKITE_PULL_REQUEST
 else
   is_pull_request=0
-  export BRANCH_NAME=$BUILDKITE_BRANCH
+  BRANCH_NAME=$BUILDKITE_BRANCH
 fi
+export BRANCH_NAME=${BRANCH_NAME//\//-}
 
 if [[ $BUILDKITE_BRANCH == "master" || $BUILDKITE_BRANCH == "release_"* ]]
 then
