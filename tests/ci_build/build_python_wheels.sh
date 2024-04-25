f96f118b6f78e152d54d27be3feadcd21f472a28
Hyunsu Cho <phcho@nvidia.com>
2024-04-25 13:05:42 -0700

    debug

--- tests/ci_build/build_python_wheels.sh
+++ tests/ci_build/build_python_wheels.sh
@@ -47,6 +47,7 @@ if [[ "$platform_id" == macosx_* ]]; then
     export CIBW_TEST_SKIP='*-macosx_arm64'
     export CIBW_BUILD_VERBOSITY=3
 
+    source ~/.bash_profile
     mamba create -n build $OPENMP_URL
     PREFIX="/usr/local/miniconda/envs/build"
 
