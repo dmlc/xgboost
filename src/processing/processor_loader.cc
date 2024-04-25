9ff293595a26dba1cdf09e85afb6521b10614197
Zhihong Zhang <zhihongz@nvidia.com>
2024-04-25 13:20:18 -0400

    Added support for horizontal secure XGBoost

--- src/processing/processor_loader.cc
+++ src/processing/processor_loader.cc
@@ -4,28 +4,28 @@
 
 #include <iostream>
 
-#if defined(_WIN32)
+#if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #else
 #include <dlfcn.h>
 #endif
 
 #include "./processor.h"
-#include "plugins/dummy_processor.h"
+#include "plugins/mock_processor.h"
 
 namespace processing {
     using LoadFunc = Processor *(const char *);
 
     Processor* ProcessorLoader::load(const std::string& plugin_name) {
         // Dummy processor for unit testing without loading a shared library
-        if (plugin_name == kDummyProcessor) {
-            return new DummyProcessor();
+        if (plugin_name == kMockProcessor) {
+            return new MockProcessor();
         }
 
         auto lib_name = "libproc_" + plugin_name;
 
         auto extension =
-#if defined(_WIN32)
+#if defined(_WIN32) || defined(_WIN64)
             ".dll";
 #elif defined(__APPLE__) || defined(__MACH__)
             ".dylib";
@@ -46,19 +46,18 @@ namespace processing {
             lib_path = p + lib_file_name;
         }
 
-#if defined(_WIN32)
-        HMODULE handle_ = LoadLibrary(lib_path.c_str());
+#if defined(_WIN32) || defined(_WIN64)
+        handle_ = reinterpret_cast<void *>(LoadLibrary(lib_path.c_str()));
         if (!handle_) {
             std::cerr << "Failed to load the dynamic library" << std::endl;
             return NULL;
         }
 
-        void* func_ptr = GetProcAddress(handle_, kLoadFunc);
+        void* func_ptr = reinterpret_cast<void *>(GetProcAddress((HMODULE)handle_, kLoadFunc));
         if (!func_ptr) {
             std::cerr << "Failed to find loader function." << std::endl;
             return NULL;
         }
-
 #else
         handle_ = dlopen(lib_path.c_str(), RTLD_LAZY);
         if (!handle_) {
@@ -70,7 +69,6 @@ namespace processing {
             std::cerr << "Failed to find loader function: " << dlerror() << std::endl;
             return NULL;
         }
-
 #endif
 
         auto func = reinterpret_cast<LoadFunc *>(func_ptr);
