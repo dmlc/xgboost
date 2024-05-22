7633ecf88a6224ca57add591a1d50e11b51b109a
Hyunsu Cho <phcho@nvidia.com>
2024-05-21 22:23:41 -0700

    Write Python script to generate meta.json

--- tests/buildkite/build-cuda.sh
+++ tests/buildkite/build-cuda.sh
@@ -35,14 +35,18 @@ $command_wrapper tests/ci_build/build_via_cmake.sh \
 echo "--- Build binary wheel"
 $command_wrapper bash -c \
   "cd python-package && rm -rf dist/* && pip wheel --no-deps -v . --wheel-dir dist/"
-$command_wrapper python tests/ci_build/rename_whl.py python-package/dist/*.whl \
-  ${BUILDKITE_COMMIT} ${WHEEL_TAG}
+$command_wrapper python tests/ci_build/rename_whl.py  \
+  --wheel-path python-package/dist/*.whl  \
+  --commit-hash ${BUILDKITE_COMMIT}  \
+  --platform-tag ${WHEEL_TAG}
 
 echo "--- Audit binary wheel to ensure it's compliant with manylinux2014 standard"
 tests/ci_build/ci_build.sh auditwheel_x86_64 auditwheel repair \
   --plat ${WHEEL_TAG} python-package/dist/*.whl
-$command_wrapper python tests/ci_build/rename_whl.py wheelhouse/*.whl \
-  ${BUILDKITE_COMMIT} ${WHEEL_TAG}
+$command_wrapper python tests/ci_build/rename_whl.py  \
+  --wheel-path wheelhouse/*.whl  \
+  --commit-hash ${BUILDKITE_COMMIT}  \
+  --platform-tag ${WHEEL_TAG}
 mv -v wheelhouse/*.whl python-package/dist/
 # Make sure that libgomp.so is vendored in the wheel
 tests/ci_build/ci_build.sh auditwheel_x86_64 bash -c \
@@ -56,9 +60,12 @@ then
     --acl public-read --no-progress
 
   # Generate the meta info which includes xgboost version and the commit info
-  whl_name=`basename $(ls python-package/dist/*.whl | head -n 1)`
-  tests/ci_build/generate_meta.sh $whl_name python-package/dist/meta.xml
-  aws s3 cp python-package/dist/meta.xml s3://xgboost-nightly-builds/${BRANCH_NAME}/ \
+  $command_wrapper python tests/ci_build/format_wheel_meta.py \
+    --wheel-path python-package/dist/*.whl  \
+    --commit-hash ${BUILDKITE_COMMIT}  \
+    --platform-tag ${WHEEL_TAG}  \
+    --meta-path python-package/dist/
+  aws s3 cp python-package/dist/meta.json s3://xgboost-nightly-builds/${BRANCH_NAME}/ \
     --acl public-read --no-progress
 fi
 echo "-- Stash C++ test executable (testxgboost)"
