5086decb0c4e219b8383ad655cb684a76901de4c
david-cortes <david.cortes.rivera@gmail.com>
2024-05-24 11:28:30 +0200

    [R] Reshape predictions for custom eval metric when they are 2D (#10323)

--- R-package/R/utils.R
+++ R-package/R/utils.R
@@ -213,7 +213,7 @@ xgb.iter.eval <- function(bst, evals, iter, feval) {
     res <- sapply(seq_along(evals), function(j) {
       w <- evals[[j]]
       ## predict using all trees
-      preds <- predict(bst, w, outputmargin = TRUE, iterationrange = "all")
+      preds <- predict(bst, w, outputmargin = TRUE, reshape = TRUE, iterationrange = "all")
       eval_res <- feval(preds, w)
       out <- eval_res$value
       names(out) <- paste0(evnames[j], "-", eval_res$metric)
