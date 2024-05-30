814fa9954b2436f228adb761205e03aaf1272703
Jiaming Yuan <jm.yuan@outlook.com>
2024-05-30 21:02:14 +0800

    Move the rabit poll helper.
    Cleanup all references to the rabit directory.

--- src/collective/loop.cc
+++ src/collective/loop.cc
@@ -14,10 +14,10 @@
 #include <thread>     // for thread
 #include <utility>    // for move
 
-#include "rabit/internal/socket.h"      // for PollHelper
-#include "xgboost/collective/result.h"  // for Fail, Success
-#include "xgboost/collective/socket.h"  // for FailWithCode
-#include "xgboost/logging.h"            // for CHECK
+#include "xgboost/collective/poll_utils.h"  // for PollHelper
+#include "xgboost/collective/result.h"      // for Fail, Success
+#include "xgboost/collective/socket.h"      // for FailWithCode
+#include "xgboost/logging.h"                // for CHECK
 
 namespace xgboost::collective {
 Result Loop::ProcessQueue(std::queue<Op>* p_queue) const {
