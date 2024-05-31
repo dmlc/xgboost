7f3c66613403538dbc53d76eac2bfbcbd2562757
Hyunsu Cho <phcho@nvidia.com>
2024-05-31 10:48:55 -0700

    Address reviewers' comments

--- tests/buildkite/build-containers.sh
+++ tests/buildkite/build-containers.sh
@@ -20,13 +20,7 @@ case "${container}" in
   cpu)
     ;;
 
-  gpu | gpu_dev_ver)
-    BUILD_ARGS="$BUILD_ARGS --build-arg CUDA_VERSION_ARG=$CUDA_VERSION"
-    BUILD_ARGS="$BUILD_ARGS --build-arg NCCL_VERSION_ARG=$NCCL_VERSION"
-    BUILD_ARGS="$BUILD_ARGS --build-arg RAPIDS_VERSION_ARG=$RAPIDS_VERSION"
-    ;;
-
-  gpu_build_centos7)
+  gpu*)
     BUILD_ARGS="$BUILD_ARGS --build-arg CUDA_VERSION_ARG=$CUDA_VERSION"
     BUILD_ARGS="$BUILD_ARGS --build-arg NCCL_VERSION_ARG=$NCCL_VERSION"
     BUILD_ARGS="$BUILD_ARGS --build-arg RAPIDS_VERSION_ARG=$RAPIDS_VERSION"
