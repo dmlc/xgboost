d0140923ffcc9312c8f316ebcf8b030d906c0663
Hyunsu Cho <phcho@nvidia.com>
2024-06-14 01:23:11 -0700

    Try running it on Buildkite

--- tests/buildkite/build-jvm-packages-win64.ps1
+++ tests/buildkite/build-jvm-packages-win64.ps1
@@ -4,14 +4,12 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "--- Build and test JVM packages on Windows"
 
-mkdir build
-cd build
 $env:JAVA_HOME="C:\Program Files\Eclipse Adoptium\jdk-11.0.23.9-hotspot\"
-cmake .. -G"Visual Studio 17 2022" -A x64 -DJVM_BINDINGS=ON
+conda activate
+cd jvm-packages/
+python create_jni.py
 if ($LASTEXITCODE -ne 0) { throw "Last command failed" }
-cmake --build . --config Release -- /m /nodeReuse:false `
-  "/consoleloggerparameters:ShowCommandLine;Verbosity=minimal"
-if ($LASTEXITCODE -ne 0) { throw "Last command failed" }
+conda deactivate
 
 Write-Host "--- Stash xgboost4j.dll"
 cd ../lib
@@ -30,5 +28,5 @@ New-Item -ItemType Directory -Path jvm-packages/xgboost4j/src/main/resources/lib
 Copy-Item -Path lib/xgboost4j.dll -Destination jvm-packages/xgboost4j/src/main/resources/lib/windows/x86_64/xgboost4j.dll
 cd jvm-packages
 conda activate
-mvn test -B -pl :xgboost4j_2.12
+mvn test -B -D"skip.native.build=true" -pl :xgboost4j_2.12
 if ($LASTEXITCODE -ne 0) { throw "Last command failed" }
