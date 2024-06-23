70db11d96257482634fd20bd48cf888c6aa013e6
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-06-22 20:19:59 -0700

    Fix

--- tests/buildkite/build-manylinux2014-x86_64.sh
+++ tests/buildkite/build-manylinux2014-x86_64.sh
@@ -17,6 +17,7 @@ $command_wrapper ${python_bin} tests/ci_build/rename_whl.py  \
   --commit-hash ${BUILDKITE_COMMIT}  \
   --platform-tag ${WHEEL_TAG}
 rm -rf python-package/dist/
+mkdir python-package/dist/
 mv -v wheelhouse/*.whl python-package/dist/
 
 echo "--- Upload Python wheel"
