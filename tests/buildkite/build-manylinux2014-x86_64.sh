084bff660869faa6a48e36427cb56e5a2d51a4e9
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-06-22 19:29:21 -0700

    Fix

--- tests/buildkite/build-manylinux2014-x86_64.sh
+++ tests/buildkite/build-manylinux2014-x86_64.sh
@@ -6,12 +6,13 @@ source tests/buildkite/conftest.sh
 
 WHEEL_TAG=manylinux2014_x86_64
 command_wrapper="tests/ci_build/ci_build.sh manylinux2014_x86_64"
+python_bin="/opt/python/cp310-cp310/bin/python"
 
 echo "--- Build binary wheel for ${WHEEL_TAG}"
 $command_wrapper bash -c \
-  "cd python-package && pip wheel --no-deps -vvv . --wheel-dir dist/"
+  "cd python-package && ${python_bin} -m pip wheel --no-deps -vvv . --wheel-dir dist/"
 $command_wrapper auditwheel repair --plat ${WHEEL_TAG} python-package/dist/*.whl
-$command_wrapper python tests/ci_build/rename_whl.py  \
+$command_wrapper ${python_bin} tests/ci_build/rename_whl.py  \
   --wheel-path wheelhouse/*.whl  \
   --commit-hash ${BUILDKITE_COMMIT}  \
   --platform-tag ${WHEEL_TAG}
@@ -19,8 +20,10 @@ mv -v wheelhouse/*.whl python-package/dist/
 
 echo "--- Upload Python wheel"
 buildkite-agent artifact upload python-package/dist/*.whl
+$command_wrapper bash -c "${python_bin} -m pip install awscli"
 if [[ ($is_pull_request == 0) && ($is_release_branch == 1) ]]
 then
-  aws s3 cp python-package/dist/*.whl s3://xgboost-nightly-builds/${BRANCH_NAME}/ \
+  ${python_bin} -m awscli s3 \
+    cp python-package/dist/*.whl s3://xgboost-nightly-builds/${BRANCH_NAME}/ \
     --acl public-read --no-progress
 fi
