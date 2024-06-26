d482ba1a4c75bbbd73974e1a4add09e62c2a79e5
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-06-26 06:21:46 -0700

    [CI] [Hotfix] Make S3 upload conditional

--- tests/buildkite/build-jvm-macos-m1.sh
+++ tests/buildkite/build-jvm-macos-m1.sh
@@ -31,11 +31,11 @@ pushd lib
 libname=libxgboost4j_m1_${BUILDKITE_COMMIT}.dylib
 mv -v libxgboost4j.dylib ${libname}
 buildkite-agent artifact upload ${libname}
-#if [[ ($is_pull_request == 0) && ($is_release_branch == 1) ]]
-#then
+if [[ ($is_pull_request == 0) && ($is_release_branch == 1) ]]
+then
   aws s3 cp ${libname} \
     s3://xgboost-nightly-builds/${BRANCH_NAME}/libxgboost4j/ \
     --acl public-read --no-progress
-#fi
+fi
 popd
 set +x
