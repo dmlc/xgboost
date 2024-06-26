3bddbed9617f58b98fd31ddc3e751c2c9e00665e
Jiaming Yuan <jm.yuan@outlook.com>
2024-06-26 19:26:02 +0800

    isort.

--- python-package/xgboost/__init__.py
+++ python-package/xgboost/__init__.py
@@ -5,14 +5,7 @@ Contributors: https://github.com/dmlc/xgboost/blob/master/CONTRIBUTORS.md
 
 from . import tracker  # noqa
 from . import collective, dask
-from .core import (
-    Booster,
-    DataIter,
-    DMatrix,
-    QuantileDMatrix,
-    _py_version,
-    build_info,
-)
+from .core import Booster, DataIter, DMatrix, QuantileDMatrix, _py_version, build_info
 from .tracker import RabitTracker  # noqa
 from .training import cv, train
 
