0def8e0bae41e0eeeb4ee078e6e699ad6bef1986
Dmitry Razdoburdin <d.razdoburdin@gmail.com>
2024-08-14 21:50:17 +0200

    [sycl] fix fitting for fp32 devices (#10702)
    Co-authored-by: Dmitry Razdoburdin <>

--- plugin/sycl/tree/hist_updater.h
+++ plugin/sycl/tree/hist_updater.h
@@ -67,6 +67,7 @@ class HistUpdater {
     if (param.max_depth > 0) {
       snode_device_.Resize(&qu, 1u << (param.max_depth + 1));
     }
+    has_fp64_support_ = qu_.get_device().has(::sycl::aspect::fp64);
     const auto sub_group_sizes =
       qu_.get_device().get_info<::sycl::info::device::sub_group_sizes>();
     sub_group_size_ = sub_group_sizes.back();
@@ -183,6 +184,7 @@ class HistUpdater {
 
   //  --data fields--
   const Context* ctx_;
+  bool has_fp64_support_;
   size_t sub_group_size_;
 
   // the internal row sets
