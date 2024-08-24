44958f1d1fcff79eee1f3c429c7442e9a4a24ead
Michael Mayer <mayermichael79@gmail.com>
2024-08-24 15:11:27 +0200

    more safe assignment to parameter list

--- R-package/tests/testthat/test_helpers.R
+++ R-package/tests/testthat/test_helpers.R
@@ -487,6 +487,15 @@ test_that("check.deprecation works", {
     res <- ttt(a = 1, dumm = 22, z = 3)
   , "\'dumm\' was partially matched to \'dummy\'")
   expect_equal(res, list(a = 1, DUMMY = 22))
+  
+  # More complex object to be assigned
+  dm <- xgb.DMatrix(cbind(x1 = 1:2, x2 = 2:3), label = 2:3)
+  expect_warning(
+    xgb.train(
+      data = dm, watchlist = list(valid = dm), nrounds = 2, nthread = 2, verbose = 0
+    ),
+    "watchlist"
+  )
 })
 
 test_that('convert.labels works', {
