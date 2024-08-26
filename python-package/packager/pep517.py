fe3cf0e8786c7dc05e1deced3a1c92cd79094735
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-08-26 14:33:01 -0700

    Remove custom tag generation

--- python-package/packager/pep517.py
+++ python-package/packager/pep517.py
@@ -70,7 +70,6 @@ def build_wheel(
         logger.info("Copying project files to temporary directory %s", str(workspace))
 
         copy_with_logging(TOPLEVEL_DIR / "pyproject.toml", workspace, logger=logger)
-        copy_with_logging(TOPLEVEL_DIR / "hatch_build.py", workspace, logger=logger)
         copy_with_logging(TOPLEVEL_DIR / "README.rst", workspace, logger=logger)
 
         pkg_path = workspace / "xgboost"
@@ -116,7 +115,6 @@ def build_sdist(
         logger.info("Copying project files to temporary directory %s", str(workspace))
 
         copy_with_logging(TOPLEVEL_DIR / "pyproject.toml", workspace, logger=logger)
-        copy_with_logging(TOPLEVEL_DIR / "hatch_build.py", workspace, logger=logger)
         copy_with_logging(TOPLEVEL_DIR / "README.rst", workspace, logger=logger)
 
         copytree_with_logging(
