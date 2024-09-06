8c3c1ef9de2d4eb9d1263fa14090c10707eed5c5
divya2108 <divya.kotadiya@fujitsu.com>
2024-09-06 14:57:18 +0530

    Modified the cmake logic
    Signed-off-by: divya2108 <divya.kotadiya@fujitsu.com>

--- cmake/CheckSVEsupport.cmake
+++ cmake/CheckSVEsupport.cmake
@@ -20,30 +20,6 @@ if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
 
     if(XGBOOST_COMPILER_HAS_ARM_SVE)
         message(STATUS "ARM SVE compiler support detected")
-
-        # Check for hardware support
-        set(SOURCE_CODE "
-        #include <sys/prctl.h>
-        int main() {
-            int ret = prctl(PR_SVE_GET_VL);
-            return ret >= 0 ? 0 : 1;
-        }
-        ")
-        file(WRITE ${CMAKE_BINARY_DIR}/check_sve_support.c "${SOURCE_CODE}")
-        try_run(RUN_RESULT COMPILE_RESULT
-            ${CMAKE_BINARY_DIR}/check_sve_support_output
-            ${CMAKE_BINARY_DIR}/check_sve_support.c
-        )
-
-        if(RUN_RESULT EQUAL 0)
-            message(STATUS "ARM SVE hardware support detected")
-            # Apply the SVE flags and definitions specifically to the xgboost target
-            set(XGBOOST_ARM_SVE_HARDWARE_SUPPORT TRUE PARENT_SCOPE)
-            set(XGBOOST_SVE_FLAGS "-march=armv8-a+sve" PARENT_SCOPE)
-            set(XGBOOST_SVE_DEFINITIONS "-DXGBOOST_SVE_SUPPORT_DETECTED" PARENT_SCOPE)
-        else()
-            message(STATUS "ARM SVE hardware support not detected")
-        endif()
     else()
         message(STATUS "ARM SVE compiler support not detected")
     endif()
