6175965934990f7b07550402779ada588d529eab
Jiaming Yuan <jm.yuan@outlook.com>
2024-09-27 16:08:15 +0800

    Fix

--- src/common/device_helpers.cu
+++ src/common/device_helpers.cu
@@ -8,7 +8,7 @@ namespace dh {
 PinnedMemory::PinnedMemory() {
 #if (defined(_MSC_VER) || defined(__MINGW32__)) || \
     !(__CUDACC_VER_MAJOR__ >= 12 && __CUDACC_VER_MINOR__ >= 5)
-  this->impl_.emplace<detail::GrowOnlyVirtualMemVec>(CU_MEM_LOCATION_TYPE_HOST_NUMA);
+  this->impl_.emplace<detail::GrowOnlyPinnedMemoryImpl>(CU_MEM_LOCATION_TYPE_HOST_NUMA);
 #else
   std::int32_t major{0}, minor{0};
   xgboost::curt::RtVersion(&major, &minor);
