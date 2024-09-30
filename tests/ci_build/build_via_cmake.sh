a4fadc9365eff3ecf611d7a60535f56e3d8c02fc
Jiaming Yuan <jm.yuan@outlook.com>
2024-09-30 21:14:07 +0800

    don't clean.

--- tests/ci_build/build_via_cmake.sh
+++ tests/ci_build/build_via_cmake.sh
@@ -27,6 +27,5 @@ cd build
 # Disable CMAKE_COMPILE_WARNING_AS_ERROR option temporarily until
 # https://github.com/dmlc/xgboost/issues/10400 is fixed
 cmake .. ${cmake_args} -DGOOGLE_TEST=ON -DUSE_DMLC_GTEST=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DENABLE_ALL_WARNINGS=ON -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF -GNinja ${cmake_prefix_flag} -DHIDE_CXX_SYMBOLS=ON -DBUILD_DEPRECATED_CLI=ON
-ninja clean
 time ninja -v
 cd ..
