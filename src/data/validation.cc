e7df79f0ce69a502f20c399ed4ae006b6822a383
Jiaming Yuan <jm.yuan@outlook.com>
2024-10-22 22:00:35 +0800

    CPU build.

--- src/data/validation.cc
+++ src/data/validation.cc
@@ -5,6 +5,12 @@
 
 #include "../common/error_msg.h"  // for InconsistentFeatureTypes
 
+#if !defined(XGBOOST_USE_CUDA)
+
+#include "../common/common.h"  // for AssertGPUSupport
+
+#endif  // !defined(XGBOOST_USE_CUDA)
+
 namespace xgboost::data {
 void CheckFeatureTypes(HostDeviceVector<FeatureType> const& lhs,
                        HostDeviceVector<FeatureType> const& rhs) {
@@ -17,4 +23,12 @@ void CheckFeatureTypes(HostDeviceVector<FeatureType> const& lhs,
   auto ft_is_same = std::equal(h_lhs.cbegin(), h_lhs.cend(), h_rhs.cbegin());
   CHECK(ft_is_same) << error::InconsistentFeatureTypes();
 }
+
+#if !defined(XGBOOST_USE_CUDA)
+namespace cuda_impl {
+void CheckFeatureTypes(HostDeviceVector<FeatureType> const&, HostDeviceVector<FeatureType> const&) {
+  common::AssertGPUSupport();
+}
+}  // namespace cuda_impl
+#endif  // !defined(XGBOOST_USE_CUDA)
 }  // namespace xgboost::data
