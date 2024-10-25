578d0db6475b0e7ed7d58a6d639387f6d271778e
Jiaming Yuan <jm.yuan@outlook.com>
2024-10-25 14:52:38 +0800

    Add support for CUDA virtual device memory.

--- src/common/resource.cuh
+++ src/common/resource.cuh
@@ -6,7 +6,7 @@
 #include <functional>  // for function
 
 #include "cuda_pinned_allocator.h"  // for SamAllocator
-#include "device_vector.cuh"        // for DeviceUVector
+#include "device_vector.cuh"        // for DeviceUVector, GrowOnlyVirtualMemVec
 #include "io.h"                     // for ResourceHandler, MMAPFile
 #include "xgboost/string_view.h"    // for StringView
 
@@ -28,6 +28,35 @@ class CudaMallocResource : public ResourceHandler {
   [[nodiscard]] void* Data() override { return storage_.data(); }
   [[nodiscard]] std::size_t Size() const override { return storage_.size(); }
   void Resize(std::size_t n_bytes) { this->storage_.resize(n_bytes); }
+};
+
+/**
+ * @brief Device resource that only grows in size.
+ */
+class CudaGrowOnlyResource : public ResourceHandler {
+  static auto MakeNew() {
+    return std::make_unique<dh::detail::GrowOnlyVirtualMemVec>(CU_MEM_LOCATION_TYPE_DEVICE);
+  }
+
+  std::unique_ptr<dh::detail::GrowOnlyVirtualMemVec> alloc_;
+  std::size_t n_bytes_{0};
+
+ public:
+  explicit CudaGrowOnlyResource(std::size_t n_bytes)
+      : ResourceHandler{kCudaGrowOnly}, alloc_{MakeNew()} {
+    this->Resize(n_bytes);
+  }
+  void Resize(std::size_t n_bytes) {
+    this->alloc_->GrowTo(n_bytes);
+    this->n_bytes_ = n_bytes;
+  }
+  void Clear() {
+    this->alloc_.reset();
+    this->alloc_ = MakeNew();
+    this->n_bytes_ = 0;
+  }
+  [[nodiscard]] void* Data() final { return this->alloc_->data(); }
+  [[nodiscard]] std::size_t Size() const final { return this->n_bytes_; }
 };
 
 class CudaPinnedResource : public ResourceHandler {
