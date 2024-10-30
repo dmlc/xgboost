f387555cbe8dbfa384ca2ed294176dc4fff96dae
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-10-30 10:42:47 -0700

    Simplify stash_artifacts

---
+++ ops/stash_artifacts.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+Param(
+    [Parameter(
+      Mandatory=$true,
+      Position=0,
+      ValueFromRemainingArguments=$true
+    )][string[]]$artifacts
+)
+
+## Convenience wrapper for ops/stash_artifacts.py
+## Meant to be used inside GitHub Actions
+
+$ENV_VAR_DOC = @'
+Inputs
+  - COMMAND: Either "upload" or "download"
+  - KEY:     Unique string to identify a group of artifacts
+'@
+
+$ErrorActionPreference = "Stop"
+
+. ops/task/enforce-ci.ps1
+
+foreach ($env in "COMMAND", "KEY", "GITHUB_REPOSITORY", "GITHUB_RUN_ID",
+                 "RUNS_ON_S3_BUCKET_CACHE") {
+  $val = [Environment]::GetEnvironmentVariable($env)
+  if ($val -eq $null) {
+    Write-Host "Error: $env must be set.`n${ENV_VAR_DOC}"
+    exit 1
+  }
+}
+
+$artifact_stash_prefix = "cache/${Env:GITHUB_REPOSITORY}/stash/${Env:GITHUB_RUN_ID}"
+
+conda activate
+
+Write-Host @"
+python ops/stash_artifacts.py `
+  --command "${Env:COMMAND}"  `
+  --s3-bucket "${Env:RUNS_ON_S3_BUCKET_CACHE}" `
+  --prefix "${artifact_stash_prefix}/${Env:KEY}" `
+  -- $artifacts
+"@
+python ops/stash_artifacts.py `
+  --command "${Env:COMMAND}"  `
+  --s3-bucket "${Env:RUNS_ON_S3_BUCKET_CACHE}" `
+  --prefix "${artifact_stash_prefix}/${Env:KEY}" `
+  -- $artifacts
---
+++ ops/stash_artifacts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+## Convenience wrapper for ops/stash_artifacts.py
+## Meant to be used inside GitHub Actions
+
+ENV_VAR_DOC=$(
+cat <<-EOF
+Inputs
+  - COMMAND: Either "upload" or "download"
+  - KEY:     Unique string to identify a group of artifacts
+EOF
+)
+
+set -euo pipefail
+
+source ops/task/enforce-ci.sh
+
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 [artifact] [artifact ...]"
+  exit 1
+fi
+
+for arg in "COMMAND" "KEY" "GITHUB_REPOSITORY" "GITHUB_RUN_ID" "RUNS_ON_S3_BUCKET_CACHE"
+do
+  if [[ -z "${!arg:-}" ]]
+  then
+    echo -e "Error: $arg must be set.\n${ENV_VAR_DOC}"
+    exit 1
+  fi
+done
+
+artifact_stash_prefix="cache/${GITHUB_REPOSITORY}/stash/${GITHUB_RUN_ID}"
+
+set -x
+python3 ops/stash_artifacts.py \
+  --command "${COMMAND}"  \
+  --s3-bucket "${RUNS_ON_S3_BUCKET_CACHE}" \
+  --prefix "${artifact_stash_prefix}/${KEY}" \
+  -- "$@"
