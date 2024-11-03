078fd56cc67396051dceb0fa6483f40cdb4f9d50
Jiaming Yuan <jm.yuan@outlook.com>
2024-11-03 21:00:06 +0800

    jvm package.

--- jvm-packages/xgboost4j/src/native/xgboost4j-gpu.cpp
+++ jvm-packages/xgboost4j/src/native/xgboost4j-gpu.cpp
@@ -5,27 +5,16 @@
 
 #include <jni.h>
 
-#include "../../../../src/common/common.h"
 #include "../../../../src/c_api/c_api_error.h"
+#include "../../../../src/common/common.h"
 
-namespace xgboost {
-namespace jni {
-XGB_DLL int XGDeviceQuantileDMatrixCreateFromCallbackImpl(JNIEnv *jenv, jclass jcls,
-                                                          jobject jiter,
-                                                          jfloat jmissing,
-                                                          jint jmax_bin, jint jnthread,
-                                                          jlongArray jout) {
+namespace xgboost::jni {
+XGB_DLL int XGQuantileDMatrixCreateFromCallbackImpl(JNIEnv *jenv, jclass jcls, jobject jdata_iter,
+                                                    jlongArray jref, char const *config,
+                                                    jlongArray jout) {
   API_BEGIN();
   common::AssertGPUSupport();
   API_END();
 }
-XGB_DLL int XGQuantileDMatrixCreateFromCallbackImpl(JNIEnv *jenv, jclass jcls,
-                                                    jobject jdata_iter, jlongArray jref,
-                                                    char const *config, jlongArray jout) {
-  API_BEGIN();
-  common::AssertGPUSupport();
-  API_END();
-}
-} // namespace jni
-} // namespace xgboost
+}  // namespace xgboost::jni
 #endif  // XGBOOST_USE_CUDA
