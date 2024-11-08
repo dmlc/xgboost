947f4dbdc5cce6000fcb9ba142a98d712fef981b
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-11-07 21:45:24 -0800

    Skip all tests in deploy

--- tests/ci_build/deploy_jvm_packages.sh
+++ tests/ci_build/deploy_jvm_packages.sh
@@ -17,25 +17,25 @@ rm -rf ../build/
 ## Deploy JVM packages to xgboost-maven-repo
 
 # Scala 2.12, CPU variant
-mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests
+mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
 mvn clean
 mvn clean -Pdefault,release-to-s3
 
 # Scala 2.12, GPU variant
-mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests
-mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests
+mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
 
 # Scala 2.13, CPU variant
 pushd ..
 python dev/change_scala_version.py --scala-version 2.13 --purge-artifacts
 popd
-mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests
+mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
 mvn clean
 mvn clean -Pdefault,release-to-s3
 
 # Scala 2.13, GPU variant
-mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests
-mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests
+mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
 
 set +x
 set +e
