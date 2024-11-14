02592d5a5e97329ca3eac3e0f160b12b06bfe764
Jiaming Yuan <jm.yuan@outlook.com>
2024-11-14 14:40:39 +0800

    Tests.

--- tests/python/test_updaters.py
+++ tests/python/test_updaters.py
@@ -54,13 +54,15 @@ class TestTreeMethod:
             num_boost_round=2,
         )
 
-    def test_hist_colsample_rng(self) -> None:
+    @pytest.mark.parametrize("tree_method", ["approx", "hist"])
+    def test_colsample_rng(self, tree_method: str) -> None:
         """Test rng has an effect on column sampling."""
         X, y, _ = tm.make_regression(128, 16, use_cupy=False)
         reg0 = xgb.XGBRegressor(
             n_estimators=2,
             colsample_bynode=0.5,
             random_state=42,
+            tree_method=tree_method,
         )
         reg0.fit(X, y)
 
@@ -68,6 +70,7 @@ class TestTreeMethod:
             n_estimators=2,
             colsample_bynode=0.5,
             random_state=43,
+            tree_method=tree_method,
         )
         reg1.fit(X, y)
 
