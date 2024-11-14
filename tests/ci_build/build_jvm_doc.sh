b83591749e74b6685ed778ee31ab3e150d878972
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2024-11-14 12:33:54 -0800

    [jvm-packages] Clean up CI (#10982)
    - Properly handle shaded JARs
    - Remove xgboost4j-tester; fold integration tests as part of the test suite, so that `mvn test` would suffice.
    - Add test coverage for all demos. Update the demos so that they can run without failing.
    - Remove unused Maven profiles
    - Update the CI pipelines to deploy the shaded JARs (xgboost4j-spark / xgboost4j-spark-gpu)

--- tests/ci_build/build_jvm_doc.sh
+++ tests/ci_build/build_jvm_doc.sh
@@ -8,9 +8,6 @@ fi
 set -e
 set -x
 
-# Initialize local Maven repository
-./tests/ci_build/initialize_maven.sh
-
 rm -rf build/
 cd jvm-packages
 
--- tests/ci_build/deploy_jvm_packages.sh
+++ tests/ci_build/deploy_jvm_packages.sh
@@ -10,28 +10,32 @@ fi
 
 spark_version=$1
 
-# Initialize local Maven repository
-./tests/ci_build/initialize_maven.sh
-
 cd jvm-packages
 rm -rf $(find . -name target)
 rm -rf ../build/
 
-# Re-build package
-# Maven profiles:
-# `default`           includes modules: xgboost4j, xgboost4j-spark, xgboost4j-flink, xgboost4j-example
-# `gpu`               includes modules: xgboost4j-gpu, xgboost4j-spark-gpu, sets `use.cuda = ON`
-# `scala-2.13`        sets the scala binary version to the 2.13
-# `release-to-s3`     sets maven deployment targets
+## Deploy JVM packages to xgboost-maven-repo
 
-# Deploy to S3 bucket xgboost-maven-repo
-mvn --no-transfer-progress package deploy -P default,gpu,release-to-s3 -Dspark.version=${spark_version} -DskipTests
-# Deploy scala 2.13 to S3 bucket xgboost-maven-repo
-cd ..
+# Scala 2.12, CPU variant
+mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn clean
+mvn clean -Pdefault,release-to-s3
+
+# Scala 2.12, GPU variant
+mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+
+# Scala 2.13, CPU variant
+pushd ..
 python dev/change_scala_version.py --scala-version 2.13 --purge-artifacts
-cd jvm-packages/
-mvn --no-transfer-progress package deploy -P default,gpu,release-to-s3 -Dspark.version=${spark_version} -DskipTests
+popd
+mvn --no-transfer-progress deploy -Pdefault,release-to-s3 -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn clean
+mvn clean -Pdefault,release-to-s3
 
+# Scala 2.13, GPU variant
+mvn --no-transfer-progress install -Pgpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
+mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu -Dspark.version=${spark_version} -DskipTests -Dmaven.test.skip=true
 
 set +x
 set +e
