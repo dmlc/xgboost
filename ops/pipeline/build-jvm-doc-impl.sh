9f9db2b54d1ea836a485b9bb70110ff94a922f17
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-11-18 18:51:05 -0800

    Various fixes

--- ops/pipeline/build-jvm-doc-impl.sh
+++ ops/pipeline/build-jvm-doc-impl.sh
@@ -27,7 +27,7 @@ mvn --no-transfer-progress javadoc:javadoc -Pdocs
 
 # Package JVM docs in a tarball
 mkdir -p tmp/scaladocs
-cp -rv xgboost4j/target/site/apidocs/ ./tmp/javadocs/
+cp -rv xgboost4j/target/reports/apidocs/ ./tmp/javadocs/
 cp -rv xgboost4j/target/site/scaladocs/ ./tmp/scaladocs/xgboost4j/
 cp -rv xgboost4j-spark/target/site/scaladocs/ ./tmp/scaladocs/xgboost4j-spark/
 cp -rv xgboost4j-spark-gpu/target/site/scaladocs/ ./tmp/scaladocs/xgboost4j-spark-gpu/
