e148e29b0662f81b1ee3acab75cacf85e77fd0f8
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-11-19 16:53:02 -0800

    Fix update_rapids.sh

--- ops/script/update_rapids.sh
+++ ops/script/update_rapids.sh
@@ -7,7 +7,10 @@ echo "LATEST_RAPIDS_VERSION = $LATEST_RAPIDS_VERSION"
 DEV_RAPIDS_VERSION=$(date +%Y-%m-%d -d "20${LATEST_RAPIDS_VERSION//./-}-01 + 2 month" | cut -c3-7 | tr - .)
 echo "DEV_RAPIDS_VERSION = $DEV_RAPIDS_VERSION"
 
-PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+OPS_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )
+CONTAINER_YAML="$OPS_PATH/docker/ci_container.yml"
 
-sed -i "s/^RAPIDS_VERSION=[[:digit:]]\+\.[[:digit:]]\+/RAPIDS_VERSION=${LATEST_RAPIDS_VERSION}/" $PARENT_PATH/conftest.sh
-sed -i "s/^DEV_RAPIDS_VERSION=[[:digit:]]\+\.[[:digit:]]\+/DEV_RAPIDS_VERSION=${DEV_RAPIDS_VERSION}/" $PARENT_PATH/conftest.sh
+sed -i "s/\&rapids_version \"[[:digit:]]\+\.[[:digit:]]\+\"/\&rapids_version \"${LATEST_RAPIDS_VERSION}\"/" \
+  "$CONTAINER_YAML"
+sed -i "s/\&dev_rapids_version \"[[:digit:]]\+\.[[:digit:]]\+\"/\&dev_rapids_version \"${DEV_RAPIDS_VERSION}\"/" \
+  "$CONTAINER_YAML"
