f5061203074477b8219ea229805109cdf586a92b
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-11-25 19:25:56 -0800

    Deploy JVM: Make scala version as matrix arg

--- ops/pipeline/deploy-jvm-packages-impl.sh
+++ ops/pipeline/deploy-jvm-packages-impl.sh
@@ -4,42 +4,33 @@
 
 set -euox pipefail
 
-if [[ "$#" -lt 1 ]]
+if [[ "$#" -lt 2 ]]
 then
-  echo "Usage: $0 {cpu,gpu}"
+  echo "Usage: $0 {cpu,gpu} {scala_version}"
   exit 1
 fi
 
 variant="$1"
+scala_version="$2"
 maven_options="-DskipTests -Dmaven.test.skip=true -Dskip.native.build=true"
 
 case "$variant" in
   cpu)
     # CPU variant
-    for scala_version in 2.12 2.13
-    do
-      python ops/script/change_scala_version.py --scala-version ${scala_version} --purge-artifacts
-      bash ops/script/inject_jvm_lib.sh
-      pushd jvm-packages
-      mvn --no-transfer-progress deploy -Pdefault,release-to-s3 ${maven_options}
-      mvn clean
-      mvn clean -Pdefault,release-to-s3      
-      popd
-    done
+    python ops/script/change_scala_version.py --scala-version ${scala_version} --purge-artifacts
+    bash ops/script/inject_jvm_lib.sh
+    pushd jvm-packages
+    mvn --no-transfer-progress deploy -Pdefault,release-to-s3 ${maven_options}
+    popd
     ;;
   gpu)
     # GPU variant
-    for scala_version in 2.12 2.13
-    do
-      python ops/script/change_scala_version.py --scala-version ${scala_version} --purge-artifacts
-      bash ops/script/inject_jvm_lib.sh
-      pushd jvm-packages
-      mvn --no-transfer-progress install -Pgpu ${maven_options}
-      mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu ${maven_options}
-      mvn clean
-      mvn clean -Pgpu,release-to-s3
-      popd
-    done
+    python ops/script/change_scala_version.py --scala-version ${scala_version} --purge-artifacts
+    bash ops/script/inject_jvm_lib.sh
+    pushd jvm-packages
+    mvn --no-transfer-progress install -Pgpu ${maven_options}
+    mvn --no-transfer-progress deploy -Pgpu,release-to-s3 -pl xgboost4j-spark-gpu ${maven_options}
+    popd
     ;;
   *)
     echo "Unrecognized argument: $variant"
--- ops/pipeline/deploy-jvm-packages.sh
+++ ops/pipeline/deploy-jvm-packages.sh
@@ -5,18 +5,19 @@ set -euox pipefail
 
 source ops/pipeline/enforce-ci.sh
 
-if [[ "$#" -lt 2 ]]
+if [[ "$#" -lt 3 ]]
 then
-  echo "Usage: $0 {cpu,gpu} {container_id}"
+  echo "Usage: $0 {cpu,gpu} {container_id} {scala_version}"
   exit 1
 fi
 
 variant="$1"
 container_id="$2"
+scala_version="$3"
 
 if [[ ($is_pull_request == 0) && ($is_release_branch == 1) ]]
 then
   echo "--- Deploy JVM packages to xgboost-maven-repo S3 repo"
   python3 ops/docker_run.py --container-id "${container_id}" \
-  -- ops/pipeline/deploy-jvm-packages-impl.sh "${variant}"
+  -- ops/pipeline/deploy-jvm-packages-impl.sh "${variant}" "${scala_version}"
 fi
