48a8306d9ff40801db7256dfe07068bfe216b63e
Hyunsu Cho <phcho@nvidia.com>
2025-01-31 22:20:11 +0000

    Revert base class change

--- python-package/xgboost/dask/__init__.py
+++ python-package/xgboost/dask/__init__.py
@@ -1771,7 +1771,7 @@ class DaskScikitLearnBase(XGBModel):
 @xgboost_model_doc(
     """Implementation of the Scikit-Learn API for XGBoost.""", ["estimators", "model"]
 )
-class DaskXGBRegressor(XGBRegressorBase, DaskScikitLearnBase):
+class DaskXGBRegressor(DaskScikitLearnBase, XGBRegressorBase):
     """dummy doc string to workaround pylint, replaced by the decorator."""
 
     async def _fit_async(
@@ -1861,7 +1861,7 @@ class DaskXGBRegressor(XGBRegressorBase, DaskScikitLearnBase):
     "Implementation of the scikit-learn API for XGBoost classification.",
     ["estimators", "model"],
 )
-class DaskXGBClassifier(XGBClassifierBase, DaskScikitLearnBase):
+class DaskXGBClassifier(DaskScikitLearnBase, XGBClassifierBase):
     # pylint: disable=missing-class-docstring
     async def _fit_async(
         self,
@@ -2049,7 +2049,7 @@ class DaskXGBClassifier(XGBClassifierBase, DaskScikitLearnBase):
             For dask implementation, group is not supported, use qid instead.
 """,
 )
-class DaskXGBRanker(XGBRankerMixIn, DaskScikitLearnBase):
+class DaskXGBRanker(DaskScikitLearnBase, XGBRankerMixIn):
     @_deprecate_positional_args
     def __init__(self, *, objective: str = "rank:pairwise", **kwargs: Any):
         if callable(objective):
