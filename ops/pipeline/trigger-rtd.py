76c850c771d0cd92f2c81173cae64ca93109020b
Jiaming Yuan <jm.yuan@outlook.com>
2025-02-07 22:51:08 +0800

    cleanup.

--- ops/pipeline/trigger-rtd.py
+++ ops/pipeline/trigger-rtd.py
@@ -15,10 +15,8 @@ def trigger_build(token: str) -> None:
     """Trigger RTD build."""
 
     event_path = os.environ["GITHUB_EVENT_PATH"]
-    print(f"Event path: {event_path}")
     with open(event_path, "r") as fd:
         event: dict = json.load(fd)
-        print(f"Event: {event}")
 
     if event.get("pull_request", None) is None:
         # refs/heads/branch-name
@@ -27,15 +25,11 @@ def trigger_build(token: str) -> None:
         branch = event["pull_request"]["number"]
 
     URL = f"https://readthedocs.org/api/v3/projects/xgboost/versions/{branch}/builds/"
-    print(f"URL: {URL}")
     HEADERS = {"Authorization": f"token {token}"}
     response = requests.post(URL, headers=HEADERS)
-    print("response:", response)
     # 202 means the build is successfully triggered.
     assert response.status_code == 202
-    response = response.json()
-    msg = json.dumps(response, indent=2)
-    print("response:", response)
+    assert response.json()
 
 
 if __name__ == "__main__":
@@ -44,7 +38,6 @@ if __name__ == "__main__":
     # (expose) the token in the CI.
     assert token is not None
     if not isinstance(token, str) or len(token) != 40:
-        # fixme: remove this once we have a valid token
-        raise ValueError(f"Invalid token: {len(token)}")
+        raise ValueError(f"Invalid token.")
 
     trigger_build(token)
