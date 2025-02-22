2fc6257a6551e9deb49ee97159ab3bccf1c6fca6
Jiaming Yuan <jm.yuan@outlook.com>
2025-02-23 00:41:06 +0800

    Fix timer nvtx annotation.

--- src/data/batch_utils.h
+++ src/data/batch_utils.h
@@ -43,7 +43,7 @@ constexpr std::int64_t MatchingPageBytes() { return 0; }
 constexpr std::int32_t MaxNumDevicePages() { return 1; }
 // Default size of the cached page
 constexpr double CachePageRatio() { return 0.125; }
-// Indicator for XGBoost to not concatenate any page.
+// Indicator for XGBoost to automatically concatenate pages.
 constexpr std::int64_t AutoCachePageBytes() { return -1; }
 // Use two batch for prefecting. There's always one batch being worked on, while the other
 // batch being transferred.
