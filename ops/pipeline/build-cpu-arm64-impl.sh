27f0279a8a1f8461885513633bce39dc23a70e48
Jiaming Yuan <jm.yuan@outlook.com>
2025-03-31 23:37:13 +0800

    check version.

--- ops/pipeline/build-cpu-arm64-impl.sh
+++ ops/pipeline/build-cpu-arm64-impl.sh
@@ -9,12 +9,14 @@ source activate aarch64_test
 echo "--- Build libxgboost from the source"
 mkdir -p build
 pushd build
+cmake --version
 cmake .. \
   -GNinja \
   -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
   -DUSE_OPENMP=ON \
   -DHIDE_CXX_SYMBOLS=ON \
   -DGOOGLE_TEST=ON \
+  -DUSE_DMLC_GTEST=ON \
   -DENABLE_ALL_WARNINGS=ON \
   -DCMAKE_COMPILE_WARNING_AS_ERROR=OFF \
   -DBUILD_DEPRECATED_CLI=ON
