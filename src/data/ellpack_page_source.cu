29eb6cf21312606a483ec74dfc857ce3d547ea32
Jiaming Yuan <jm.yuan@outlook.com>
2025-04-08 15:38:29 +0800

    [backport][EM] Optimization for deep trees. (#11387)
    - Decouple the row partition batch size from the driver batch size. This will allow us to process more nodes for each data batch.
    - Pick a heuristic to use ATS instead of data copy to handle cases where we have a large number of small nodes.
    - Make sure a new page that happens to be the last is placed on the host.

--- src/data/ellpack_page_source.cu
+++ src/data/ellpack_page_source.cu
@@ -190,11 +190,12 @@ class EllpackHostCacheStreamImpl {
       auto& new_impl = this->cache_->pages.back();
       auto offset = new_impl->Copy(&ctx, impl, this->cache_->offsets.back());
       this->cache_->offsets.back() += offset;
-      // No need to copy if it's already in device.
-      if (last_page && !this->cache_->on_device.back()) {
-        auto commited = commit_host_page(this->cache_->pages.back().get());
-        this->cache_->pages.back() = std::move(commited);
-      }
+    }
+
+    // No need to copy if it's already in device.
+    if (last_page && !this->cache_->on_device.back()) {
+      auto commited = commit_host_page(this->cache_->pages.back().get());
+      this->cache_->pages.back() = std::move(commited);
     }
 
     return new_page;
