b2f71a40ea1d7d26c707aed286b3477347fa2fd9
Jiaming Yuan <jm.yuan@outlook.com>
2025-04-13 14:35:09 +0800

    GPU deps.

--- tests/python-gpu/test_gpu_plotting.py
+++ tests/python-gpu/test_gpu_plotting.py
@@ -1,9 +1,10 @@
 import pytest
 
 from xgboost import testing as tm
-from xgboost.testing.plotting import run_categorical
 
 pytestmark = pytest.mark.skipif(**tm.no_multiple(tm.no_matplotlib(), tm.no_graphviz()))
+
+from xgboost.testing.plotting import run_categorical
 
 
 class TestPlotting:
--- tests/python/test_plotting.py
+++ tests/python/test_plotting.py
@@ -1,5 +1,3 @@
-import json
-
 import numpy as np
 import pytest
 
