60b1ff1b7cac0d058f3475058012bf02327d9ec8
James Bourbeau <jrbourbeau@gmail.com>
2025-05-14 15:51:29 -0500

    Dask 2025.4.0 scheduler info compatibility

--- python-package/xgboost/dask/__init__.py
+++ python-package/xgboost/dask/__init__.py
@@ -254,15 +254,11 @@ class CommunicatorContext(collective.CommunicatorContext):
         super().__init__(**args)
 
         worker = distributed.get_worker()
-        with distributed.worker_client() as client:
-            info = client.scheduler_info()
-            w = info["workers"][worker.address]
-            wid = w["id"]
         # We use task ID for rank assignment which makes the RABIT rank consistent (but
         # not the same as task ID is string and "10" is sorted before "2") with dask
-        # worker ID. This outsources the rank assignment to dask and prevents
+        # worker address. This outsources the rank assignment to dask and prevents
         # non-deterministic issue.
-        self.args["DMLC_TASK_ID"] = f"[xgboost.dask-{wid}]:" + str(worker.address)
+        self.args["DMLC_TASK_ID"] = f"[xgboost.dask]:{worker.address}"
 
 
 def _get_client(client: Optional["distributed.Client"]) -> "distributed.Client":
