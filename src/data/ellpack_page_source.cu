9f737b9353e67eb97628436dbba3ebc2a99c0b2a
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-25 17:33:19 +0800

    increase the ratio.

--- src/data/ellpack_page_source.cu
+++ src/data/ellpack_page_source.cu
@@ -184,7 +184,7 @@ class EllpackHostCacheStreamImpl {
         // FIXME(jiamingy): The decomp_ratio is not exposed to the user and we don't yet
         // have auto configuration for this parameter. We can make it more flexible. More
         // profiling is needed.
-        auto r = std::isnan(this->cache_->hw_decomp_ratio) ? 1.0 : this->cache_->hw_decomp_ratio;
+        auto r = std::isnan(this->cache_->hw_decomp_ratio) ? 0.4 : this->cache_->hw_decomp_ratio;
         CHECK_LE(r, 1.0);
         CHECK_GE(r, 0.0);
         n_comp_bytes = n_bytes * r;
