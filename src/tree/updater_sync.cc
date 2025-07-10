14eb4455ee106b94ad0805237eb42c9a1fafca2a
Jiaming Yuan <jm.yuan@outlook.com>
2025-07-10 11:20:46 +0800

    Remove cpp tests for deprecated binary model. (#11552)

--- src/tree/updater_sync.cc
+++ src/tree/updater_sync.cc
@@ -1,17 +1,17 @@
 /**
- * Copyright 2014-2024, XBGoost Contributors
+ * Copyright 2014-2025, XBGoost Contributors
  * \file updater_sync.cc
  * \brief synchronize the tree in all distributed nodes
  */
-#include <xgboost/tree_updater.h>
-
 #include <string>
 #include <vector>
 
-#include "../collective/broadcast.h"
-#include "../collective/communicator-inl.h"
-#include "../common/io.h"
-#include "xgboost/json.h"
+#include "../collective/broadcast.h"         // for Broadcast
+#include "../collective/communicator-inl.h"  // for GetRank, GetWorldSize
+#include "xgboost/context.h"                 // for Context
+#include "xgboost/json.h"                    // for Json, Object
+#include "xgboost/linalg.h"                  // for Matrix
+#include "xgboost/tree_updater.h"            // for TreeUpdater
 
 namespace xgboost::tree {
 
@@ -23,31 +23,35 @@ DMLC_REGISTRY_FILE_TAG(updater_sync);
  */
 class TreeSyncher : public TreeUpdater {
  public:
-  explicit TreeSyncher(Context const* tparam) : TreeUpdater(tparam) {}
-  void Configure(const Args&) override {}
+  explicit TreeSyncher(Context const* tparam) : TreeUpdater{tparam} {}
+  void Configure(Args const&) override {}
 
   void LoadConfig(Json const&) override {}
   void SaveConfig(Json*) const override {}
 
-  [[nodiscard]] char const* Name() const override { return "prune"; }
+  [[nodiscard]] char const* Name() const override { return "sync"; }
 
   void Update(TrainParam const*, linalg::Matrix<GradientPair>*, DMatrix*,
               common::Span<HostDeviceVector<bst_node_t>> /*out_position*/,
               const std::vector<RegTree*>& trees) override {
-    if (collective::GetWorldSize() == 1) return;
-    std::string s_model;
-    common::MemoryBufferStream fs(&s_model);
-    int rank = collective::GetRank();
+    if (collective::GetWorldSize() == 1) {
+      return;
+    }
+
+    Json model{Object{}};
+    auto rank = collective::GetRank();
     if (rank == 0) {
       for (auto tree : trees) {
-        tree->Save(&fs);
+        tree->SaveModel(&model);
       }
     }
-    fs.Seek(0);
-    auto rc = collective::Broadcast(ctx_, linalg::MakeVec(s_model.data(), s_model.size()), 0);
+    std::vector<char> jmodel;
+    Json::Dump(model, &jmodel, std::ios::binary);
+    auto rc = collective::Broadcast(ctx_, linalg::MakeVec(jmodel.data(), jmodel.size()), 0);
     SafeColl(rc);
+
     for (auto tree : trees) {
-      tree->Load(&fs);
+      tree->LoadModel(model);
     }
   }
 };
