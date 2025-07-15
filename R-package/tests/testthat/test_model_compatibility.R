4d85f37ef0ed061f4d11f138e412d6e58dfde01b
Jiaming Yuan <jm.yuan@outlook.com>
2025-07-15 14:50:45 +0800

    Replace R models.

--- R-package/tests/testthat/test_model_compatibility.R
+++ R-package/tests/testthat/test_model_compatibility.R
@@ -1,7 +1,9 @@
+library(testthat)
+library(xgboost)
 context("Models from previous versions of XGBoost can be loaded")
 
 metadata <- list(
-  kRounds = 2,
+  kRounds = 4,
   kRows = 1000,
   kCols = 4,
   kForests = 2,
@@ -10,69 +12,114 @@ metadata <- list(
 )
 
 run_model_param_check <- function(config) {
-  testthat::expect_equal(config$learner$learner_model_param$num_feature, '4')
-  testthat::expect_equal(config$learner$learner_train_param$booster, 'gbtree')
+  testthat::expect_equal(config$learner$learner_model_param$num_feature, "4")
+  testthat::expect_equal(config$learner$learner_train_param$booster, "gbtree")
+}
+
+get_n_rounds <- function(model_file) {
+  is_10 <- grepl("1.0.0rc1", model_file, fixed = TRUE)
+  if (is_10) {
+    2
+  } else {
+    metadata$kRounds
+  }
 }
 
 get_num_tree <- function(booster) {
   dump <- xgb.dump(booster)
-  m <- regexec('booster\\[[0-9]+\\]', dump, perl = TRUE)
+  m <- regexec("booster\\[[0-9]+\\]", dump, perl = TRUE)
   m <- regmatches(dump, m)
-  num_tree <- Reduce('+', lapply(m, length))
-  return(num_tree)
+  num_tree <- Reduce("+", lapply(m, length))
+  num_tree
 }
 
-run_booster_check <- function(booster, name) {
+run_booster_check <- function(booster, model_file) {
   config <- xgb.config(booster)
   run_model_param_check(config)
-  if (name == 'cls') {
-    testthat::expect_equal(get_num_tree(booster),
-                           metadata$kForests * metadata$kRounds * metadata$kClasses)
-    testthat::expect_equal(as.numeric(config$learner$learner_model_param$base_score), 0.5)
-    testthat::expect_equal(config$learner$learner_train_param$objective, 'multi:softmax')
-    testthat::expect_equal(as.numeric(config$learner$learner_model_param$num_class),
-                           metadata$kClasses)
-  } else if (name == 'logitraw') {
-    testthat::expect_equal(get_num_tree(booster), metadata$kForests * metadata$kRounds)
-    testthat::expect_equal(as.numeric(config$learner$learner_model_param$num_class), 0)
-    testthat::expect_equal(config$learner$learner_train_param$objective, 'binary:logitraw')
-  } else if (name == 'logit') {
-    testthat::expect_equal(get_num_tree(booster), metadata$kForests * metadata$kRounds)
-    testthat::expect_equal(as.numeric(config$learner$learner_model_param$num_class), 0)
-    testthat::expect_equal(config$learner$learner_train_param$objective, 'binary:logistic')
-  } else if (name == 'ltr') {
-    testthat::expect_equal(get_num_tree(booster), metadata$kForests * metadata$kRounds)
-    testthat::expect_equal(config$learner$learner_train_param$objective, 'rank:ndcg')
+  is_model <- function(typ) {
+    grepl(typ, model_file, fixed = TRUE)
+  }
+  n_rounds <- get_n_rounds(model_file = model_file)
+  if (is_model("cls")) {
+    testthat::expect_equal(
+      get_num_tree(booster), metadata$kForests * n_rounds * metadata$kClasses
+    )
+    testthat::expect_equal(
+      as.numeric(config$learner$learner_model_param$base_score), 0.5
+    )
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "multi:softmax"
+    )
+    testthat::expect_equal(
+      as.numeric(config$learner$learner_model_param$num_class),
+      metadata$kClasses
+    )
+  } else if (is_model("logitraw")) {
+    testthat::expect_equal(get_num_tree(booster), metadata$kForests * n_rounds)
+    testthat::expect_equal(
+      as.numeric(config$learner$learner_model_param$num_class), 0
+    )
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "binary:logitraw"
+    )
+  } else if (is_model("logit")) {
+    testthat::expect_equal(get_num_tree(booster), metadata$kForests * n_rounds)
+    testthat::expect_equal(
+      as.numeric(config$learner$learner_model_param$num_class), 0
+    )
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "binary:logistic"
+    )
+  } else if (is_model("ltr")) {
+    testthat::expect_equal(get_num_tree(booster), metadata$kForests * n_rounds)
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "rank:ndcg"
+    )
+  } else if (is_model("aft")) {
+    testthat::expect_equal(get_num_tree(booster), metadata$kForests * n_rounds)
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "survival:aft"
+    )
   } else {
-    testthat::expect_equal(name, 'reg')
-    testthat::expect_equal(get_num_tree(booster), metadata$kForests * metadata$kRounds)
-    testthat::expect_equal(as.numeric(config$learner$learner_model_param$base_score), 0.5)
-    testthat::expect_equal(config$learner$learner_train_param$objective, 'reg:squarederror')
+    testthat::expect_true(is_model("reg"))
+    testthat::expect_equal(get_num_tree(booster), metadata$kForests * n_rounds)
+    testthat::expect_equal(
+      as.numeric(config$learner$learner_model_param$base_score), 0.5
+    )
+    testthat::expect_equal(
+      config$learner$learner_train_param$objective, "reg:squarederror"
+    )
   }
 }
 
 test_that("Models from previous versions of XGBoost can be loaded", {
-  bucket <- 'xgboost-ci-jenkins-artifacts'
-  region <- 'us-west-2'
-  file_name <- 'xgboost_r_model_compatibility_test.zip'
+  bucket <- "xgboost-ci-jenkins-artifacts"
+  region <- "us-west-2"
+  file_name <- "xgboost_model_compatibility_tests-3.0.1.zip"
   zipfile <- tempfile(fileext = ".zip")
   extract_dir <- tempdir()
-  download.file(paste('https://', bucket, '.s3-', region, '.amazonaws.com/', file_name, sep = ''),
-                destfile = zipfile, mode = 'wb', quiet = TRUE)
+  download.file(
+    paste(
+      "https://", bucket, ".s3-", region, ".amazonaws.com/", file_name, sep = ""
+    ),
+    destfile = zipfile, mode = "wb", quiet = TRUE
+  )
   unzip(zipfile, exdir = extract_dir, overwrite = TRUE)
-  model_dir <- file.path(extract_dir, 'models')
+  model_dir <- file.path(extract_dir, "models")
 
-  pred_data <- xgb.DMatrix(matrix(c(0, 0, 0, 0), nrow = 1, ncol = 4), nthread = 2)
+  pred_data <- xgb.DMatrix(
+    matrix(c(0, 0, 0, 0), nrow = 1, ncol = 4), nthread = 2
+  )
 
   lapply(list.files(model_dir), function(x) {
     model_file <- file.path(model_dir, x)
-    m <- regexec("xgboost-([0-9\\.]+)\\.([a-z]+)\\.[a-z]+", model_file, perl = TRUE)
-    m <- regmatches(model_file, m)[[1]]
-    name <- m[3]
-
+    is_skl <- grepl("scikit", model_file, fixed = TRUE)
+    if (is_skl) {
+      return()
+    }
     booster <- xgb.load(model_file)
     xgb.model.parameters(booster) <- list(nthread = 2)
     predict(booster, newdata = pred_data)
-    run_booster_check(booster, name)
+    run_booster_check(booster, model_file)
   })
 })
