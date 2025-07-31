268cd2c0a7bf9653b3798f67bb63950fbb8f6b6a
Jiaming Yuan <jm.yuan@outlook.com>
2025-07-31 18:27:34 +0800

    CPU check.

--- python-package/xgboost/testing/utils.py
+++ python-package/xgboost/testing/utils.py
@@ -5,6 +5,7 @@ from typing import Any, Literal, TypeAlias
 import numpy as np
 
 from ..compat import import_cupy
+from ..data import _is_cupy_alike
 
 Device: TypeAlias = Literal["cpu", "cuda"]
 
@@ -13,7 +14,7 @@ def assert_allclose(
     device: Device, a: Any, b: Any, *, rtol: float = 1e-7, atol: float = 0
 ) -> None:
     """Dispatch the assert_allclose for devices."""
-    if device == "cpu" and not hasattr(a, "get") and not hasattr(b, "get"):
+    if device == "cpu" and not _is_cupy_alike(a) and not _is_cupy_alike(b):
         np.testing.assert_allclose(a, b, atol=atol, rtol=rtol)
     else:
         cp = import_cupy()
