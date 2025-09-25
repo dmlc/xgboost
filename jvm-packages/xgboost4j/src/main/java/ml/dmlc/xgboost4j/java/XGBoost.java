bf0925ebca58513e8b19b55cce3c4f6add8ef231
Kunal Jani <168258319+kunaljani1100@users.noreply.github.com>
2025-09-25 10:51:44 -0700

    Implement suggestion.
    Co-authored-by: Jiaming Yuan <jm.yuan@outlook.com>

--- jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoost.java
+++ jvm-packages/xgboost4j/src/main/java/ml/dmlc/xgboost4j/java/XGBoost.java
@@ -392,7 +392,7 @@ public class XGBoost {
    *
    * @param evalInfo The evaluation log string from which the metric name is inferred.
    * @param evalNames The names of the evaluation matrices.
-   * @param params The parameters that contain information regarding whethe the
+   * @param params The parameters that contain information regarding whether the
    *  evaluation metrics are to be maximized or not.
    * @return True if the evaluation metrics are to be maximized, false otherwise.
    */
