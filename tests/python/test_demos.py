2dcfa0e3bf0d5558bac477bc2b4e3da9f998b04b
Jiaming Yuan <jm.yuan@outlook.com>
2025-10-02 18:26:46 +0800

    sys exe.

--- tests/python/test_demos.py
+++ tests/python/test_demos.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import tempfile
 
 import pytest
@@ -14,9 +15,12 @@ PYTHON_DEMO_DIR = os.path.join(DEMO_DIR, "guide-python")
 CLI_DEMO_DIR = os.path.join(DEMO_DIR, "CLI")
 
 
+PYTHON = sys.executable
+
+
 def test_basic_walkthrough() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "basic_walkthrough.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     with tempfile.TemporaryDirectory() as tmpdir:
         subprocess.check_call(cmd, cwd=tmpdir)
 
@@ -24,42 +28,42 @@ def test_basic_walkthrough() -> None:
 @pytest.mark.skipif(**tm.no_pandas())
 def test_categorical() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "categorical.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_pandas())
 def test_cat_pipeline() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "cat_pipeline.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_matplotlib())
 def test_custom_multiclass_objective() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "custom_softmax.py")
-    cmd = ["python", script, "--plot=0"]
+    cmd = [PYTHON, script, "--plot=0"]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_matplotlib())
 def test_custom_rmsle_objective() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "custom_rmsle.py")
-    cmd = ["python", script, "--plot=0"]
+    cmd = [PYTHON, script, "--plot=0"]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_matplotlib())
 def test_feature_weights_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "feature_weights.py")
-    cmd = ["python", script, "--plot=0"]
+    cmd = [PYTHON, script, "--plot=0"]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_sklearn())
 def test_sklearn_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "sklearn_examples.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
     assert os.path.exists("best_calif.pkl")
     os.remove("best_calif.pkl")
@@ -69,68 +73,68 @@ def test_sklearn_demo() -> None:
 @pytest.mark.timeout(60)
 def test_sklearn_parallel_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "sklearn_parallel.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_sklearn())
 def test_sklearn_evals_result_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "sklearn_evals_result.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_boost_from_prediction_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "boost_from_prediction.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_predict_first_ntree_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "predict_first_ntree.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_individual_trees() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "individual_trees.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_predict_leaf_indices_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "predict_leaf_indices.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_generalized_linear_model_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "generalized_linear_model.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_cross_validation_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "cross_validation.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
 def test_external_memory_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "external_memory.py")
-    cmd = ["python", script, "--device=cpu"]
+    cmd = [PYTHON, script, "--device=cpu"]
     subprocess.check_call(cmd)
 
 
 def test_distributed_extmem_basic_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "distributed_extmem_basic.py")
-    cmd = ["python", script, "--device=cpu"]
+    cmd = [PYTHON, script, "--device=cpu"]
     subprocess.check_call(cmd)
 
 
 def test_evals_result_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "evals_result.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
@@ -138,7 +142,7 @@ def test_evals_result_demo() -> None:
 @pytest.mark.skipif(**tm.no_pandas())
 def test_aft_demo() -> None:
     script = os.path.join(DEMO_DIR, "aft_survival", "aft_survival_demo.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
     assert os.path.exists("aft_model.json")
     os.remove("aft_model.json")
@@ -147,13 +151,13 @@ def test_aft_demo() -> None:
 @pytest.mark.skipif(**tm.no_matplotlib())
 def test_callbacks_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "callbacks.py")
-    cmd = ["python", script, "--plot=0"]
+    cmd = [PYTHON, script, "--plot=0"]
     subprocess.check_call(cmd)
 
 
 def test_continuation_demo() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "continuation.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
@@ -161,14 +165,14 @@ def test_continuation_demo() -> None:
 @pytest.mark.skipif(**tm.no_matplotlib())
 def test_multioutput_reg() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "multioutput_regression.py")
-    cmd = ["python", script, "--plot=0"]
+    cmd = [PYTHON, script, "--plot=0"]
     subprocess.check_call(cmd)
 
 
 @pytest.mark.skipif(**tm.no_sklearn())
 def test_quantile_reg() -> None:
     script = os.path.join(PYTHON_DEMO_DIR, "quantile_regression.py")
-    cmd = ["python", script]
+    cmd = [PYTHON, script]
     subprocess.check_call(cmd)
 
 
@@ -180,12 +184,12 @@ def test_json_model() -> None:
         with tempfile.TemporaryDirectory() as tmpdir:
             path = os.path.join(tmpdir, "reg.json")
             reg.save_model(path)
-            cmd = ["python", script, f"--model={path}"]
+            cmd = [PYTHON, script, f"--model={path}"]
             subprocess.check_call(cmd)
 
             path = os.path.join(tmpdir, "reg.ubj")
             reg.save_model(path)
-            cmd = ["python", script, f"--model={path}"]
+            cmd = [PYTHON, script, f"--model={path}"]
             subprocess.check_call(cmd)
 
     # numerical
