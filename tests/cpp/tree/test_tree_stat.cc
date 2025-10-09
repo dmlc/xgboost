86fd5b0e106539eb83a94ea137e61ffe242d3402
Jiaming Yuan <jm.yuan@outlook.com>
2025-10-09 15:42:09 +0800

    walk.

--- tests/cpp/tree/test_tree_stat.cc
+++ tests/cpp/tree/test_tree_stat.cc
@@ -119,10 +119,9 @@ class TestSplitWithEta : public ::testing::Test {
     CHECK_GE(p_tree0->NumExtraNodes(), 32);
 
     bst_node_t n_nodes{0};
-    auto sc_tree = tree::ScalarTreeView{p_tree0.get()};
-    sc_tree.WalkTree([&](bst_node_t nidx) {
-      if (sc_tree.IsLeaf(nidx)) {
-        CHECK(sc_tree.IsLeaf(nidx));
+    auto walk = [&](bst_node_t nidx) {
+      if (p_tree0->IsLeaf(nidx)) {
+        CHECK(p_tree0->IsLeaf(nidx));
         if (p_tree0->IsMultiTarget()) {
           CHECK(p_tree1->IsMultiTarget());
           auto leaf_0 = p_tree0->GetMultiTargetTree()->LeafValue(nidx);
@@ -131,21 +130,27 @@ class TestSplitWithEta : public ::testing::Test {
           for (std::size_t i = 0; i < leaf_0.Size(); ++i) {
             CHECK_EQ(leaf_0(i) * eta_ratio, leaf_1(i));
           }
-          CHECK_EQ(DftBadValue(), sc_tree.SplitCond(nidx));
+          CHECK_EQ(DftBadValue(), p_tree0->SplitCond(nidx));
           CHECK_EQ(DftBadValue(), p_tree1->SplitCond(nidx));
         } else {
           // Non-mt tree reuses split cond for leaf value.
-          auto leaf_0 = sc_tree.SplitCond(nidx);
+          auto leaf_0 = p_tree0->SplitCond(nidx);
           auto leaf_1 = p_tree1->SplitCond(nidx);
           CHECK_EQ(leaf_0 * eta_ratio, leaf_1);
         }
       } else {
         CHECK(!p_tree1->IsLeaf(nidx));
-        CHECK_EQ(sc_tree.SplitCond(nidx), p_tree1->SplitCond(nidx));
+        CHECK_EQ(p_tree0->SplitCond(nidx), p_tree1->SplitCond(nidx));
       }
       n_nodes++;
       return true;
-    });
+    };
+    if (n_targets == 1) {
+      tree::ScalarTreeView{p_tree0.get()}.WalkTree(walk);
+    } else {
+      tree::MultiTargetTreeView{ctx, p_tree0.get()}.WalkTree(walk);
+    }
+
     ASSERT_EQ(n_nodes, p_tree0->NumExtraNodes() + 1);
   }
 };
