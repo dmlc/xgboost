2d919b129486c8a5bf815afa5871e8d9dbf476cd
Jiaming Yuan <jm.yuan@outlook.com>
2025-10-21 21:44:57 +0800

    Bump release to 3.1.1.

--- include/xgboost/version_config.h
+++ include/xgboost/version_config.h
@@ -6,6 +6,6 @@
 
 #define XGBOOST_VER_MAJOR 3  /* NOLINT */
 #define XGBOOST_VER_MINOR 1  /* NOLINT */
-#define XGBOOST_VER_PATCH 0  /* NOLINT */
+#define XGBOOST_VER_PATCH 1  /* NOLINT */
 
 #endif  // XGBOOST_VERSION_CONFIG_H_
