dc9173d8b071fadba843b6e39159c460c1d0ec4b
Dmitry Razdoburdin <>
2025-11-12 23:40:16 -0800

    detect untrusted cases

--- src/common/cache_manager.cc
+++ src/common/cache_manager.cc
@@ -36,9 +36,12 @@ void RunCpuid(uint32_t eax, uint32_t ecx, uint32_t* abcd) {
 
 #define _CPUID_CACHE_INFO 0x4U
 
-void GetCacheInfo(int cache_num, int * type, int * level, int64_t * sets, int * line_size, int * partitions, int * ways) {
+bool GetCacheInfo(int cache_num, int * type, int * level, int64_t * sets, int * line_size, int * partitions, int * ways) {
   static uint32_t abcd[4];
   RunCpuid(_CPUID_CACHE_INFO, cache_num, abcd);
+
+  bool under_hypervisor = (abcd[2] >> 31) & 1;
+
   const uint32_t eax = abcd[0];
   const uint32_t ebx = abcd[1];
   const uint32_t ecx = abcd[2];
@@ -49,6 +52,9 @@ void GetCacheInfo(int cache_num, int * type, int * level, int64_t * sets, int * 
   *line_size         = _CPUID_CACHE_INFO_GET_LINE_SIZE(ebx);
   *partitions        = _CPUID_CACHE_INFO_GET_PARTITIONS(ebx);
   *ways              = _CPUID_CACHE_INFO_GET_WAYS(ebx);
+
+  bool trust_cpuid = !under_hypervisor;
+  return trust_cpuid;
 }
 
 constexpr int kCPUID_CACHE_INFO_TYPE_NULL = 0;
@@ -56,12 +62,14 @@ constexpr int kCPUID_CACHE_INFO_TYPE_DATA = 1;
 constexpr int kCPUID_CACHE_INFO_TYPE_INST = 2;
 constexpr int kCPUID_CACHE_INFO_TYPE_UNIF = 3;
 
-void DetectDataCaches(int cache_sizes_len, int64_t* cache_sizes) {
+bool DetectDataCaches(int cache_sizes_len, int64_t* cache_sizes) {
   int cache_num = 0, cache_sizes_idx = 0;
   while (cache_sizes_idx < cache_sizes_len) {
     int type, level, line_size, partitions, ways;
     int64_t sets, size;
-    GetCacheInfo(cache_num++, &type, &level, &sets, &line_size, &partitions, &ways);
+    bool trust_cpuid =
+      GetCacheInfo(cache_num++, &type, &level, &sets, &line_size, &partitions, &ways);
+    if (!trust_cpuid) return trust_cpuid;
 
     if (type == kCPUID_CACHE_INFO_TYPE_NULL) break;
     if (type == kCPUID_CACHE_INFO_TYPE_INST) continue;
@@ -69,23 +77,28 @@ void DetectDataCaches(int cache_sizes_len, int64_t* cache_sizes) {
     size                           = ways * partitions * line_size * sets;
     cache_sizes[cache_sizes_idx++] = size;
   }
+  return true;
 }
+#endif // defined(__x86_64__)
 
-#else // defined(__x86_64__)
-
-void DetectDataCaches(int cache_sizes_len, int64_t* cache_sizes) {
+void SetDefaultCaches(int64_t* cache_sizes) {
   cache_sizes[0] = 32 * 1024;    // L1
   cache_sizes[1] = 1024 * 1024;  // L2
   cache_sizes[2] = 0;            // L3 place holder
   cache_sizes[3] = 0;            // L4 place holder
 }
 
-#endif // defined(__x86_64__)
 
 namespace xgboost::common {
 
 CacheManager::CacheManager() {
-  DetectDataCaches(kMaxCacheSize, cache_size_.data());
+#if defined(__x86_64__)
+  bool trust_cpuid = DetectDataCaches(kMaxCacheSize, cache_size_.data());
+  if (trust_cpuid) SetDefaultCaches(cache_size_.data());
+#else
+  SetDefaultCaches(cache_size_.data());
+#endif // defined(__x86_64__)
+
   LOG(INFO) << "Detected: " << "\t"
             << "L1: " << cache_size_[0] << "\t"
             << "L2: " << cache_size_[1] << "\t"
