c1e61bd3e6ee68950307e479767c1295fe0e456f
Hyunsu Cho <phcho@nvidia.com>
2025-11-24 09:39:59 -0800

    Use main

--- ops/pipeline/get-image-tag.sh
+++ ops/pipeline/get-image-tag.sh
@@ -1,4 +1,4 @@
 ## Update the following line to test changes to CI images
 ## See https://xgboost.readthedocs.io/en/latest/contrib/ci.html#making-changes-to-ci-containers
 
-IMAGE_TAG=PR-49
+IMAGE_TAG=main
