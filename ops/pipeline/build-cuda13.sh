41cc339e28107a6d167aa139e1998661eb018a08
Hyunsu Cho <phcho@nvidia.com>
2025-12-09 20:49:01 -0800

    Use matrix to de-duplicate jobs for x86_64, aarch64

--- ops/pipeline/build-cuda13.sh
+++ ops/pipeline/build-cuda13.sh
@@ -9,29 +9,21 @@ then
   exit 1
 fi
 
+if [[ "$#" -lt 2 ]]
+then
+  echo "Usage: $0 [image_repo] {x86_64,aarch64}"
+  exit 2
+fi
+image_repo="$1"
+arch="$2"
 export USE_RMM=0
 export USE_FEDERATED=0
-
-ARCH=$(uname -m)
-case "${ARCH}" in
-  x86_64)
-    IMAGE_REPO="xgb-ci.gpu_build_cuda13_rockylinux8"
-    WHEEL_TAG=manylinux_2_28_x86_64
-    ;;
-  aarch64)
-    IMAGE_REPO="xgb-ci.gpu_build_cuda13_rockylinux8_aarch64"
-    WHEEL_TAG=manylinux_2_28_aarch64
-    ;;
-  *)
-    echo "Unsupported architecture: ${ARCH}"
-    exit 1
-    ;;
-esac
 
 source ops/pipeline/classify-git-branch.sh
 source ops/pipeline/get-docker-registry-details.sh
 source ops/pipeline/get-image-tag.sh
 
+WHEEL_TAG=manylinux_2_28_${arch}
 BUILD_IMAGE_URI="${DOCKER_REGISTRY_URL}/${IMAGE_REPO}:${IMAGE_TAG}"
 MANYLINUX_IMAGE_URI="${DOCKER_REGISTRY_URL}/xgb-ci.${WHEEL_TAG}:${IMAGE_TAG}"
 
