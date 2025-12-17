5a0cbaac0d68e11360ca843c94d43651feea675e
Jiaming Yuan <jm.yuan@outlook.com>
2025-12-18 01:06:59 +0800

    sync.

--- src/data/array_page_source.cu
+++ src/data/array_page_source.cu
@@ -8,6 +8,7 @@ namespace cuda_impl {
 void ReadArrayPage(Context const* ctx, common::Span<GradientPair> d_dst,
                    common::Span<GradientPair const> h_src) {
   curt::MemcpyAsync(d_dst.data(), h_src.data(), d_dst.size_bytes(), ctx->CUDACtx()->Stream());
+  ctx->CUDACtx()->Stream().Sync();
 }
 }  // namespace cuda_impl
 }  // namespace xgboost::data
