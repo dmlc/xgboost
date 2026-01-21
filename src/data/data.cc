991c601b6521737c9c4d5a21f0495e12444f0bb1
Jiaming Yuan <jm.yuan@outlook.com>
2026-01-21 15:14:14 +0800

    display.

--- src/data/data.cc
+++ src/data/data.cc
@@ -572,7 +572,9 @@ void MetaInfo::SetInfo(Context const& ctx, StringView key, StringView interface_
   // it.
   if (this->num_row_ != 0 && this->base_margin_.Shape(0) != this->num_row_) {
     // API functions that don't use array interface don't understand shape.
-    CHECK(this->base_margin_.Size() % this->num_row_ == 0) << "Incorrect size for base margin.";
+    CHECK(this->base_margin_.Size() % this->num_row_ == 0)
+        << "Invalid size for base margin:(" << base_margin_.Shape(0) << "," << base_margin_.Shape(1)
+        << "). n_samples:" << this->num_row_;
     size_t n_groups = this->base_margin_.Size() / this->num_row_;
     this->base_margin_.Reshape(this->num_row_, n_groups);
   }
