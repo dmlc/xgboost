65856728ca34e6c281f52e1b3440a53fe18f69e3
jochs <jochswillard@gmail.com>
2026-01-27 11:04:49 -0500

    make temporary buffers an instance variable

--- python-package/xgboost/_data_utils.py
+++ python-package/xgboost/_data_utils.py
@@ -735,9 +735,9 @@ class TransformedDf(ABC):
 
     """
 
-    temporary_buffers: List[Tuple] = []
+    temporary_buffers: List[Tuple]
 
-    def __init__(self, ref_categories: Optional[Categories], aitfs: AifType) -> None:
+    def __init__(self, ref_categories: Optional[Categories], aitfs: AifType, temporary_buffers: List[Tuple] = []) -> None:
         self.ref_categories = ref_categories
         if ref_categories is not None and ref_categories.get_handle() is not None:
             aif = ref_categories.get_handle()
@@ -746,6 +746,7 @@ class TransformedDf(ABC):
             self.ref_aif = None
 
         self.aitfs = aitfs
+        self.temporary_buffers = temporary_buffers
 
     def array_interface(self) -> bytes:
         """Return a byte string for JSON encoded array interface."""
