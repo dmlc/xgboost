f00fe4645b9a4882baece315fd48604b17a5b264
Jiaming Yuan <jm.yuan@outlook.com>
2026-02-06 19:41:18 +0800

    test.

--- tests/cpp/common/test_cuda_rt_utils.cu
+++ tests/cpp/common/test_cuda_rt_utils.cu
@@ -1,15 +1,25 @@
 /**
- * Copyright 2025, XGBoost contributors
+ * Copyright 2025-2026, XGBoost contributors
  */
 
 #include <gtest/gtest.h>
 
 #include <cstdint>  // for int32_t
 #include <set>      // for set
-
+#include "xgboost/span.h"  // for Span
 #include "../../../src/common/cuda_stream_pool.h"
+#include "../../../src/common/cuda_rt_utils.h"
 
 namespace xgboost::curt {
+TEST(RtUtils, Uuid) {
+  std::array<unsigned char, kUuidLength> uuid;
+  GetUuid(uuid, 0);
+  auto str = PrintUuid(uuid);
+  ASSERT_EQ(str.substr(0, 4), "GPU-");
+  ASSERT_EQ(str.length(), 40);
+  ASSERT_EQ(str.size(), str.length());
+}
+
 TEST(RtUtils, StreamPool) {
   auto n_streams = 16;
   auto pool = std::make_unique<StreamPool>(n_streams);
