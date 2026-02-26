3c459927f630efa9edf22d6c280e1bd6ec62c9c0
Jiaming Yuan <jm.yuan@outlook.com>
2026-02-26 15:47:12 +0800

    pre-commit.

--- tests/python/test_tracker.py
+++ tests/python/test_tracker.py
@@ -6,10 +6,9 @@ from typing import Dict, Union
 import numpy as np
 import pytest
 from hypothesis import HealthCheck, given, settings, strategies
-from xgboost.testing.collective import get_avail_port
-
 from xgboost import RabitTracker, collective
 from xgboost import testing as tm
+from xgboost.testing.collective import get_avail_port
 
 
 def test_rabit_tracker() -> None:
@@ -187,9 +186,8 @@ def test_broadcast():
 @pytest.mark.skipif(**tm.no_dask())
 def test_rank_assignment() -> None:
     from distributed import Client, LocalCluster
-    from xgboost.testing.dask import get_rabit_args
-
     from xgboost import dask as dxgb
+    from xgboost.testing.dask import get_rabit_args
 
     def local_test(worker_id):
         with dxgb.CommunicatorContext(**args) as ctx:
