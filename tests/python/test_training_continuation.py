a8eb950dca931fa352b02e403f69d77ed3e091a3
Jiaming Yuan <jm.yuan@outlook.com>
2026-03-13 23:05:05 +0800

    cleanup.

--- tests/python/test_training_continuation.py
+++ tests/python/test_training_continuation.py
@@ -10,8 +10,6 @@ from xgboost.testing.continuation import (
     run_training_continuation_model_output,
 )
 
-rng = np.random.RandomState(1337)
-
 
 class TestTrainingContinuation:
     num_parallel_tree = 3
@@ -167,32 +165,32 @@ class TestTrainingContinuation:
     def test_model_output(self, tree_method: str) -> None:
         run_training_continuation_model_output("cpu", tree_method)
 
-    @given(
-        subsample=strategies.floats(0.5, 1.0),
-        colsample_bytree=strategies.floats(0.5, 1.0),
-        colsample_bylevel=strategies.floats(0.5, 1.0),
-        colsample_bynode=strategies.floats(0.5, 1.0),
-        booster=strategies.sampled_from(["gbtree", "dart"]),
-        num_class=strategies.sampled_from([1, 3]),
+
+@given(
+    subsample=strategies.floats(0.5, 1.0),
+    colsample_bytree=strategies.floats(0.5, 1.0),
+    colsample_bylevel=strategies.floats(0.5, 1.0),
+    colsample_bynode=strategies.floats(0.5, 1.0),
+    booster=strategies.sampled_from(["gbtree", "dart"]),
+    num_class=strategies.sampled_from([1, 3]),
+)
+@settings(deadline=None, print_blob=True, max_examples=20)
+@pytest.mark.skipif(**tm.no_sklearn())
+def test_continuation_determinism(
+    subsample: float,
+    colsample_bytree: float,
+    colsample_bylevel: float,
+    colsample_bynode: float,
+    booster: str,
+    num_class: int,
+) -> None:
+    run_training_continuation_determinism(
+        device="cpu",
+        booster=booster,
+        subsample=subsample,
+        sampling_method="uniform",
+        colsample_bytree=colsample_bytree,
+        colsample_bylevel=colsample_bylevel,
+        colsample_bynode=colsample_bynode,
+        num_class=num_class,
     )
-    @settings(deadline=None, print_blob=True, max_examples=20)
-    @pytest.mark.skipif(**tm.no_sklearn())
-    def test_continuation_determinism(
-        self,
-        subsample: float,
-        colsample_bytree: float,
-        colsample_bylevel: float,
-        colsample_bynode: float,
-        booster: str,
-        num_class: int,
-    ) -> None:
-        run_training_continuation_determinism(
-            device="cpu",
-            booster=booster,
-            subsample=subsample,
-            sampling_method="uniform",
-            colsample_bytree=colsample_bytree,
-            colsample_bylevel=colsample_bylevel,
-            colsample_bynode=colsample_bynode,
-            num_class=num_class,
-        )
