1fc3307e4bf8b53e74cbf2ca524a2c046e819505
Jiaming Yuan <jm.yuan@outlook.com>
2026-06-22 00:44:54 +0800

    [backport][R] More fixes for test n_threads. (#12259)

--- R-package/tests/testthat.R
+++ R-package/tests/testthat.R
@@ -2,5 +2,6 @@ library(testthat)
 library(xgboost)
 library(Matrix)
 
-test_check("xgboost", reporter = ProgressReporter)
 RhpcBLASctl::omp_set_num_threads(1)
+data.table::setDTthreads(1)
+test_check("xgboost", reporter = ProgressReporter)
