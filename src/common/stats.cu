b790536159019b68e8628aaaaa5bd2b6ddee13bd
Jiaming Yuan <jm.yuan@outlook.com>
2026-07-20 05:33:18 +0800

    Fix & cleanup the stats.

--- src/common/stats.cu
+++ src/common/stats.cu
@@ -1,11 +1,11 @@
 /**
- * Copyright 2022-2025, XGBoost Contributors
+ * Copyright 2022-2026, XGBoost Contributors
  */
 
-#include <thrust/iterator/counting_iterator.h>  // thrust::make_counting_iterator
+#include <thrust/iterator/counting_iterator.h>  // for make_counting_iterator
+#include <thrust/reduce.h>                      // for reduce_by_key
 
 #include <cstddef>  // size_t
-#include <tuple>    // for apply
 
 #include "../collective/aggregator.h"  // for GlobalSum
 #include "cuda_context.cuh"            // CUDAContext
@@ -21,26 +21,30 @@ namespace xgboost::common::cuda_impl {
 void Median(Context const* ctx, linalg::TensorView<float const, 2> t,
             common::OptionalWeights weights, linalg::Tensor<float, 1>* out) {
   CHECK_GE(t.Shape(1), 1);
-  HostDeviceVector<std::size_t> segments(t.Shape(1) + 1, 0);
+  auto n_rows = t.Shape(0);
+  auto n_columns = t.Shape(1);
+  HostDeviceVector<std::size_t> segments(n_columns + 1, 0);
   segments.SetDevice(ctx->Device());
   auto d_segments = segments.DeviceSpan();
   dh::LaunchN(d_segments.size(), ctx->CUDACtx()->Stream(),
-              [=] XGBOOST_DEVICE(std::size_t i) { d_segments[i] = t.Shape(0) * i; });
-  auto val_it = dh::MakeTransformIterator<float>(
-      thrust::make_counting_iterator(0ul),
-      [=] XGBOOST_DEVICE(size_t i) { return std::apply(t, linalg::UnravelIndex(i, t.Shape())); });
+              [=] XGBOOST_DEVICE(std::size_t i) { d_segments[i] = n_rows * i; });
+  auto val_it = dh::MakeTransformIterator<float>(thrust::make_counting_iterator(0ul),
+                                                 [=] XGBOOST_DEVICE(std::size_t i) {
+                                                   auto cidx = i / n_rows;
+                                                   auto ridx = i % n_rows;
+                                                   return t(ridx, cidx);
+                                                 });
 
   out->SetDevice(ctx->Device());
-  out->Reshape(t.Shape(1));
+  out->Reshape(n_columns);
   if (weights.Empty()) {
     common::SegmentedQuantile(ctx, 0.5, dh::tcbegin(d_segments), dh::tcend(d_segments), val_it,
                               val_it + t.Size(), out->Data());
   } else {
-    CHECK_NE(t.Shape(1), 0);
     auto w_it = dh::MakeTransformIterator<float>(thrust::make_counting_iterator(0ul),
                                                  [=] XGBOOST_DEVICE(std::size_t i) {
-                                                   auto sample_idx = i / t.Shape(1);
-                                                   return weights[sample_idx];
+                                                   auto ridx = i % n_rows;
+                                                   return weights[ridx];
                                                  });
     common::SegmentedWeightedQuantile(ctx, 0.5, dh::tcbegin(d_segments), dh::tcend(d_segments),
                                       val_it, val_it + t.Size(), w_it, w_it + t.Size(),
@@ -81,8 +85,8 @@ void SampleMean(Context const* ctx, bool is_column_split, linalg::MatrixView<flo
                                                   });
   auto cuctx = ctx->CUDACtx();
   thrust::reduce_by_key(cuctx->CTP(), column_it, column_it + d_v.Size(), val_it,
-                        thrust::make_discard_iterator(), d_out.Values().data(),
-                        thrust::equal_to<>{}, thrust::plus<double>{});
+                        thrust::make_discard_iterator(), d_out.Values().data(), std::equal_to<>{},
+                        cuda::std::plus<double>{});
   SafeColl(collective::GlobalSum(ctx, is_column_split, d_out));
 }
 
@@ -103,7 +107,7 @@ void WeightedSampleMean(Context const* ctx, bool is_column_split,
                                                           });
   auto cuctx = ctx->CUDACtx();
   auto sum_w =
-      dh::Reduce(cuctx->CTP(), d_w.data(), d_w.data() + d_w.size(), 0.0, thrust::plus<double>{});
+      dh::Reduce(cuctx->CTP(), d_w.data(), d_w.data() + d_w.size(), 0.0, cuda::std::plus<double>{});
   auto cpu = ctx->MakeCPU();
   SafeColl(collective::GlobalSum(&cpu, is_column_split, linalg::MakeVec(&sum_w, 1)));
   CHECK_GT(sum_w, 0.0) << "weights must contain at least one non-zero value.";
@@ -114,8 +118,8 @@ void WeightedSampleMean(Context const* ctx, bool is_column_split,
                                                     return d_v(ridx, cidx) / sum_w * d_w(ridx);
                                                   });
   thrust::reduce_by_key(cuctx->CTP(), column_it, column_it + d_v.Size(), val_it,
-                        thrust::make_discard_iterator(), d_out.Values().data(),
-                        thrust::equal_to<>{}, thrust::plus<double>{});
+                        thrust::make_discard_iterator(), d_out.Values().data(), std::equal_to<>{},
+                        cuda::std::plus<double>{});
   SafeColl(collective::GlobalSum(ctx, is_column_split, d_out));
 }
 }  // namespace xgboost::common::cuda_impl
