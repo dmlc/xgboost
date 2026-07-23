240f80c9161279d736f5eb2ee5d51e50e68c604d
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2026-07-23 13:31:26 +0200

    Fix rebased colmaker tree check

--- src/tree/updater_colmaker.cc
+++ src/tree/updater_colmaker.cc
@@ -118,7 +118,7 @@ class ColMaker : public TreeUpdater {
     CHECK_EQ(gpair->Shape(1), 1) << MTNotImplemented();
     for (std::size_t i = 0; i < trees.size(); ++i) {
       CHECK(ctx_);
-      CHECK(!tree->IsMultiTarget()) << "exact" << MTNotImplemented();
+      CHECK(!trees[i]->IsMultiTarget()) << "exact" << MTNotImplemented();
       Builder builder(*param, colmaker_param_, interaction_constraints_, ctx_, column_densities_,
                       column_sampler_);
       builder.Update(gpair->Data()->ConstHostVector(), dmat, trees[i], &out_position[i]);
