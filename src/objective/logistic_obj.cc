62781318954dc7fa3bb4abe2da21fbeaaf37676f
Jiaming Yuan <jm.yuan@outlook.com>
2026-08-18 21:08:14 +0800

    fmt.

--- src/objective/logistic_obj.cc
+++ src/objective/logistic_obj.cc
@@ -146,8 +146,8 @@ XGBOOST_REGISTER_OBJECTIVE(LogisticRaw, LogisticRaw::Name())
         "Logistic regression for classification, output score "
         "before logistic transformation.")
     .set_body([]() {
-      LOG(WARNING) << "binary:logitraw is now deprecated in favor of binary:logistic with "
-                      "output_margin=true.";
+      LOG(WARNING) << "`binary:logitraw` is now deprecated in favor of `binary:logistic` with "
+                      "`output_margin=true`.";
       return new LogisticRaw();
     });
 }  // namespace xgboost::obj
